3124001b952e444db21b1fef31934eb3bf41c1d0
Michael Rose <michael@rosenetwork.net>
2016-02-16 22:11:48 -0800

    editing function-iterations.fish

--- function-iterations.fish
+++ function-iterations.fish
@@ -1,6 +1,9 @@
 function function-iterations
 	set revisions (get-function-revisions $argv)
+  set cnt 1
 	for c in $revisions
+    echo \($cnt\)
     eval git -C ~/.config/fish/functions --no-pager show $c:$argv.fish
+    set cnt (math $cnt + 1)
   end
 end
