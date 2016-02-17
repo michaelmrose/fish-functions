7ea77db3390bd6294f7379676163e603b5b08d09
Michael Rose <michael@rosenetwork.net>
2016-02-17 11:33:59 -0800

    editing filter-with-expr.fish

--- filter-with-expr.fish
+++ filter-with-expr.fish
@@ -1,9 +1,11 @@
 function filter-with-expr
 	set expr $argv[1]
+  if test (count $argv) -gt 1
     set lst $argv[2..-1]
     for i in $lst
         if eval $expr $i
             echo $i
         end
     end
+  end
 end
