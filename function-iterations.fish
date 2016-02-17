eee0ed22bfb4505123ee4930a8988a6160d9151a
Michael Rose <michael@rosenetwork.net>
2016-02-16 22:45:57 -0800

    editing function-iterations.fish

--- function-iterations.fish
+++ function-iterations.fish
@@ -1,21 +1,23 @@
 function function-iterations
 	switch $argv[1]
     case -p
+      set fn $argv[2]
       set prettyprint true
       set revisions (get-function-revisions $argv[2])
     case '*'
+      set fn $argv[1]
       set revisions (get-function-revisions $argv)
   end
   set cnt 1
 	for c in $revisions
 
-    set src (eval git -C ~/.config/fish/functions --no-pager show $c:$argv.fish)
+    set src (eval git -C ~/.config/fish/functions --no-pager show $c:$fn.fish)
     set checksum (echo $src | checksum-simple)
     if not contains $checksum $checksums
       set checksums $checksums $checksum
       if exists $prettyprint
         echo \($cnt\)
-        println $src | pymentize
+        println $src | pygmentize
       else
         println $src
       end
