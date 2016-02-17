9697b6b9cb4e0b3bc3ad6de0afe87cea78001af6
Michael Rose <michael@rosenetwork.net>
2016-02-16 21:47:43 -0800

    editing function-set-iteration.fish

--- function-set-iteration.fish
+++ function-set-iteration.fish
@@ -1,4 +1,5 @@
 function function-set-iteration
 	set revision (get-function-revisions $argv[1])[$argv[2]]
-  eval git -C ~/.config/fish/functions --no-pager show $revision:$argv[1].fish
+  echo $revision
+  # eval git -C ~/.config/fish/functions --no-pager show $revision:$argv[1].fish
 end
