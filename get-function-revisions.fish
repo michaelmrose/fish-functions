caddf1091d374002df95832b33c92f513d138cbd
Michael Rose <michael@rosenetwork.net>
2016-02-17 01:52:35 -0800

    editing get-function-revisions.fish

--- get-function-revisions.fish
+++ get-function-revisions.fish
@@ -1,4 +1,12 @@
 function get-function-revisions
 	set revs (git -C ~/.config/fish/functions lgg | grep "editing $argv.fish" | cut -d " " -f2)
-  println $revs
+  for r in $revs
+    set src (git -C ~/.config/fish/functions --no-pager show $r:$argv.fish)
+    set checksum (echo $src | checksum-simple)
+    if not contains $checksum $checksums
+      set checksums $checksums $checksum
+      echo $r
+    end
+  end
+  # println $revs
 end
