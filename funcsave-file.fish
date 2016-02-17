7ad4b388e5cb82252abc3eec72a8d7add5baf435
Michael Rose <michael@rosenetwork.net>
2016-02-16 23:03:14 -0800

    editing funcsave-file.fish

--- funcsave-file.fish
+++ funcsave-file.fish
@@ -2,8 +2,6 @@ function funcsave-file
 	set fnames (println (return-fnames-of-file $argv))
   source $argv
   for fn in $fnames
-    funcsave $fn
-    # git -C ~/.config/fish/functions add $fn.fish
-    # git -C ~/.config/fish/functions commit -m "editing $fn.fish"
+    fn save $fn
   end
 end
