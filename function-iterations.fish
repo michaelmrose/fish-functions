15f9f187060bcc616eb9124cd73f64566ce5da03
Michael Rose <michael@rosenetwork.net>
2016-02-16 22:21:52 -0800

    editing function-iterations.fish

--- function-iterations.fish
+++ function-iterations.fish
@@ -2,14 +2,16 @@ function function-iterations
 	set revisions (get-function-revisions $argv)
   set cnt 1
 	for c in $revisions
-    echo \($cnt\)
+
     set src (eval git -C ~/.config/fish/functions --no-pager show $c:$argv.fish)
     set checksum (echo $src | checksum-simple)
     if not contains $checksum $checksums
       set checksums $checksums $checksum
+      echo \($cnt\)
       println $src
+      set cnt (math $cnt + 1)
     else
     end
-    set cnt (math $cnt + 1)
+
   end
 end
