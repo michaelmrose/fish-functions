d70bf5ab2df25ec67b85b04bb86887877cbd65ee
Michael Rose <michael@rosenetwork.net>
2016-02-17 11:26:50 -0800

    editing apply-to-list.fish

--- apply-to-list.fish
+++ apply-to-list.fish
@@ -1,7 +1,9 @@
 function apply-to-list
 	set fn $argv[1]
-    set lst $argv[2..-1]
-    for i in $lst
-        eval $fn $i
+    if test (count $argv) -gt 1
+      set lst $argv[2..-1]
+      for i in $lst
+          eval $fn $i
+      end
     end
 end
