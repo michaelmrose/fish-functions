a956c3edf95cffc3ff22d3dcee9b43195a2269ce
Michael Rose <michael@rosenetwork.net>
2016-02-16 22:54:45 -0800

    editing fish-color.fish

--- fish-color.fish
+++ fish-color.fish
@@ -1,10 +1,10 @@
 function fish-color
 	if exists $argv
-    cat $argv | pygmentize -l Fish -O style=monokai
+    cat $argv | pygmentize -l Fish -O style=emacs
   else
     while read -l line
       set input $input $line
     end
-    println $input | pygmentize -l Fish -O style=monokai
+    println $input | pygmentize -l Fish -O style=emacs
   end
 end
