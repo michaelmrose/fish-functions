118b65f06ef5947d1b28d4b8d2e10de7335630eb
Michael Rose <michael@rosenetwork.net>
2016-02-17 18:45:43 -0800

    editing vals.fish

--- vals.fish
+++ vals.fish
@@ -1,7 +1,9 @@
 function vals
-	set min $argv[1]
-  set max $argv[2]
-  set rest $argv[3..-1]
+	# set min $argv[1]
+  # set max $argv[2]
+  set min (echo $argv[1] | cut -c1)
+  set max (echo $argv[1] | cut -c(sizeof $argv[1]))
+  set rest $argv[2..-1]
   set last (lesserof $max (count $rest))  
   for i in $rest[$min..$last]
     echo $i
