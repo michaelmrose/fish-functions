1c3c54981ed006930139c4ad2c72daa9c720d7ab
Michael Rose <michael@rosenetwork.net>
2016-02-18 02:52:28 -0800

    editing books.fish

--- books.fish
+++ books.fish
@@ -44,8 +44,8 @@ function books --description 'open books given either as a title or criteria que
             case -e
                 books --erase
             case --query
-                # gvfs-open (choose-format (get-fname-of-book (select-book (query-calibre-title (return-query $tail)))))
-                get-fname-of-book (select-book (query-calibre-title (return-query $tail)))
+                gvfs-open (choose-format (get-fname-of-book (select-book (query-calibre-title (return-query $tail)))))
+                # get-fname-of-book (select-book (query-calibre-title (return-query $tail)))
                 # e open choose-format get-fname-of-book select-book query-calibre-title return-query $tail
             case -q
                 books --query $tail
