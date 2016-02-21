250fc121c28e89e76bfdb3dae101947ce92095f5
Michael Rose <michael@rosenetwork.net>
2016-02-21 13:30:00 -0800

    editing vi3_workspace.fish

--- vi3_workspace.fish
+++ vi3_workspace.fish
@@ -1,13 +1,13 @@
 function vi3_workspace --description 'switch workspace [a-z]'
 	switch (count $argv)
-      case 1
-        i3-msg $argv
-      case'*'
-        for i in (explode-words $argv)
-            echo changing to $i
-            i3-msg workspace $i
-        end
-        er vi3op
-        update-op-status
+    case 1
+      i3-msg $argv
+    case'*'
+      for i in (explode-words $argv)
+        echo changing to $i
+        i3-msg workspace $i
       end
+      er vi3op
+      update-op-status
+  end
 end
