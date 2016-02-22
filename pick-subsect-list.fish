ef0fcc3bb1c103d39acd37b4498f95d3cb6c99bf
Michael Rose <michael@rosenetwork.net>
2016-02-21 22:49:35 -0800

    editing pick-subsect-list.fish

--- pick-subsect-list.fish
+++ pick-subsect-list.fish
@@ -3,5 +3,6 @@ function pick-subsect-list
   set choice (rfi match "select start: " $argv)
   set start (findindex $choice $list)
   set end (rfi match "select end: " $argv[$start..-1])
+  set end (findindex $choice $list)
   println $list[1..3]
 end
