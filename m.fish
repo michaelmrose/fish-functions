305550f77014403c994d45e78e3c90ecbceeed6b
Michael Rose <michael@rosenetwork.net>
2016-02-21 22:58:56 -0800

    editing m.fish

--- m.fish
+++ m.fish
@@ -5,6 +5,15 @@ function m
     switch $argv[1]
       case -s
         m (pick-subsect-list $LAST_PLAYLIST)
+      case -w
+        echo $LAST_PLAYLIST > ~/playlists/$argv[2]
+      case -p
+        switch (count $argv)
+          case 2
+            m (cat ~/playlists/$argv[2])
+          case 1
+            echo pick a playlist
+        end
       case '*'
         for i in $argv
           set acc $acc (pathof $i)
