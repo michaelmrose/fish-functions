c878d7e1b16c199c5fa1f3258f8305ea65ecdb71
Michael Rose <michael@rosenetwork.net>
2016-02-21 22:53:57 -0800

    editing m.fish

--- m.fish
+++ m.fish
@@ -2,10 +2,15 @@ function m
 	if not exists $argv
     m $LAST_PLAYLIST
   else
-    for i in $argv
-      set acc $acc (pathof $i)
+    switch $argv[1]
+      case -s
+        m (pick-subsect-list $LAST_PLAYLIST)
+      case '*'
+        for i in $argv
+          set acc $acc (pathof $i)
+        end
+        set -U LAST_PLAYLIST $acc
+        umpv $argv
+      end
     end
-    set -U LAST_PLAYLIST $acc
-    umpv $argv
-  end
 end
