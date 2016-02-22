116c9dfd0009d7402e3f2834ff2ba7d47fc6d3a5
Michael Rose <michael@rosenetwork.net>
2016-02-21 23:43:01 -0800

    editing subsect.fish

--- subsect.fish
+++ subsect.fish
@@ -9,6 +9,8 @@ function subsect
   echo 1 is $argv[1]
   echo 2 is $argv[2]
   set foo (println 1 2 3)
+  set list (println $argv[1])
+  println $list
   println $foo[$argv[2]]
   # eval println (println $argv[1])[$argv[2]]
 end
