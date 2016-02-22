97a099ce3cf6cea813cf6b27e127acc5e4e6fe08
Michael Rose <michael@rosenetwork.net>
2016-02-21 21:23:31 -0800

    editing endswith-special.fish

--- endswith-special.fish
+++ endswith-special.fish
@@ -1,3 +1,11 @@
 function endswith-special
-	startswith-special $argv[1] (reverse $argv[2..-1])
+	set chars (explode $argv[1])
+  set end (sizeof $arg[2..-1])
+  set matching (echo $argv[2] | cut -c$end)
+  for c in $chars
+    if [ $c = $matching ]
+      return 0
+    end
+  end
+  return 1
 end
