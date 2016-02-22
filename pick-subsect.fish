0975caacb9bd9fa46690c4a42088511715eab64d
Michael Rose <michael@rosenetwork.net>
2016-02-21 21:45:04 -0800

    editing pick-subsect.fish

--- pick-subsect.fish
+++ pick-subsect.fish
@@ -1,8 +1,7 @@
 function pick-subsect
-	set files (subsect $argv)
 	set choice (rfi match "select start: " (subsect $argv))
   set start (findindex $choice $files)
-	set choice (rfi match "select start: " $files)
+	set choice (rfi match "select start: " (subsect $argv $start))
   set end (findindex $choice $files)
   subsect $argv $start $end
 end
