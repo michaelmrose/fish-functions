6e2171e05a58d6b84cb4a100825c51700d80ad81
Michael Rose <michael@rosenetwork.net>
2016-02-21 22:29:01 -0800

    editing subsect.fish

--- subsect.fish
+++ subsect.fish
@@ -1,20 +1,20 @@
 function subsect
 	set target $argv[1]
-  switch (count $argv)
-    case 1
-      set begin 1
-      set end -1
-    case 2
-      set begin $argv[2]
-      set end -1
-    case 3
-      set begin $argv[2]
-      set end $argv[3]
-    end
+  set range $argv[2]
+  set begin (echo $range | cut -d "." -f1)
+  set end (echo $range | cut -d "." -f3)
+  # switch (count $argv)
+  #   case 1
+  #     set begin 1
+  #     set end -1
+  #   case 2
+  #     set begin $argv[2]
+  #     set end -1
+  #   case 3
+  #     set begin $argv[2]
+  #     set end $argv[3]
+  #   end
+  # println (println $target)[$begin..$end]
     set list (eval println $target)
-    # set list (quote $list)
     println $list[$begin..$end]
-    # println $list[$begin..$end]
-    # echo fn is $fn target is $target begin is $begin end is $end
-    # echo eval $fn (println (println $target)[$begin..end])
 end
