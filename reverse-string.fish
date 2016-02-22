bd18a0addbfd8801247164d491ce0e3ec43c211e
Michael Rose <michael@rosenetwork.net>
2016-02-21 21:13:55 -0800

    editing reverse-string.fish

--- reverse-string.fish
+++ reverse-string.fish
@@ -3,9 +3,10 @@ function reverse-string
   set size (sizeof $argv)
   set cnt $size
   for i in (seq $size)
-    set res $res (echo $argv | cut -c$cnt)
+    set c (echo $argv | cut -c$cnt)
+    set $res $res$c
     echo $cnt
     set cnt (math $cnt - 1)
   end
-  squish $res
+  echo $res
 end
