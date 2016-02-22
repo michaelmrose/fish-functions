3bee69fcc8f717ef51d55dc8a005a303201d4e1f
Michael Rose <michael@rosenetwork.net>
2016-02-21 22:13:09 -0800

    editing m.fish

--- m.fish
+++ m.fish
@@ -2,7 +2,10 @@ function m
 	if not exists $argv
     m $LAST_PLAYLIST
   else
-    set -U LAST_PLAYLIST $argv
+    for i in $argv
+      set acc $acc (pathof $i)
+    end
+    set -U LAST_PLAYLIST $acc
     umpv $argv
   end
 end
