a026dd79f7eaa20d98368a197ab5b35c7ddd8f7c
Michael Rose <michael@rosenetwork.net>
2016-02-21 23:39:39 -0800

    editing subsect.fish

--- subsect.fish
+++ subsect.fish
@@ -1,8 +1,10 @@
 function subsect
-	set target $argv[1]
-  set range $argv[2]
-  set begin (echo $range | cut -d "." -f1)
-  set end (echo $range | cut -d "." -f3)
-  set list (eval println $target)
-  println $list[$begin..$end]
+	# set target $argv[1]
+  # set range $argv[2]
+  # set begin (echo $range | cut -d "." -f1)
+  # set end (echo $range | cut -d "." -f3)
+  # set list (eval println $target)
+  # println $list[$begin..$end]
+  # eval println (ls $argv[1])[$argv[2]]
+  println (println $argv[1])[$argv[2]]
 end
