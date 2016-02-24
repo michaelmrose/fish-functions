982c336fff901a3585e3740742045eb66d6a490b
Michael Rose <michael@rosenetwork.net>
2016-02-23 19:37:58 -0800

    editing m2.fish

--- m2.fish
+++ m2.fish
@@ -7,12 +7,15 @@ function m2
       case ls
         cat ~/playlists/$argv[2]
         return 0
+      case play
+        m2 (cat ~/playlists/$argv[2])
+        return 0
     end
   end
 	set vals (vals 1..-1 $argv)
   set arguments (filter-with-expr "not startswith @" $vals)
   # echo v is $vals t is $tags and a is $arguments
-  if not exists $arguments or get-tag play or get-tag ls or get-tag list-playlists
+  if not exists $arguments or get-tag play
     while read -l line
       set arguments $arguments $line
     end
@@ -27,16 +30,16 @@ function m2
       println $arguments >> ~/playlists/$p
     end
   end
-  if get-tag play $vals
-    for p in (get-tag play $vals)
-      echo 
-      m2 (cat ~/playlists/$p)
-    end
-  end
-  if get-tag ls $vals
-    for p in (get-tag ls $vals)
-      cat ~/playlists/$p
-    end
-  end
+  # if get-tag play $vals
+  #   for p in (get-tag play $vals)
+  #     echo 
+  #     m2 (cat ~/playlists/$p)
+  #   end
+  # end
+  # if get-tag ls $vals
+  #   for p in (get-tag ls $vals)
+  #     cat ~/playlists/$p
+  #   end
+  # end
   umpv $arguments &
 end
