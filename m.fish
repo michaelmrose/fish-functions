e6de165234f83ea0ff9692fdc239b9586f00ab6c
Michael Rose <michael@rosenetwork.net>
2016-02-23 19:43:04 -0800

    editing m.fish

--- m.fish
+++ m.fish
@@ -15,22 +15,22 @@ function m
         return 0
     end
   end
-  set vals (vals 1..-1 $argv)
+  # set vals (vals 1..-1 $argv)
   set arguments (filter-with-expr "not startswith @" $vals)
-  if not exists $arguments or get-tag play
-    while read -l line
-      set arguments $arguments $line
-    end
-  end
-  if get-tag replace $vals
-    for p in (get-tag replace $vals)
-      println $arguments > ~/playlists/$p
-    end
-  end
-  if get-tag append $vals
-    for p in (get-tag append $vals)
-      println $arguments >> ~/playlists/$p
-    end
-  end
+  # if not exists $arguments or get-tag play
+  #   while read -l line
+  #     set arguments $arguments $line
+  #   end
+  # end
+  # if get-tag replace $vals
+  #   for p in (get-tag replace $vals)
+  #     println $arguments > ~/playlists/$p
+  #   end
+  # end
+  # if get-tag append $vals
+  #   for p in (get-tag append $vals)
+  #     println $arguments >> ~/playlists/$p
+  #   end
+  # end
   umpv $arguments &
 end
