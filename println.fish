e3f587d7f436b6a814dc7b67776be58cecbd36d4
Michael Rose <michael@rosenetwork.net>
2016-02-23 19:08:30 -0800

    editing println.fish

--- println.fish
+++ println.fish
@@ -1,9 +1,9 @@
 function println
 	for i in $argv
-        if test -f $i
-          cat $i
-        else
+        # if test -f $i
+        #   cat $i
+        # else
           echo -e $i
-        end
+        # end
     end
 end
