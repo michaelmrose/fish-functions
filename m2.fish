8a47df110235ad94e5d67b59331d0e5b903a4a95
Michael Rose <michael@rosenetwork.net>
2016-02-23 19:38:56 -0800

    editing m2.fish

--- m2.fish
+++ m2.fish
@@ -14,7 +14,6 @@ function m2
   end
 	set vals (vals 1..-1 $argv)
   set arguments (filter-with-expr "not startswith @" $vals)
-  # echo v is $vals t is $tags and a is $arguments
   if not exists $arguments or get-tag play
     while read -l line
       set arguments $arguments $line
@@ -30,16 +29,5 @@ function m2
       println $arguments >> ~/playlists/$p
     end
   end
-  # if get-tag play $vals
-  #   for p in (get-tag play $vals)
-  #     echo 
-  #     m2 (cat ~/playlists/$p)
-  #   end
-  # end
-  # if get-tag ls $vals
-  #   for p in (get-tag ls $vals)
-  #     cat ~/playlists/$p
-  #   end
-  # end
   umpv $arguments &
 end
