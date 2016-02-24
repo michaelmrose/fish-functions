cf0b80866eb8d0b5af407bdd08f109ea6b49adf1
Michael Rose <michael@rosenetwork.net>
2016-02-23 18:49:17 -0800

    editing m2.fish

--- m2.fish
+++ m2.fish
@@ -1,4 +1,11 @@
 function m2
-	set args (vals 1..-1 $argv)
-	set tags (get-tags $args)
+	set vals (vals 1..-1 $argv)
+	set tags (get-tags $vals)
+  set arguments (filter-with-expr "not startswith @" $vals)
+  if not exists $arguments
+    while read -l line
+      set arguments $arguments $line
+    end
+  end
+  echo v is $vals t is $tags and a is $arguments
 end
