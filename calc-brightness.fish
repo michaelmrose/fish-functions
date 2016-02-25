6fdcd23b51c296942ed3b726b3f9802d8a1229c2
Michael Rose <michael@rosenetwork.net>
2016-02-25 01:50:28 -0800

    editing calc-brightness.fish

--- calc-brightness.fish
+++ calc-brightness.fish
@@ -1,14 +1,15 @@
 function calc-brightness
 	set display $argv[2]
 	set number $argv[1]
-  set type (match-lists $number "+ -" "adj adj" num)
-  echo t is $type
-  set adjustment (stripsign $number)
-  set currentval (get-brightness $display)
+  set adjustment $number
   if expr $number : +[0-9]\* > /dev/null
+      set adjustment (stripsign $number)
+      set currentval (get-brightness $display)
       set adjustment (math $currentval + $adjustment)
   end
   if expr $number : -[0-9]\* > /dev/null
+      set adjustment (stripsign $number)
+      set currentval (get-brightness $display)
       set adjustment (math $currentval - $adjustment)
   end
   
