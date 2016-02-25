11e0d8c92c9252024d4226befddb3bfd748a49ed
Michael Rose <michael@rosenetwork.net>
2016-02-25 01:25:35 -0800

    editing bright.fish

--- bright.fish
+++ bright.fish
@@ -1,3 +1,14 @@
 function bright
-	set-brightness-all-displays $argv; 
+	if test (count $argv) -eq 1
+    set displays (get-connected-displays)
+  else
+    set displays $argv[2]
+  end
+  set vals (apply-to-list "calc-brightness $argv" $displays)
+  set cnt (count $displays)
+  set com "xrandr"
+  for i in (seq $cnt)
+    set com $com --output $displays[$i] --brightness $vals[$i]
+  end
+  eval $com
 end
