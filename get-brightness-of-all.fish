ef5396f85c4339747828ca8c2c9ca0e71f45d812
Michael Rose <michael@rosenetwork.net>
2016-02-25 01:40:48 -0800

    editing get-brightness-of-all.fish

--- get-brightness-of-all.fish
+++ get-brightness-of-all.fish
@@ -1,5 +1,5 @@
 function get-brightness-of-all
-	set result (xrandr --verbose | grep -i brightness |  cut -d " " -f2)
+	set result (xrandr --verbose | grep Brightness |  cut -d " " -f2)
     for i in $result
         echo (wcalc -q "$i * 100")
     end
