b9760bf860a2821f1ff4fc3a9a970219f08433e5
Michael Rose <michael@rosenetwork.net>
2016-02-25 01:54:35 -0800

    editing calc-brightness.fish

--- calc-brightness.fish
+++ calc-brightness.fish
@@ -1,19 +1,16 @@
 function calc-brightness
 	set display $argv[2]
 	set number $argv[1]
-  # set adjustment $number
   if expr $number : [0-9]\* > /dev/null
     set adjustment $number
-  end
-  if expr $number : +[0-9]\* > /dev/null
-      set adjustment (stripsign $number)
-      set currentval (get-brightness $display)
-      set adjustment (math $currentval + $adjustment)
-  end
-  if expr $number : -[0-9]\* > /dev/null
-      set adjustment (stripsign $number)
-      set currentval (get-brightness $display)
-      set adjustment (math $currentval - $adjustment)
+  else if expr $number : +[0-9]\* > /dev/null
+    set adjustment (stripsign $number)
+    set currentval (get-brightness $display)
+    set adjustment (math $currentval + $adjustment)
+  else if expr $number : -[0-9]\* > /dev/null
+    set adjustment (stripsign $number)
+    set currentval (get-brightness $display)
+    set adjustment (math $currentval - $adjustment)
   end
   
   wcalc -q "$adjustment / 100"
