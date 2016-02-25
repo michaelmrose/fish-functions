b10219e0fc52ee1bc6aec7e0dc87df6e2ae459c8
Michael Rose <michael@rosenetwork.net>
2016-02-25 01:18:40 -0800

    editing set-bright.fish

--- set-bright.fish
+++ set-bright.fish
@@ -1,3 +1,10 @@
 function set-bright
 	set displays (get-connected-displays)
+  set vals (apply-to-list 'calc-brightness 70' $displays)
+  set cnt (count $displays)
+  set com "xrander"
+  for i in (seq $cnt)
+    set com $com --output $displays[$i] --brightness $vals[$i]
+  end
+  echo $com
 end
