2e78aa10160bb5508f2cb13f0e86f89ddea15efe
Michael Rose <michael@rosenetwork.net>
2016-02-26 13:49:46 -0800

    editing edit.fish

--- edit.fish
+++ edit.fish
@@ -1,3 +1,13 @@
 function edit
-	
+	if not exists $argv
+    while read -l line
+      set acc $acc $line
+    end
+  else
+    set acc $argv
+  end
+  set tmp /tmp/(uid)
+  println $acc > $tmp   
+  eval $EDITOR $tmp
+  cat $tmp
 end
