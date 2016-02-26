f5c6aa511c9af9815ad5989974c4c2a39d09fbbe
Michael Rose <michael@rosenetwork.net>
2016-02-26 14:16:48 -0800

    editing lockme.fish

--- lockme.fish
+++ lockme.fish
@@ -5,10 +5,10 @@ function lockme
   for window in (windows-list)
     transset -i $window 0
   end
-  sleep 1
-  i3-msg mode locked
   for i in (get-connected-displays)
       i3-msg focus output $i
       i3-msg workspace {$i}_is_locked
   end
+  sleep 1
+  i3-msg mode locked
 end
