c3b09eb085447934ddb38eb4224d2138cb1d2df2
Michael Rose <michael@rosenetwork.net>
2016-02-26 12:06:23 -0800

    editing lockme.fish

--- lockme.fish
+++ lockme.fish
@@ -1,9 +1,12 @@
 function lockme
 	save-workspaces
-    i3-msg mode locked
-    for i in (get-connected-displays)
-        i3-msg focus output $i
-        i3-msg workspace {$i}_is_locked
-    end
-    i3-msg bar mode invisible
+  i3-msg mode locked
+  for window in (windows-list)
+    transset -i $window 0
+  end
+  # for i in (get-connected-displays)
+  #     i3-msg focus output $i
+  #     i3-msg workspace {$i}_is_locked
+  # end
+  i3-msg bar mode invisible
 end
