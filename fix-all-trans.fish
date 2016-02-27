a29b13ecb284fe154aee27fc7efa26fd0b36c294
Michael Rose <michael@rosenetwork.net>
2016-02-26 22:01:03 -0800

    editing fix-all-trans.fish

--- fix-all-trans.fish
+++ fix-all-trans.fish
@@ -1,7 +1,3 @@
 function fix-all-trans
 	apply-to-list fix-trans (list-windows)
-  set hidden (filter-with-expr is-window-hidden (list-windows))
-  for id in $hidden
-    transset -i $id 0 
-  end
 end
