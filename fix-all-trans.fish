41348efe5047446009630a9db3806efc0fccdd09
Michael Rose <michael@rosenetwork.net>
2016-02-26 23:43:05 -0800

    editing fix-all-trans.fish

--- fix-all-trans.fish
+++ fix-all-trans.fish
@@ -1,7 +1,7 @@
 function fix-all-trans
 	set mpv (windows-list | grep gl.mpv | cut -d ' ' -f1)
   for i in $mpv
-    transset id $i 1.0
+    transset -i $i 1.0
   end
 	apply-to-list fix-trans (list-windows)
 end
