bd8ef02e4d7cd6ee04e0a8fbb65f4fc16f979a7b
Michael Rose <michael@rosenetwork.net>
2016-02-27 17:17:31 -0800

    editing vpn-status-line2.fish

--- vpn-status-line2.fish
+++ vpn-status-line2.fish
@@ -3,11 +3,11 @@ function vpn-status-line2
 	switch $vpnstatus
         case none
             set symbol  
-            set_color gree;echo -e {$blue}$symbol $vpnstatus
+            set_color red;echo -e {$blue}$symbol $vpnstatus
             # echo -e {$blue}$symbol $vpnstatus
         case "*"
             set symbol  
-            set_color red; echo -e $symbol $vpnstatus
+            set_color green; echo -e $symbol $vpnstatus
             # echo -e {$red} $symbol $vpnstatus
     end
 end
