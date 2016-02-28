5e8f9e08661b167357fb7e43c10610b643406a58
Michael Rose <michael@rosenetwork.net>
2016-02-27 17:12:22 -0800

    editing vpn-status-line2.fish

--- vpn-status-line2.fish
+++ vpn-status-line2.fish
@@ -6,6 +6,6 @@ function vpn-status-line2
             echo -e {$blue}$symbol $vpnstatus
         case "*"
             set symbol  
-            echo -e {$red}$symbol $vpnstatus
+            echo -e {$red} $symbol $vpnstatus
     end
 end
