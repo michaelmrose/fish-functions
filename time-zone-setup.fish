5a2309ef6fb6902b0ff07bd5b2a069a0275b815d
Michael Rose <michael@rosenetwork.net>
2016-02-28 19:19:03 -0800

    editing time-zone-setup.fish

--- time-zone-setup.fish
+++ time-zone-setup.fish
@@ -1,5 +1,5 @@
 function time-zone-setup
 	set zone (rfi match "pick a zone" (find /usr/share/zoneinfo/ -type f))
-  echo sudo ln -s $zone /etc/localtime
-  echo (cutlastn / -2 $zone) \> /etc/timezone
+  sudo ln -s $zone /etc/localtime
+  sudo echo (cutlastn / -2 $zone) > /etc/timezone
 end
