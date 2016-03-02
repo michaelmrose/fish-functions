e58f36491307261c3a5c6c96400fc610f69c1d54
Michael Rose <michael@rosenetwork.net>
2016-03-02 13:42:39 -0800

    editing i3-colors.fish

--- i3-colors.fish
+++ i3-colors.fish
@@ -1,4 +1,10 @@
 function i3-colors
-	set com (i3-color-rofi)
-  echo $com | sed 's/#[0-9]+/blub/g'
+	set info (i3-colors-rofi)
+  set switches (odds $info)
+  set colors (evens $info)
+  for i in (seq (count $switches))
+    set com $com $switches[$i]
+    set com $com (singlequote $colors[$i])
+  end
+  echo $com
 end
