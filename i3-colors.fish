8dafedaa1d56cb0c15e778a03de9cc6b99a171dd
Michael Rose <michael@rosenetwork.net>
2016-03-02 13:49:51 -0800

    editing i3-colors.fish

--- i3-colors.fish
+++ i3-colors.fish
@@ -1,9 +1,10 @@
 function i3-colors
-	set info (p (i3-color-rofi))
-  set switches (odds $info)
-  set colors (evens $info)
-  for i in (seq (count $switches))
-    set com $com $switches[$i] fuck \"$colors[$i]\"
-  end
-  echo $com
+	set info (println (i3-color-rofi))
+  count $info
+  # set switches (odds $info)
+  # set colors (evens $info)
+  # for i in (seq (count $switches))
+  #   set com $com $switches[$i] fuck \"$colors[$i]\"
+  # end
+  # echo $com
 end
