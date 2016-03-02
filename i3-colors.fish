c55118117edf8878fd621b5ee517e69338dfb00c
Michael Rose <michael@rosenetwork.net>
2016-03-02 13:43:18 -0800

    editing i3-colors.fish

--- i3-colors.fish
+++ i3-colors.fish
@@ -4,7 +4,7 @@ function i3-colors
   set colors (evens $info)
   for i in (seq (count $switches))
     set com $com $switches[$i]
-    set com $com (singlequote $colors[$i])
+    # set com $com (singlequote $colors[$i])
   end
   echo $com
 end
