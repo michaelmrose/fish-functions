085e0d30b075860ba52da8e2e1c50f204468d586
Michael Rose <michael@rosenetwork.net>
2016-03-02 13:56:01 -0800

    editing rfi.fish

--- rfi.fish
+++ rfi.fish
@@ -1,17 +1,11 @@
 function rfi
 	set bindings -kb-accept-custom "Return"
-  set info (explode-words (i3-color-rofi))[1..-2]
-  set switches (odds $info)
-  set colors (evens $info)
-  for i in (seq (count $switches))
-    set args $args $switches[$i] \"$colors[$i]\"
-  end
     switch $argv[1]
         case run
-            rofi $args $bindings -show run
+            rofi $bindings -show run
             #dmenu_run -nb "black" -sf "#036300" -sb "#A6CD01" -nf "grey" -b -l 10 -i -p "Command: "
         case window
-            rofi $args -show window
+            rofi -show window
         case match
             if test (count $argv) -lt 3
                 return 1
