fb93a1abf9e4674c961dca040547f36a5d6118fc
Michael Rose <michael@rosenetwork.net>
2016-03-02 13:45:11 -0800

    editing i3-colors.fish

--- i3-colors.fish
+++ i3-colors.fish
@@ -5,7 +5,8 @@ function i3-colors
   for i in (seq (count $switches))
     set com $com $switches[$i]
     set com $com $colors[$i]
-    set com $com (quote $colors[$i])
+    set quoted (quote $colors[$i])
+    # set com $com (quote $colors[$i])
   end
   echo $com
 end
