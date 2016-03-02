acdcd5961b7a029b908d88aead77100018ecad04
Michael Rose <michael@rosenetwork.net>
2016-03-01 19:54:29 -0800

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -3,9 +3,8 @@ function set-path
 
     set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games
 
-    set mypaths /opt/racket/bin /opt/i3blocks /opt/bin /opt/dmenu /opt/scala/bin /opt/vim-qt/bin /home/michael/bin /home/michael/scripts  /opt/cxoffice/bin ~/games/dustaet/ /opt/popcorntime /home/michael/proj/contrib ~/.autojump/bin /home/michael/proj/fzf/bin /usr/games/bin
+    set mypaths /opt/racket/bin /opt/i3blocks /opt/bin /home/michael/bin /home/michael/scripts  /opt/cxoffice/bin /home/michael/proj/contrib ~/.autojump/bin /usr/games/bin
 
-    set vi3 /opt/vi3/bin
 
-    set PATH $mypaths $javapaths $builtinpaths $vi3
+    set PATH $mypaths $javapaths $builtinpaths
 end
