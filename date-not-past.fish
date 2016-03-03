89116aa5e65e80a6ea8f4799bc317cf19e564919
Michael Rose <michael@rosenetwork.net>
2016-03-02 17:55:58 -0800

    editing date-not-past.fish

--- date-not-past.fish
+++ date-not-past.fish
@@ -1,9 +1,9 @@
 function date-not-past
 	set currentdate (date +%j)
     set date (convert-date-to-day-of-year $argv)
-    set diff (math "$date - $currentdate")
-    if test $diff -lt -300
-        set date (math $date + 365)
-    end
+    # set diff (math "$date - $currentdate")
+    # if test $diff -lt -300
+    #     set date (math $date + 365)
+    # end
     test $date -ge $currentdate
 end
