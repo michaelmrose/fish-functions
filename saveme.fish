fb0856af32152a1af3aad31a831aa4db4bcc71ed
Michael Rose <michael@rosenetwork.net>
2016-03-03 14:36:42 -0800

    editing saveme.fish

--- saveme.fish
+++ saveme.fish
@@ -1,7 +1,11 @@
 function saveme
-	i3-save-tree --workspace (get-focused-workspace) | sed 's-^\([[:blank:]]*\)//\([[:blank:]]"class".*\),$-\1\2-' > ~/.i3/sessions/{$argv}.json
+	i3-save-tree --workspace (get-focused-workspace) | sed 's-^\([[:blank:]]*\)//\([[:blank:]]"class".*\),$--' > ~/.i3/sessions/{$argv}.json
     set sessionscript ~/sessions/{$argv}
     echo '#!/usr/bin/fish' > $sessionscript
     chmod +x $sessionscript
-    eval $EDITOR $sessionscript
+    if in-terminal
+      eval $EDITOR $sessionscript
+    else
+      qvim $sessionscript
+    end
 end
