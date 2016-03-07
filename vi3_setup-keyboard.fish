8f2bd2f19ec3b679d002e7269bd1244645f41e53
Michael Rose <michael@rosenetwork.net>
2016-03-07 12:07:50 -0800

    editing vi3_setup-keyboard.fish

--- vi3_setup-keyboard.fish
+++ vi3_setup-keyboard.fish
@@ -1,6 +1,6 @@
 function vi3_setup-keyboard
 	killall xcape
-	/opt/bin/xcape -t 250 -e 'Super_L=XF86LaunchB'
-	/opt/bin/xcape -t 250 -e 'Alt_L=Page_Up'
-	/opt/bin/xcape -t 250 -e 'Control_L=Page_Down'
+	/usr/bin/xcape -t 250 -e 'Super_L=XF86LaunchB'
+	/usr/bin/xcape -t 250 -e 'Alt_L=Page_Up'
+	/usr/bin/xcape -t 250 -e 'Control_L=Page_Down'
 end
