281762f6acf0a0e029e65f4886d75bd34de23b1d
Michael Rose <michael@rosenetwork.net>
2016-03-07 12:08:53 -0800

    editing vi3_bind-shift-keys.fish

--- vi3_bind-shift-keys.fish
+++ vi3_bind-shift-keys.fish
@@ -1,4 +1,4 @@
 function vi3_bind-shift-keys
-	/opt/bin/xcape -t 250 -e 'Shift_L=XF86Launch1'
-    /opt/bin/xcape -t 250 -e 'Shift_R=XF86Launch2'
+	/usr/bin/xcape -t 250 -e 'Shift_L=XF86Launch1'
+  /usr/bin/xcape -t 250 -e 'Shift_R=XF86Launch2'
 end
