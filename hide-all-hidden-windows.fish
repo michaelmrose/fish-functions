13cbf92b9481e5656e2a1c5a8efa7bb349cd60b6
Michael Rose <michael@rosenetwork.net>
2016-03-08 00:09:02 -0800

    editing hide-all-hidden-windows.fish

--- hide-all-hidden-windows.fish
+++ hide-all-hidden-windows.fish
@@ -1,6 +1,5 @@
 function hide-all-hidden-windows
-	fix-all-trans
-  set hidden (filter-with-expr is-window-hidden (list-windows))
+	set hidden (filter-with-expr is-window-hidden (list-windows))
   for id in $hidden
     transset -i $id 0 
   end
