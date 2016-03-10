facd4b47a87c9c3e2d802be26b845046c3932253
Michael Rose <michael@rosenetwork.net>
2016-03-10 04:27:57 -0800

    editing fade-in-trans-window.fish

--- fade-in-trans-window.fish
+++ fade-in-trans-window.fish
@@ -9,7 +9,7 @@ function fade-in-trans-window
   for i in (seq $steps)
     set val (wcalc -q "$transstep * $i")
     # transset -i $id $val > /dev/null
-    echo transset -i (squish (apply-to-list "echo -i " $argv) | sed 's#-i# -i #g' | trim)
+    transset (squish (apply-to-list "echo -i " $argv) | sed 's#-i# -i #g' | trim)
     sleep $step
   end
 end
