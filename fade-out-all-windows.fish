22fccfeb0af981308d33c85c5b544823e888b1b5
Michael Rose <michael@rosenetwork.net>
2016-03-10 11:22:33 -0800

    editing fade-out-all-windows.fish

--- fade-out-all-windows.fish
+++ fade-out-all-windows.fish
@@ -1,17 +1,9 @@
 function fade-out-all-windows
-	set steps 10
-  set vals 0.75 0.675 0.6 0.525 0.45 0.375 0.3 0.225 0.150 0.0
+	set vals 0.75 0.675 0.6 0.525 0.45 0.375 0.3 0.225 0.150 0.0
   for i in $vals
     for j in (list-windows)
       transset -i $j $i > /dev/null
     end
+    sleep 0.01
   end
-  # for i in (reverse 0 (seq $steps))
-  #   for j in (list-windows)
-  #     set start (return-real-trans-value $j)
-  #     set transstep (wcalc -q "$start / $steps")
-  #     set val (wcalc -q "$transstep * $i")
-  #     transset -i $j $val > /dev/null
-  #   end
-  # end
 end
