bec49c54821c38020b6e38c1092eaf2bfd4d7664
Michael Rose <michael@rosenetwork.net>
2016-03-10 05:53:44 -0800

    editing lockme.fish

--- lockme.fish
+++ lockme.fish
@@ -1,8 +1,9 @@
 function lockme
 	i3-elements hide
-  for window in (windows-list)
-    transset -i $window 0
-  end
+  # for window in (windows-list)
+  #   transset -i $window 0
+  # end
+  fade-out-all-windows
   sleep 0.25
   i3-msg mode locked
 end
