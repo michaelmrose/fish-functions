aa1d2d781ad73d9f63b93dbf076ac20b4e75b34c
Michael Rose <michael@rosenetwork.net>
2016-03-10 03:44:44 -0800

    editing window:focus.fish

--- window:focus.fish
+++ window:focus.fish
@@ -1,5 +1,7 @@
 function window:focus
+	set -U OLD_WINDOW $NEW_WINDOW
+  set -U NEW_WINDOW (xdotool getactivewindow)
 	hide-all-hidden-windows
   sleep 0.1
-  fade-in-trans-window (xdotool getactivewindow)
+  fade-in-trans-window $NEW_WINDOW
 end
