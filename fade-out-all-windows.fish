3ef2b9c222d6a48c85ac46320195643d3610f990
Michael Rose <michael@rosenetwork.net>
2016-03-10 11:45:31 -0800

    editing fade-out-all-windows.fish

--- fade-out-all-windows.fish
+++ fade-out-all-windows.fish
@@ -1,7 +1,8 @@
 function fade-out-all-windows
 	set vals 0.75 0.725 0.7 0.675 0.65 0.625 0.6 0.575 0.550 0.525 0.5 0.475 0.45 0.425 0.4 0.375 0.350 0.325 0.3 0.275 0.25 0.225 0.2 0.175 0.150 0.125 0.1 0.075 0.050 0.025 0.0
+  set windows (list-windows) (i3-elements ids)
   for i in $vals
-    for j in (list-windows)
+    for j in $windows
       transset -i $j $i > /dev/null
     end
   end
