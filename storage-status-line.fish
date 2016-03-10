0927f7869ee728e838860aeccada87dc64496655
Michael Rose <michael@rosenetwork.net>
2016-03-10 03:56:40 -0800

    editing storage-status-line.fish

--- storage-status-line.fish
+++ storage-status-line.fish
@@ -1,4 +1,4 @@
 function storage-status-line
-	set res (zf list -H tank | condense_spaces | cut -d ' ' -f1-3)
+	set res (println (zf list -H tank | condense_spaces | cut -d ' ' -f1-3))
   echo $res[1] holds $res[2] with $res[3] remaining
 end
