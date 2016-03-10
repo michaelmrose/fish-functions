211e947178545d6dccfa54d63a2cb405c221b58a
Michael Rose <michael@rosenetwork.net>
2016-03-10 03:39:03 -0800

    editing fade-in-trans-window.fish

--- fade-in-trans-window.fish
+++ fade-in-trans-window.fish
@@ -1,14 +1,14 @@
 function fade-in-trans-window
-	set step 0.03
+	set step 0.02
   set steps 7
   set id $argv
   set final (return-trans-value (return-winclass $argv))
   set transstep (wcalc -q "$final / $steps")
-  echo final is $final
-  echo transstep is $transstep
+  # echo final is $final
+  # echo transstep is $transstep
   for i in (seq $steps)
     set val (wcalc -q "$transstep * $i")
-    transset -i $id $val
+    transset -i $id $val > /dev/null
     sleep $step
   end
 end
