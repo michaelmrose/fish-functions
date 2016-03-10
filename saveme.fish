21b7637fdd26eddba287fae7435453961fb8f558
Michael Rose <michael@rosenetwork.net>
2016-03-10 14:01:52 -0800

    removed saveme.fish

--- saveme.fish
+++
@@ -1,8 +0,0 @@
-function saveme
-	# i3-save-tree --workspace (get-focused-workspace) | sed 's-^\([[:blank:]]*\)//\([[:blank:]]"class".*\),$--' > ~/.i3/sessions/{$argv}.json
-	i3-save-tree --workspace (get-focused-workspace) | sed 's-^\([[:blank:]]*\)//\([[:blank:]]"class".*\),$--' > ~/.i3/sessions/{$argv}.json
-  set sessionscript ~/sessions/{$argv}
-  echo '#!/bin/fish' > $sessionscript
-  chmod +x $sessionscript
-  eval $EDITOR $sessionscript
-end
