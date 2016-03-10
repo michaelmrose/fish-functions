ff48b1b0d720add2b3178bc7b767e394510b6587
Michael Rose <michael@rosenetwork.net>
2016-03-10 05:22:56 -0800

    editing new-fix-trans.fish

--- new-fix-trans.fish
+++ new-fix-trans.fish
@@ -2,5 +2,6 @@ function new-fix-trans
 	set switches (repeat (count (list-windows)) -i)
   set windows (list-windows)
   set vals (apply-to-list return-trans-value (apply-to-list return-winclass (list-windows)))
-  zip-lists (zip-lists $switches $windows) $vals | sed ':a;N;$!ba;s/\n/ /g'
+  transset (zip-lists (zip-lists $switches $windows) $vals | sed ':a;N;$!ba;s/
+/ /g')
 end
