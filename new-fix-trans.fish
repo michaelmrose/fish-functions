aec5ec86a54d80d4ed61eb166b54037330e848a2
Michael Rose <michael@rosenetwork.net>
2016-03-10 05:19:44 -0800

    editing new-fix-trans.fish

--- new-fix-trans.fish
+++ new-fix-trans.fish
@@ -1,4 +1,6 @@
 function new-fix-trans
 	set switches (repeat (count (list-windows)) -i)
-  p $switches
+  set windows (list-windows)
+  set vals (apply-to-list return-trans-value (apply-to-list return-winclass (list-windows)))
+  p $vals
 end
