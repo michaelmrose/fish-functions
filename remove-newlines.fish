3cc4eca2d130d1c76931e13d8c286b9453cdd2b9
Michael Rose <michael@rosenetwork.net>
2016-03-10 04:39:38 -0800

    editing remove-newlines.fish

--- remove-newlines.fish
+++ remove-newlines.fish
@@ -1,10 +1,3 @@
 function remove-newlines
-	if exists argv
-    echo $argv | sed ':a;N;$!ba;s/\n/ /g'
-  else
-    while read -l line
-      set acc $acc $line
-    end
-    echo $acc | sed ':a;N;$!ba;s/\n/ /g'
-  end
+	echo $argv | sed ':a;N;$!ba;s/\n/ /g'
 end
