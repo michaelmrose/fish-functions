843279c3d0179b31716b9e53bc8bcb9f7c3f9659
Michael Rose <michael@rosenetwork.net>
2016-03-10 04:00:52 -0800

    editing storage-status-line.fish

--- storage-status-line.fish
+++ storage-status-line.fish
@@ -1,4 +1,4 @@
 function storage-status-line
 	set res (explode (zf list -H tank | condense_spaces | cut -d ' ' -f1-3))
-  echo $res[1]  size: $res[2] remaining: $res[3]
+  echo 🖴  ZFS: $res[1]  size: $res[2] remaining: $res[3]
 end
