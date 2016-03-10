ac46374bf4b184bdc75d80cb3f7fd7c02b39fd4f
Michael Rose <michael@rosenetwork.net>
2016-03-10 05:44:13 -0800

    editing fade-in-all-windows.fish

--- fade-in-all-windows.fish
+++ fade-in-all-windows.fish
@@ -1,15 +1,12 @@
 function fade-in-all-windows
 	set step 0.01
   set steps 5
-  set id $argv
-  set final (return-trans-value (return-winclass $argv))
-  set transstep (wcalc -q "$final / $steps")
   for i in (seq $steps)
-    for i in (list-windows)
-      set final (return-real-trans-value $argv)
+    for j in (list-windows)
+      set final (return-real-trans-value $j)
       set transstep (wcalc -q "$final / $steps")
       set val (wcalc -q "$transstep * $i")
-      transset -i $id $val > /dev/null
+      transset -i $j $val > /dev/null
     end
     sleep $step
   end
