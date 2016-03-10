ddcbdfffc15caa61396a86910e43132f5996e628
Michael Rose <michael@rosenetwork.net>
2016-03-10 05:52:43 -0800

    editing fade-out-all-windows.fish

---
+++ fade-out-all-windows.fish
@@ -0,0 +1,11 @@
+function fade-out-all-windows
+	set steps 7
+  for i in (reverse 0 (seq $steps))
+    for j in (list-windows)
+      set start (return-real-trans-value $j)
+      set transstep (wcalc -q "$start / $steps")
+      set val (wcalc -q "$transstep * $i")
+      transset -i $j $val > /dev/null
+    end
+  end
+end
