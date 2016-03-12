e5366a05df33a268e8fe1a3676674503c5b2ea62
Michael Rose <michael@rosenetwork.net>
2016-03-11 17:20:33 -0800

    editing window:focus.fish

--- window:focus.fish
+++ window:focus.fish
@@ -1,7 +1,8 @@
 function window:focus
-	fix-all-trans
-	# set -U OLD_WINDOW $NEW_WINDOW
-  # set -U NEW_WINDOW (xdotool getactivewindow)
+	set -U OLD_WINDOW $NEW_WINDOW
+  set -U NEW_WINDOW (xdotool getactivewindow)
+  fix-trans-fade $OLD_WINDOW
+  fix-trans-fade $NEW_WINDOW
   # # hide-all-hidden-windows
   # # fade-in-trans-window (xdotool getactivewindow)
   # if is-window-hidden $OLD_WINDOW
