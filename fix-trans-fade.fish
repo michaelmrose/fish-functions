cd608510e2b8f026483d583f7c9723e6b396aca1
Michael Rose <michael@rosenetwork.net>
2016-03-11 17:19:14 -0800

    editing fix-trans-fade.fish

---
+++ fix-trans-fade.fish
@@ -0,0 +1,14 @@
+function fix-trans-fade
+	set class (return-winclass $argv)
+  set val (return-trans-value $class)
+  if test $val -eq 0
+    set vals 0.75 0.65 0.55 0.45 0.35 0.25 0.15 0.05 0.0
+  else
+    set vals 0.05 0.15 0.25 0.35 0.45 0.55 0.65 0.75 $val
+  end
+  for i in $vals
+    transset -i $argv $i
+    sleep 0.05
+  end
+	# transset -i $argv (return-real-trans-value $argv)
+end
