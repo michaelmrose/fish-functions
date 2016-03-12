9107266c30f3e483d7891a24fd3f5f5d3d5ccfc3
Michael Rose <michael@rosenetwork.net>
2016-03-11 17:15:56 -0800

    editing fix-trans.fish

--- fix-trans.fish
+++ fix-trans.fish
@@ -1,5 +1,13 @@
 function fix-trans
 	set class (return-winclass $argv)
   set val (return-trans-value $class)
-	transset -i $argv (return-real-trans-value $argv)
+  if test $val -eq 0
+    set vals 0.75 0.65 0.55 0.45 0.35 0.25 0.15 0.05 0.0
+  else
+    set vals 0.05 0.15 0.25 0.35 0.45 0.55 0.65 0.75 $val
+  end
+  for i in $vals
+    transset -i $argv $i
+  end
+	# transset -i $argv (return-real-trans-value $argv)
 end
