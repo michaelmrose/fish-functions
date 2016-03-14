4114b8bc1e9da3e7145626552872e84026be0bf9
Michael Rose <michael@rosenetwork.net>
2016-03-13 23:45:17 -0700

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -1,18 +1,19 @@
 function focus-app
-	set currentclass (winclass)
-    set command (appkey $argv)
-    set targetclass (return-windowclass $command)
-    sendit t is $targetclass c is $currentclass
-    if match $currentclass $targetclass
-        nextwindow
-        msg next
-    else
-        set -U lasttarget $class
-        if focus class (tolower $class)
-        else
-            focus $command
-        end
-    end
-    er vi3op
-    update-op-status
+	# set currentclass (winclass)
+  # set command (appkey $argv)
+  echo focus (tolower (return-windowclass (appkey $argv)))
+  # set targetclass (return-windowclass $command)
+  # sendit t is $targetclass c is $currentclass
+  # if match $currentclass $targetclass
+  #     nextwindow
+  #     msg next
+  # else
+  #     set -U lasttarget $class
+  #     if focus class (tolower $class)
+  #     else
+  #         focus $command
+  #     end
+  # end
+  er vi3op
+  update-op-status
 end
