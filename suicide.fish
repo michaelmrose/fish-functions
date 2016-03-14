1f1dd6963aa3ec1b259ff8947c2421c3c41dfc48
Michael Rose <michael@rosenetwork.net>
2016-03-13 19:02:09 -0700

    editing suicide.fish

--- suicide.fish
+++ suicide.fish
@@ -2,10 +2,7 @@ function suicide
 	set -U ZERO_HOUR (seconds)
   function fish_prompt
     if greater-than (seconds) $ZERO_HOUR
-      echo if true
       kill -9 %self
-    else
-      echo if false
     end
   end
 end
