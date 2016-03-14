d2837675a81b9c27e22659ec4c7f701c0828e508
Michael Rose <michael@rosenetwork.net>
2016-03-13 23:53:58 -0700

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -2,9 +2,7 @@ function focus-app
 	msg focus app run and argv is $argv
 	# set currentclass (winclass)
   # set command (appkey $argv)
-  set -U COMMAND focus (tolower (return-windowclass (appkey $argv)))
-  msg $COMMAND
-  eval $COMMAND
+  focus (tolower (return-windowclass (appkey $argv)))
   # set targetclass (return-windowclass $command)
   # sendit t is $targetclass c is $currentclass
   # if match $currentclass $targetclass
