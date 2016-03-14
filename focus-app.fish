fc953840429824b953fd0a88a006bddd2651a181
Michael Rose <michael@rosenetwork.net>
2016-03-13 23:55:39 -0700

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -1,6 +1,6 @@
 function focus-app
 	msg focus app run and argv is $argv
-  echo focus (tolower (return-windowclass (appkey $argv)))
+  echo focus (appkey $argv)
   er vi3op
   update-op-status
 end
