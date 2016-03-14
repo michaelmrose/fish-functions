e00b91540834d55e8ae444bf39f1db761da357cc
Michael Rose <michael@rosenetwork.net>
2016-03-13 18:58:54 -0700

    editing suicide.fish

--- suicide.fish
+++ suicide.fish
@@ -1,8 +1,8 @@
 function suicide
 	set -U ZERO_HOUR (seconds)
   function fish_prompt
-    echo (seconds) $ZERO_HOUR
     if test (seconds) -gt ZERO_HOUR
+      echo if true
       kill -9 %self
     end
   end
