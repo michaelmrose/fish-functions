4e3d05593bd81394d837b233de4a65948b0aa77e
Michael Rose <michael@rosenetwork.net>
2016-03-16 14:28:33 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,4 +1,7 @@
 function setvolume
-	ponymix (match-lists (car $argv) "+ -" "increase decrease" set-volume) (stripsign $argv)
-    signal-i3blocks output
+	# ponymix (match-lists (car $argv) "+ -" "increase decrease" set-volume) (stripsign $argv)
+  set vol (ponymix get-volume)
+  set newvol (wcalc -q "$vol + $argv")
+  ponymix (set-volume $newvol)
+  signal-i3blocks output
 end
