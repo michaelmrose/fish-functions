a5e942052c4de649756bcfe8b7a5e15462418581
Michael Rose <michael@rosenetwork.net>
2016-03-17 16:43:49 -0700

    editing pick-version.fish

--- pick-version.fish
+++ pick-version.fish
@@ -1,3 +1,3 @@
 function pick-version
-	pkg i ={$argv}-(rfi match "select a version of $argv" (equery y $argv | condense_spaces | cut -d " " -f1 | sed 's/\[I\]//g' | grep -E '[0-9]'))
+	pkg i ={$argv}-(rfi match "select a version of $argv" (equery y $argv | condense_spaces | cut -d " " -f1 | sed 's/\[I\]//g' | grep -E '[0-9]')) --ask
 end
