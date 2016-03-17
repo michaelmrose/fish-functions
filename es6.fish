4989a60484b8434a1cc7f6b82f915d269cee46d2
Michael Rose <michael@rosenetwork.net>
2016-03-16 22:44:26 -0700

    editing es6.fish

--- es6.fish
+++ es6.fish
@@ -1,9 +1,5 @@
 function es6
-	set containingFolder (cutlastn / 2- $argv)
-  if substr $argv /
-    set file (cutlastn / 1 $argv | cut -d . -f1) 
-  else
-    set file $argv
-  end
-  echo traceur --out $containingFolder/$file.js --script $argv
+	set in $argv
+  set out (echo $in | sed 's/#.js$#.es6$#g')
+  echo traceur --out $out --script $in
 end
