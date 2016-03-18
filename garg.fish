65379aecaf30637306a8f42f8aad94f3bb2a645b
Michael Rose <michael@rosenetwork.net>
2016-03-17 23:01:03 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -4,22 +4,25 @@ function garg
       gargoyle ~/if/$LAST_GAME
       return 0
     case select
-      switch (count $argv)
-        case 1
-          set games (ls ~/if)
-        case '*'
+      if exists $argv[2]
           set games (ls ~/if | grep -i $argv[2..-1])
+        else
+          set games (ls ~/if)
       end
+      if test (count $games) -gt 1
+        set game (rfi match "select a game: " $games)
+      else
+        set game $games
+      end
+      if exists $game
+        set -U LAST_GAME $game
+      end
+      gargoyle ~/if/$game
+      # switch (count $argv)
+      #   case 1
+      #     set games (ls ~/if)
+      #   case '*'
+      #     set games (ls ~/if | grep -i $argv[2..-1])
+      # end
   end
-  echo game is $game and games is $games
-  if test (count $games) -gt 1
-    set game (rfi match "select a game: " $games)
-  else
-    set game $games
-  end
-  echo game is $game and games is $games
-  if exists $game
-    set -U LAST_GAME $game
-  end
-  gargoyle ~/if/$game
 end
