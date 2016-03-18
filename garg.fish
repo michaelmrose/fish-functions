0e72fbce0c493e380cd0cfeaaa5ba6f9a4fc452f
Michael Rose <michael@rosenetwork.net>
2016-03-17 22:48:18 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -1,10 +1,16 @@
 function garg
-	switch (count $argv)
-    case 0
-      set games (ls ~/if)
-    case 1
-      set games (ls ~/if | grep -i $argv)
+	switch $argv
+    case last
+      set game $LAST_GAME
+    case select
+       switch (count $argv)
+         case 1
+          set game (ls ~/if)
+         case '*'
+          set games (ls ~/if | grep -i $argv[2..-1])
+        end
   end
 	set game (rfi match "select a game: " $games)
+  set -U LAST_GAME $game
   gargoyle ~/if/$game
 end
