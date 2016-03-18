6da0ebb74a41856f91218b5f48ffea93ca89eae1
Michael Rose <michael@rosenetwork.net>
2016-03-17 22:35:10 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -1,6 +1,6 @@
 function garg
 	set args (vals 1..-1 $argv)
-	set game (rfi match "select a game: " (ls ~/if/*$args*))
+	set game (rfi match "select a game: " (ls ~/if/))
   echo $game
   # gargoyle ~/if/$game
 end
