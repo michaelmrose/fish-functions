bbe2e91044424aa34d03af7b5a04ff8e3e9cb811
Michael Rose <michael@rosenetwork.net>
2016-03-17 23:04:19 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -6,9 +6,9 @@ function garg
     case select
       switch (count $argv)
         case 1
-          set games (ls ~/if)
+          set games (findall ~/if z5)
         case '*'
-          set games (ls ~/if | grep -i $argv[2..-1])
+          set games (findall ~/if z5 | grep -i $argv[2..-1])
       end
       if test (count $games) -gt 1
         set game (rfi match "select a game: " $games)
