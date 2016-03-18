f5b07f663fcae19c50c36ed206b066ed146728e4
Michael Rose <michael@rosenetwork.net>
2016-03-17 22:53:47 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -10,11 +10,13 @@ function garg
           set games (ls ~/if | grep -i $argv[2..-1])
       end
   end
+  echo game is $game and games is $games
   if test (count $games) -gt 1
     set game (rfi match "select a game: " $games)
   else
     set game $games
   end
+  echo game is $game and games is $games
   if exists $game
     set -U LAST_GAME $game
   end
