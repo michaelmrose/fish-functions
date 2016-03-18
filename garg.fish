ace477a917a19b79bc931c8090ea13bcd007b535
Michael Rose <michael@rosenetwork.net>
2016-03-17 22:50:59 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -3,14 +3,18 @@ function garg
     case last
       set game $LAST_GAME
     case select
-       switch (count $argv)
-         case 1
-          set game (ls ~/if)
-         case '*'
+      switch (count $argv)
+        case 1
+          set games (ls ~/if)
+        case '*'
           set games (ls ~/if | grep -i $argv[2..-1])
-        end
+      end
   end
-	set game (rfi match "select a game: " $games)
+  if test (count $games) -gt 1
+    set game (rfi match "select a game: " $games)
+  else
+    set game $games
+  end
   set -U LAST_GAME $game
   gargoyle ~/if/$game
 end
