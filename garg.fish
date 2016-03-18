1b1eeda21a556af03694928970d725dfcbcecea3
Michael Rose <michael@rosenetwork.net>
2016-03-17 22:39:56 -0700

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -1,9 +1,10 @@
 function garg
-	if exists $argv
-    set games (ls ~/if)
-  else
-    set games (ls ~/if | grep -i $argv)
-  end
+	# if exists $argv
+    # set games (ls ~/if)
+  # else
+    # set games (ls ~/if | grep -i $argv)
+  # end
+  set games 1 2 3
 	set game (rfi match "select a game: " $games)
   echo $game
   # gargoyle ~/if/$game
