16f4ad0f0dbab8984c46a0f9f7f3cf34eec342bb
Michael Rose <michael@rosenetwork.net>
2016-03-20 16:20:37 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -33,6 +33,7 @@ function m
         eval $EDITOR ~/playlists/$argv[2]
         return 0
       case play
+        echo in switch
         set pl ~/playlists/$argv[2]
         if test -f $pl
           # m (cat $pl)
