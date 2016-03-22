b7109deb69c52d076645e234378918d868df8f85
Michael Rose <michael@rosenetwork.net>
2016-03-21 22:35:01 -0700

    editing date-in-future.fish

---
+++ date-in-future.fish
@@ -0,0 +1,13 @@
+function date-in-future
+	set currentdate (date +%j)
+  set date (convert-date-to-day-of-year $argv)
+  set diff (math "$date - $currentdate")
+  # echo d is $diff
+  if test $diff -lt -300
+      set date (math $date + 365)
+  end
+  if test $diff -gt 60
+    return 1
+  end
+  test $date -gt $currentdate
+end
