cb8f4c7cc92a23274705ed26f00842d569b9419f
Michael Rose <michael@rosenetwork.net>
2016-03-25 15:45:55 -0700

    editing bounded.fish

---
+++ bounded.fish
@@ -0,0 +1,11 @@
+function bounded
+	set val $argv[1]
+  set min $argv[2]
+  set max $argv[3]
+  if test $val -gt $max
+    set val $max
+  else if test $val -lt $min
+    set val $min
+  end
+  echo $val
+end
