97e4b09a7da04cd48ff25fb7134c94b8f1585c9c
Michael Rose <michael@rosenetwork.net>
2016-03-25 15:50:47 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,9 +1,8 @@
 function setvolume
 	# ponymix (match-lists (car $argv) "+ -" "increase decrease" set-volume) (stripsign $argv)
-  if echo 5 | ag  '^\+|-' > /dev/null
-    echo if true
+  if echo $argv | ag  '^\+|-' > /dev/null
     set vol (ponymix get-volume)
-    set newvol (wcalc -q "$vol + $argv")
+    set newvol (bounded (wcalc -q "$vol + $argv") 0 130)
   else
     set newvol $argv
   end
