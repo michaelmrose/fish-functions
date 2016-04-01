1c95ed6d5d9c160b489c16992eef1135682fac09
Michael Rose <michael@rosenetwork.net>
2016-04-01 11:49:59 -0700

    editing event-in-future.fish

--- event-in-future.fish
+++ event-in-future.fish
@@ -1,3 +1,12 @@
 function event-in-future
-	date-in-future (echo $argv | cut -d " " -f5)
+	set date (echo $argv | cut -d " " -f5)
+  set time 42
+  if date-in-future $date
+    return 0
+  end
+  if date-is-current -a not time-has-passed $time
+    return 0
+  else
+    return 1
+  end
 end
