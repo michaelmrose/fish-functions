66f404c3c914e5bf1fc652809b48a0a5fb2631cd
Michael Rose <michael@rosenetwork.net>
2016-04-01 13:21:43 -0700

    editing add-to-recent-reads.fish

--- add-to-recent-reads.fish
+++ add-to-recent-reads.fish
@@ -3,7 +3,7 @@ function add-to-recent-reads --description 'keep a list of the 10 most recent un
         set title $argv
         # set library (get-current-calibre-library)
         # set entry \"$title @$library\"
-        set -U recent_reads (take 10 (unique (println "$title" $recent_reads)))
+        set -U recent_reads (take 15 (unique (println "$title" $recent_reads)))
         echo \"$title\"
     else
         msg could not add to recent reads
