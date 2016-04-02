2afca3d79180c6119769d85c412c79b292e4b3e6
Michael Rose <michael@rosenetwork.net>
2016-04-02 12:04:26 -0700

    editing browser-memory-info.fish

--- browser-memory-info.fish
+++ browser-memory-info.fish
@@ -1,9 +1,9 @@
 function browser-memory-info
 	set res ""
-  if pgrep firefox
+  if pgrep firefox > /dev/null
     set res $res firefox: (meminfo firefox)
   end
-  if pgrep aurora
+  if pgrep aurora > /dev/null
     set res $res aurora: (meminfo aurora)
   end
   echo $res
