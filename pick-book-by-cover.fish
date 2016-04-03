b2566f67ad7f96d534f3e005b1c8a8e7747907ca
Michael Rose <michael@rosenetwork.net>
2016-04-02 20:02:01 -0700

    editing pick-book-by-cover.fish

--- pick-book-by-cover.fish
+++ pick-book-by-cover.fish
@@ -1,4 +1,5 @@
 function pick-book-by-cover
 	set choice (findindex (sxiv -tbfor $RECENT_BOOK_COVERS) $RECENT_BOOK_COVERS)
-  zathura $recent_reads[$choice] &
+  books $recent_reads[$choice]
+	# set choice (findindex (sxiv -tbfor $RECENT_BOOK_COVERS) $RECENT_BOOK_COVERS)
 end
