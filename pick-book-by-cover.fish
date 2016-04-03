2b704264f341e2c52030bb937463137d75c36c2a
Michael Rose <michael@rosenetwork.net>
2016-04-02 20:00:20 -0700

    editing pick-book-by-cover.fish

--- pick-book-by-cover.fish
+++ pick-book-by-cover.fish
@@ -1,7 +1,6 @@
 function pick-book-by-cover
 	# set title (get-title-from-cover (sxiv -tbfor $RECENT_BOOK_COVERS))
-  set cover (sxiv -tbfor $RECENT_BOOK_COVERS)
-  set choice (findindex $cover $RECENT_BOOK_COVERS)
+  set choice (findindex (sxiv -tbfor $RECENT_BOOK_COVERS) $RECENT_BOOK_COVERS)
   books $recent_reads[$choice]
   # echo $title
   # eval books $title
