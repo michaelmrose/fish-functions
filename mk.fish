40283d3d1178ce2d91539b94643d3825e2c62da7
Michael Rose <michael@rosenetwork.net>
2016-04-05 12:07:26 -0700

    editing mk.fish

--- mk.fish
+++ mk.fish
@@ -2,13 +2,17 @@ function mk
 	switch $argv[1]
     case -e
       er MARKED
+    case -p
+      p $MARKED
     case -m
       for i in $MARKED
         mv $i $argv[2]
+        er MARKED
       end
     case -c
       for i in $MARKED
         cp -R $i $argv[2]
+        er MARKED
       end
     case '*'
       for i in $argv
