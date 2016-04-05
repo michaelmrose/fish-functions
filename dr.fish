8ed7af77989e756f4f3618b2f79c42e8fbdb7650
Michael Rose <michael@rosenetwork.net>
2016-04-05 11:46:11 -0700

    editing dr.fish

---
+++ dr.fish
@@ -0,0 +1,10 @@
+function dr
+	if test -d $argv
+    set dir $argv
+  else if bmark $argv > /dev/null
+    set dir (bmark $argv) > /dev/null
+  else
+    set dir (autojump $argv) > /dev/null
+  end
+  echo $dir
+end
