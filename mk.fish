1d16e962ce5fdc1f8cb74d279b32f8940b05ece4
Michael Rose <michael@rosenetwork.net>
2016-04-05 12:01:55 -0700

    editing mk.fish

---
+++ mk.fish
@@ -0,0 +1,18 @@
+function mk
+	switch $argv
+    case -e
+      er $MARKED
+    case -m
+      for i in $MARKED
+        mv $i $argv[2]
+      end
+    case -c
+      for i in $MARKED
+        cp $i $argv[2]
+      end
+    case '*'
+      for i in $argv
+        set -U MARKED $MARKED (pathof $i)
+      end
+  end
+end
