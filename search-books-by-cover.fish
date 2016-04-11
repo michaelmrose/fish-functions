e5192c0e5e96ccd6d803ba61732af0cafbade516
Michael Rose <michael@rosenetwork.net>
2016-04-10 19:28:05 -0700

    editing search-books-by-cover.fish

---
+++ search-books-by-cover.fish
@@ -0,0 +1,11 @@
+function search-books-by-cover
+	set titles (query-calibre-title $argv)
+  set covers (apply-to-list 'get-title-from-cover' $titles)
+  set choices (sxiv -tbfor $covers)
+  for choice in $choices
+    set books $books $titles[(findindex $choice $covers)]
+  end
+  for book in $books
+    books $book &
+  end
+end
