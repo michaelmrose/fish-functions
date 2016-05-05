c39398bf61af597e0004228b88823e1d247a3b8c
Michael Rose <michael@rosenetwork.net>
2016-05-05 13:07:48 -0700

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -1,11 +1,9 @@
 function set-path
 	set javapaths /usr/lib/jvm/java-8-oracle/bin /usr/lib/jvm/java-8-oracle/db/bin /usr/lib/jvm/java-8-oracle/jre/bin/
 
-    set wtf /home/michael/proj/depot_tools
-
     set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games
 
     set mypaths /opt/racket/bin /opt/i3blocks /opt/bin /home/michael/bin /home/michael/scripts  /opt/cxoffice/bin /home/michael/proj/contrib ~/.autojump/bin /usr/games/bin /opt/vmware/bin
 
-    set PATH $wtf $mypaths $javapaths $builtinpaths
+    set -U PATH $mypaths $javapaths $builtinpaths
 end
