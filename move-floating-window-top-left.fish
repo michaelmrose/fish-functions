ad823a4b7a9ff9c454c3c3965cfd70ec03fcb14f
Michael Rose <michael@rosenetwork.net>
2016-05-06 14:40:54 -0700

    editing move-floating-window-top-left.fish

--- move-floating-window-top-left.fish
+++ move-floating-window-top-left.fish
@@ -1,3 +1,4 @@
 function move-floating-window-top-left
-	
+	set xpos (math (get-focused-display-x-offset) + (get-border-floating) + 1)
+  set ypos (math (get-focused-display-y-offset) + (get-border-floating) + 1)
 end
