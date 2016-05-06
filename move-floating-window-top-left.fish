4e728ae503345a1038b66dd357ba56cc9f3a49ab
Michael Rose <michael@rosenetwork.net>
2016-05-06 14:41:44 -0700

    editing move-floating-window-top-left.fish

--- move-floating-window-top-left.fish
+++ move-floating-window-top-left.fish
@@ -1,4 +1,5 @@
 function move-floating-window-top-left
 	set xpos (math (get-focused-display-x-offset) + (get-border-floating) + 1)
   set ypos (math (get-focused-display-y-offset) + (get-border-floating) + 1)
+  xdotool getactivewindow windowmove  $xpos $ypos
 end
