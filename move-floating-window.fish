d3c39a4bba44d2aaf2c24d481ae8947c22295cc5
Michael Rose <michael@rosenetwork.net>
2016-05-06 14:57:12 -0700

    editing move-floating-window.fish

---
+++ move-floating-window.fish
@@ -0,0 +1,19 @@
+function move-floating-window
+	set border (math (get-border-floating) + 1)
+  set xoff (get-focused-display-x-offset)
+  set yoff (get-focused-display-y-offset)
+  set dwidth (get-focused-display-width)
+  set dheight (get-focused-display-height)
+  set winwidth (get-window-width)
+  set winheight (get-window-height)
+  set ypos (math $yoff + $border)
+  switch $argv[1]
+    case left
+      set xpos (math $xoff + $border)
+    case right
+      set xpos (math $xoff  + $dwidth - $winwidth - $border)
+    case center
+      nil
+  end
+  xdotool getactivewindow windowmove  $xpos $ypos
+end
