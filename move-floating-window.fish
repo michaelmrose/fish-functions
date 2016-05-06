7993be3b10549aab793057014e1e055769a1a037
Michael Rose <michael@rosenetwork.net>
2016-05-06 16:43:07 -0700

    editing move-floating-window.fish

--- move-floating-window.fish
+++ move-floating-window.fish
@@ -15,40 +15,52 @@ function move-floating-window
   set ycenter (math $availableheight / 2 + $yoff) 
   set xcenter (math $dwidth / 2 + $xoff)
   switch $argv[1]
+    case resize
+      xdotool getactivewindow windowsize $argv[2] $argv[3]
+      move-floating-window $LAST_WIN_POS
+      return 0
     case center
       set xpos (math $xcenter - $halfwinwidth)
       set ypos (math $ycenter - $halfwinheight)
+      set -U LAST_WIN_POS center
     case top
     set ypos (math $yoff + $border)
     if test (count $argv) -lt 2 > /dev/null
       set xpos (math $xoff + $halfdispwidth - $halfwinwidth)
+      set -U LAST_WIN_POS top
     else
       switch $argv[2]
         case left
           set xpos (math $xoff + $border)
+          set -U LAST_WIN_POS top left
         case right
           set xpos (math $xoff  + $dwidth - $winwidth - $border)
+          set -U LAST_WIN_POS top right
       end
     end
    case bottom
      set ypos (math $dheight - \($border + $barheight + $winheight\))
      if test (count $argv) -lt 2 > /dev/null
        set xpos (math $xoff + $halfdispwidth - $halfwinwidth)
+       set -U LAST_WIN_POS bottom
     else
       switch $argv[2]
          case left
            set xpos (math $xoff + $border)
+           set -U LAST_WIN_POS bottom left
          case right
            set xpos (math $xoff + $dwidth - $winwidth - $border)
+           set -U LAST_WIN_POS bottom right
        end
      end
    case left
      set ypos (math $ycenter - $halfwinheight)
      set xpos (math $xoff + $border)
+     set -U LAST_WIN_POS left 
    case right
      set ypos (math $ycenter - $halfwinheight)
      set xpos (math $xoff + $dwidth - $winwidth - $border)
-     # set xpos (math $xcenter - $halfwinwidth - $border)
+     set -U LAST_WIN_POS right
    end
    # echo xy $xpos $ypos
    xdotool getactivewindow windowmove  $xpos $ypos
