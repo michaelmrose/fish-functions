cdc9aa2784ae45bbddd4d3b4825669ef71a32d82
Michael Rose <michael@rosenetwork.net>
2016-05-08 15:09:53 -0700

    editing wtf-keyboard-setup.fish

--- wtf-keyboard-setup.fish
+++ wtf-keyboard-setup.fish
@@ -1,12 +1,6 @@
 function wtf-keyboard-setup
-	mate-session &
-  sleep 3
-  sudo killall caja
-  sudo killall mate-session
-  sudo kill -9 (pgrep mate-screensave)
-  sudo kill -9 (pgrep mate-volume-con)
+	/usr/libexec/mate-settings-daemon &
   g15macro &
-  wp recall
   xmodmap ~/.i3/keys
   vi3_setup-keyboard
   vi3_bind-shift-keys
