57d04bd70cde0b30aa7dec574fb11c575a937049
Michael Rose <michael@rosenetwork.net>
2016-05-08 13:00:02 -0700

    editing vi3_start-vi3.fish

--- vi3_start-vi3.fish
+++ vi3_start-vi3.fish
@@ -1,7 +1,5 @@
 function vi3_start-vi3
-	xmodmap keys
-    toggle-symbol-lock
-    vi3_define-vars
-    vi3_setup-keyboard
-    vi3_bind-shift-keys
+	vi3_define-vars
+    # vi3_setup-keyboard
+    # vi3_bind-shift-keys
 end
