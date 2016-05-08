02ba6fbcf737f0a50daa043233e408d318187a54
Michael Rose <michael@rosenetwork.net>
2016-05-08 13:58:04 -0700

    editing vi3_setup-keyboard.fish

--- vi3_setup-keyboard.fish
+++ vi3_setup-keyboard.fish
@@ -1,6 +1,6 @@
 function vi3_setup-keyboard
 	killall xcape
-	/usr/bin/xcape -t 250 -e 'Super_L=F18'
-	/usr/bin/xcape -t 250 -e 'Alt_L=F19'
-	/usr/bin/xcape -t 250 -e 'Control_L=F20'
+  /usr/bin/xcape -t 250 -e 'Control_L=F18'
+	/usr/bin/xcape -t 250 -e 'Super_L=F19'
+  /usr/bin/xcape -t 250 -e 'Alt_L=F20'
 end
