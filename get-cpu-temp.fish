bdaf8903277ef8041130f841331913601b09bfa3
Michael Rose <michael@rosenetwork.net>
2016-05-24 18:42:04 -0700

    editing get-cpu-temp.fish

--- get-cpu-temp.fish
+++ get-cpu-temp.fish
@@ -1,4 +1,5 @@
 function get-cpu-temp
 	# ctof (sensors | grep CPU\ Temp | cut -d ":" -f2 | cut -c 8-11)
- sensors -f | grep temp1 | cut -d + -f2 | cut -d ' ' -f1
+ # sensors -f | grep temp1 | cut -d + -f2 | cut -d ' ' -f1
+ sensors | grep temp1 | cut -d + -f2 | cut -d ' ' -f1
 end
