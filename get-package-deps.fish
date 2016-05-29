8e0f9b04c2528400eb0119d26417c39eeb2cfc6a
Michael Rose <michael@rosenetwork.net>
2016-05-28 23:26:39 -0700

    editing get-package-deps.fish

--- get-package-deps.fish
+++ get-package-deps.fish
@@ -1,3 +1,3 @@
 function get-package-deps
-	p (pkg depgraph twmn | condense_spaces | cut -d ' ' -f3-)[3..-1]
+	p (pkg depgraph twmn | condense_spaces | cut -d ' ' -f3-)[3..-1] | cut -d '-' -f1-2
 end
