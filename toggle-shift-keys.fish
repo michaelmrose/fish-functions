9cd4ef22b9a46421a3a63856cf30f2635c9f0262
Michael Rose <michael@rosenetwork.net>
2016-05-30 22:02:21 -0700

    editing toggle-shift-keys.fish

--- toggle-shift-keys.fish
+++ toggle-shift-keys.fish
@@ -7,7 +7,7 @@ function toggle-shift-keys
     else
         set shiftkeys enabled
         msg shift keys enabled
-        /opt/bin/xcape -e 'Shift_L=XF86Launch1'
-        /opt/bin/xcape -e 'Shift_R=XF86Launch2'
+        xcape -e 'Shift_L=XF86Launch1'
+        xcape -e 'Shift_R=XF86Launch2'
     end
 end
