9af230f37fa5a1389806c814074b75af503812ef
Michael Rose <michael@rosenetwork.net>
2016-06-05 02:41:15 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,9 +1,11 @@
 function xrr
 	set fn xrandr --auto --output $argv[1]
     set prior $argv[1]
-    for output in $argv[2..-1]
-        set fn $fn --auto --output $output --right-of $prior
-        set prior $output
+    if test (count $argv) -gt 1
+      for output in $argv[2..-1]
+          set fn $fn --auto --output $output --right-of $prior
+          set prior $output
+      end
     end
     echo $fn;eval $fn
 end
