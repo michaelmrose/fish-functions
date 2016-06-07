31af7cc02c24356f0267035f92b43f067142d8f8
Michael Rose <michael@rosenetwork.net>
2016-06-06 21:34:40 -0700

    editing unread-status-line.fish

--- unread-status-line.fish
+++ unread-status-line.fish
@@ -2,7 +2,7 @@ function unread-status-line
 	set -U LAST_UNREAD_COUNT $CURRENT_UNREAD_COUNT
   set -U CURRENT_UNREAD_COUNT (redditCount.py 2> /dev/null)
   if test $CURRENT_UNREAD_COUNT -gt $LAST_UNREAD_COUNT
-    msg you have $reddit unread messages on reddit
+    msg you have $CURRENT_UNREAD_COUNT unread messages on reddit
   end
   echo $CURRENT_UNREAD_COUNT
   return 0
