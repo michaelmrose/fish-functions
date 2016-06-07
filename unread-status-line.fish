8d81c4cfb336b55a556b12a2726527d8aa03354e
Michael Rose <michael@rosenetwork.net>
2016-06-06 19:33:41 -0700

    editing unread-status-line.fish

--- unread-status-line.fish
+++ unread-status-line.fish
@@ -1,5 +1,5 @@
 function unread-status-line
-	set reddit (get-unread-count-reddit)
+	set reddit (redditCount.py 2> /dev/null)
   if test $reddit -gt 0
     msg you have $reddit unread messages on reddit
   end
