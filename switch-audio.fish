cbcfa16a4aa21f23192889c61fd860314cd4b194
Michael Rose <michael@rosenetwork.net>
2016-06-16 01:18:40 -0700

    editing switch-audio.fish

--- switch-audio.fish
+++ switch-audio.fish
@@ -3,7 +3,9 @@ function switch-audio
   set current (ponymix -t sink defaults --short | head -1 | awk '{print $2}')
   set streams (ponymix -t sink-input list --short | awk '{print $2}')
   set next    (next-valid-index $current $sinks)
-  apply-to "pacmd move-sink-input \$i $next" $streams
+  if exists $streams
+    apply-to "pacmd move-sink-input \$i $next" $streams
+  end
   pacmd set-default-sink $next
   signal-i3blocks output
 end
