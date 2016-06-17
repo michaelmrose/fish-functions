f304297ed275cf4623dc74adb5d73213d459aa9e
Michael Rose <michael@rosenetwork.net>
2016-06-17 00:05:28 -0700

    editing next-valid-index-brief.fish

--- next-valid-index-brief.fish
+++ next-valid-index-brief.fish
@@ -1,5 +1,7 @@
 function next-valid-index-brief
 	set lst $argv[2..-1]
-  set ndx (ternary "test (findindex $argv[1] $last) -ge (count $lst)" "echo 1" "math $n + 1")
-  # echo $lst[$ndx]
+  set n (findindex $argv[1] $lst)
+  # set ndx (ternary "test $n -ge (count $lst)" "echo 1" "math $n + 1")
+  set ndx (ternary test $n -ge (count $lst) @ 1 @ math $n + 1)
+  echo $lst[$ndx]
 end
