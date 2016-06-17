558aa62133889149fd40a8e3c8bc26db4429b280
Michael Rose <michael@rosenetwork.net>
2016-06-17 00:22:40 -0700

    editing next-valid-index.fish

--- next-valid-index.fish
+++ next-valid-index.fish
@@ -1,14 +1,14 @@
 function next-valid-index
-	# set lst $argv[2..-1]
-  # set n (findindex $argv[1] $lst)
-  # ternary test $n -ge (count $lst) @ $lst[1] @ $lst[(math $n + 1)]
-	set val $argv[1]
-  set lst $argv[2..-1]
-  set n (findindex $val $lst)
-  if test $n -ge (count $lst)
-    set ndx 1
-  else
-    set ndx (math $n + 1)
-  end
-  echo $lst[$ndx]
+	set lst $argv[2..-1]
+  set n (findindex $argv[1] $lst)
+  ternary test $n -ge (count $lst) @ $lst[1] @ '$lst[(math $n + 1)]'
+	# set val $argv[1]
+  # set lst $argv[2..-1]
+  # set n (findindex $val $lst)
+  # if test $n -ge (count $lst)
+    # set ndx 1
+  # else
+    # set ndx (math $n + 1)
+  # end
+  # echo $lst[$ndx]
 end
