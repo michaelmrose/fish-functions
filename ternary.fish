493ee7d449f6e45b87f33a4859cf9b92d84048d9
Michael Rose <michael@rosenetwork.net>
2016-06-17 00:01:29 -0700

    editing ternary.fish

--- ternary.fish
+++ ternary.fish
@@ -3,8 +3,16 @@ function ternary
   set s1 (echo $argv | cut -d '@' -f2)
   set s2 (echo $argv | cut -d '@' -f3)
 	if eval $test
-        eval $s1
+    if defined $s1
+      eval $s1
     else
+      echo $s1
+    end
+    else
+      if defined $s2
         eval $s2
+      else
+        echo $s2
+      end
     end
 end
