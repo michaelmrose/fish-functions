133c600cc117c0fd9ba8a7d85181a5388f96d6b8
Michael Rose <michael@rosenetwork.net>
2016-06-28 10:46:44 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,8 +1,8 @@
 function setvolume
 	if echo $argv | ag  '^\+|-' > /dev/null
-    # set vol (ponymix get-volume)
-    # set newvol (bounded (wcalc -q "$vol + $argv") 0 130)
-    set netvol (bounded (wcalc -q (ponymix get-volume) + $argv) 0 130)
+    set vol (ponymix get-volume)
+    set newvol (bounded (wcalc -q "$vol + $argv") 0 130)
+    # set netvol (bounded (wcalc -q (ponymix get-volume) + $argv) 0 130)
   else
     set newvol (bounded $argv 0 150)
   end
