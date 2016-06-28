057fc4bf6ef9c0229e5b6ffcc443be0711916753
Michael Rose <michael@rosenetwork.net>
2016-06-28 11:14:22 -0700

    editing startswith.fish

--- startswith.fish
+++ startswith.fish
@@ -2,6 +2,6 @@ function startswith
 	set begin $argv[1]
   set str $argv[2..-1]
   set com echo $str \| ag \"\^$begin\"
-  set com (echo $com | sed "s#+#\\+#g")
+  # set com (echo $com | sed "s#+#\+#g")
   eval $com > /dev/null
 end
