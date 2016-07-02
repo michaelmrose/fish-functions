cdb4523cc5de189803111147a9604b32b1e3acd9
Michael Rose <michael@rosenetwork.net>
2016-07-01 23:33:37 -0700

    editing get-current-work-sched.fish

--- get-current-work-sched.fish
+++ get-current-work-sched.fish
@@ -7,10 +7,12 @@ function get-current-work-sched
     for i in (seq (count $dates))
         set addhour (println $schedule | grep -A1 "$dates[$i]" | grep Length | cut -d " " -f2 | cut -d : -f1)
         set addminute (println $schedule | grep -A1 "$dates[$i]" | grep Length | cut -d " " -f2 | cut -d : -f2)
-        set end (date '+%-I:%M%p' --date="$starts[$i] + $addhour hour $addminute minute")
-        set date (echo $dates[$i] | sed 's#-#/#g')
-        set finalval (echo $starts[$i] - $end $date | sed -e 's/PM/p/g' -e 's/pm/p/g' -e 's/AM/a/g' -e 's/am/a/g')
-        set events[$i] "work-at-thd $finalval"
+        if date '+%-I:%M%p' --date="$starts[$i] + $addhour hour $addminute minute"
+          set end (date '+%-I:%M%p' --date="$starts[$i] + $addhour hour $addminute minute")
+          set date (echo $dates[$i] | sed 's#-#/#g')
+          set finalval (echo $starts[$i] - $end $date | sed -e 's/PM/p/g' -e 's/pm/p/g' -e 's/AM/a/g' -e 's/am/a/g')
+          set events[$i] "work-at-thd $finalval"
+        end
     end
     set events (filter-with-expr event-not-past $events)
     println $events
