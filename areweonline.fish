62bbb526b565d15f7f6f2d25076f087a09a6e4c4
Michael Rose <michael@rosenetwork.net>
2016-07-05 20:22:12 -0700

    editing areweonline.fish

--- areweonline.fish
+++ areweonline.fish
@@ -1,3 +1,8 @@
 function areweonline
-	ping 8.8.8.8 -c 1 -W 1 > /dev/null 2> /dev/null
+	if exists $argv
+    set ip $argv
+  else
+    set ip 8.8.8.8
+  end
+	ping $ip -c 1 -W 1 > /dev/null 2> /dev/null
 end
