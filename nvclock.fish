2e313719ea4f01f7349bb6da97fbbdf2663553ec
Michael Rose <michael@rosenetwork.net>
2016-07-23 10:28:01 -0700

    editing nvclock.fish

--- nvclock.fish
+++ nvclock.fish
@@ -1,4 +1,7 @@
 function nvclock
+	set clock (ops c $argv)
+  set mem (ops m $argv)
+  set fan (ops f $argv)
 	nvidia-settings -a GPUGraphicsClockOffset[3]=$argv[1]
   nvidia-settings -a GPUMemoryTransferRateOffset[3]=$argv[2]
 end
