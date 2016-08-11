dce05d7a6ac69f6d52d0e2c090722b2dea90b56d
Michael Rose <michael@rosenetwork.net>
2016-08-11 01:38:24 -0700

    editing testfn.fish

--- testfn.fish
+++ testfn.fish
@@ -1,9 +1,5 @@
 function testfn
-	# if test (count $argv) -gt 0
-    # echo true
-  # else;echo false;end
-  true
-  and echo hi
-  false
-  and echo there
+	while read -l line
+    echo updated-info
+  end
 end
