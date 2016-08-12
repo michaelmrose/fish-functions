a269a30163e74d425fda511a28e104ab3b21221c
Michael Rose <michael@rosenetwork.net>
2016-08-11 21:31:08 -0700

    editing testfn.fish

--- testfn.fish
+++ testfn.fish
@@ -1,7 +1,6 @@
 function testfn
-	set activewindow (ensure-hex (xdotool getactivewindow))
-  set activepid (wmctrl -lxp | grep $activewindow | awk '{print $3}')
-  if pstree --show-parents $activepid | grep '\-steam\-'
+	set p (xprop -id (xdotool getactivewindow) | grep PID | cut -d ' ' -f3)
+  if pstree --show-parents $p | grep '\-steam\-'
     i3-msg move window to workspace r
   end
 	# if pstree --show-parents (wmctrl -lxp | grep (ensure-hex (xdotool getactivewindow)) | awk '{print $3}') | grep '\-steam\-'
