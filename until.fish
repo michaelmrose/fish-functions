95096f8b6b132dd045a947b147484af850a2e66c
Michael Rose <michael@rosenetwork.net>
2016-08-13 11:02:20 -0700

    editing until.fish

--- until.fish
+++ until.fish
@@ -1,4 +1,10 @@
 function until
+	set timeout (echo "$argv" | cut -d '#' -f2)
+  set origin (seconds)
 	while not eval $argv
+    set difference (expr (seconds) - $origin)
+    if test $difference -gt $timeout
+      break
+    end
   end
 end
