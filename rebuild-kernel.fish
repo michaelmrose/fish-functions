93dd943e18239f9cdb1e776c4f34df6395dd9e89
Michael Rose <michael@rosenetwork.net>
2016-08-19 22:03:20 -0700

    editing rebuild-kernel.fish

--- rebuild-kernel.fish
+++ rebuild-kernel.fish
@@ -1,5 +1,5 @@
 function rebuild-kernel
-	set com s genkernel all --install --zfs --no-clean --no-mountboot --gconfig $argv --callback=\"emerge spl zfs-kmod zfs g15daemon nvidia-drivers libg15 libg15render solaar\"
+	set com s genkernel all --install --zfs --no-clean --no-mountboot --xconfig $argv --callback=\"emerge spl zfs-kmod zfs g15daemon nvidia-drivers libg15 libg15render solaar\"
     echo command is $com
     countdown 10
     eval $com
