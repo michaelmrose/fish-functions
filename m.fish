d3a769fedb09ae769b1726f316064ecb20913c6e
Michael Rose <michael@rosenetwork.net>
2016-08-23 12:25:15 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -45,14 +45,10 @@ function m
         eval $EDITOR ~/playlists/$argv[2]
         return 0
       case pl
-        # set pl ~/playlists/$argv[2]
-        # if test -f $pl
-        #   m open (m ls $argv[2])
-        # else
-        #   m ytube-pl $argv[2..-1]
-        # end
         umpv (m ls $argv[2])
         return 0
+      case pick
+        rfi match "select a playlist: " (p (cat ~/.youtube-playlists) (ls ~/playlists))
       case open
         umpv $argv[2..-1] &
         return 0
