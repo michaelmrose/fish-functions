344312b80e3be31c50950658d7b3b5a4c2a5badc
Michael Rose <michael@rosenetwork.net>
2016-09-07 10:46:21 -0700

    editing manage-share.fish

--- manage-share.fish
+++ manage-share.fish
@@ -1,8 +1,12 @@
 function manage-share
 	switch $argv[1]
     case ensure-mounted
-      if all linda-desktop-exits share-mounted
-        echo hi
+      if all linda-desktop-exits 'not share-mounted'
+        ssh linda loaddirs.sh
+      end
+    case ensure-unmounted
+      if all linda-desktop-exists share-mounted
+        ssh linda umount /mnt/michael
       end
     end
 end
