e44ce299bef4cea1f5e83c3cfe0f27e52af6c2cc
Michael Rose <michael@rosenetwork.net>
2016-09-07 10:13:16 -0700

    editing manage-share.fish

--- manage-share.fish
+++ manage-share.fish
@@ -2,13 +2,13 @@ function manage-share
 	switch $argv[1]
     case ensure-mounted
       if ping 192.168.1.3 -c 1 -W 1
-        if not ssh ls /mnt/michael/med
+        if not ssh linda ls /mnt/michael/med
           mount /mnt/michael
         end
       end
     case ensure-unmounted
       if ping 192.168.1.3 -c 1 -W 1
-        if ssh ls /mnt/michael/med
+        if ssh linda ls /mnt/michael/med
           umount /mnt/michael
         end
       end
