d73f53b884e32a6e4de27078c2b9e92fc3daf2ea
Michael Rose <michael@rosenetwork.net>
2016-09-07 18:22:42 -0700

    editing get-unread-count-reddit.fish

--- get-unread-count-reddit.fish
+++ get-unread-count-reddit.fish
@@ -1,3 +1,3 @@
 function get-unread-count-reddit
-	curl -b ~/.cjar.text -A 'Mozilla/4.0' 'https://www.reddit.com/message/unread/' | pup | grep '<span class="subject-text">' | wc -l
+	curl -b ~/.cjar.txt -A 'Mozilla/4.0' 'https://www.reddit.com/message/unread/' | pup | grep '<span class="subject-text">' | wc -l
 end
