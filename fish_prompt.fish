3a9d01781a68f436439ce77ca8380a7e147f8790
Michael Rose <michael@rosenetwork.net>
2016-09-08 02:52:54 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-	if echo (pwd) | grep '^/usr/portage' > /dev/null                                                                                           ❮ 2 ms ❮ 
+	if pwd | grep '^/usr/portage' > /dev/null                                                                                           ❮ 2 ms ❮ 
     fish_fallback_prompt
   else
     /opt/bin/powerlineshell.py $status --shell bare
