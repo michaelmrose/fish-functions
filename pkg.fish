3ea0662690dc99b0a674d7e263046b128e550e1e
Michael Rose <michael@rosenetwork.net>
2016-09-17 18:12:51 -0700

    editing pkg.fish

--- pkg.fish
+++ pkg.fish
@@ -43,7 +43,7 @@ function pkg
     case updateworld
       # sudo zfs snapshot tank/funtoo/root@(preferred-date)
       sudo emerge --sync
-      and sudo emerge -auDN @world --keep-going=y --with-bdeps=y --backtrack=300 --complete-graph --alert --ask (vals 2..-1 $argv)
+      and sudo emerge -auDNt @world --keep-going=y --with-bdeps=y --backtrack=300 --complete-graph --alert --ask (vals 2..-1 $argv)
       and sudo emerge @preserved-rebuild --ask
       and sudo emerge --depclean -a --complete-graph --ask
     case depends
