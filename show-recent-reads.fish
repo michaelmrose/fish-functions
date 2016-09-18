a9cbd01a5e6535051f019bd2bb6b45fc3e991598
Michael Rose <michael@rosenetwork.net>
2016-09-18 01:34:05 -0700

    editing show-recent-reads.fish

--- show-recent-reads.fish
+++ show-recent-reads.fish
@@ -1,5 +1,5 @@
 function show-recent-reads --description 'use rofi to pick one of the items from recent_reads and open it with sopen if it is a file or books if it is a title'
-	set choice (rfi match "choose a book" $recent_reads)
+	set choice (rfi match "choose a book: " $recent_reads)
     if exists $choice
         if test -f $choice
             sopen $choice
