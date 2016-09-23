8ebd11dd4c511feea8a69f749db32ca89e94c081
Michael Rose <michael@rosenetwork.net>
2016-09-22 19:01:19 -0700

    editing manage-share.fish

--- manage-share.fish
+++ manage-share.fish
@@ -3,10 +3,12 @@ function manage-share
     case ensure-mounted
       if all linda-desktop-exists 'not share-mounted'
         ssh linda loaddirs.sh
+        sleep 3
       end
     case ensure-unmounted
       if all linda-desktop-exists share-mounted
         ssh linda umount -l /mnt/michael
+        sleep 3
       end
     end
 end
