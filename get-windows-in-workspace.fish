92ba402e100f0b8a79c84ab7271b1f9fa8db6626
Michael Rose <michael@rosenetwork.net>
2016-09-27 20:45:53 -0700

    editing get-windows-in-workspace.fish

--- get-windows-in-workspace.fish
+++ get-windows-in-workspace.fish
@@ -1,4 +1,4 @@
 function get-windows-in-workspace
-	math (findindex $argv (list-workspaces)) -1
-	xdotool search --all --onlyvisible --desktop (math (findindex $argv (list-workspaces)) -1) "" 2>/dev/null
+	set ws (math (findindex $argv (list-workspaces)) -1)
+	xdotool search --all --onlyvisible --desktop 4 "" 2>/dev/null
 end
