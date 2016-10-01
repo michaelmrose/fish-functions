43ffc6fe30b5c6e58b7ad959b1bd8df88393d17f
Michael Rose <michael@rosenetwork.net>
2016-10-01 00:19:44 -0700

    editing gr.fish

--- gr.fish
+++ gr.fish
@@ -1,7 +1,11 @@
 function gr
 	switch $argv[1]
     case -w
-      nil
+      while read -l line
+        set input $input $line
+      end
+      # for word in $argv[2..-1]
+      #   set res (echo $res | gr )
     case "*"
       grep -iE $argv;
     end
