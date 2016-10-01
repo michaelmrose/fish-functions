a0966b3d00014783104344d2a4a328691d4410ef
Michael Rose <michael@rosenetwork.net>
2016-10-01 00:33:59 -0700

    editing find-video.fish

--- find-video.fish
+++ find-video.fish
@@ -1,16 +1,3 @@
 function find-video
-	# set files (findall /med/videos video)
-    # set words $argv
-
-    # # echo w is $words
-
-    # for i in $words
-    #     set files (println $files | grep -i $i)
-    #     # echo files is
-    #     # println $files
-    # end
-    
-    # # echo finally files is
-    # println $files
-    findall /med/videos video | gr -w $argv
+	findall /med/videos video | gr -w $argv
 end
