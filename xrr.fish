b458f4c399fccac373efca60a31998cf2b1fd795
Michael Rose <michael@rosenetwork.net>
2016-10-07 15:38:42 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -8,6 +8,7 @@ function xrr
       end
     end
     for output in (get-connected-displays)
+      echo o is $output a is $argv
       if not contains $argv $output
         echo $output
         # set fn $fn --output $output --off
