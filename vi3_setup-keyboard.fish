38b9bd31626328aa9e93232d9830750fa3aa4a89
Michael Rose <michael@rosenetwork.net>
2016-10-11 10:44:17 -0700

    removed vi3_setup-keyboard.fish

--- vi3_setup-keyboard.fish
+++
@@ -1,8 +0,0 @@
-function vi3_setup-keyboard
-	killall xcape
-  /usr/bin/xcape -t 250 -e 'Control_L=F21'
-	/usr/bin/xcape -t 250 -e 'Super_L=F22'
-  /usr/bin/xcape -t 250 -e 'Alt_L=F23'
-	/usr/bin/xcape -t 250 -e 'Shift_L=XF86Launch1'
-  /usr/bin/xcape -t 250 -e 'Shift_R=XF86Launch2'
-end
