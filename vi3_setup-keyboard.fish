603aa8e3c218acfb3731d11e053cc9a9884039dc
Michael Rose <michael@rosenetwork.net>
2016-10-11 10:43:33 -0700

    editing vi3_setup-keyboard.fish

--- vi3_setup-keyboard.fish
+++ vi3_setup-keyboard.fish
@@ -3,4 +3,6 @@ function vi3_setup-keyboard
   /usr/bin/xcape -t 250 -e 'Control_L=F21'
 	/usr/bin/xcape -t 250 -e 'Super_L=F22'
   /usr/bin/xcape -t 250 -e 'Alt_L=F23'
+	/usr/bin/xcape -t 250 -e 'Shift_L=XF86Launch1'
+  /usr/bin/xcape -t 250 -e 'Shift_R=XF86Launch2'
 end
