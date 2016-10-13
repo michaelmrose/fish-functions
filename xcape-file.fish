b1e62a54dda7d7ef1d065a3c18dbca270e9d86a4
Michael Rose <michael@rosenetwork.net>
2016-10-13 12:24:18 -0700

    editing xcape-file.fish

--- xcape-file.fish
+++ xcape-file.fish
@@ -2,10 +2,7 @@ function xcape-file
 	killall xcape
 
 	set file $argv[-1]
-  # if count $argv -gt 1 > /dev/null
-  #   set options $argv
-  # end
-  
+
   if not test -f $file
     echo not a file
     return 1
