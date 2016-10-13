8f7b1e941fd46f868da9db3125b959bf6ab03e2f
Michael Rose <michael@rosenetwork.net>
2016-10-13 11:35:51 -0700

    editing optvalue.fish

--- optvalue.fish
+++ optvalue.fish
@@ -2,5 +2,10 @@ function optvalue
 	set option $argv[1]
   set default $argv[2]
   set list $argv[3..-1]
-  getopts $list | grep "^$option " | cut -d ' ' -f2;or echo $default
+  set res (getopts $list | grep "^$option " | cut -d ' ' -f2)
+  if exists $res
+    echo $res
+  else
+    echo $default
+  end
 end
