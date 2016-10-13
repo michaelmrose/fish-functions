14d8ddc133f6429785022f3661be36a547729cbe
Michael Rose <michael@rosenetwork.net>
2016-10-13 12:22:38 -0700

    editing xcape-file.fish

--- xcape-file.fish
+++ xcape-file.fish
@@ -2,16 +2,15 @@ function xcape-file
 	killall xcape
 
 	set file $argv[-1]
-  if count $argv -gt 1 > /dev/null
-    set options $argv
-  end
+  # if count $argv -gt 1 > /dev/null
+  #   set options $argv
+  # end
   
   if not test -f $file
     echo not a file
     return 1
   end
 
-  set timeout (optvalue t 500 $options) #val default
-
+  set timeout (optvalue t 500 $argv) #val default
 	eval xcape -t $timeout -e \'(cat $file | condense_lines | sed 's/ /;/g')\'
 end
