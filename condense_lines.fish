e705ff6c299f8a62b9e14d18518a0bbfb0375931
Michael Rose <michael@rosenetwork.net>
2016-10-13 11:19:18 -0700

    editing condense_lines.fish

--- condense_lines.fish
+++ condense_lines.fish
@@ -1,7 +1,6 @@
 function condense_lines
 	for i in $argv
-        set item (echo $i | sed 's#\n##g')
-        set acc $acc $item
+        set acc $acc $i
     end
     echo $acc | condense_spaces
 end
