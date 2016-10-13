17de1c0336e20a688cbd7f468750bb2ab5a8ddd7
Michael Rose <michael@rosenetwork.net>
2016-10-13 03:09:38 -0700

    editing xcape-file.fish

--- xcape-file.fish
+++ xcape-file.fish
@@ -1,7 +1,8 @@
 function xcape-file
-	set timeout $argv[1]
-  set file $argv[2]
-  for line in (cat $file)
-    eval xcape -t $timeout -e $line
-  end
+	set timeout (getopts $argv | grep '^t ' | cut -d ' ' -f2)
+  echo $timeout
+  # set file $argv[2]
+  # for line in (cat $file)
+  #   eval xcape -t $timeout -e $line
+  # end
 end
