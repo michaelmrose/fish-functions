a43e62882aca1521b228171f798eb91e03fd832a
Michael Rose <michael@rosenetwork.net>
2016-10-13 11:54:17 -0700

    editing setup-keyboard.fish

--- setup-keyboard.fish
+++ setup-keyboard.fish
@@ -1,8 +1,5 @@
 function setup-keyboard
-	killall xcape
-  /usr/bin/xcape -t 250 -e 'Control_L=F21'
-	/usr/bin/xcape -t 250 -e 'Super_L=F22'
-  /usr/bin/xcape -t 250 -e 'Alt_L=F23'
-	/usr/bin/xcape -t 250 -e 'Shift_L=XF86Launch1'
-  /usr/bin/xcape -t 250 -e 'Shift_R=XF86Launch2'
+	xmodmap ~/.config/xkeys/keys
+  xcafe-file ~/.config/xkeys/xcapes
+  xdotool key Num_Lock
 end
