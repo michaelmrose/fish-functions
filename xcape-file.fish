4259250a8fbf5dbbcf8af9a25fb1516163130998
Michael Rose <michael@rosenetwork.net>
2016-10-13 03:11:06 -0700

    editing xcape-file.fish

--- xcape-file.fish
+++ xcape-file.fish
@@ -1,5 +1,5 @@
 function xcape-file
-	set timeout (getopts $argv | grep '^t ' | cut -d ' ' -f2)
+	set timeout (getopts $argv | grep '^t ' | cut -d ' ' -f2; or echo 500)
   echo $timeout
   # set file $argv[2]
   # for line in (cat $file)
