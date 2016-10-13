025f85a6f75a03cbac8b8e8ec7adcf427b37f607
Michael Rose <michael@rosenetwork.net>
2016-10-13 11:45:09 -0700

    editing xcape-file.fish

--- xcape-file.fish
+++ xcape-file.fish
@@ -1,5 +1,6 @@
 function xcape-file
 	killall xcape
+
 	set file $argv[-1]
   if count $argv -gt 1 > /dev/null
     set options $argv
@@ -10,19 +11,7 @@ function xcape-file
     return 1
   end
 
-  set timeout (optvalue t 500 $options)
-	# set timeout (getopts $argv | grep '^t ' | cut -d ' ' -f2)
-  # if not exists $timeout
-    # set timeout 500
-  # end
+  set timeout (optvalue t 500 $options) #val default
 
 	eval xcape -t $timeout -e \'(condense_lines (cat $file) | sed 's/ /;/g')\'
-  # if test -f $target
-    # killall xcape
-    # for line in (cat $target)
-      # eval xcape -t $timeout -e \'$line\'
-    # end
-  # else
-    # xcape -t $timeout -e "$target"
-  # end
 end
