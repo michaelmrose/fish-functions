a75c3acf5005f0cca1fbfaabbc8f8f082ff7846c
Michael Rose <michael@rosenetwork.net>
2016-10-16 21:41:41 -0700

    editing sus.fish

--- sus.fish
+++ sus.fish
@@ -1,5 +1,11 @@
 function sus
 	manage-share ensure-unmounted
   sudo pm-suspend
+  if exists $argv
+    switch $argv[1]
+      case greet
+        greeter
+    end
+  end
   manage-share ensure-mounted
 end
