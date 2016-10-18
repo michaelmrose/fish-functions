2b4283b0f24419517b3e361c91599ac87cbe7fbb
Michael Rose <michael@rosenetwork.net>
2016-10-18 10:04:15 -0700

    editing vertical-selector.fish

--- vertical-selector.fish
+++ vertical-selector.fish
@@ -1,5 +1,5 @@
 function vertical-selector
 	set label $argv[1]
   set input $argv[2]
-  yad --text "$label" --text-align center --scale --value "$input" --geometry=10x300 --vertical --button gtk-ok:0
+  yad --text "$label" --text-align center --scale --value "$input" --geometry=10x300 --vertical --button gtk-ok:0 --print-partial
 end
