e12129dcc5e0fafb9b96c7408bbf18587265e481
Michael Rose <michael@rosenetwork.net>
2016-10-23 23:51:31 -0700

    editing build-time-remaining.fish

--- build-time-remaining.fish
+++ build-time-remaining.fish
@@ -1,3 +1,8 @@
 function build-time-remaining
-	s genlop -ntc | gr eta | awk '{print $2}' | condense_lines | fishstdin sumof
+	set res (s genlop -ntc | gr eta | awk '{print $2}' | condense_lines | fishstdin sumof)
+  if test $res -lt 1
+    echo any time
+  else
+    echo $res
+  end
 end
