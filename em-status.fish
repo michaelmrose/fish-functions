daee62aee76231ad2689db6fef9df79782ee30ff
Michael Rose <michael@rosenetwork.net>
2016-10-24 00:03:25 -0700

    editing em-status.fish

--- em-status.fish
+++ em-status.fish
@@ -1,16 +1,18 @@
 function em-status
 	if pgrep emerge > /dev/null
-        set res (sudo genlop -ntc 2> /dev/null)
-        # echo res is $res
-        if exists $res
-            if startswith ! $res
-              return 1
-            end
-        end
-    else
-        return 1
-    end
-    echo $res
+    echo progress: (build-progress) complete eta: (build-time-remaining) min
+  end
+        # set res (sudo genlop -ntc 2> /dev/null)
+        # # echo res is $res
+        # if exists $res
+        #     if startswith ! $res
+        #       return 1
+        #     end
+        # end
+    # else
+        # return 1
+    # end
+    # echo $res
     # if startswith ! $output
     #     return 1
     # else
