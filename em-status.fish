a05f0be9754ecd4f7d3ee2f41f11e6960ebe00cf
Michael Rose <michael@rosenetwork.net>
2016-10-23 23:38:34 -0700

    editing em-status.fish

--- em-status.fish
+++ em-status.fish
@@ -1,6 +1,6 @@
 function em-status
 	if pgrep emerge > /dev/null
-        set res (sudo genlop -ntc 2> /dev/null)
+        set res (sudo genlop -ntc 2> /dev/null | condense_spaces)
         echo res is $res
         if exists $res
             if startswith ! $res
