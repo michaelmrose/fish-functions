5b661b18e0d828b33ab1b7de607e51ec53deed3f
Michael Rose <michael@rosenetwork.net>
2016-10-23 23:31:48 -0700

    editing em-status.fish

--- em-status.fish
+++ em-status.fish
@@ -2,7 +2,7 @@ function em-status
 	if pgrep emerge > /dev/null
         set res (sudo genlop -ntc 2> /dev/null)
         if exists $res (sudo genlop -ntc 2> /dev/null)
-            set output (condense_lines $res)
+            set output (condense_lines $res | condense_spaces)
         end
     else
         return 1
