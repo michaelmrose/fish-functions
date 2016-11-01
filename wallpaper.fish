1f7bb8ddee9a297a850980784066bb381ec7476a
Michael Rose <michael@rosenetwork.net>
2016-11-01 10:24:37 -0700

    editing wallpaper.fish

--- wallpaper.fish
+++ wallpaper.fish
@@ -87,7 +87,7 @@ function wallpaper
             set dest (echo $src | sed "s#/$old#/$new#g")
             sed -i "s#/$old/#/$new/#g" ~/.fehbg
             mv $src $dest
-            set -U recent_backgrounds (p $recent_backgrounds | sed "s#/$argv[2]/#/$argv[3]/#g")
+            set -U recent_backgrounds (p $recent_backgrounds | sed "s#/$old/#/$new/#g")
             wp recall
             return 0
         case save
