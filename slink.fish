c94e6ee163a907580869a885e6f12ede14adae9d
Michael Rose <michael@rosenetwork.net>
2016-11-24 20:05:15 -0800

    editing slink.fish

--- slink.fish
+++ slink.fish
@@ -2,10 +2,10 @@ function slink
 	set target (echo $argv | cut -d = -f2)
   set name (echo $argv | cut -d = -f1)
   if test -f $name
-    echo rm $name
-  end
-  if test -d $name
-    echo rm $name
+    echo $name will be removed
+    if read_confirm
+      echo rm $name
+    end
   end
 
   echo ln -s $target $name
