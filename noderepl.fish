2e439c7c27a66e0cec71be7f9996650b108e8043
Michael Rose <michael@rosenetwork.net>
2016-11-24 19:00:14 -0800

    editing noderepl.fish

--- noderepl.fish
+++ noderepl.fish
@@ -1,4 +1,5 @@
 function noderepl
 	set args (vals 1..-1 $argv)
-  node --harmony --harmony_default_parameters --harmony_arrow_functions $args
+  # node --harmony --harmony_default_parameters --harmony_arrow_functions $args
+eval node --harmony (node --v8-options | gr "in progress" | trim | cut -d ' ' -f1 | condense_lines) $args
 end
