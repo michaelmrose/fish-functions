7ac20a43de6b4ef95fc02d3e6a733710294ac67f
Michael Rose <michael@rosenetwork.net>
2016-12-03 00:04:42 -0800

    editing m.fish

--- m.fish
+++ m.fish
@@ -5,13 +5,13 @@ function m
       return 0
     end
   end
-	# usage examples
+  # usage examples
   # m play ytube or local playlist
   # m files @replace|append:playlist
   # somecommand | m  @replace|append:playlist
   # m yt pl=playlist
   # m watch some key words, to play local files
-	if test (count $argv) -gt 0
+  if test (count $argv) -gt 0
     switch $argv[1]
       case list-playlists
         println (ls ~/playlists)
@@ -31,41 +31,45 @@ function m
             umpv $files
           case '*'
             umpv (rfi match 'select video: ' (println $files))
-          end
+        end
         # umpv (rfi match 'select video: ' (find-video $argv[2..-1]))
         return 0
-      case yt 
-        ytube (vals 2..-1 $argv)
-        return 0
-      case ls
-        if test -f ~/playlists/$argv[2]
-          cat ~/playlists/$argv[2]
-        else
-          list-youtube-playlist $argv[2..-1]
-        end
-        return 0
-      case rm
-        rm ~/playlists/$argv[2]
-        return 0
-      case edit
-        eval $EDITOR ~/playlists/$argv[2]
-        return 0
-      case pl
-        umpv (m ls $argv[2])
-        return 0
-      case pick
-        m pl (rfi match "select a playlist: " (p (cat ~/.youtube-playlists) (ls ~/playlists)))
-        return 0
-      case open
-        umpv $argv[2..-1] &
-        return 0
-      case narrow
-        m ls $argv[2] | pick | m @replace:$argv @noplay
-        return 0
-      case from-clipboard
-        set url (xclip -o -primary)
-        m append-to $argv[2] "$url"
-        return 0
+          case yt 
+            ytube (vals 2..-1 $argv)
+            return 0
+          case ls
+            if test -f ~/playlists/$argv[2]
+              cat ~/playlists/$argv[2]
+            else
+              list-youtube-playlist $argv[2..-1]
+            end
+            return 0
+          case rm
+            rm ~/playlists/$argv[2]
+            return 0
+          case edit
+            eval $EDITOR ~/playlists/$argv[2]
+            return 0
+          case pl
+            umpv (m ls $argv[2])
+            return 0
+          case queue
+            umpv (m ls $argv[2])
+            return 0
+            
+          case pick
+            m pl (rfi match "select a playlist: " (p (cat ~/.youtube-playlists) (ls ~/playlists)))
+            return 0
+          case open
+            mpv $argv[2..-1] &
+            return 0
+          case narrow
+            m ls $argv[2] | pick | m @replace:$argv @noplay
+            return 0
+          case from-clipboard
+            set url (xclip -o -primary)
+            m append-to $argv[2] "$url"
+            return 0
     end
   end
   set vals (vals 1..-1 $argv)
