874de62b64f12a360bf2e4efc925e0bde830a7df
Michael Rose <michael@rosenetwork.net>
2016-12-14 02:32:31 -0800

    editing get-focused-display-resolution.fish

--- get-focused-display-resolution.fish
+++ get-focused-display-resolution.fish
@@ -1,6 +1,7 @@
 function get-focused-display-resolution
-	set res (xrandr --verbose | grep " connected" | grep (get-focused-display) | cut -d "x" -f1-2 | cut -d "+" -f1)
-    set res (explode $res)
-    set res $res[-1]
-    echo $res
+	# set res (xrandr --verbose | grep " connected" | grep (get-focused-display) | cut -d "x" -f1-2 | cut -d "+" -f1)
+  #   set res (explode $res)
+  #   set res $res[-1]
+  #   echo $res
+	xrandr --verbose | grep " connected" | grep (get-focused-display) | cut -d "x" -f3 | cut -d "+" -f1
 end
