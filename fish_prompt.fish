474b286503243b0fcbac504cd8ef2c5ecc39da20
Michael Rose <michael@rosenetwork.net>
2016-12-14 19:26:30 -0800

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
-	# /opt/bin/powerlineshell.py $status --shell bare ^/dev/null
-  echo $USER:\$
+	/opt/bin/powerlineshell.py $status --shell bare ^/dev/null
+  # echo $USER:\$
 end
