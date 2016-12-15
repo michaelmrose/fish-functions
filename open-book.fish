13475b47b3cbe700034bea9cbe3f48bd414b2215
Michael Rose <michael@rosenetwork.net>
2016-12-14 18:03:47 -0800

    editing open-book.fish

--- open-book.fish
+++ open-book.fish
@@ -6,6 +6,7 @@ function open-book
         set title (query-calibre-title title (escape-chars (extract-title $fullpath)))
         add-to-recent-reads "$title"
         add-to-recent-reads-fnames "$argv"
+        set-recent-book-covers
     end
 
     switch $ext
@@ -16,5 +17,4 @@ function open-book
         case "*"
             ebook-viewer "$argv" &
     end
-    set-recent-book-covers
 end
