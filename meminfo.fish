0b3b4a1f135b8dd88048795f7c6399e5110e3535
Michael Rose <michael@rosenetwork.net>
2016-12-21 13:22:48 -0800

    editing meminfo.fish

--- meminfo.fish
+++ meminfo.fish
@@ -1,6 +1,6 @@
 function meminfo
 	set acc 0
-    for i in (pgrep $argv)
+    for i in (pgrep -U $USER $argv[1])
         set val (math (pmap -x $i | grep total | condense_spaces | cut -d " " -f5) / 1024)
         set acc (math "$acc" + "$val")
     end
