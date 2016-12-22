9268857d309bf4a4a62cf9f14b80bb0bf3ea62b9
Michael Rose <michael@rosenetwork.net>
2016-12-22 11:38:30 -0800

    editing numberkeys.fish

---
+++ numberkeys.fish
@@ -0,0 +1,12 @@
+function numberkeys
+	remap code 10 1 exclam
+        remap code 11 2 at
+        remap code 12 3 numbersign
+        remap code 13 4 dollar
+        remap code 14 5 percent
+        remap code 15 6 asciicircum
+        remap code 16 7 ampersand
+        remap code 17 8 asterisk
+        remap code 18 9 parenleft
+        remap code 19 0 parenright
+end
