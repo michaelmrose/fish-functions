22504c8e70f4f03e27817e7b3d82459267677115
Michael Rose <michael@rosenetwork.net>
2016-12-22 11:38:30 -0800

    editing symbolkeys.fish

---
+++ symbolkeys.fish
@@ -0,0 +1,12 @@
+function symbolkeys
+	remap code 10 exclam 1
+        remap code 11 at 2
+        remap code 12 numbersign 3
+        remap code 13 dollar 4
+        remap code 14 percent 5
+        remap code 15 asciicircum 6
+        remap code 16 ampersand 7
+        remap code 17 asterisk 8
+        remap code 18 parenleft 9
+        remap code 19 parenright 0
+end
