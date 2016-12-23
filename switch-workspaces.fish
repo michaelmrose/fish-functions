08ac9b024b17a9b487e4b19d8bb541a07f815ff5
Michael Rose <michael@rosenetwork.net>
2016-12-22 23:01:05 -0800

    editing switch-workspaces.fish

--- switch-workspaces.fish
+++ switch-workspaces.fish
@@ -1,12 +1,12 @@
 function switch-workspaces
-	save-workspaces
+	save-workpaces
   select-all-in-workspace
-  i3 move container to workspace foo
+  i3 move container to workpace foo
   i3 focus output right
   select-all-in-workspace
-  i3 move container to workspace bar
+  i3 move container to workpace bar
   restore-workspaces
-  vi3_get-workspace foo
+  vi3_get-ws foo
   i3 focus output right
-  vi3_get-workspace bar
+  vi3_get-ws bar
 end
