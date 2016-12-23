ce101cd3f2ad92794d9b04c61996f3a9772436ae
Michael Rose <michael@rosenetwork.net>
2016-12-22 23:13:14 -0800

    editing switch-workspaces.fish

--- switch-workspaces.fish
+++ switch-workspaces.fish
@@ -2,9 +2,11 @@ function switch-workspaces
 	set w (get-active-workspaces)
   ws $w[1]
   select-all-in-workspace
+  i3 move container to workspace left
   ws $w[2]
   sleep 3
   select-all-in-workspace
+  i3 move container to workspace right
   sleep 3
   ws $w
 end
