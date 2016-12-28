c500732fb0815b61e30247a51f92a44d4def84ca
Michael Rose <michael@rosenetwork.net>
2016-12-28 12:51:32 -0800

    editing configure-gaps.fish

--- configure-gaps.fish
+++ configure-gaps.fish
@@ -1,6 +1,6 @@
 function configure-gaps
 	if i3 --version | gr gaps
-    i3 gaps inner all set 20
-    i3 gaps outer all set 10
+    i3 gaps inner all set 10
+    i3 gaps outer all set 5
   end
 end
