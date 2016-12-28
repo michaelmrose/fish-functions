a2c5e63f8d0192664020df53ebe869d42eb76890
Michael Rose <michael@rosenetwork.net>
2016-12-28 11:37:01 -0800

    editing wallpaper.fish

--- wallpaper.fish
+++ wallpaper.fish
@@ -193,7 +193,7 @@ function wallpaper
                 set lst $lst /tmp/pano-$i.jpg
             end
             set img $lst
-            set format center
+            set format zoom
     end
     feh --bg-{$format} $img
     if pgrep i3blocks > /dev/null
