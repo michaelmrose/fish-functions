c02910f3e4496b42daf74f72bb768cc3e9de2803
Michael Rose <michael@rosenetwork.net>
2016-12-28 12:36:59 -0800

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -3,7 +3,7 @@ function set-path
 
     set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games
 
-    set mypaths /opt/i3blocks /opt/bin /home/michael/bin /home/michael/scripts  /opt/cxoffice/bin ~/.autojump/bin /usr/games/bin /opt/vmware/bin /home/michael/.cargo/bin /usr/share/gopreload/bin
+    set mypaths /opt/i3gaps/bin /opt/i3blocks /opt/bin /home/michael/bin /home/michael/scripts  /opt/cxoffice/bin ~/.autojump/bin /usr/games/bin /opt/vmware/bin /home/michael/.cargo/bin /usr/share/gopreload/bin
 
     set -xg PATH $mypaths $javapaths $builtinpaths
 end
