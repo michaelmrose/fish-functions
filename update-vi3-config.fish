3e0aa06186ba924db92fa69b42d101a85dd9483c
Michael Rose <michael@rosenetwork.net>
2016-12-28 13:07:54 -0800

    editing update-vi3-config.fish

--- update-vi3-config.fish
+++ update-vi3-config.fish
@@ -3,8 +3,8 @@ function update-vi3-config
     rm ~/.i3/config
     cat /opt/vi3/header.txt ~/.i3/colors/{$colors} ~/.i3/personalconfig /opt/vi3/vi3config > ~/.i3/config
     i3-msg restart
-    configure-gaps
     sleep 3
+    configure-gaps
     i3-elements trans
     fix-all-trans
     hide-all-hidden-windows
