a4bf9e85341682fad6274c2748db764b3a6a421a
Michael Rose <michael@rosenetwork.net>
2017-01-04 15:35:17 -0800

    editing write-iso.fish

--- write-iso.fish
+++ write-iso.fish
@@ -1,11 +1,13 @@
 function write-iso
 	set file $argv[1]
   set disk $argv[3]
-  echo The following disk will be overwritten...
-  echo -----------------------------------------
-  s fdisk -l $disk
-  echo -----------------------------------------
-  if test -f $file; and read_confirm
+  if test -f $file; and test -b $disk
+    echo The following disk will be overwritten...
+    echo -----------------------------------------
+    s fdisk -l $disk
+    echo -----------------------------------------
+  end
+  if read_confirm
     echo dd if=$file of=$disk bs=1M status=progress#;and sync
   end
 end
