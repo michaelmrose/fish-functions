54a97ee60adc8e4bbd95f5f534f3e0c9babba286
Michael Rose <michael@rosenetwork.net>
2017-01-04 15:36:15 -0800

    editing write-iso.fish

--- write-iso.fish
+++ write-iso.fish
@@ -6,6 +6,9 @@ function write-iso
     echo -----------------------------------------
     s fdisk -l $disk
     echo -----------------------------------------
+  else
+    echo invalid arguments
+    return 1
   end
   if read_confirm
     echo dd if=$file of=$disk bs=1M status=progress#;and sync
