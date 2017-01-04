fa8ba8b1c758b9fadcc3f41506a02e9fa9e94cab
Michael Rose <michael@rosenetwork.net>
2017-01-04 15:36:55 -0800

    editing write-iso.fish

--- write-iso.fish
+++ write-iso.fish
@@ -11,6 +11,6 @@ function write-iso
     return 1
   end
   if read_confirm
-    echo dd if=$file of=$disk bs=1M status=progress#;and sync
+    dd if=$file of=$disk bs=1M status=progress;and sync
   end
 end
