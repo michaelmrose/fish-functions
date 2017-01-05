8502157333476ff23f1c0d98b5d3f19c9bccd255
Michael Rose <michael@rosenetwork.net>
2017-01-04 16:37:14 -0800

    editing vi3_firstrun.fish

--- vi3_firstrun.fish
+++ vi3_firstrun.fish
@@ -1,8 +1,8 @@
 function vi3_firstrun
 	cat ~/.config/fish/config.fish /opt/vi3/fishconfig.txt > /tmp/tmpvi3fish
     cp /tmp/tmpvi3fish ~/.config/fish/config.fish
-    cp /opt/vi3/personalconfigexample ~/.i3/personalconfig
-    cp /opt/vi3/template.json ~/.i3/sessions/template.json
+    cp ~/proj/vi3/personalconfigexample ~/.i3/personalconfig
+    cp ~/proj/vi3/template.json ~/.i3/sessions/template.json
     mkdir ~/sessions
     mkdir ~/.i3/sessions
     mkdir ~/.i3/colors
