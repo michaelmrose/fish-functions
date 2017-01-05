ae315a5d4b05142a1e94258311d6fad8ae8e2e83
Michael Rose <michael@rosenetwork.net>
2017-01-04 16:39:20 -0800

    editing update-vi3-config.fish

--- update-vi3-config.fish
+++ update-vi3-config.fish
@@ -1,12 +1,12 @@
 function update-vi3-config
 	killall i3blocks
     rm ~/.i3/config
-    cat /opt/vi3/header.txt ~/.i3/colors/{$colors} ~/.i3/personalconfig /opt/vi3/vi3config > ~/.i3/config
+    cat ~/proj/header.txt ~/.i3/colors/{$colors} ~/.i3/personalconfig ~/proj/vi3config > ~/.i3/config
     i3-msg restart
     sleep 3
     configure-gaps
     i3-elements trans
     fix-all-trans
-    hide-all-hidden-windows
+    # hide-all-hidden-windows
     wp recall
 end
