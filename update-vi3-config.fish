87f794362d64a3366ab9b82e4bf2693e0f9278af
Michael Rose <michael@rosenetwork.net>
2017-01-04 16:50:57 -0800

    editing update-vi3-config.fish

--- update-vi3-config.fish
+++ update-vi3-config.fish
@@ -1,7 +1,7 @@
 function update-vi3-config
 	killall i3blocks
     rm ~/.i3/config
-    cat ~/proj/header.txt ~/.i3/colors/{$colors} ~/.i3/personalconfig ~/proj/vi3config > ~/.i3/config
+    cat ~/proj/vi3/header.txt ~/.i3/colors/{$colors} ~/.i3/personalconfig ~/proj/vi3/vi3config > ~/.i3/config
     i3-msg restart
     sleep 3
     configure-gaps
