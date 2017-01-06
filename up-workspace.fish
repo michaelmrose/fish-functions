9ee63a02ac8a778519c3c208faa66ad1ce0d2652
Michael Rose <michael@rosenetwork.net>
2017-01-05 21:48:38 -0800

    editing up-workspace.fish

--- up-workspace.fish
+++ up-workspace.fish
@@ -2,6 +2,8 @@ function up-workspace
 	set current (get-workspace-index)
 	set next (math "$current + 1")
   echo c is $current n is $next
+	set com vi3_workspace (get-workspace-letter)$next
+  eval $com
 	# vi3_workspace (get-workspace-letter)$next
-  # set-workspace-index $next
+  set-workspace-index $next
 end
