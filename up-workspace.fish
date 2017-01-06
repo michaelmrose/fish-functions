645476f8da2be639b00a0c572b48e910f8bdf259
Michael Rose <michael@rosenetwork.net>
2017-01-05 21:36:11 -0800

    editing up-workspace.fish

--- up-workspace.fish
+++ up-workspace.fish
@@ -1,7 +1,7 @@
 function up-workspace
-	set next (math (get-workspace-index) + 1)
+	set current (get-workspace-index)
+	set next (math "$current + 1")
 	vi3_workspace (get-focused-workspace)$next
 	set var workspaceindex_(get-focused-workspace)
-  set value workspaceindex_$current
-  set -U $value $next
+  set -U $var $next
 end
