4d4ac3f58dd079cb326e8ed9e88a3cd654c0abc4
Michael Rose <michael@rosenetwork.net>
2017-01-05 21:58:27 -0800

    editing vi3_workspace.fish

--- vi3_workspace.fish
+++ vi3_workspace.fish
@@ -1,8 +1,8 @@
 function vi3_workspace --description 'switch workspace [a-z]'
 	for i in (explode-words $argv)
         echo changing to $i
-        # set ws (get-workspace-letter)(get-workspace-index)
-        i3-msg workspace $i
+        set ws $i(get-workspace-index $i)
+        i3-msg workspace $ws
       end
       er vi3op
       update-op-status
