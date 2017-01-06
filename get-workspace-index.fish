8330f1d35c3a1f354c4917b508fb322b7004b108
Michael Rose <michael@rosenetwork.net>
2017-01-05 21:57:06 -0800

    editing get-workspace-index.fish

--- get-workspace-index.fish
+++ get-workspace-index.fish
@@ -1,4 +1,9 @@
 function get-workspace-index
-	set var workspaceindex_(get-workspace-letter)
-  echo $$var
+	if exists $argv
+    set var workspaceindex_$argv
+    echo $$var
+  else
+    set var workspaceindex_(get-workspace-letter)
+    echo $$var
+  end
 end
