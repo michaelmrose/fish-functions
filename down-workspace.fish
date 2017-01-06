1b4e3a8ffc9a22c2555cfcd18607fc1ed069ec47
Michael Rose <michael@rosenetwork.net>
2017-01-05 21:50:31 -0800

    editing down-workspace.fish

--- down-workspace.fish
+++ down-workspace.fish
@@ -1,6 +1,6 @@
 function down-workspace
 	set current (get-workspace-index)
-	set next (math "$current - 1")
+	set next (bounded  (math "$current - 1")1 5)
   # echo c is $current n is $next
 	set com vi3_workspace (get-workspace-letter)$next
   eval $com
