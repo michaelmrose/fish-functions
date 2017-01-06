98a6ec0a76f8c88af9c60086276932873742097c
Michael Rose <michael@rosenetwork.net>
2017-01-05 21:33:32 -0800

    editing down-workspace.fish

--- down-workspace.fish
+++ down-workspace.fish
@@ -1,8 +1,9 @@
 function down-workspace
 	set current (get-workspace-index)
 	set next (math "$current - 1")
-	vi3_workspace (get-focused-workspace)$next
-	set var workspaceindex_(get-focused-workspace)
-  set value workspaceindex_$current
-  set -U $value $next
+  echo (get-focused-workspace)$next
+	# vi3_workspace (get-focused-workspace)$next
+	# set var workspaceindex_(get-focused-workspace)
+  # set value workspaceindex_$current
+  # set -U $value $next
 end
