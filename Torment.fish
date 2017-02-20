036fcb114496120146f849e07f6e857989c375d2
Michael Rose <michael@rosenetwork.net>
2017-02-20 10:53:57 -0800

    editing Torment.fish

--- Torment.fish
+++ Torment.fish
@@ -1,5 +1,5 @@
 function Torment
 	xrr single
-  /home/michael/.cxoffice/Planescape_Torment/desktopdata/cxmenu/Desktop.C^5E3A_users_crossover_Desktop/Torment.lnk
+  sudo /home/michael/.cxoffice/Planescape_Torment/desktopdata/cxmenu/Desktop.C^5E3A_users_crossover_Desktop/Torment.lnk
   xrr dual
 end
