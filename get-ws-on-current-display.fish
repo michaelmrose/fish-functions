a477de9539caa4a24deac99072bb34ff9f464a5a
Michael Rose <michael@rosenetwork.net>
2017-02-27 03:57:14 -0800

    editing get-ws-on-current-display.fish

--- get-ws-on-current-display.fish
+++ get-ws-on-current-display.fish
@@ -2,5 +2,11 @@ function get-ws-on-current-display
 	set names (i3-msg -t get_workspaces|jq .[].name)
   set outputs (i3-msg -t get_workspaces|jq .[].output)
   set display (i3-msg -t get_workspaces|jq .[]|jq -r "select(.focused == true).output")
-  echo $display
+  set res ""
+  set n 1
+  for i in $names
+    if matches $outputs[$n] $display
+      set res $res $i
+    end
+  end
 end
