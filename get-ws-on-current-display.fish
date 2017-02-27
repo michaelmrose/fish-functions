069b67c091c2b0f4e4e4155b487e8221eccc8d2c
Michael Rose <michael@rosenetwork.net>
2017-02-27 04:02:44 -0800

    editing get-ws-on-current-display.fish

--- get-ws-on-current-display.fish
+++ get-ws-on-current-display.fish
@@ -1,5 +1,5 @@
 function get-ws-on-current-display
-	set names (i3-msg -t get_workspaces|jq .[].name)
+	set names (i3-msg -t get_workspaces|jq -r .[].name)
   set outputs (i3-msg -t get_workspaces|jq .[].output)
   set display (i3-msg -t get_workspaces|jq .[]|jq -r "select(.focused == true).output")
   set res ""
