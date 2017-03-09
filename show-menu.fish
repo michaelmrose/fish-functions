94d1c044f15a3bf48b73b6e2b03ce30a5c88dd4a
Michael Rose <michael@rosenetwork.net>
2017-03-08 18:11:33 -0800

    editing show-menu.fish

--- show-menu.fish
+++ show-menu.fish
@@ -1,10 +1,11 @@
 function show-menu
-	if exists $vi3op
+	if pgrep rofi
+    set msg "run"
+	else if exists $vi3op
         set msg (show-op)
     else
         set msg (show-mode)
     end
 
     echo $msg
-
 end
