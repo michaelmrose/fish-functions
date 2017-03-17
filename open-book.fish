84aa5e569df9f2e785434a4b72f625eec80b1dc6
Michael Rose <michael@rosenetwork.net>
2017-03-16 21:11:55 -0700

    editing open-book.fish

--- open-book.fish
+++ open-book.fish
@@ -1,20 +1,18 @@
 function open-book
 	set fullpath (pwd)/$argv
-    set ext (cutlast "." $argv)
-    set library /med/ebooks
-    if substr $fullpath $library #if path is in $library
-        set title (query-calibre-title title (escape-chars (extract-title $fullpath)))
-        add-to-recent-reads "$title"
-        add-to-recent-reads-fnames "$argv"
-        # set-recent-book-covers
-    end
+  set ext (cutlast "." $argv)
+  set library $ebook_library
+  if substr $fullpath $library #if path is in $library
+    set title (query-calibre-title title (escape-chars (extract-title $fullpath)))
+    add-to-recent-reads "$title"
+    add-to-recent-reads-fnames "$argv"
+    # set-recent-book-covers
+  end
 
-    switch $ext
-        case "pdf"
-            nohup zathura "$argv" > /dev/null &
-          case "epub"
-            nohup zathura "$argv" > /dev/null &
-        case "*"
-            nohup ebook-viewer "$argv" > /dev/null &
-    end
+  if contains $ext epub pdf djvu
+    set app zathura
+  else
+    set app ebook-viewer
+  end
+  nh $app "$argv"
 end
