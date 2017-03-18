5b3099aa08e91a7621eb8afdc0b2d9aff3bd6576
Michael Rose <michael@rosenetwork.net>
2017-03-17 17:25:56 -0700

    editing get-workspaces-on-focused-display.fish

--- get-workspaces-on-focused-display.fish
+++ get-workspaces-on-focused-display.fish
@@ -1,4 +1,4 @@
 function get-workspaces-on-focused-display
 	set offset (get-ws-in get rect.x where focused = true)
-  get-ws-info get name where rect.x = offset
+  get-ws-info get name where rect.x = $offset
 end
