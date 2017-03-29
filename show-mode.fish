a0b09ad5e6c30b24e85b531e3ded8b9c8d7de3cf
Michael Rose <michael@rosenetwork.net>
2017-03-29 01:15:19 -0700

    editing show-mode.fish

--- show-mode.fish
+++ show-mode.fish
@@ -1,7 +1,8 @@
 function show-mode
 	switch $vi3mode
         case "kill"
-            set msg '(a) all appkey (d) workspace (o) others in workspace (w) window (z) all visible'
+          set msg 'kill (q) selected (w)orkspace (o)ther (a)ll'
+            # set msg '(a) all appkey (d) workspace (o) others in workspace (w) window (z) all visible'
         case "default"
             set msg '(mod+hjkl change focus (RShift) command (Lshift) switch workspace (mod+a) audio (PrintScreen) screenshot'
             set msg 'default'
