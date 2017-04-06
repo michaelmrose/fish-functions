aa3f1cc0ca8a196f98489f370e1d83dbffce27a2
Michael Rose <michael@rosenetwork.net>
2017-04-06 14:35:48 -0700

    editing display-number.fish

--- display-number.fish
+++ display-number.fish
@@ -1,4 +1,11 @@
 function display-number
-	inverse-of (math (findindex (get-focused-display) (get-connected-displays)) -1)
+	set d (get-connected-displays)
+	set n (math (findindex (get-focused-display) $d) -1)
+  if test (count $d) -gt 1
+    inverse-of n
+  else
+    echo n
+  end
+
 	# math (findindex (get-focused-display) (get-connected-displays)) -1
 end
