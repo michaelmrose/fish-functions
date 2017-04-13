c498389b13648124f95368a14b5d540f000a04dd
Michael Rose <michael@rosenetwork.net>
2017-04-13 16:49:28 -0700

    editing make-fancy-lock.fish

--- make-fancy-lock.fish
+++ make-fancy-lock.fish
@@ -1,4 +1,4 @@
 function make-fancy-lock
 	cp $bgimage /tmp/i3lock.png
-convert '/tmp/i3lock.png' '-level' '0%,100%,0.6' '-blur' '2x8' '/home/michael/ext-projs/i3lock-fancy-multimonitor/lock.png' '-geometry' '+755+440' '-composite' '/home/michael/ext-projs/i3lock-fancy-multimonitor/text.png' '-geometry' '+606+702' '-composite' '/home/michael/ext-projs/i3lock-fancy-multimonitor/lock.png' '-geometry' '+2555+455' '-composite' '/home/michael/ext-projs/i3lock-fancy-multimonitor/text.png' '-geometry' '+2406+717' '-composite' '/tmp/i3lock.png'
+  ~/ext-projs/i3lock-fancy-multimonitor/lock
 end
