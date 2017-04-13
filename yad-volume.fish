9c1718bc6fc43edcaeb3b6336bdbd2cf9bd59fec
Michael Rose <michael@rosenetwork.net>
2017-04-13 10:45:32 -0700

    editing yad-volume.fish

---
+++ yad-volume.fish
@@ -0,0 +1,13 @@
+function yad-volume
+	yad --text="Volume"
+      --text-align center
+      --scale
+      --value (getvolume)
+      --geometry=10x300 --vertical
+      --button next
+      --print-partial
+      --no-close --step 5
+      --max-value 130 | while read -l line
+        setvolume $line
+       end
+end
