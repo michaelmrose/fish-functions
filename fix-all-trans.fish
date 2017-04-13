5eb889850d4dd2f4c1835d4240be5171401aa4ac
Michael Rose <michael@rosenetwork.net>
2017-04-12 22:07:16 -0700

    editing fix-all-trans.fish

--- fix-all-trans.fish
+++ fix-all-trans.fish
@@ -1,5 +1,5 @@
 function fix-all-trans
 	# apply-to-list "transset -i" (list-windows)
 	apply-to-list fix-trans (list-windows)
-  transset -i (windows-list | gr firefox | cut -d ' ' -f1) 0.9999
+  transset -i (windows-list | gr 'firefox|mpv' | cut -d ' ' -f1) 0.9999
 end
