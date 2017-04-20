fd6ba552e3cd65b8fed7e2505dcde8270d280980
Michael Rose <michael@rosenetwork.net>
2017-04-20 12:14:23 -0700

    editing foreach.fish

--- foreach.fish
+++ foreach.fish
@@ -1,10 +1,7 @@
 function foreach
 	set fn $argv[-1]
   set l $argv[1..-2]
-  echo f is $fn and l is $l
     for i in $l
-        echo body is $body
-        echo i is $i
         eval (echo $fn | sed "s#_#$i#g")
     end
 end
