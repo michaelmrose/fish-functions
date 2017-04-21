a1cda25bbe1f8d9481c33d2061ba2f7ecb27c98f
Michael Rose <michael@rosenetwork.net>
2017-04-21 09:40:10 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,10 +1,12 @@
 function setvolume
 	set current (ponymix get-volume)
 	if echo $argv | ag  '^\+' > /dev/null
-    set vol (bounded (wcalc -q {$current}$argv) 0 130)
-    set vol (greaterof $current $vol)
+    set vol (greaterof $current (bounded (wcalc -q {$current}$argv) 0 130))
+   # threadf set vol (wcalc -q {current}$argv) 
+   #         bounded 0 130
+   #         greaterof $current
   else if echo $argv | ag '^-' > /dev/null
-    set vol (wcalc -q (ponymix get-volume)$argv)
+    set vol (wcalc -q {$current}$argv)
   else
     set vol (bounded $argv 0 150)
   end
