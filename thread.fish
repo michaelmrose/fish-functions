ef14eaf5174f14fe2d4ae49d9047b8d4c85b02ac
Michael Rose <michael@rosenetwork.net>
2017-04-21 09:50:39 -0700

    editing thread.fish

--- thread.fish
+++ thread.fish
@@ -1,5 +1,11 @@
 function thread
-	for line in $argv
+	if defined $argv[1]
+    set res (eval $argv[1])
+  else
+    set res $argv[1]
+  end
+  
+	for line in $argv[2..-1]
     set res (eval $line $res)
   end
 end
