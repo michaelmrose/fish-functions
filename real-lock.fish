e9a3cba8abbefd873b2b54adfb46316797d5053a
Michael Rose <michael@rosenetwork.net>
2017-05-02 08:41:38 -0700

    editing real-lock.fish

--- real-lock.fish
+++ real-lock.fish
@@ -1,20 +1,23 @@
 function real-lock
-	set focused (get-focused-workspace)
-  # set oldsum $bgimage_checksum
-  # set -U bgimage_checksum (checksum-simple $bgimage)
-  save-workspaces
-	i3-elements hide
-  i3 workspace left
-  i3 workspace right
-  # echo before if
-  # if match $oldsum $bgimage_checksum
-    # i3lock -i /tmp/i3lock.png -t
-  # else
-    ~/ext-projs/i3lock-fancy-multimonitor/lock
-  # end
-  # echo after if
+	# set focused (get-focused-workspace)
+  # # set oldsum $bgimage_checksum
+  # # set -U bgimage_checksum (checksum-simple $bgimage)
+  # save-workspaces
+	# i3-elements hide
+  # i3 workspace left
+  # i3 workspace right
+  # # echo before if
+  # # if match $oldsum $bgimage_checksum
+  #   # i3lock -i /tmp/i3lock.png -t
+  # # else
+  #   ~/ext-projs/i3lock-fancy-multimonitor/lock
+  # # end
+  # # echo after if
 
-  i3-elements trans
-  restore-workspaces
-  ws $focused
+  # i3-elements trans
+  # restore-workspaces
+  # ws $focused
+  i3lock -i /med/images/locked.png
+  sleep 0.5
+  lightsout
 end
