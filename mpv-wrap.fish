5e82a9e61f2ab6a1629b1c61ff8a776a044c362b
Michael Rose <michael@rosenetwork.net>
2017-05-04 10:50:58 -0700

    editing mpv-wrap.fish

--- mpv-wrap.fish
+++ mpv-wrap.fish
@@ -7,7 +7,7 @@ function mpv-wrap
         set opts $argv[1..-2]
         set opts (echo $opts | sed 's#-a#--no-video#g')
     end
-    set com mpv
+    set com umpv
     add-to-recent-videos $vid
     eval $com $opts \"$vid\"
 end
