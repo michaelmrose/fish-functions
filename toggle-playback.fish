51c9cd643313eb7378937989e22edbbb6713dede
Michael Rose <michael@rosenetwork.net>
2017-05-10 10:13:27 -0700

    editing toggle-playback.fish

--- toggle-playback.fish
+++ toggle-playback.fish
@@ -1,5 +1,5 @@
 function toggle-playback
-	set playing alternatively which-playing $lastPlaying
+	set playing (alternatively which-playing $lastPlaying)
   playerctl -p $playing play-pause
   set -U lastPlaying $playing
 end
