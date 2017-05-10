2f4b170035f53ea167a9da1e654fa6b21fc0b552
Michael Rose <michael@rosenetwork.net>
2017-05-10 10:49:55 -0700

    editing which-playing.fish

--- which-playing.fish
+++ which-playing.fish
@@ -1,12 +1,3 @@
 function which-playing
-	for player in (playerctl -l)
-    if match (playerctl -p $player status) Playing
-      set players $players $player
-    end
-  end
-  if exists $players
-    p $players
-  else
-    return 1
-  end
+	filter-with-expr is-playing (playerctl -l)
 end
