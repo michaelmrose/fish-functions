fc5f7b61f645ec83d1596c54f5ffb77fa4b7c975
Michael Rose <michael@rosenetwork.net>
2017-05-10 13:19:37 -0700

    editing toggle-playback.fish

--- toggle-playback.fish
+++ toggle-playback.fish
@@ -2,16 +2,15 @@ function toggle-playback
 	set players (playerctl -l)
   set active (filter-with-expr is-playing $players)
   echo p is $playing a is $active
-  # set ins (intersect $lastPLaying $players)
 	# set playing (alternatively echo $active @ intersect lastPlaying players @ $players[1])
-  if exists $active
-    set playing $active
-  else if contains $lastPlaying $players
-    set playing $lastPlaying
-  else
-    set playing $players[1]
-  end
+  # if exists $active
+  #   set playing $active
+  # else if contains $lastPlaying $players
+  #   set playing $lastPlaying
+  # else
+  #   set playing $players[1]
+  # end
   
-  playerctl -p $playing play-pause
-  set -U lastPlaying $playing
+  # playerctl -p $playing play-pause
+  # set -U lastPlaying $playing
 end
