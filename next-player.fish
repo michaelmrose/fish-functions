d8a71b70b0cb6601e2d24607977981190fbd42be
Michael Rose <michael@rosenetwork.net>
2017-05-10 12:33:55 -0700

    editing next-player.fish

--- next-player.fish
+++ next-player.fish
@@ -1,7 +1,7 @@
 function next-player
-	if which-playing
-    toggle-playback
-  end
+	# if which-playing
+  #   toggle-playback
+  # end
   set players (playerctl -l)
   next-valid-index $lastPlaying $players
 end
