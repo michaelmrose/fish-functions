58396d74301d15074bb080e61af18c84762e484f
Michael Rose <michael@rosenetwork.net>
2017-05-10 12:59:02 -0700

    editing toggle-playback.fish

--- toggle-playback.fish
+++ toggle-playback.fish
@@ -1,7 +1,7 @@
 function toggle-playback
 	set players (ff playerctl -l)
-  set active (filter-with-expr is-playing $players)
-	set playing (alternatively echo $active @ intersect lastPlaying players @ $players[1])
-  playerctl -p $playing play-pause
-  set -U lastPlaying $playing
+  # set active (filter-with-expr is-playing $players)
+	# set playing (alternatively echo $active @ intersect lastPlaying players @ $players[1])
+  # playerctl -p $playing play-pause
+  # set -U lastPlaying $playing
 end
