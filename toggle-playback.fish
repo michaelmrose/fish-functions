a357940b94c38ef36eab78c5adcaefb94126c35d
Michael Rose <michael@rosenetwork.net>
2017-05-10 10:13:05 -0700

    editing toggle-playback.fish

--- toggle-playback.fish
+++ toggle-playback.fish
@@ -1,17 +1,5 @@
 function toggle-playback
-	set players (playerctl -l)
-  if contains $lastPlaying $players
-    set playing $lastPlaying
-  else
-    set playing clementine
-  end
-  
-  for player in $players
-    if match (playerctl -p $player status) Playing
-      set playing $player
-    end
-  end
-
+	set playing alternatively which-playing $lastPlaying
   playerctl -p $playing play-pause
   set -U lastPlaying $playing
 end
