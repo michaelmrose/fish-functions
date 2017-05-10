aaf0389dff32043767880fbc38eb327817f33d7e
Michael Rose <michael@rosenetwork.net>
2017-05-10 11:25:16 -0700

    editing alternatively.fish

--- alternatively.fish
+++ alternatively.fish
@@ -1,10 +1,22 @@
 function alternatively
-	set com $argv[1..-2]
-  set val $argv[-1]
-  set res (eval $com)
-  if exists $res
-    p $res
-  else
-    p $val
+	set words (split-by $argv)
+  set coms $words[1..-2]
+  set val $words[-1]
+  for com in $coms
+    set res (eval $com)
+    if exists $res
+      p $res
+      break
+    end
   end
+  p $val
+    
+	# set com $argv[1..-2]
+  # set val $argv[-1]
+  # set res (eval $com)
+  # if exists $res
+  #   p $res
+  # else
+  #   p $val
+  # end
 end
