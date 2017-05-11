a9a0011e7b6193353bba09d2f4cd65639aa156f1
Michael Rose <michael@rosenetwork.net>
2017-05-10 18:18:00 -0700

    editing tracks.fish

--- tracks.fish
+++ tracks.fish
@@ -2,7 +2,7 @@ function tracks
 	# clementine -a (rfi match "select tracks: " (beet ls -p $argv) | collect)
   set tracks (beet ls -p $argv)
   if test (count $tracks) -gt 1
-	  clementine -a (rfi match "select tracks: " tracks | collect)
+	  clementine -a (rfi match "select tracks: " $tracks | collect)
   else
     clementine -a $tracks
   end
