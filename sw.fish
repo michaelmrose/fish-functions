9e8ef6f4287d7a4d8810b0816f206081f1c97232
Michael Rose <michael@rosenetwork.net>
2017-05-12 10:27:00 -0700

    editing sw.fish

--- sw.fish
+++ sw.fish
@@ -2,10 +2,13 @@ function sw
 	set v $argv[1]
   set cases (odds $argv[2..-1])
   set results (evens $argv[2..-1])
+  set index 1
   for c in $cases
     if match $v $c > /dev/null
-      echo c is $c and v is $v
+      echo $results[$index]
       return 0
+    else
+      set index (increase index)
     end
   end
 end
