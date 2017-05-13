5961a9d5859113985d2e640ec85aa46ca10a744f
Michael Rose <michael@rosenetwork.net>
2017-05-13 12:17:28 -0700

    editing match-lists2.fish

--- match-lists2.fish
+++ match-lists2.fish
@@ -1,7 +1,7 @@
 function match-lists2
 	set val $argv[1]
-  set l1 (odds $argv[2..-1])
-  set l2 (evens $argv[2..-1])
+  set l1 (first-half $argv[2..-1])
+  set l2 (last-half $argv[2..-1])
   echo l1 is $l1
   echo l2 is $l2
   echo $l2[(findindex $val $l1)]
