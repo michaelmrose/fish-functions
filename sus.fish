d3edf14d6e641bd3fdee8302d6e6dce9a2894b62
Michael Rose <michael@rosenetwork.net>
2017-05-13 11:12:51 -0700

    editing sus.fish

--- sus.fish
+++ sus.fish
@@ -2,6 +2,10 @@ function sus
 	manage-share ensure-unmounted
   sudo pm-suspend
   wp recall
+  if [ (whoami) = michael ]
+    setup-keyboard
+  end
+  
   if exists $argv
     switch $argv[1]
       case greet
