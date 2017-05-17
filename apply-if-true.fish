3394af8f3c84ff5875899630257a794336e8820f
Michael Rose <michael@rosenetwork.net>
2017-05-17 14:04:30 -0700

    editing apply-if-true.fish

---
+++ apply-if-true.fish
@@ -0,0 +1,10 @@
+function apply-if-true
+	set fn $argv[1]
+    if test (count $argv) -gt 1
+      set lst $argv[2..-1]
+      for i in $lst
+          set str (string replace @ $i $fn)
+          eval $str
+      end
+    end
+end
