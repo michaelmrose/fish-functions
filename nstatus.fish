e321503bce6579346060650d86b7be833f14e87b
Michael Rose <michael@rosenetwork.net>
2017-05-17 13:52:23 -0700

    editing nstatus.fish

--- nstatus.fish
+++ nstatus.fish
@@ -2,6 +2,6 @@ function nstatus
 	set google 8.8.8.8 net
   set router 192.168.1.1 router
   set modem 192.168.0.1 modem
-  set routes $router $modem $google
+  set routes[1] $google
   echo $routes[1]
 end
