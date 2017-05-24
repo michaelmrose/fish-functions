e833994b552a5c5a69a5bf647b8e09eedaf8fd2c
Michael Rose <michael@rosenetwork.net>
2017-05-24 10:29:42 -0700

    editing pithoscli.fish

--- pithoscli.fish
+++ pithoscli.fish
@@ -1,22 +1,24 @@
 function pithoscli
+	set dbus dbus-send --print-reply --dest=net.kevinmehall.Pithos /net/kevinmehall/Pithos net.kevinmehall.Pithos
+
 	if not exists $argv
-    pithos &
-    return 0
+    set command 'pithos &'
+  else
+    switch $argv
+      case help
+        set command 'echo try love ban tired unrate or help'
+      case love
+        set command $dbus.LoveCurrentSong
+      case ban
+        set command $dbus.BanCurrentSong
+      case tired
+        set command $dbus.TiredSong
+      case unrate
+        set command $dbus.UnRateSong
+    end
   end
     
-	switch $argv
-    case help
-      echo try love ban tired unrate or help
-      return 0
-    case love
-      set command LoveCurrentSong
-    case ban
-      set command BanCurrentSong
-    case tired
-      set command TiredSong
-    case unrate
-      set command UnRateSong
-  end
   
-dbus-send --print-reply --dest=net.kevinmehall.Pithos /net/kevinmehall/Pithos net.kevinmehall.Pithos.$command
+# dbus-send --print-reply --dest=net.kevinmehall.Pithos /net/kevinmehall/Pithos net.kevinmehall.Pithos.$command
+  eval $command
 end
