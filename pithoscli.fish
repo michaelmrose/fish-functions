79e92e4c79d71399f0e3d24430b5afa1a455aeb9
Michael Rose <michael@rosenetwork.net>
2017-05-24 10:24:37 -0700

    editing pithoscli.fish

--- pithoscli.fish
+++ pithoscli.fish
@@ -1,9 +1,20 @@
 function pithoscli
+	if not exists $argv
+    pithos &
+    return 0
+  end
+    
 	switch $argv
+    case help
+      echo try love ban tired unrate or help
     case love
       set command LoveCurrentSong
     case ban
       set command BanCurrentSong
+    case tired
+      set command TiredSong
+    case unrate
+      set command UnRateSong
   end
   
 dbus-send --print-reply --dest=net.kevinmehall.Pithos /net/kevinmehall/Pithos net.kevinmehall.Pithos.$command
