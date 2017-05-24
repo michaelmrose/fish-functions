89a755443e8a24211fdc6a4afa4283d4c5535329
Michael Rose <michael@rosenetwork.net>
2017-05-24 10:40:01 -0700

    editing pithoscli.fish

--- pithoscli.fish
+++ pithoscli.fish
@@ -2,7 +2,7 @@ function pithoscli
 	set dbus 'dbus-send --print-reply --dest=net.kevinmehall.Pithos /net/kevinmehall/Pithos net.kevinmehall.Pithos'
 
 	if not exists $argv
-    set command 'pith'
+    set command 'pithos &'
   else
     switch $argv
       case help
