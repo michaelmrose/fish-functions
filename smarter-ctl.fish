7beb4f911a34fb0841a3f3538ec2e5abd51a5211
Michael Rose <michael@rosenetwork.net>
2017-05-30 12:31:28 -0700

    editing smarter-ctl.fish

--- smarter-ctl.fish
+++ smarter-ctl.fish
@@ -1,14 +1,7 @@
 function smarter-ctl
-	# set players (playerctl -l)
-  # set active (filter-with-expr is-playing $players)
-  # for player in $players
-  #   if [ (playerctl -p $player status) = Playing ]
-  #     set active $player
-  #   end
-  # end
+	# set active (filter-with-expr is-playing $players)
   set active (active-player)
   
-  # echo p is $players a is $active
 	# set playing (alternatively echo $active @ intersect lastPlaying players @ $players[1])
   if exists $active
     set playing $active
@@ -17,7 +10,6 @@ function smarter-ctl
   else
     set playing $players[1]
   end
-  echo playing is $playing
-  echo playerctl -p $playing $argv
+  playerctl -p $playing $argv
   set -U lastPlaying $playing
 end
