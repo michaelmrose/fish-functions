5c5c669c6e41c7fdbf3e25f102e60f5cba031c0f
Michael Rose <michael@rosenetwork.net>
2017-05-30 12:29:17 -0700

    editing active-player.fish

--- active-player.fish
+++ active-player.fish
@@ -2,7 +2,7 @@ function active-player
 	set players (playerctl -l)
   for player in $players
     if [ (playerctl -p $player status) = Playing ]
-      set active $active $player
+      set active $player
     end
   end
 end
