c4b48318425ee6e2c97640afcc2ce98ff066647f
Michael Rose <michael@rosenetwork.net>
2017-05-30 12:45:20 -0700

    editing playing.fish

--- playing.fish
+++ playing.fish
@@ -1,11 +1,5 @@
 function playing
-	# set players (playerctl -l)
-	# for player in $players
-  #   if [ (playerctl -p $player status) = Playing ]
-  #     set active $player
-  #   end
-  # end
-  set active (active-player)
+	set active (active-player)
   set title (playerctl -p $active metadata title)
   set artist (playerctl -p $active metadata artist)
   if exists $title
