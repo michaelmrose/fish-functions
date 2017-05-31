fbdbddb8aff7d404ef8a88efc3b66d74e85a983a
Michael Rose <michael@rosenetwork.net>
2017-05-31 02:52:54 -0700

    editing apctl.fish

--- apctl.fish
+++ apctl.fish
@@ -1,6 +1,15 @@
 function apctl
 	set players (playerctl -l)
-	set active (r (filter-with-expr is-playing $players) $lastPlaying)
+	# set active (r (filter-with-expr is-playing $players) $lastPlaying)
+  for player in $players
+    if is-playing $player
+      set active $player
+    end
+  end
+  if not exists $active
+    set active $lastPlaying
+  end
+  
   playerctl -p $active $argv
   set -U lastPlaying $active
   signal-i3blocks playing
