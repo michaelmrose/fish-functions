a7569c78b194673f42902613b0a7d7d043fa5ee0
Michael Rose <michael@rosenetwork.net>
2017-05-31 02:17:16 -0700

    editing smarter-ctl.fish

--- smarter-ctl.fish
+++ smarter-ctl.fish
@@ -7,7 +7,7 @@ function smarter-ctl
   # end
   # set active (alt (filter-with-expr is-playing (playerctl -l)) $lastPlaying)
   set playing (filter-with-expr is-playing (playerctl -l))
-  set $active (alt $playing $lastPlaying)
+  set active (alt $playing $lastPlaying)
   echo playing is $playing
   echo active is $active
   # if not exists $active
