b313ed27437e20059a6ad9981ae4b1447bbbcda7
Michael Rose <michael@rosenetwork.net>
2017-05-30 23:59:45 -0700

    editing player-status-line.fish

--- player-status-line.fish
+++ player-status-line.fish
@@ -1,21 +1,20 @@
 function player-status-line
-	set title (active-playerctl metadata title)
-  set artist (active-playerctl metadata artist)
+	set title (smarter-ctl metadata title)
+  set artist (smarter-ctl metadata artist)
 
   if exists $title
     set metadata $title by $artist
   else
     set metadata None
   end
-	if set val (active-playerctl status) 2> /dev/null
-    switch $val
-          case Paused
-              set symbol "  "
-          case Playing
-              set symbol "  "
-      end
-      echo $symbol $metadata
-  else
-    echo none
+  set val (smarter-ctl status) 2> /dev/null
+  switch $val
+    case Paused
+      set symbol "  "
+    case Playing
+      set symbol "  "
+    case Stopped
+      set symbol ""
   end
+  echo $symbol $metadata
 end
