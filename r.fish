9f02c22472fe62cb826eaeaf2d1cd9ac48e0f7f9
Michael Rose <michael@rosenetwork.net>
2017-05-31 02:39:46 -0700

    editing r.fish

--- r.fish
+++ r.fish
@@ -1,7 +1,14 @@
 function r
-	if exists $argv[1]
-    echo $argv[1]
-  else
-    echo $argv[2]
+	for item in $argv
+    if exists item
+      echo $item
+      return 0
+    end
   end
+  
+	# if exists $argv[1]
+  #   echo $argv[1]
+  # else
+  #   echo $argv[2]
+  # end
 end
