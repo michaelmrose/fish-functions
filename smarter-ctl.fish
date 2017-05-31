6e989ed2415af52fed58a190c2401b1ef36aa669
Michael Rose <michael@rosenetwork.net>
2017-05-30 23:53:21 -0700

    editing smarter-ctl.fish

--- smarter-ctl.fish
+++ smarter-ctl.fish
@@ -1,5 +1,14 @@
 function smarter-ctl
-	active-playerctl $argv
+	set players (playerctl -l)
+  for player in $players
+    if [ (playerctl -p $player status) = Playing ]
+      set active $player
+    end
+  end
+  if not exists $active
+    set active $lastPlaying
+  end
+  playerctl -p $active $argv
   set -U lastPlaying $active
   signal-i3blocks playing
 end
