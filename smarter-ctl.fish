737ec623428a2e8451fd6c4b92d94722e22c5c49
Michael Rose <michael@rosenetwork.net>
2017-05-31 02:29:17 -0700

    removed smarter-ctl.fish

--- smarter-ctl.fish
+++
@@ -1,6 +0,0 @@
-function smarter-ctl
-	set active (r (filter-with-expr is-playing (playerctl -l)) $lastPlaying)
-  playerctl -p $active $argv
-  set -U lastPlaying $active
-  signal-i3blocks playing
-end
