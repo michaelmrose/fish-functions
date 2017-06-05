d7293c9adb16d12fdcf99142098f639d8aafc735
Michael Rose <michael@rosenetwork.net>
2017-06-05 03:36:50 -0700

    editing apctl.fish

--- apctl.fish
+++ apctl.fish
@@ -1,17 +1,5 @@
 function apctl
 	set players (playerctl -l)
-  # for player in $players
-  #   if [ (playerctl -p $player status) = Playing ]
-  #     set active $player
-  #   end
-  # end
-  # if not exists $active
-  #   set active $lastPlaying
-  # end
-  # if not contains $active $players
-  #   set active $players[1]
-  # end
-
   set playing (filter-with-expr is-playing $players | nth 1)
   set previously (containse $lastPlaying $players)
   set first $players[1]
