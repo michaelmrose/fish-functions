8465b9fe12118a84d28cf6d84b606b5940d7dacb
Michael Rose <michael@rosenetwork.net>
2017-06-05 12:12:54 -0700

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -1,6 +1,11 @@
 function focus-app
-	set class (return-windowclass (appkey $argv))
-  set com i3-msg \'[class=$class]\' focus
+	set criteria (return-windowclass (appkey $argv))
+  switch (count $argv)
+    case 1
+      set com i3-msg \'[class=$criteria]\' focus
+    case 2
+      set com i3-msg \'[class=$criteria[1] title=\"$criteria[2]\"]\' focus
+  end
   eval $com
   er vi3op
   update-op-status
