3150a1ca47e84a7083810c87c1488b2c7d1513fa
Michael Rose <michael@rosenetwork.net>
2017-06-05 03:26:48 -0700

    editing testfn.fish

--- testfn.fish
+++ testfn.fish
@@ -1,7 +1,8 @@
 function testfn
-	msg %self
-  ps -A | grep %self
-	if pstree -s %self | grep 'steam'
-    i3-msg move window to workspace r
-  end
+	echo $argv[1]
+	# msg %self
+  # ps -A | grep %self
+	# if pstree -s %self | grep 'steam'
+  #   i3-msg move window to workspace r
+  # end
 end
