c830cf412024100b697594feab2e4954dcd3ff26
Michael Rose <michael@rosenetwork.net>
2017-06-12 12:03:20 -0700

    editing setup-mouse.fish

--- setup-mouse.fish
+++ setup-mouse.fish
@@ -1,4 +1,8 @@
 function setup-mouse
 	mouse-set smooth-scroll on
     mouse-set dpi 900
+    xset m 3/2
+    set id (xinput list | gr M510 | nth 5 | cut -d = -f2)
+    xinput --set-prop $id 'Device Accel Constant Deceleration' 05
+    xinput --set-prop $id 'Device Accel Velocity Scaling' 9
 end
