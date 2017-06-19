ba168eb9bd2e071ab553aea0ac350ccf722da2f2
Michael Rose <michael@rosenetwork.net>
2017-06-19 13:32:00 -0700

    editing appkey.fish

--- appkey.fish
+++ appkey.fish
@@ -10,8 +10,8 @@ function appkey
         end
     else if matches "$argv"  'find [a-zA-Z\-]+'
         appkey show | grep -i --color=never $argv[2..-1]
-    else if matches "$argv"  'which [a-zA-Z\-]+'
-        appkey show | grep -i --color=never $argv[2..-1] | cut -c1
+    # else if matches "$argv"  'which [a-zA-Z\-]+'
+    #     appkey show | grep -i --color=never $argv[2..-1] | cut -c1
     else if matches "$argv" '[a-zA-Z] .+' # set
         set value appkey_$argv[1]
         set -U $value $argv[2..-1]
