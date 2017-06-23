6f707a3e0f21a87546d64af7224f2e08fd6ff3c1
Michael Rose <michael@rosenetwork.net>
2017-06-23 12:07:47 -0700

    editing sumof.fish

--- sumof.fish
+++ sumof.fish
@@ -1,7 +1,13 @@
 function sumof
+	if not exists $argv
+    while read -l $line
+        set acc (math "$acc + $line")
+    end
+  else
 	set acc 0
     for i in $argv
         set acc (math "$acc + $i")
     end
+  end
     echo $acc
 end
