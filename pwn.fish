2a5e43cccb6cc0fac7d33335cd5446b760cbc1e9
Michael Rose <michael@rosenetwork.net>
2017-06-26 12:33:30 -0700

    editing pwn.fish

---
+++ pwn.fish
@@ -0,0 +1,14 @@
+function pwn
+	set owner $argv[1]
+  set target $argv[2]
+  if test -f $target
+    set optional ""
+  else if test -d $target
+    set optional "-R"
+  else
+    echo invalid argument
+    return 1
+  end
+
+  echo sudo chown $optional $owner:$owner $target
+end
