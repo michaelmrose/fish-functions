a8c587c6c15c5c96416ab5883f42079dc2f3684a
Michael Rose <michael@rosenetwork.net>
2017-06-26 00:46:58 -0700

    editing set-wp-based-on-url.fish

--- set-wp-based-on-url.fish
+++ set-wp-based-on-url.fish
@@ -1,17 +1,18 @@
 function set-wp-based-on-url
-	xdotool key y y
-  sleep 0.3
-  set url (xclip -o)
-  set n (echo "$url" | cut -d / -f5)   
-  curl $url > /tmp/wallpaper.html
-  if grep full/wallhaven-$n.jpg /tmp/wallpaper.html
-    set ext jpg
-  else
-    set ext png
-  end
+	msg fuck
+	# xdotool key y y
+  # sleep 0.3
+  # set url (xclip -o)
+  # set n (echo "$url" | cut -d / -f5)   
+  # curl $url > /tmp/wallpaper.html
+  # if grep full/wallhaven-$n.jpg /tmp/wallpaper.html
+  #   set ext jpg
+  # else
+  #   set ext png
+  # end
   
-  set image https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-$n.$ext
-  # echo u is $url and i is $image
-  curl $image > /tmp/wallpaper.$ext
-  wp /tmp/wallpaper.$ext
+  # set image https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-$n.$ext
+  # # echo u is $url and i is $image
+  # curl $image > /tmp/wallpaper.$ext
+  # wp /tmp/wallpaper.$ext
 end
