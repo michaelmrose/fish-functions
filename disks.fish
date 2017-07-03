0c53f941afa113a5023be307c4133b926ee643d9
Michael Rose <michael@rosenetwork.net>
2017-07-03 12:29:56 -0700

    editing disks.fish

--- disks.fish
+++ disks.fish
@@ -1,4 +1,5 @@
 function disks
 	# sudo fdisk -l | grep 'Disk /dev/sd' | awk '{print $2}' | cut -d : -f1 | cut -d '/' -f3
-  s fdisk -l | gr '^/dev/sd' | nth 1 | cut -c6-8 | sort -u
+  # s fdisk -l | gr '^/dev/sd' | nth 1 | cut -c6-8 | sort -u
+  p /sys/block/sd* | cut -d / -f4
 end
