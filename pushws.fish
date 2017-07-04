e5f57a15d13a5635a62670ac16b0a1aed6aafff7
Michael Rose <michael@rosenetwork.net>
2017-07-04 02:40:20 -0700

    editing pushws.fish

--- pushws.fish
+++ pushws.fish
@@ -2,6 +2,7 @@ function pushws
 	set current (get-focused-workspace)
   set letter (echo $current | cut -c1)
   set n (echo $current | cut -c2)
+  set n (bounded $n 1 5)
   switch $argv
     case up
       set n (math $n + 1)
