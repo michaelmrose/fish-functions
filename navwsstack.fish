9b1a8ce5f0d2a437b1e5d014b739284e426773f2
Michael Rose <michael@rosenetwork.net>
2017-07-04 02:04:13 -0700

    editing navwsstack.fish

--- navwsstack.fish
+++ navwsstack.fish
@@ -2,13 +2,16 @@ function navwsstack
 	set current (get-focused-workspace)
   set letter (echo $current | cut -c1)
   set n (echo $current | cut -c2)
+  echo n is $n
   switch $argv
     case up
       set n (increase n)
+  echo n is $n
     case down
       set n (decrease n)
   end
 
+  echo n is $n
   setwsstack $letter $n
   ws $letter$n
 end
