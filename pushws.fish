bc9f560eac0758524a95d4047150458e6c67f980
Michael Rose <michael@rosenetwork.net>
2017-07-04 02:32:17 -0700

    editing pushws.fish

---
+++ pushws.fish
@@ -0,0 +1,14 @@
+function pushws
+	set current (get-focused-workspace)
+  set letter (echo $current | cut -c1)
+  set n (echo $current | cut -c2)
+  set n (math $n +1)
+  switch $argv
+    case up
+      set n (math $n + 1)
+    case down
+      set n (math $n - 1)
+  end
+  setwsstack $letter $n
+  ws $letter
+end
