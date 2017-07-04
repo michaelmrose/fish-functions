732e174362eab1c4822a56403894bcbb4ee9165b
Michael Rose <michael@rosenetwork.net>
2017-07-04 02:02:07 -0700

    editing navwsstack.fish

---
+++ navwsstack.fish
@@ -0,0 +1,13 @@
+function navwsstack
+	set current (get-focused-workspace)
+  set letter (echo $current | cut -c1)
+  set n (echo $current | cut -c2)
+  switch $argv
+    case up
+      set n (increase n)
+    case down
+      set n (decrease n)
+  end
+
+  setwsstack $letter $n
+end
