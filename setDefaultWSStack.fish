ab1a12003309e3a941d89143235898e8f4a1c8ec
Michael Rose <michael@rosenetwork.net>
2017-07-04 01:42:29 -0700

    editing setDefaultWSStack.fish

--- setDefaultWSStack.fish
+++ setDefaultWSStack.fish
@@ -1,5 +1,25 @@
 function setDefaultWSStack
-	for i in (alphabet)
-    eval set ws{$i}level 1
-  end
+	set wsstacka 1
+  set wsstackb 1
+  set wsstackc 1
+  set wsstackd 1
+  set wsstacke 1
+  set wsstackf 1
+  set wsstackg 1
+  set wsstackh 1
+  set wsstackl 1
+  set wsstackm 1
+  set wsstackn 1
+  set wsstacko 1
+  set wsstackp 1
+  set wsstackq 1
+  set wsstackr 1
+  set wsstacks 1
+  set wsstackt 1
+  set wsstacku 1
+  set wsstackv 1
+  set wsstackw 1
+  set wsstackx 1
+  set wsstacky 1
+  set wsstackz 1
 end
