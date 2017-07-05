3fbfb4a51aa3226be177e49875cf413185e7d7c4
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:06:08 -0700

    editing ws-status-line.fish

--- ws-status-line.fish
+++ ws-status-line.fish
@@ -6,11 +6,12 @@ function ws-status-line
   set current $argv
   set letter (echo $current | cut -c1)
   set n (echo $current | cut -c2)
-  set workspaces (list-workspaces | gr "^$letter" | cut -c2)
+  set workspaces (list-workspaces)
+  set numbers (echo workspaces | gr "^$letter" | cut -c2)
   # echo w is $workspaces l is $letter and n is $n
-  if test (count $workspaces) -eq 1
-    echo "$letter"  
+  if test (count $numbers) -eq 1
+    echo "$letter"   
   else
-    echo $letter: (echo $workspaces | sed "s/$n/\[$n\]/g")
+    echo $letter: (echo $numbers | sed "s/$n/\[$n\]/g")
   end
 end
