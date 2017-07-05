d9a970903f5c180602ad1c327ff6cdcaa0d01bc5
Michael Rose <michael@rosenetwork.net>
2017-07-04 23:28:34 -0700

    editing ws-status-line.fish

--- ws-status-line.fish
+++ ws-status-line.fish
@@ -9,7 +9,7 @@ function ws-status-line
   set workspaces (list-workspaces | gr "^$letter" | cut -c2)
   # echo w is $workspaces l is $letter and n is $n
   if test (count $workspaces) -eq 1
-    echo "$letter:     "  
+    echo "$letter: [1]"  
   else
     echo $letter: (echo $workspaces | sed "s/$n/\[$n\]/g")
   end
