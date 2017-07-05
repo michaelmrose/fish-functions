11df496a269b2db486b21c6ef4006596245405c5
Michael Rose <michael@rosenetwork.net>
2017-07-05 01:58:31 -0700

    editing arrangews.fish

--- arrangews.fish
+++ arrangews.fish
@@ -1,3 +1,18 @@
 function arrangews
 	set letter (get-focused-workspace | cut -c1)
+  if wsispaged $letter
+    condensewspages
+  end
+  switch $argv
+    case tabbed
+      i3-msg layout tabbed
+    case splith
+      i3-msg layout splith
+    case splitv
+      i3-msg layout splitv
+    case stacked
+      i3-msg layout stacked
+    case paged
+      spreadwspages
+  end
 end
