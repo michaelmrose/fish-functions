5facdc76b7dbb7deb6ee2306397b5a7ff263348c
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:55:09 -0700

    editing condensewspages.fish

--- condensewspages.fish
+++ condensewspages.fish
@@ -1,8 +1,8 @@
 function condensewspages
 	set focused (get-focused-workspace)
   set letter (echo $focused | cut -c1)
-  ws {$letter}1
-  for n in (seq 2 5)
-    gws $letter{$n}
+  set pages (list-workspaces | gr "^$letter")
+  for page in $pages
+    echo gws $page
   end
 end
