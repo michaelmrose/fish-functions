6bd886bac86721246953862344115e4e8d6d2158
Michael Rose <michael@rosenetwork.net>
2017-07-05 01:44:25 -0700

    editing spreadwspages.fish

--- spreadwspages.fish
+++ spreadwspages.fish
@@ -2,10 +2,11 @@ function spreadwspages
 	set letter (get-focused-workspace |cut -c1)
   for n in (seq 2 5)
     i3-msg move window to workspace {$letter}$n
-    if not windowfocused
-      echo i3-msg [workspace={$letter}n move container to workspace {$letter}(math n - 1)]
-      signal-i3blocks pages
-      return 0
-    end
+    # if windowfocused
+    #   # i3-msg [workspace={$letter}n move container to workspace {$letter}(math n - 1)]
+    # else
+    #   return 0
+    # end
   end
+   signal-i3blocks pages
 end
