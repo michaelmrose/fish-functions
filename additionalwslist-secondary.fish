49f10e1e2d23a3b2d5b5f3c8754fce6165ad57d8
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:13:05 -0700

    removed additionalwslist-secondary.fish

--- additionalwslist-secondary.fish
+++
@@ -1,9 +0,0 @@
-function additionalwslist-secondary
-	set l1 (get-ws-info get name where rect.x = 0)
-  set l2 (get-ws-info get name where visible = false | cut -c1)
-  for i in $l1
-    if startswith f $i
-      echo $i | cut -c1
-    end
-  end
-end
