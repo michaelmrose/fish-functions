90a3c939c8b477cf237ba00b572b58fbdeba7404
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:09:20 -0700

    editing ws-status-line.fish

--- ws-status-line.fish
+++ ws-status-line.fish
@@ -12,7 +12,7 @@ function ws-status-line
   if test (count $numbers) -eq 1
     echo "$letter"   
   else
-    echo ns is $numbers
+    echo ns is $numbers w is $workspaces
     echo $letter: (echo $numbers | sed "s/$n/\[$n\]/g")
   end
 end
