8354cbfb8c44140cc22bd1df941aed321cdb9ef9
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:19:48 -0700

    editing ws-status-line-primary.fish

--- ws-status-line-primary.fish
+++ ws-status-line-primary.fish
@@ -1,4 +1,4 @@
 function ws-status-line-primary
 	set val (ws-status-line (get-active-workspaces)[2])
-  echo $val
+  echo $val :: (get-ws-info get name where rect.x = 1680 | cut -c1 | sort -u)
 end
