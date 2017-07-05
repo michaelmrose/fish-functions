c585f77bfd514dba1d049996b8c1b45f9b23c2fe
Michael Rose <michael@rosenetwork.net>
2017-07-04 23:21:37 -0700

    editing ws-status-line.fish

--- ws-status-line.fish
+++ ws-status-line.fish
@@ -2,7 +2,8 @@ function ws-status-line
 	# echo 'f a c {1 [2] 3}' 
   # echo 'f {[1]} a {[1] c{1 [2] 3}}'
   # echo 'a [1] 2 s d f 1 [2] 3'
-  set current (get-focused-workspace)
+  # set current (get-focused-workspace)
+  set current $argv
   set letter (echo $current | cut -c1)
   set n (echo $current | cut -c2)
   set workspaces (list-workspaces | gr "^$letter" | cut -c2)
