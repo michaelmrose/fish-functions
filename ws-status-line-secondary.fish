629d00214fa633b28077b85da4b8bcc8c13f5850
Michael Rose <michael@rosenetwork.net>
2017-07-04 23:42:48 -0700

    editing ws-status-line-secondary.fish

--- ws-status-line-secondary.fish
+++ ws-status-line-secondary.fish
@@ -1,3 +1,6 @@
 function ws-status-line-secondary
-	ws-status-line (get-active-workspaces)[1]
+	set l1 (get-ws-info get name where rect.x = 0)
+  set l2 (get-ws-info get name where visible = false)
+  set additional (intersect l1 l2 | cut -c1)
+	ws-status-line (get-active-workspaces)[1] :: $additional
 end
