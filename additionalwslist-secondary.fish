71e43d5980fe05600666b80f73e96c4272b05bc7
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:06:08 -0700

    editing additionalwslist-secondary.fish

--- additionalwslist-secondary.fish
+++ additionalwslist-secondary.fish
@@ -1,8 +1,8 @@
 function additionalwslist-secondary
 	set l1 (get-ws-info get name where rect.x = 0)
-  set l2 (get-ws-info get name where visible = false)
+  set l2 (get-ws-info get name where visible = false | cut -c1)
   for i in $l1
-    if contains $i $l2
+    if startswith f $i
       echo $i | cut -c1
     end
   end
