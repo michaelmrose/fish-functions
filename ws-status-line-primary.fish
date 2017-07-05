e1c936f8090c54dc2e0f1e32c701fde9afd11e0d
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:56:44 -0700

    editing ws-status-line-primary.fish

--- ws-status-line-primary.fish
+++ ws-status-line-primary.fish
@@ -1,3 +1,6 @@
 function ws-status-line-primary
-	ws-status-line (get-active-workspaces)[2]
+	set current (ws-status-line (get-active-workspaces)[2])
+  set first (echo $current | cut -c1)
+  set others (get-ws-info get name where rect.x = 1680 | cut -c1 | sort -u | sed "s/$first//g")
+  echo $current :: $others
 end
