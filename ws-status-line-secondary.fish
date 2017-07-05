dc4598eecbded3bf38728ec2f4eed25885bd831e
Michael Rose <michael@rosenetwork.net>
2017-07-05 00:25:54 -0700

    editing ws-status-line-secondary.fish

--- ws-status-line-secondary.fish
+++ ws-status-line-secondary.fish
@@ -1,6 +1,6 @@
 function ws-status-line-secondary
 	set current (ws-status-line (get-active-workspaces)[1])
   set first (echo $current | cut -c1)
-  set others (get-ws-info get name where rect.x = 0 | cut -c1 | sort -u | sed "s/$first/_/g")
+  set others (condense_lines (get-ws-info get name where rect.x = 0 | cut -c1 | sort -u | sed "s/$first/_/g"))
   echo $current :: $others
 end
