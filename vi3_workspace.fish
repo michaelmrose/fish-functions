515f4fa1c9d6a6b94648bcc95c771d9d6cc11941
Michael Rose <michael@rosenetwork.net>
2017-07-05 23:06:29 -0700

    editing vi3_workspace.fish

--- vi3_workspace.fish
+++ vi3_workspace.fish
@@ -2,9 +2,15 @@ function vi3_workspace --description 'switch workspace [a-z]'
 	for i in (explode-words $argv)
     set n (getwsstack $i)
     set w {$i}$n
+    set ws (list-workspaces)
     echo changing to $w
     if test $n -ne 1
       echo not branch
+      while not contains $w $ws
+        set n (decrease $n)
+        set w {$i}$n
+        echo w is now $w
+      end
     end
     
     # set ws $i(get-workspace-index $i)
