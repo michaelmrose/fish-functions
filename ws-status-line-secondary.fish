18be6f9b496911d947689799e69c0eff335cfef3
Michael Rose <michael@rosenetwork.net>
2017-07-05 23:37:06 -0700

    editing ws-status-line-secondary.fish

--- ws-status-line-secondary.fish
+++ ws-status-line-secondary.fish
@@ -2,5 +2,5 @@ function ws-status-line-secondary
 	set current (ws-status-line (get-active-workspaces)[1])
   set first (echo $current | cut -c1)
   set others (get-ws-info get name where rect.x = 0 | cut -c1 | sort -u | sed "s/$first//g" | condense_spaces)
-  echo $current :: $others
+  echo "<span color='#22FF22'> $current </span> <span color='white'>::</span> <span color='#22FF22'>$others</span>"
 end
