b82ae38e41cff75ca810eecc47e4f312d7e4aaa8
Michael Rose <michael@rosenetwork.net>
2017-07-05 23:29:42 -0700

    editing ws-status-line-primary.fish

--- ws-status-line-primary.fish
+++ ws-status-line-primary.fish
@@ -3,5 +3,5 @@ function ws-status-line-primary
   set first (echo $current | cut -c1)
   set others (get-ws-info get name where rect.x = 1680 | cut -c1 | sort -u | sed "s/$first//g" | condense_spaces)
   # echo $current :: $others
-  echo "<span color='red'> $current </span> :: $others"
+  echo "$current <span color='white'>::<span> $others"
 end
