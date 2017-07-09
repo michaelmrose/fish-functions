44a40143e48163165fdc07f5e4fe61bcac56a06a
Michael Rose <michael@rosenetwork.net>
2017-07-09 01:27:20 -0700

    editing numenera.fish

---
+++ numenera.fish
@@ -0,0 +1,7 @@
+function numenera
+	xrr single
+  killall compton
+# LANG='en_US.UTF-8' ./TidesOfNumenera
+  set -x LANG en_US.UTF-8
+  med/dl/numenera/Torment.Tides.of.Numenera/TidesOfNumenera
+end
