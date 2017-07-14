3325a088fcc730b06f65f358f1124e0c246cf959
Michael Rose <michael@rosenetwork.net>
2017-07-14 00:20:48 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,15 +1,17 @@
+# Defined in /home/michael/.config/fish/buffer/setvolume.fish @ line 2
 function setvolume
-	set current (ponymix get-volume)
-	if echo $argv | ag  '^\+' > /dev/null
-    set vol (greaterof $current (bounded (wcalc -q {$current}$argv) 0 150))
-   # threadf set vol (wcalc -q {current}$argv) 
-   #         bounded 0 130
-   #         greaterof $current
-  else if echo $argv | ag '^-' > /dev/null
-    set vol (wcalc -q {$current}$argv)
-  else
-    set vol (bounded $argv 0 180)
-  end
+	wcalc -q (ponymix get-volume) $argv
+	# set current (ponymix get-volume)
+	# if echo $argv | ag  '^\+' > /dev/null
+  #   set vol (greaterof $current (bounded (wcalc -q {$current}$argv) 0 150))
+  #  # threadf set vol (wcalc -q {current}$argv) 
+  #  #         bounded 0 130
+  #  #         greaterof $current
+  # else if echo $argv | ag '^-' > /dev/null
+  #   set vol (wcalc -q {$current}$argv)
+  # else
+  #   set vol (bounded $argv 0 180)
+  # end
   ponymix set-volume $vol
   signal-i3blocks output
 end
