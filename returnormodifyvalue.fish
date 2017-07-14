bef564434a1a0ae3bb949f5f33058968ad2c55e8
Michael Rose <michael@rosenetwork.net>
2017-07-14 00:36:59 -0700

    editing returnormodifyvalue.fish

--- returnormodifyvalue.fish
+++ returnormodifyvalue.fish
@@ -1,10 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/setvolume.fish @ line 25
+# Defined in /home/michael/.config/fish/buffer/returnormodifyvalue.fish @ line 2
 function returnormodifyvalue
 	set val $argv[1]
   if echo $argv[2] | ag '^\+|-' > /dev/null
 	  set val (wcalc -q "$vol $argv" | trim)
   else
-    set val $argv
+    set val $argv[2]
   end
   echo $val
 end
