65ea36022ba2e14469b07b89da67ff017442e34a
Michael Rose <michael@rosenetwork.net>
2017-07-14 00:40:22 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,6 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/returnormodifyvalue_setvolume.fish @ line 13
 function setvolume
-	bounded ( returnormodifyvalue (ponymix get-volume) $argv) 0 150
+	ponymix set-volume (bounded ( returnormodifyvalue (ponymix get-volume) $argv) 0 150)
+  signal-i3blocks output
 	# set vol (ponymix get-volume)
   # if echo $argv | ag '^\+|-' > /dev/null
 	#   set vol (bounded (wcalc -q "$vol $argv" | trim) 0 150)
