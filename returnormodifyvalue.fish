cb75f182c2ff2c8141698be08f60975f0bc023be
Michael Rose <michael@rosenetwork.net>
2017-07-14 00:38:44 -0700

    editing returnormodifyvalue.fish

--- returnormodifyvalue.fish
+++ returnormodifyvalue.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/returnormodifyvalue.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/returnormodifyvalue_setvolume.fish @ line 2
 function returnormodifyvalue
 	set val $argv[1]
   if echo $argv[2] | ag '^\+|-' > /dev/null
