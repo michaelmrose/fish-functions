d920a3d8c84393dad0c149d4e1999e11cc2094aa
Michael Rose <michael@rosenetwork.net>
2017-07-14 13:02:01 -0700

    editing setwsstack.fish

--- setwsstack.fish
+++ setwsstack.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/getwsstack_setwsstack.fish @ line 13
+# Defined in /home/michael/.config/fish/buffer/getwsstack_setwsstack.fish @ line 12
 function setwsstack
 	set var wsstack$argv[1]
   set -U $var $argv[2]
