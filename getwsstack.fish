aad1e30f938b9708cc9ca8a29c2d3d0e04063cbe
Michael Rose <michael@rosenetwork.net>
2017-07-14 12:57:40 -0700

    editing getwsstack.fish

--- getwsstack.fish
+++ getwsstack.fish
@@ -1,4 +1,9 @@
+# Defined in /home/michael/.config/fish/buffer/getwsstack_setwsstack.fish @ line 2
 function getwsstack
 	set val wsstack$argv
-  echo $$val
+  if [ $argv = l ]
+    echo 1
+  else
+    echo $$val
+  end
 end
