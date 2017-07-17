9a5f7722dbcef7b0e7f772a6dd6c42a8e4510f78
Michael Rose <michael@rosenetwork.net>
2017-07-17 11:48:45 -0700

    editing numenera.fish

--- numenera.fish
+++ numenera.fish
@@ -1,7 +1,8 @@
+# Defined in /home/michael/.config/fish/buffer/numenera.fish @ line 2
 function numenera
 	xrr single
   killall compton
 # LANG='en_US.UTF-8' ./TidesOfNumenera
   set -x LANG en_US.UTF-8
-  med/dl/numenera/Torment.Tides.of.Numenera/TidesOfNumenera
+  /med/dl/numenera/Torment.Tides.of.Numenera/TidesOfNumenera
 end
