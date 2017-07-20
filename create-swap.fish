72a6365b27b265d04e5e9cb72c48e73f44b83b6f
Michael Rose <michael@rosenetwork.net>
2017-07-20 12:10:33 -0700

    editing create-swap.fish

--- create-swap.fish
+++ create-swap.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/create-swap.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/create-swap.fish @ line 2
 function create-swap
 	sudo zfs create -V 4G -b (getconf PAGESIZE) -o compression=zle \
-      -o logbias=throughput -o sync=always \
-      -o primarycache=metadata -o secondarycache=none \
-      -o com.sun:auto-snapshot=false tank/swap
-# sudo mkswap -f /dev/zvol/tank/swap
+    -o logbias=throughput -o sync=always \
+    -o primarycache=metadata -o secondarycache=none \
+    -o com.sun:auto-snapshot=false tank/swap
+  sudo mkswap -f /dev/zvol/tank/swap
 end
