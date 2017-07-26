af724c08ac1b701609d449e2f17a250486e8a4d0
Michael Rose <michael@rosenetwork.net>
2017-07-26 13:58:20 -0700

    editing pandora.fish

---
+++ pandora.fish
@@ -0,0 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/pandora.fish @ line 1
+function pandora
+	/opt/google/chrome/google-chrome --profile-directory=Default --app-id=bkjpdbfppempdnmnfdfngknppbcffkag
+end
