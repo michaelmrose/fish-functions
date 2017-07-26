00465a81022187f793694358a41bcff2bb47fe2a
Michael Rose <michael@rosenetwork.net>
2017-07-26 11:06:41 -0700

    editing clicky.fish

--- clicky.fish
+++ clicky.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/clicky.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/clicky.fish @ line 2
 function clicky
-	xdotool mousemove 1690 80;xdotool click 1
+	xdotool mousemove 1690 100;xdotool click 1
 end
