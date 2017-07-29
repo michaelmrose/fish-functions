65b12dddbe7c5a561106d16bc7a9cab80135c13c
Michael Rose <michael@rosenetwork.net>
2017-07-29 11:06:15 -0700

    editing get-hd-temp.fish

--- get-hd-temp.fish
+++ get-hd-temp.fish
@@ -1,3 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/get-hd-temp.fish @ line 2
 function get-hd-temp
-	s hddtemp /dev/sda | condense_spaces | cut -d ' ' -f3
+	s hddtemp /dev/sda | condense_spaces | cut -d ' ' -f4
 end
