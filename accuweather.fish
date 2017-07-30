591a82ad2d7970d4a3987a00c1b8ad7d737fcce6
Michael Rose <michael@rosenetwork.net>
2017-07-29 23:49:42 -0700

    editing accuweather.fish

--- accuweather.fish
+++ accuweather.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/accuweather.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/accuweather.fish @ line 2
 function accuweather
 	set url 'https://www.accuweather.com/en/us/bremerton-wa/98337/weather-forecast/331425'
   set file /tmp/weather
-  curl '$url' > $file
+  curl $url > $file
   set temp (cat $file | gr temp_f | cut -d : -f2 | cut -d "'" -f2)
   set realfeel (cat $file | pup 'li[data-href="https://www.accuweather.com/en/us/bremerton-wa/98337/current-weather/331425"] span[class="realfeel"] text{}' | condense_spaces | cut -d ' ' -f2)
   echo t $temp r $realfeel
