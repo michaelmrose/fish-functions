c9f3c144c151b879a575023948b0b6cc56e207dc
Michael Rose <michael@rosenetwork.net>
2017-08-03 11:22:12 -0700

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -1,9 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/set-path.fish @ line 2
 function set-path
 	# set javapaths /usr/lib/jvm/java-8-oracle/bin /usr/lib/jvm/java-8-oracle/db/bin /usr/lib/jvm/java-8-oracle/jre/bin/
 
     set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games
 
-    set mypaths /opt/i3blocks /opt/bin /home/michael/bin /home/michael/.local/bin /home/michael/scripts  /opt/cxoffice/bin ~/.autojump/bin /usr/games/bin /opt/vmware/bin /home/michael/.cargo/bin /usr/share/gopreload/bin ~/ext-projs/go/bin /opt/android-sdk-update-manager/tools/
+    set mypaths /opt/i3blocks /opt/bin /home/michael/bin /home/michael/.local/bin /home/michael/scripts  /opt/cxoffice/bin ~/.autojump/bin /usr/games/bin /opt/vmware/bin /home/michael/.cargo/bin /usr/share/gopreload/bin ~/ext-projs/go/bin /opt/android-sdk-update-manager/tools/ /opt/emercoin/bin
 
     set -xg PATH $mypaths $javapaths $builtinpaths
 end
