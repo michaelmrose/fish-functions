b8b0019552f259a2498f5fd581dd1237a0f2b901
Michael Rose <michael@rosenetwork.net>
2017-08-03 09:32:34 -0700

    editing restore-workspaces.fish

--- restore-workspaces.fish
+++ restore-workspaces.fish
@@ -1,5 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/restore-workspaces_save-worksp.fish @ line 2
 function restore-workspaces
-	for i in (reverse $workspaces)
+	for i in $workspaces
         vi3_workspace $i
     end
+ vi3_workspace $activeWorkspace
 end
