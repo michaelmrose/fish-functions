4388ffda7057387708ae10e6b96913ed3b334626
Michael Rose <michael@rosenetwork.net>
2017-08-03 09:32:34 -0700

    editing save-workspaces.fish

--- save-workspaces.fish
+++ save-workspaces.fish
@@ -1,3 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/restore-workspaces_save-worksp.fish @ line 10
 function save-workspaces
 	set -U workspaces (get-active-workspaces)
+  set -U activeWorkspace (get-focused-workspace)
 end
