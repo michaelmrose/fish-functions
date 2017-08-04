1ddbd60fe64dc85da2294166bae54531d599d68b
Michael Rose <michael@rosenetwork.net>
2017-08-04 11:15:52 -0700

    editing get-ws-info.fish

--- get-ws-info.fish
+++ get-ws-info.fish
@@ -8,5 +8,5 @@ function get-ws-info
         set desired rect.$desired
     end
     # i3-msg -t get_workspaces|jq .[]|jq -r "select(.$var == $val).$desired"
-    i3-msg -t get_workspaces|jq ".[]| select(.$var == $val).$desired"
+    i3-msg -t get_workspaces|jq ".[] | select(.$var == $val).$desired"
 end
