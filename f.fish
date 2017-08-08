fdee571d121d18af9e1ede221630ed6bc621dda1
Michael Rose <michael@rosenetwork.net>
2017-08-08 04:15:40 -0700

    editing f.fish

--- f.fish
+++ f.fish
@@ -1,5 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/f.fish @ line 2
 function f
-	set command cutlastn \; 3- (history)[2]  
-  echo command: "$command" has finished
+	echo command: (history)[1] has finished
 end
