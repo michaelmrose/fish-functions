bc1339cadd911a583979c06c60d2c5926ee6a06f
Michael Rose <michael@rosenetwork.net>
2017-08-08 04:22:09 -0700

    editing f.fish

--- f.fish
+++ f.fish
@@ -1,5 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/f.fish @ line 2
 function f
-	set fuck fuck
-	echo command: (history)[2] has finished
+	eval $argv
+  echo command: $argv has finished
 end
