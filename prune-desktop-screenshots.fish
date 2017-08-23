4b4c4305c2112db601aafd34fc5674da3612f312
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:17:13 -0700

    editing prune-desktop-screenshots.fish

--- prune-desktop-screenshots.fish
+++ prune-desktop-screenshots.fish
@@ -4,7 +4,6 @@ function prune-desktop-screenshots
   cd /tmp/screenshots 
   set screenshots (ls | cut -d . -f1)
   for i in $screenshots
-    echo i is $i s is $screenshots and wslist is $wslist
     if not contains $i $wslist
       rm /tmp/screenshots/$i.png
     end
