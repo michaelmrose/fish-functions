65f7225686b7647f5159c59af41d056ec90b4966
Michael Rose <michael@rosenetwork.net>
2017-08-23 12:51:56 -0700

    editing screenshot.fish

--- screenshot.fish
+++ screenshot.fish
@@ -2,7 +2,7 @@
 function screenshot
 	switch (count $argv)
     case 1
-      set target ~/screenshots/(prefferred-date).png
+      set target ~/screenshots/(preferred-date).png
     case 2
       set target $argv[2]
   end
