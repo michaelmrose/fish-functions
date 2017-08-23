fab043fb2f33ae7c1a7681a9834544091cf9190b
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:21:11 -0700

    editing show-desktops.fish

--- show-desktops.fish
+++ show-desktops.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/screenshot_screenshot-desktop.fish @ line 37
+# Defined in /home/michael/.config/fish/buffer/show-desktops.fish @ line 2
 function show-desktops
-	sxiv -tbfor /tmp/screenshots
+	set choice (cutlast / (sxiv -tbfor /tmp/screenshots)) | cut -d . -f1
+  ws $choice
 end
