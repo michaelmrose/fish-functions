ff9eeb86b52b47d8a281345f71dca1b16c02aa47
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:11:54 -0700

    editing screenshot-desktop.fish

--- screenshot-desktop.fish
+++ screenshot-desktop.fish
@@ -1,4 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/screenshot_screenshot-desktop.fish @ line 21
 function screenshot-desktop
 	set current (get-focused-workspace)
+  maim --opengl -x (get-focused-display-x-offset) -y (get-focused-display-y-offset) -w (get-focused-display-width) -h (get-focused-display-height) /tmp/screenshots/$current
 end
