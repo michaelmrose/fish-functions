8d94686147cb9513064af4f8eade5d173eb2247c
Michael Rose <michael@rosenetwork.net>
2017-08-23 12:51:30 -0700

    editing screenshot.fish

--- screenshot.fish
+++ screenshot.fish
@@ -1,17 +1,18 @@
+# Defined in /home/michael/.config/fish/buffer/screenshot.fish @ line 2
 function screenshot
 	switch (count $argv)
-        case 1
-            set target ~/screenshots/(mydate).png
-        case 2
-            set target $argv[2]
-    end
-    switch $argv[1]
-        case "everything"
-            maim $target
-        case "window"
-            maim -x (get-window-x-pos) -y (get-window-y-pos) -w (get-window-width) -h (get-window-height) $target
-        case "display"
-            maim -x (get-focused-display-x-offset) -y (get-focused-display-y-offset) -w (get-focused-display-width) -h (get-focused-display-height) $target
-   end
-   echo $target
+    case 1
+      set target ~/screenshots/(prefferred-date).png
+    case 2
+      set target $argv[2]
+  end
+  switch $argv[1]
+    case "everything"
+      maim $target
+    case "window"
+      maim -x (get-window-x-pos) -y (get-window-y-pos) -w (get-window-width) -h (get-window-height) $target
+    case "display"
+      maim -x (get-focused-display-x-offset) -y (get-focused-display-y-offset) -w (get-focused-display-width) -h (get-focused-display-height) $target
+  end
+  echo $target
 end
