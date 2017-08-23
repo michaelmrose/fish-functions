61da2bb5d02560d65d66cd4eceb00b142d4e3c3a
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:23:13 -0700

    editing show-desktops.fish

--- show-desktops.fish
+++ show-desktops.fish
@@ -1,5 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/show-desktops.fish @ line 2
 function show-desktops
 	set choice (echo (cutlast / (sxiv -tbfor /tmp/screenshots)) | cut -d . -f1)
-  echo ws $choice
+  ws $choice
 end
