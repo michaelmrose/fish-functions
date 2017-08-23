d29c3c611862968802c129dabeb253bd680a3684
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:03:48 -0700

    editing screenshot-desktop.fish

--- screenshot-desktop.fish
+++ screenshot-desktop.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/screenshot-desktop.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/screenshot_screenshot-desktop.fish @ line 21
 function screenshot-desktop
 	set current (get-focused-workspace)
 end
