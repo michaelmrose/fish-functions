cc7a6b42e4ebcca9a5061f3fec6ce7c908c1607b
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:45:25 -0700

    editing show-desktops.fish

--- show-desktops.fish
+++ show-desktops.fish
@@ -2,8 +2,10 @@
 function show-desktops
 	screenshot-desktop
 	prune-desktop-screenshots
-	set choice (echo (cutlast / (sxiv -tbfor /tmp/screenshots)) | cut -d . -f1)
+	# set choice (echo (cutlast / (sxiv -tbfor /tmp/screenshots)) | cut -d . -f1)
+  # ws $choice
+  set choices (sxiv -tbfor /tmp/screenshots | cut -d . -f1)
   for c in $choice
-    ws $c
+    ws (custlast / $c)
   end
 end
