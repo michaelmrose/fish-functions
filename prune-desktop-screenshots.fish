f2d6b6bad961e64ae02fa36dc7a6ad8dd52a73bb
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:16:35 -0700

    editing prune-desktop-screenshots.fish

--- prune-desktop-screenshots.fish
+++ prune-desktop-screenshots.fish
@@ -2,11 +2,11 @@
 function prune-desktop-screenshots
 	set wslist (list-workspaces)
   cd /tmp/screenshots 
-  set screenshots (ls)
+  set screenshots (ls | cut -d . -f1)
   for i in $screenshots
     echo i is $i s is $screenshots and wslist is $wslist
-    # if not contains $i $wslist
-    #   rm /tmp/screenshots/$i
-    # end
+    if not contains $i $wslist
+      rm /tmp/screenshots/$i.png
+    end
   end
 end
