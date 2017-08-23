46e90bdf0868e7edbe3a3725010b518384ffb7d2
Michael Rose <michael@rosenetwork.net>
2017-08-23 13:02:08 -0700

    editing get-focused-display-y-offset.fish

--- get-focused-display-y-offset.fish
+++ get-focused-display-y-offset.fish
@@ -1,3 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/get-focused-display-y-offset.fish @ line 2
 function get-focused-display-y-offset
-	get-ws-info get rect.y where output = (quote (get-focused-display)) | sort -u
+	# get-ws-info get rect.y where output = (quote (get-focused-display)) | sort -u
+  echo 0
 end
