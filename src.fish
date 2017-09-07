4b3512d22739faf88745735158053376c1a738ea
Michael Rose <michael@rosenetwork.net>
2017-09-07 14:34:38 -0700

    editing src.fish

--- src.fish
+++ src.fish
@@ -1,7 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/src.fish @ line 2
 function src
 	set vals (without-options $argv)
-  p $vals
   for v in $vals
     switch (short-type $v)
       case file
