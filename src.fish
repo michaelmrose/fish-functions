981998ddcf8105bdef08c43f1608fafc46c19667
Michael Rose <michael@rosenetwork.net>
2017-09-07 14:09:23 -0700

    editing src.fish

--- src.fish
+++ src.fish
@@ -1,8 +1,13 @@
+# Defined in /home/michael/.config/fish/buffer/src.fish @ line 2
 function src
-	switch (short-type $argv)
-        case file
-            cat (which $argv) | pygmentize
-        case function
-            functions $argv
+	switch (short-type $argv[1])
+    case file
+      if test (count $argv) -gt 1
+        emc (which $argv[1])
+      else
+        cat (which $argv[1]) | pygmentize
+      end
+    case function
+      functions $argv[1]
     end
 end
