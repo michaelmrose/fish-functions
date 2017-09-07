89a93ec0c6e8139aa791b03cec1466a1da84cd7d
Michael Rose <michael@rosenetwork.net>
2017-09-07 14:29:48 -0700

    editing src.fish

--- src.fish
+++ src.fish
@@ -1,14 +1,15 @@
-# Defined in /home/michael/.config/fish/buffer/src.fish @ line 14
+# Defined in /home/michael/.config/fish/buffer/src.fish @ line 2
 function src
 	set vals (without-options $argv)
-  for v in $vals
-    switch (short-type $v)
-      case file
-        set files $files $v
-      case function
-        set fns $fns $v
-      end
-    end
-    p $fns
-    f $files
+  p $vals
+  # for v in $vals
+  #   switch (short-type $v)
+  #     case file
+  #       set files $files $v
+  #     case function
+  #       set fns $fns $v
+  #     end
+  #   end
+  #   p $fns
+  #   f $files
 end
