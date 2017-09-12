38da369b2846ef4ccf854c81900ac41350d29fd3
Michael Rose <michael@rosenetwork.net>
2017-09-12 00:49:15 -0700

    editing ws-status-line-secondary.fish

--- ws-status-line-secondary.fish
+++ ws-status-line-secondary.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/ws-status-line-primary_ws-stat.fish @ line 11
 function ws-status-line-secondary
-	set current (ws-status-line (get-active-workspaces)[1])
+	set current (ws-status-line (get-active-workspaces)[2])
   set first (echo $current | cut -c1)
   set others (get-ws-info get name where rect.x = 0 | cut -c1 | sort -u | sed "s/$first//g" | condense_spaces)
   echo "<span color='#22FF22'> $current </span> <span color='white'>::</span> <span color='#22FF22'>$others</span>"
