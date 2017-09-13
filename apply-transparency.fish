e6ccffd58e0d30eb94c8a98fbc2fea6dbc1cec03
Michael Rose <michael@rosenetwork.net>
2017-09-12 20:33:34 -0700

    editing apply-transparency.fish

--- apply-transparency.fish
+++ apply-transparency.fish
@@ -1,8 +1,9 @@
+# Defined in /home/michael/.config/fish/buffer/apply-transparency.fish @ line 2
 function apply-transparency
 	for i in (list-windows id)
-        transset -i $i (return-trans-value (return-winclass $i)) > /dev/null
-    end
-    for i in (get-i3bar-ids)
-        transset -i $i 0.9 > /dev/null
+    # transset -i $i (return-trans-value (return-winclass $i)) > /dev/null
+  end
+  for i in (get-i3bar-ids)
+    transset -i $i 0.7 > /dev/null
     end
 end
