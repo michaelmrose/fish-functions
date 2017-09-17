e09994e4cd35a47c9f71f52998616033d58cac36
Michael Rose <michael@rosenetwork.net>
2017-09-17 11:56:46 -0700

    removed apply-to-head.fish

--- apply-to-head.fish
+++
@@ -1,7 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/apply-to-head.fish @ line 1
-function apply-to-head
-	while read -l line
-    set collection $collection $line
-  end
-  eval $argv $collection
-end
