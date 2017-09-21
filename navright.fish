69b497b2591f536dc49326db09bd4e9f46facae9
Michael Rose <michael@rosenetwork.net>
2017-09-20 21:03:51 -0700

    editing navright.fish

--- navright.fish
+++ navright.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/rightmostwindow_smarter-nav.fish @ line 34
+# Defined in /home/michael/.config/fish/buffer/navright_rightmostwindow_smart.fish @ line 2
 function navright
-	if test (math (window-right-corner) - 1800) -lt 100
+	if test (math 1800 - (window-right-corner)) -lt 100
     i3 focus output right
   else
         i3 focus right
