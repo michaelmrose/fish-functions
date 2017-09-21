2f6d2af41a4da14a8117e129dcaedbc88251eae1
Michael Rose <michael@rosenetwork.net>
2017-09-20 21:03:52 -0700

    editing window-right-corner.fish

--- window-right-corner.fish
+++ window-right-corner.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/window-right-corner.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/navright_rightmostwindow_smart.fish @ line 44
 function window-right-corner
 	xwininfo -id (xdotool getactivewindow) | gr 'absolute upper-left x' | nth 4 | subtract (get-focused-display-x-offset) | addto (window-width)
 end
