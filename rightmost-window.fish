dc8c21069fdbd6898673e51b26a88c6b42d4dd94
Michael Rose <michael@rosenetwork.net>
2017-09-20 18:58:42 -0700

    editing rightmost-window.fish

--- rightmost-window.fish
+++ rightmost-window.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/rightmost-window.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/rightmost-window.fish @ line 2
 function rightmost-window
 	set w (get-focused-display-width)
-  set c (widnow-right-corner)
+  set c (window-right-corner)
   test (math w - c) -lt 100
 end
