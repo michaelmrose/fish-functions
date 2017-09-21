eb1a06fcfb1194c47d15f6e89c3d3476200950a0
Michael Rose <michael@rosenetwork.net>
2017-09-20 19:21:55 -0700

    editing rightmostwindow.fish

--- rightmostwindow.fish
+++ rightmostwindow.fish
@@ -1,5 +1,13 @@
+# Defined in /home/michael/.config/fish/buffer/rightmostwindow.fish @ line 2
 function rightmostwindow
-	set w (get-focused-display-width)
+	# set w (get-focused-display-width)
+  switch (get-focused-display)
+    case HDMI-0
+      set w 1920
+    case DVI-I-2
+      set w 1680
+  end
+
   set c (window-right-corner)
   set diff (math $w - $c)
   test $diff -lt 100
