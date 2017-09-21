0c66ebdb53ad0193681aee71daa5ff8adfcc5dbd
Michael Rose <michael@rosenetwork.net>
2017-09-21 08:40:10 -0700

    editing navright.fish

--- navright.fish
+++ navright.fish
@@ -9,6 +9,7 @@ function navright
       set w 1680
       set x 0
   end
+  echo $wininfo
   set c (echo $wininfo | grep 'Absolute upper-left X' | nth 4 | addto (window-width) |subtract $x)
   if test (math $w - $c) -lt 100
     i3 focus output right
