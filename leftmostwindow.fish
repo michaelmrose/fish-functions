97d10a70e8bda513e6e69841070d84b33fd03d39
Michael Rose <michael@rosenetwork.net>
2017-09-21 14:14:06 -0700

    editing leftmostwindow.fish

--- leftmostwindow.fish
+++ leftmostwindow.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/leftmostwindow.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/leftmostwindow_navleft_navrigh.fish @ line 2
 function leftmostwindow
 	test (xwininfo -id (xdotool getactivewindow) | gr 'absolute upper-left x' | nth 4 | subtract (get-focused-display-x-offset)) -lt 100
 end
