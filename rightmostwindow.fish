04d09410fd969c23b11867487e14ba2624627f2d
Michael Rose <michael@rosenetwork.net>
2017-09-20 21:03:51 -0700

    editing rightmostwindow.fish

--- rightmostwindow.fish
+++ rightmostwindow.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/rightmostwindow_smarter-nav.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/navright_rightmostwindow_smart.fish @ line 11
 function rightmostwindow
 	# set w (get-focused-display-width)
   switch (get-focused-display)
