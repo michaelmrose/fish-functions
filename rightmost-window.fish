3367b4fa1084f1efeddceb8cc42210566d3c8881
Michael Rose <michael@rosenetwork.net>
2017-09-20 19:10:00 -0700

    removed rightmost-window.fish

--- rightmost-window.fish
+++
@@ -1,7 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/rightmost-window.fish @ line 2
-function rightmost-window
-	set w (get-focused-display-width)
-  set c (window-right-corner)
-  set diff (math $w - $c)
-  test $diff -lt 100
-end
