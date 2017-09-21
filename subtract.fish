860d15564ca63c11e0938ddd4281a9f3be00d0c7
Michael Rose <michael@rosenetwork.net>
2017-09-20 18:42:38 -0700

    editing subtract.fish

--- subtract.fish
+++ subtract.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/subtract.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/addto_subtract.fish @ line 8
 function subtract
 	while read -l line
     math $line - $argv
