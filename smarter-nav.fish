7bbb2f7065e306d3b9e36d01d75e65b48bb6b007
Michael Rose <michael@rosenetwork.net>
2017-09-21 08:23:03 -0700

    editing smarter-nav.fish

--- smarter-nav.fish
+++ smarter-nav.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/navright_rightmostwindow_smart.fish @ line 26
+# Defined in /home/michael/.config/fish/buffer/navright_rightmostwindow_smart.fish @ line 35
 function smarter-nav
 	switch $argv
     case left
