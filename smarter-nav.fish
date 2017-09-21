7d2a7c40479d78217a500ae3955aa751d9fadb5e
Michael Rose <michael@rosenetwork.net>
2017-09-20 19:10:08 -0700

    editing smarter-nav.fish

--- smarter-nav.fish
+++ smarter-nav.fish
@@ -1,14 +1,14 @@
-# Defined in /home/michael/.config/fish/buffer/smarter-nav.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/smarter-nav.fish @ line 2
 function smarter-nav
 	switch $argv
     case left
-      if leftmost-window
+      if leftmostwindow
         i3 focus output left
       else
         i3 focus left
       end
     case right
-      if rightmost-window
+      if rightmostwindow
         i3 focus output right
       else
         i3 focus right
