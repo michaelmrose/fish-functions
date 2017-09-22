3a5187cd061933d23c926869de46f4732b7eed80
Michael Rose <michael@rosenetwork.net>
2017-09-21 20:43:06 -0700

    editing display-number.fish

--- display-number.fish
+++ display-number.fish
@@ -1,8 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/display-number.fish @ line 2
 function display-number
 	set d (get-connected-displays)
 	set n (math (findindex (get-focused-display) $d) -1)
   if test (count $d) -gt 1
-    inverse-of $n
+    # inverse-of $n
+    echo $n
   else
     echo $n
   end
