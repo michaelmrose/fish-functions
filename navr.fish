09b6db49356b4f3ef754372fe6707e8ea8be4b3d
Michael Rose <michael@rosenetwork.net>
2017-09-29 00:28:51 -0700

    editing navr.fish

--- navr.fish
+++ navr.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/navr.fish @ line 2
 function navr
 	if [ (get-focused-display) = HDMI-0 ]
-    xdotool --clearmodifiers key F12
+    xdotool key --clearmodifiers  F12
   else
     i3 focus right
   end
