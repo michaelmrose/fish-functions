2ddaa85eb810f9f39b9e904eb9ae958af6989179
Michael Rose <michael@rosenetwork.net>
2017-09-28 23:59:42 -0700

    editing synergyconnected.fish

--- synergyconnected.fish
+++ synergyconnected.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/synergyconnected.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/synergyconnected.fish @ line 2
 function synergyconnected
-	echo (grep connected /tmp/synerglog.txt)[-1] | grep --invert-match disconnected
+	echo (grep connected /tmp/synerglog.txt)[-1] | grep --invert-match disconnected > /dev/null
 end
