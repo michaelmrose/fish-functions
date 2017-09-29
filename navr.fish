093944aff7947ca92444e7bbc40a01e1b41594d5
Michael Rose <michael@rosenetwork.net>
2017-09-29 00:24:10 -0700

    editing navr.fish

--- navr.fish
+++ navr.fish
@@ -1,12 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/navr.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/navr.fish @ line 2
 function navr
 	set d (get-focused-display)
   if [ $d = HDMI-0 ]
-    if synergyconnected
-      xdotool key F12
-    else
-      i3 focus right
-    end
+    xdotool key F12
   else
     i3 focus right
   end
