9af98cf4915ba0f80650ce9aa0ad31396aa276c0
Michael Rose <michael@rosenetwork.net>
2017-10-06 02:01:37 -0700

    editing ws-status-line-primary.fish

--- ws-status-line-primary.fish
+++ ws-status-line-primary.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/ws-status-line-primary_ws-stat.fish @ line 2
 function ws-status-line-primary
-	set ws (get-active-workspaces | grep -E '^j|^k|^l|^n|^m|^i|^p|^6|^7|^8|^9|^0')
+	set ws (get-active-workspaces | grep -E '^h|^j|^k|^l|^n|^m|^i|^p|^6|^7|^8|^9|^0')
   set current (ws-status-line $ws)
 	# set current (ws-status-line (get-active-workspaces)[2])
   set first (echo $current | cut -c1)
