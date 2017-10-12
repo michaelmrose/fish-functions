cbd5b1a5fbab85ff488cf1e9ced908b6ba5b0449
Michael Rose <michael@rosenetwork.net>
2017-10-11 20:30:41 -0700

    editing write-iso.fish

--- write-iso.fish
+++ write-iso.fish
@@ -26,6 +26,6 @@ function write-iso
     return 1
   end
   if read_confirm
-    sudo dd if=$file | pv -s $size | sudo dd of=$disk bs=1M status=progress;and sync
+    sudo dd if=$file | pv | sudo dd of=$disk bs=1M status=progress;and sync
   end
 end
