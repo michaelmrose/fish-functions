dd5c5599b62da74a4429a2724f9c95b895f1529d
Michael Rose <michael@rosenetwork.net>
2017-10-18 23:58:49 -0700

    editing dockctl.fish

--- dockctl.fish
+++ dockctl.fish
@@ -1,12 +1,15 @@
-# Defined in /home/michael/.config/fish/buffer/dockctl.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/dockctl.fish @ line 2
 function dockctl
 	switch $argv
     case dock
+      sudo ip link set dev wlp2s0 down
+      sleep 1
       sudo mount /mnt/michael
       xrandr --output DP1 --auto --output eDP1 --auto --right-of DP1 --set "scaling mode" "Full aspect" --scale 0.75x0.75
 
     case undock
       sudo umount /mnt/michael
+      sudo ip link set dev wlp2s0 up
       xrandr --output DP1 --off --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75
   end
   setup-keyboard
