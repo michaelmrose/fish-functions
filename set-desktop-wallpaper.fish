3e321d8c95a286ef3a7a8b95157a7f15ec989cb3
Michael Rose <michael@rosenetwork.net>
2017-10-19 21:32:40 -0700

    editing set-desktop-wallpaper.fish

--- set-desktop-wallpaper.fish
+++ set-desktop-wallpaper.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/set-desktop-wallpaper.fish @ line 2
 function set-desktop-wallpaper
-	scp $bgimage laptop:/tmp/laptopwallpaper
+	scp $bgimage desktop:/tmp/laptopwallpaper
   set -x DISPLAY :0
   ssh desktop wallpaper /tmp/laptopwallpaper
 end
