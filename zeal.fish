b54903fa3f296e0a31f7370783642d57607c369d
Michael Rose <michael@rosenetwork.net>
2017-10-19 17:26:06 -0700

    editing zeal.fish

--- zeal.fish
+++ zeal.fish
@@ -1,5 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/zeal.fish @ line 2
 function zeal
 	if not pgrep zeal
-    /usr/bin/zeal -style adwaita (vals 1..-1 $argv) &
+    zeal -style adwaita (vals 1..-1 $argv) &
   end
 end
