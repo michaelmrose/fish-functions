517359b2fd0073b985ccd4e663d7e7e26b613c90
Michael Rose <michael@rosenetwork.net>
2017-10-19 21:37:35 -0700

    editing get-display-order.fish

--- get-display-order.fish
+++ get-display-order.fish
@@ -1,6 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/get-display-order.fish @ line 2
 function get-display-order
 	set outputs (xrandr | grep " connected" | cut -d " " -f1)
-  set ordered (reverse (get-display-left-to-right))
+  set ordered (get-display-left-to-right)
     for i in $outputs
         math (findindex $i $ordered) - 1
     end
