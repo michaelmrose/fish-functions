e40abfc526f142f30bceaba0ee04c77a4448385c
Michael Rose <michael@rosenetwork.net>
2017-10-24 14:40:39 -0700

    editing batterylevel.fish

--- batterylevel.fish
+++ batterylevel.fish
@@ -1,5 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/batterylevel.fish @ line 2
 function batterylevel
 	set battery (upower -i (upower -e | grep 'BAT') | grep -E "state|to\ full|percentage" | condense_lines | condense_spaces | cut -d ' ' -f2-)
-  echo 🔋 $battery
+  set symbol 
+  echo $symbol $battery
 end
