9d76f6a2a0e2bd4e4a77ac7c59ed0cf29b5ec611
Michael Rose <michael@rosenetwork.net>
2017-10-23 17:37:33 -0700

    editing pac.fish

--- pac.fish
+++ pac.fish
@@ -1,3 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/pac_paci.fish @ line 2
 function pac
-	sudo PULSE_RUNTIME_PATH=/var/run/pulse -u pulse pacmd $argv
+	# sudo PULSE_RUNTIME_PATH=/var/run/pulse -u pulse pacmd $argv
+  pacaur --noconfirm --noedit $argv
 end
