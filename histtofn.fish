6ae7ac83c0aa2203b31513b719c1ebf2ed406c8e
Michael Rose <michael@rosenetwork.net>
2017-10-23 17:50:26 -0700

    editing histtofn.fish

--- histtofn.fish
+++ histtofn.fish
@@ -1,6 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/histtofn.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/histtofn.fish @ line 2
 function histtofn
-	echo $history[2] > /tmp/histfunc.fish
+	echo fn $argv > /tmp/histfunc.fish
+	echo $history[1] > /tmp/histfunc.fish
   emc /tmp/histfunc.fish
   funcsave $argv
 end
