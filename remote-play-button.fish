374cc3489f95d8955f7a8418f77f87cfd5d30fbc
Michael Rose <michael@rosenetwork.net>
2017-10-25 19:25:49 -0700

    editing remote-play-button.fish

--- remote-play-button.fish
+++ remote-play-button.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/active-playerctl_remote-play-b.fish @ line 20
+# Defined in /home/michael/.config/fish/buffer/active-playerctl_remote-play-b.fish @ line 21
 function remote-play-button
 	ssh desktop "set -x DISPLAY :0; and xdotool key XF86AudioPlay"
 end
