b2a4bdb6dfb7fe75965b0704d14f90ec3a540dcf
Michael Rose <michael@rosenetwork.net>
2017-11-01 15:55:12 -0700

    editing switch-to.fish

--- switch-to.fish
+++ switch-to.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/switch-to.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/switch-to.fish @ line 2
 function switch-to
 	switch $argv
     case laptop
       ssh laptop "set -x DISPLAY :0; xdotool key F12"
     case desktop
-      xdotool key F12
+      xdotool key F11
   end
 end
