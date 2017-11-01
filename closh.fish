0d2a86fe5dfa035290629fcfa22e105b50985548
Michael Rose <michael@rosenetwork.net>
2017-11-01 13:08:40 -0700

    editing closh.fish

--- closh.fish
+++ closh.fish
@@ -1,4 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/closh.fish @ line 2
 function closh
+	set dir (pwd)
+	cd /home/michael/extproj/lumoclosh/node_modules/closh/bin/
 	/home/michael/extproj/lumoclosh/node_modules/closh/bin/closh.js
+  cd $dir
 end
