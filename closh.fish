4cb0e3b76ea8f0f5732b7e1536a6a10c525dfe9d
Michael Rose <michael@rosenetwork.net>
2017-11-01 13:07:40 -0700

    editing closh.fish

--- closh.fish
+++ closh.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/closh.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/closh.fish @ line 2
 function closh
-	/home/michael/extproj/lumoclosh/node_modules/bin/closh.js
+	/home/michael/extproj/lumoclosh/node_modules/closh/bin/closh.js
 end
