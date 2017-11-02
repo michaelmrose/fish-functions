775e9019df1268e057984f0812e4dcb237bed911
Michael Rose <michael@rosenetwork.net>
2017-11-01 21:54:13 -0700

    removed set-desktop-wallpaper.fish

--- set-desktop-wallpaper.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/set-desktop-wallpaper.fish @ line 2
-function set-desktop-wallpaper
-	scp $bgimage desktop:/tmp/laptopwallpaper
-  set -x DISPLAY :0
-  ssh desktop wallpaper /tmp/laptopwallpaper
-end
