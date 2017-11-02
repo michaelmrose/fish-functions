d2416d48e24f86d5d61e41f18d61387430fa1a8d
Michael Rose <michael@rosenetwork.net>
2017-11-01 22:09:17 -0700

    editing fix-wallpaper-path-list.fish

--- fix-wallpaper-path-list.fish
+++ fix-wallpaper-path-list.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/fix-wallpaper-path-list.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/fix-wallpaper-path-list.fish @ line 2
 function fix-wallpaper-path-list
 	set -U recent_backgrounds (p $recent_backgrounds | sed 's%/mnt/michael%%g')
+  set -U bgimage (echo $bgimage | sed 's%/mnt/michael%%g')
 end
