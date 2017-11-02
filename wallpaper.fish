91bcd7b6085c483904c809318cf21725ad870533
Michael Rose <michael@rosenetwork.net>
2017-11-01 21:55:34 -0700

    editing wallpaper.fish

--- wallpaper.fish
+++ wallpaper.fish
@@ -200,9 +200,9 @@ function wallpaper
             set format fill
     end
     feh --bg-{$format} $img
-    # if [ (hostname) = michael-pc ]
-    #   ssh desktop "set -x DISPLAY :0; and wp $img"
-    # end
+    if [ (hostname) = michael-pc ]
+      sync-desktop-wallpaper
+    end
     
     convert $bgimage /tmp/bgimage.png
     if pgrep i3blocks > /dev/null
