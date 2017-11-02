a60b8819b4988e75ed80b5c68ff1ebf056c12667
Michael Rose <michael@rosenetwork.net>
2017-11-02 03:11:41 -0700

    editing lockall.fish

--- lockall.fish
+++ lockall.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/lockall.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/lockall.fish @ line 2
 function lockall
 	d i3lock
-  i3lock
+  i3lock -n
   d killall i3lock
 end
