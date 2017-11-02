d1024770b218e6cc94a30d3f375c524d0eb30509
Michael Rose <michael@rosenetwork.net>
2017-11-02 13:51:08 -0700

    editing desktop-available.fish

--- desktop-available.fish
+++ desktop-available.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/desktop-available.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/desktop-available.fish @ line 2
 function desktop-available
-	timeout 0.01 ping 192.168.1.2 -c 1 -W 1 -s 1
+	timeout 0.01 ping 192.168.1.2 -c 1 -W 1 -s 1 > /dev/null
 end
