2d0363e0871faef267caedf97645f3552de05d68
Michael Rose <michael@rosenetwork.net>
2017-11-01 21:53:17 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,7 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/fish_prompt.fish @ line 2
 function fish_prompt
-	if not timeout 0.5 /opt/bin/powerlineshell.py $status --shell bare ^/dev/null
-    echo (pwd) \$: 
-  end
+	# if not timeout 0.5 /opt/bin/powerlineshell.py $status --shell bare ^/dev/null
+  #   echo (pwd) \$: 
+  # end
   # echo $USER:\$
+	/opt/bin/powerlineshell.py $status --shell bare ^/dev/null
 end
