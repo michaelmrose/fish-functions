b63f4a2bc32d5c5b2c3bff3c120403a1284964fe
Michael Rose <michael@rosenetwork.net>
2017-11-02 11:32:20 -0700

    editing mmlock.fish

--- mmlock.fish
+++ mmlock.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/mmlock.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/mmlock.fish @ line 2
 function mmlock
-	/home/michael/extproj/i3lock-multimonitor/lock
+	/home/michael/extproj/i3lock-multimonitor/lock lock
 end
