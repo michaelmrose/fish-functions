0114037f3cf33fd0dbbc348595254976eae24529
Michael Rose <michael@rosenetwork.net>
2017-11-02 20:26:43 -0700

    editing output-status-line.fish

--- output-status-line.fish
+++ output-status-line.fish
@@ -1,11 +1,14 @@
+# Defined in /home/michael/.config/fish/buffer/output-status-line.fish @ line 2
 function output-status-line
 	switch (output-type)
         case headphones
             set symbol 
         case speakers
             set symbol 
-        case "*"
-            set symbol 
+    case dock
+      set symbol 
+    case "*"
+      set symbol 
     end
     echo $symbol (getvolume)
 end
