29cf6a1e0a956773f98d77f3745fcde00abe424a
Michael Rose <michael@rosenetwork.net>
2017-11-02 21:34:46 -0700

    editing vi3_get-workspace.fish

--- vi3_get-workspace.fish
+++ vi3_get-workspace.fish
@@ -1,9 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/vi3_get-workspace.fish @ line 2
 function vi3_get-workspace
 	er vi3op
     update-op-status
     set ws (get-focused-workspace)
     checkpoint-ws
-    vi3_workspace $argv
+    ws $argv
     vi3_select-all-in-workspace
     vi3_take-back
     restore-ws
