42daa46d00f040c99b219ea381a17ac13c94e9ba
Michael Rose <michael@rosenetwork.net>
2017-11-02 20:32:50 -0700

    editing output-type.fish

--- output-type.fish
+++ output-type.fish
@@ -1,7 +1,11 @@
+# Defined in /home/michael/.config/fish/buffer/output-status-line_output-type.fish @ line 17
 function output-type
-	if ponymix -t sink defaults --short | head -1 | grep Headset > /dev/null
-      echo headphones
-    else
-      echo speakers
-    end
+	set op (ponymix -t sink defaults --short | head -1)
+	if echo $op | grep Headset > /dev/null
+    echo headphones
+  else if echo $op | grep Built-in
+    echo builtin
+  else
+    echo speakers
+  end
 end
