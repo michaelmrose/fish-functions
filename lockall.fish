47db2d0ddf8b498ab00bff39e3034a96af04b4eb
Michael Rose <michael@rosenetwork.net>
2017-11-18 04:58:31 -0800

    editing lockall.fish

--- lockall.fish
+++ lockall.fish
@@ -3,7 +3,6 @@ function lockall
 	set bg /tmp/bgimage.png
 	# ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
 ssh desktop i3lock -i /tmp/bgimage.png &
-  sleep 0.35
   mmlock
   d killall i3lock
 end
