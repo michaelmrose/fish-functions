33906b8690b9da6243138429d5672578835a5432
Michael Rose <michael@rosenetwork.net>
2017-11-27 18:01:28 -0800

    editing sus.fish

--- sus.fish
+++ sus.fish
@@ -1,16 +1,18 @@
+# Defined in /home/michael/.config/fish/buffer/sus.fish @ line 2
 function sus
-	manage-share ensure-unmounted
-  sudo pm-suspend
-  wp recall
-  if [ (whoami) = michael ]
-    setup-keyboard
-  end
+	# manage-share ensure-unmounted
+  # sudo pm-suspend
+  # wp recall
+  # if [ (whoami) = michael ]
+  #   setup-keyboard
+  # end
   
-  if exists $argv
-    switch $argv[1]
-      case greet
-        greeter
-    end
-  end
-  manage-share ensure-mounted
+  # if exists $argv
+  #   switch $argv[1]
+  #     case greet
+  #       greeter
+  #   end
+  # end
+  # manage-share ensure-mounted
+  systemctl suspend;sleep 5;setup-keyboard
 end
