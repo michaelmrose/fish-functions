b2f7ef75cf47f92e95792ddab3e0066d33e444b7
Michael Rose <michael@rosenetwork.net>
2017-12-06 17:50:07 -0800

    editing nextnumber.fish

--- nextnumber.fish
+++ nextnumber.fish
@@ -1,6 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/nextnumber.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/nextnumber.fish @ line 2
 function nextnumber
 	for n in (seq 99)
-    echo $n
+    if not contains $n $argv
+      echo n
+    end
   end
 end
