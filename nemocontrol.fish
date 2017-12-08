4c68358bb9907934b9a971af99bd7d6cd8e3fc01
Michael Rose <michael@rosenetwork.net>
2017-12-07 22:04:02 -0800

    editing nemocontrol.fish

--- nemocontrol.fish
+++ nemocontrol.fish
@@ -1,6 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/nemocontrol.fish @ line 2
 function nemocontrol
-	sleep 1
-	xdotool key Control+L
+	# xdotool key Control+L
   xdotool type fuuuuuck
 end
