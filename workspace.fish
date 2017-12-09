996769d53bb7bd3df27c4f9b4ec45fe76200ddaf
Michael Rose <michael@rosenetwork.net>
2017-12-08 17:43:22 -0800

    editing workspace.fish

--- workspace.fish
+++ workspace.fish
@@ -1,24 +1,25 @@
 # Defined in /home/michael/.config/fish/buffer/workspace.fish @ line 2
 function workspace
-	set hostname (hostname)
-  set desktopScreens a s d z x c 1 2 3
-  set laptopScreens f g h j k l v b n m 4 5 6 7 8 9 0
-  set -x DISPLAY :0
-  if [ (hostname) = michael-desktop ]
-    set localscreens $desktopScreens
-    set remotehost laptop
-  else
-    set localscreens $laptopScreens
-    set remotehost desktop
-  end
-	for i in (explode-words $argv)
-    if contains $i $localscreens
-      i3-msg workspace $i
-    else
-      echo switching machines and ws
-      ssh $remotehost "set -x DISPLAY :0; and i3-msg workspace $i"
-      switch-machines
-    end
-  end
+	# set hostname (hostname)
+  # set desktopScreens a s d z x c 1 2 3
+  # set laptopScreens f g h j k l v b n m 4 5 6 7 8 9 0
+  # set -x DISPLAY :0
+  # if [ (hostname) = michael-desktop ]
+  #   set localscreens $desktopScreens
+  #   set remotehost laptop
+  # else
+  #   set localscreens $laptopScreens
+  #   set remotehost desktop
+  # end
+	# for i in (explode-words $argv)
+  #   if contains $i $localscreens
+  #     i3-msg workspace $i
+  #   else
+  #     echo switching machines and ws
+  #     ssh $remotehost "set -x DISPLAY :0; and i3-msg workspace $i"
+  #     switch-machines
+  #   end
+  # end
   # signal-i3blocks pages
+  i3-msg workspace $argv
 end
