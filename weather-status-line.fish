7579999196f4ab4bcf4c72f8bb7340aa8b080478
Michael Rose <michael@rosenetwork.net>
2018-01-02 23:08:46 -0800

    editing weather-status-line.fish

--- weather-status-line.fish
+++ weather-status-line.fish
@@ -1,6 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/weather-status-line.fish @ line 2
 function weather-status-line
-	
-    # set weather (weather $geo)
+	# set weather (weather $geo)
     # echo (weather-icon) (echo $weather | cut -d " " -f2 | trim) (echo $weather | cut -d " " -f4-)
-    weather.fish bremerton
+  set current (weather.py 2368481 --format "{text} {temp}°{unit_temperature} {wind_direction_arrow} {wind_speed} mph")
+  echo $current (realfeel)°F
 end
