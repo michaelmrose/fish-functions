dcd59cb11dd671ad54fef3a59dbdcdca2631c284
Michael Rose <michael@rosenetwork.net>
2018-01-02 17:31:45 -0800

    editing docked.fish

--- docked.fish
+++ docked.fish
@@ -1,4 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/docked.fish @ line 2
 function docked
-	test (count (get-connected-displays)) -gt 1
+	# test (count (get-connected-displays)) -gt 1
+  list-outputs | gr dock
 end
