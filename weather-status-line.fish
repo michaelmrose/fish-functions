4d9d7698e0bea85bc53b2f6c4387513bc8f05884
Michael Rose <michael@rosenetwork.net>
2018-01-02 23:09:17 -0800

    editing weather-status-line.fish

--- weather-status-line.fish
+++ weather-status-line.fish
@@ -3,5 +3,5 @@ function weather-status-line
 	# set weather (weather $geo)
     # echo (weather-icon) (echo $weather | cut -d " " -f2 | trim) (echo $weather | cut -d " " -f4-)
   set current (weather.py 2368481 --format "{text} {temp}°{unit_temperature} {wind_direction_arrow} {wind_speed} mph")
-  echo $current (realfeel)°F
+  echo $current feels like: (realfeel)°F
 end
