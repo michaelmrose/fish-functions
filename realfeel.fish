3b84750aba30e7bce53ca0e2a62dd7e5448f86b7
Michael Rose <michael@rosenetwork.net>
2018-01-02 23:12:17 -0800

    editing realfeel.fish

--- realfeel.fish
+++ realfeel.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/realfeel.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/realfeel.fish @ line 2
 function realfeel
-	explode-words (wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/') | gr real | head -1 | cut -d \' -f2
+	set url https://www.accuweather.com/en/us/bremerton-wa/98337/current-weather/331425
+	explode-words (wget -q -O- "$url" | awk -F\' '/acm_RecentLocationsCarousel\.push/') | gr real | head -1 | cut -d \' -f2
 end
