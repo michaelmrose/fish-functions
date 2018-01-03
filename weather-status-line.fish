7a330bd4098ce99d9e1d44a0d7ce00b3e279f1dd
Michael Rose <michael@rosenetwork.net>
2018-01-02 23:11:35 -0800

    editing weather-status-line.fish

--- weather-status-line.fish
+++ weather-status-line.fish
@@ -2,6 +2,7 @@
 function weather-status-line
 	# set weather (weather $geo)
     # echo (weather-icon) (echo $weather | cut -d " " -f2 | trim) (echo $weather | cut -d " " -f4-)
+  set url https://www.accuweather.com/en/us/bremerton-wa/98337/current-weather/331425
   set current (weather.py 2368481 --format "{text} {temp}°{unit_temperature} {wind_direction_arrow} {wind_speed} mph")
   echo $current feels like: (realfeel)°F
 end
