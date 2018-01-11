39d4b91e15ac53307e754827b694085831a696ec
Michael Rose <michael@rosenetwork.net>
2018-01-10 16:18:24 -0800

    editing kill-other-windows.fish

--- kill-other-windows.fish
+++ kill-other-windows.fish
@@ -1,11 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/kill-other-windows.fish @ line 2
 function kill-other-windows
 	set current (get-focused-workspace)
-	i3-msg move container to workspace keep
-  i3-msg workspace keep
+	i3-msg "move container to workspace keep;workspace keep"
   i3-msg [workspace=$current] kill
-  i3-msg move container to workspace $current
-  i3-msg workspace $current
+  i3-msg "move container to workspace $current;workspace $current"
   # kill-workspace
   # vi3_get-workspace keep
   # ws $current
