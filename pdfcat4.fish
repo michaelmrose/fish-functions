12ae4d519b869458d7c7be740ba474cb5ebe57ba
Michael Rose <michael@rosenetwork.net>
2018-01-15 02:31:10 -0800

    editing pdfcat4.fish

---
+++ pdfcat4.fish
@@ -0,0 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/pdfcat3_pdfcat4.fish @ line 11
+function pdfcat4
+	set files $argv[1..-2]
+  set out $argv[-1]
+  qdf $files[1] --pages (zip-lists $files  (repeat (count $files) 1-z) | condense_lines) -- $out
+end
