f704f03865929a29848d069c4939a26238025917
Michael Rose <michael@rosenetwork.net>
2018-01-15 02:47:13 -0800

    editing pdfcat5.fish

--- pdfcat5.fish
+++ pdfcat5.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/pdfcat3_pdfcat4.fish @ line 18
+# Defined in /home/michael/.config/fish/buffer/pdfcat5.fish @ line 2
 function pdfcat5
 	set files $argv[1..-2]
   set out $argv[-1]
-  echo qpdf $files[1] --pages (appendto $files 1-z) -- $out
+  qpdf $files[1] --pages (appendto $files 1-z) -- $out
 end
