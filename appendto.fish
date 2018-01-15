30d2a6e64ec24c103a3d551a93130a0e4c78c649
Michael Rose <michael@rosenetwork.net>
2018-01-15 02:44:13 -0800

    editing appendto.fish

--- appendto.fish
+++ appendto.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/appendto.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/appendto.fish @ line 2
 function appendto
 	set list $argv[1..-2]
   set value $argv[-1]
-  zip-lists $list (repeat (count $list) $value) | condensee_lines
+  zip-lists $list (repeat (count $list) $value) | condense_lines
 end
