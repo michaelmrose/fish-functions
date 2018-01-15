407447b467925ccaa0acc64bf0a61908fc66dae6
Michael Rose <michael@rosenetwork.net>
2018-01-15 01:02:43 -0800

    editing pdfcat3.fish

--- pdfcat3.fish
+++ pdfcat3.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/pdfcat2_pdfcat3.fish @ line 17
+# Defined in /home/michael/.config/fish/buffer/pdfcat2_pdfcat3.fish @ line 18
 function pdfcat3
 	set files $argv[1..-2]
   set out $argv[-1]
   for i in $files
     set acc $acc $i 1-z
   end
-  echo $acc
+  qpdf --pages $acc -- $out
 end
