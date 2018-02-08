6cc3606f360c70116df4d4ee9088f3831a2a367b
Michael Rose <michael@rosenetwork.net>
2018-02-08 00:10:58 -0800

    editing lockall.fish

--- lockall.fish
+++ lockall.fish
@@ -3,8 +3,7 @@ function lockall
 	set bg /tmp/bgimage.png
 	# ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
   # ssh desktop i3lock -i /tmp/bgimage.png & 
-  ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock
-
+  ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
   mmlock
   d killall i3lock
 end
