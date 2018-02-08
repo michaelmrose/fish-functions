3c9df3cef0654ddba0fdff40b379030dc3d3835c
Michael Rose <michael@rosenetwork.net>
2018-02-07 23:34:46 -0800

    editing pickclips.fish

--- pickclips.fish
+++ pickclips.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/pickclips.fish @ line 2
 function pickclips
-	greenclip print | rofi -dmenu -p clipboard | head -c -1 | clipboard
+	greenclip print | rofi -dmenu -p "clipboard: " | head -c -1 | clipboard
 end
