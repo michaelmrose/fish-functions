c749d6b725d5f65f493a364bf8bd3da72069a90b
Michael Rose <michael@rosenetwork.net>
2018-02-15 13:43:23 -0800

    editing pk.fish

--- pk.fish
+++ pk.fish
@@ -1,7 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/pk.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/pk.fish @ line 2
 function pk
 	while read -l line
     set acc $acc $line
   end
-  echo $acc | rofi -dmenu -i -p "select"
+  p $acc | rofi -dmenu -i -p "select"
 end
