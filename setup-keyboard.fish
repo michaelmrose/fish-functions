91785495ffcdf43d49bcc04f22987b25062e1bee
Michael Rose <michael@rosenetwork.net>
2018-03-01 18:41:50 -0800

    editing setup-keyboard.fish

--- setup-keyboard.fish
+++ setup-keyboard.fish
@@ -1,5 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/setup-keyboard.fish @ line 2
 function setup-keyboard
 	xmodmap ~/.config/xkeys/keys
   xcape-file ~/.config/xkeys/xcapes
   numlocked
+  msg keyboard setup
 end
