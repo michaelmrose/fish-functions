72bbb5c30c4cf5b9a464e1b98e9da8a8901cd983
Michael Rose <michael@rosenetwork.net>
2018-03-04 17:22:29 -0800

    editing emacsanywhere.fish

--- emacsanywhere.fish
+++ emacsanywhere.fish
@@ -1,8 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/emacsanywhere.fish @ line 2
 function emacsanywhere
 	set temp /tmp/tmp.org
-    # emacsclient -c  $temp
-    alacritty -e $temp
+    emacsclient -c  $temp
+    # alacritty -e $temp
     if test -f $temp
         cat $temp | clipboard
         universal-paste
