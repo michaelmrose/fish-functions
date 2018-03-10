b916b58037b3de73597d536956325eace18af84b
Michael Rose <michael@rosenetwork.net>
2018-03-09 18:56:23 -0800

    editing dockctl.fish

--- dockctl.fish
+++ dockctl.fish
@@ -19,6 +19,7 @@ function dockctl
       xrandr --output DP1 --auto --output eDP1 --auto --right-of DP1 --set "scaling mode" "Full aspect" --scale 0.75x0.75
       # ssh desktop synergyc (ipaddr)
       ssh desktop synergyc 192.168.1.141 &
+      ssh desktop xrr dual
 
     case undock
       sudo umount /mnt/michael
