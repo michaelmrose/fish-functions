002ee569e965d563942a231e8e8f2fb04ec81efa
Michael Rose <michael@rosenetwork.net>
2018-03-24 12:05:23 -0700

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -4,7 +4,7 @@ function set-path
 
     set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin ~/scripts
 
-    set mypaths /opt/bin /home/michael/bin /opt/android-sdk/platform-tools /home/michael/.cargo/bin
+    set mypaths /opt/bin /home/michael/bin /opt/android-sdk/platform-tools /home/michael/.cargo/bin /home/michael/extproj/imapnotify/node_modules/imapnotify/bin
 
     set -xg PATH $mypaths $javapaths $builtinpaths
 end
