34365cc1d9e8866033952d490e81eb7d3ec48be0
Michael Rose <michael@rosenetwork.net>
2018-03-28 15:38:49 -0700

    editing update-email-index.fish

---
+++ update-email-index.fish
@@ -0,0 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/update-email-index.fish @ line 1
+function update-email-index
+	emacsclient -a '' -e '(mu4e-update-mail-and-index t)'
+    pkill -RTMIN+11 i3blocks,
+end
