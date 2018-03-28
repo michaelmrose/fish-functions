f851f26205098405a62c9c696098f5879201192b
Michael Rose <michael@rosenetwork.net>
2018-03-28 15:17:43 -0700

    editing publish-function.fish

--- publish-function.fish
+++ publish-function.fish
@@ -1,4 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/publish-function.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/publish-function.fish @ line 2
 function publish-function
-	scp ~/.config/fish/functions/$argv.fish desktop:/home/michael/.config/fish/functions/
+	switch (hostname)
+        case michael-pc
+            set dest desktop
+        case michael-desktop
+            set dest laptop
+    end
+    scp ~/.config/fish/functions/$argv.fish $dest:/home/michael/.config/fish/functions/
 end
