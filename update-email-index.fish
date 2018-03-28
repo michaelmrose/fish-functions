44a06b6caf1cf6f7460299a067ee8d4752978637
Michael Rose <michael@rosenetwork.net>
2018-03-28 15:47:12 -0700

    removed update-email-index.fish

--- update-email-index.fish
+++
@@ -1,5 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/update-email-index.fish @ line 2
-function update-email-index
-	emacsclient -a '' -e '(mu4e-update-mail-and-index t)'
-  pkill -RTMIN+11 i3blocks
-end
