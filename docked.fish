93e318f3f2838e3400dbed8255e88be66ca3efd6
Michael Rose <michael@rosenetwork.net>
2018-04-05 16:36:33 -0700

    editing docked.fish

--- docked.fish
+++ docked.fish
@@ -1,5 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/docked.fish @ line 2
 function docked
 	# test (count (get-connected-displays)) -gt 1
-  lspci | gr dock > /dev/null
+  # lspci | gr dock > /dev/null
+
+    lsusb gr OneLink > /dev/null
 end
