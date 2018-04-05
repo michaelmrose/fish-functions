b468f47d82096e9407720f12be24d4f9044b3d4c
Michael Rose <michael@rosenetwork.net>
2018-04-05 16:16:30 -0700

    editing fakelock.fish

--- fakelock.fish
+++ fakelock.fish
@@ -1,10 +1,10 @@
 # Defined in /home/michael/.config/fish/buffer/fakelock.fish @ line 2
 function fakelock
 	save-workspaces
+    i3-elements hide
     for display in (get-connected-displays)
         ws (uid);focus output right
     end
-    i3-elements hide
     #lock command here
     sleep 2
     restore-workspaces
