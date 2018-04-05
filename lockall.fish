cbc51ed735b4d5cb0d7fe068d0bd1e455b8f8a5e
Michael Rose <michael@rosenetwork.net>
2018-04-05 16:30:24 -0700

    editing lockall.fish

--- lockall.fish
+++ lockall.fish
@@ -1,9 +1,10 @@
 # Defined in /home/michael/.config/fish/buffer/lockall.fish @ line 2
 function lockall
-	set bg /tmp/bgimage.png
-	# ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
-  # ssh desktop i3lock -i /tmp/bgimage.png & 
-  ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
-  mmlock
-  d killall i3lock
+	# set bg /tmp/bgimage.png
+	# # ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
+  # # ssh desktop i3lock -i /tmp/bgimage.png & 
+  # ssh desktop /home/michael/extproj/i3lock-multimonitor/lock lock &
+  # mmlock
+  # d killall i3lock
+    both fakelock;d killall xtrlock
 end
