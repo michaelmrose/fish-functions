13920508a1be1bcf0605a6a0ace0cfe6d14f9ff7
Michael Rose <michael@rosenetwork.net>
2018-04-11 14:41:26 -0700

    removed screenshot-workspace.fish

--- screenshot-workspace.fish
+++
@@ -1,7 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/cull-screenshots_screenshot-on.fish @ line 20
-function screenshot-workspace
-	while read -l line
-        screenshot display /home/michael/.config/i3/screenshots/(get-focused-workspace).jpg
-        cull-screenshots
-    end
-end
