a429fa5b1ae3608937c168f72c885c3f56feac9b
Michael Rose <michael@rosenetwork.net>
2018-04-11 14:06:45 -0700

    editing screenshot-workspace.fish

--- screenshot-workspace.fish
+++ screenshot-workspace.fish
@@ -1,6 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/screenshot-workspace.fish @ line 2
 function screenshot-workspace
-	while read -l line
-        screenshot display /home/michael/.config/i3/screenshots/(get-focused-workspace).jpg
-    end
+	screenshot display /home/michael/.config/i3/screenshots/(get-focused-workspace).jpg
 end
