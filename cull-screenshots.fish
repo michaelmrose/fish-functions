90999092163fdeba1d68dc73210a3b14a1c50f66
Michael Rose <michael@rosenetwork.net>
2018-04-11 14:23:14 -0700

    editing cull-screenshots.fish

--- cull-screenshots.fish
+++ cull-screenshots.fish
@@ -2,9 +2,7 @@
 function cull-screenshots
 	set workspaces (list-workspaces)
 	for i in ~/.config/i3/screenshots/*
-      echo i is $i
       set name (extract-filename $i)
-      echo name is $name
       # if the current name is no longer a valid ws
       if not contains $name $workspaces
           rm ~/.config/i3/screenshots/$name.jpg
