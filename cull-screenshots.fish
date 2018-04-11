3717e8645f5fa98453589b662e2885c699e46adf
Michael Rose <michael@rosenetwork.net>
2018-04-11 14:20:00 -0700

    editing cull-screenshots.fish

--- cull-screenshots.fish
+++ cull-screenshots.fish
@@ -1,6 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/cull-screenshots.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/cull-screenshots.fish @ line 2
 function cull-screenshots
+	set workspaces (list-workspaces)
 	for i in ~/.config/i3/screenshots/*
-        echo $i
+      set name (echo $i | cut -d . -f1)
+      # if the current name is no longer a valid ws
+      if not contains $name $workspaces
+          rm ~/.config/i3/screenshots/$name.jpg
+      end
     end
 end
