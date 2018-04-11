7ccd35120c3f2e4906b28029c3db91c2dd3eb4e3
Michael Rose <michael@rosenetwork.net>
2018-04-11 11:44:56 -0700

    editing sync-desktop-wallpaper.fish

--- sync-desktop-wallpaper.fish
+++ sync-desktop-wallpaper.fish
@@ -1,5 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/sync-desktop-wallpaper.fish @ line 2
 function sync-desktop-wallpaper
 	fix-wallpaper-path-list
-	ssh desktop "set -x DISPLAY :0; wp $bgimage"
+	# ssh desktop "set -x DISPLAY :0; wp $bgimage"
+  d wp $bgimage
 end
