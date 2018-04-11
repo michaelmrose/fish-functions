447db1a61927b81bd1be6b20e6bd4603c7314381
Michael Rose <michael@rosenetwork.net>
2018-04-11 14:44:39 -0700

    editing screenshot-on-window-event.fish

--- screenshot-on-window-event.fish
+++ screenshot-on-window-event.fish
@@ -1,4 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/cull-screenshots_screenshot-on.fish @ line 14
 function screenshot-on-window-event
-	i3subscribe window|grep -E --line-buffered 'close|new'| screenshot display ~/.config/i3/screenshots/(get-focused-workspace).jpg;and cull-screenshots
+	i3subscribe window|grep -E --line-buffered 'close|new'| screenshot-workspace
+    cull-screenshots
 end
