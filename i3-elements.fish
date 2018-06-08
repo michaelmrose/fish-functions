566472316bc560dc244582fcbad562ee94e743ea
Michael Rose <michael@rosenetwork.net>
2018-06-07 20:41:17 -0700

    editing i3-elements.fish

--- i3-elements.fish
+++ i3-elements.fish
@@ -1,5 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/i3-elements.fish @ line 2
 function i3-elements
-	set i3_bar_ids (xwininfo -all -root | grep 'i3bar for output' | condense_spaces | cut -d " " -f1)
+	set i3_bar_ids (p (xwininfo -all -root)|grep i3bar| condense_spaces | cut -d " " -f1)
   set i3_frame_ids (xwininfo -all -root | grep i3-frame | grep 'has no name' | trim | cut -d ' ' -f1)
   set ids $i3_bar_ids $i3_frame_ids
   switch $argv[1]
