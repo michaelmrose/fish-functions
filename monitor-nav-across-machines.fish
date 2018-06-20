fe8253b4e260927393c68984572e262e076d0234
Michael Rose <michael@rosenetwork.net>
2018-06-20 15:53:10 -0700

    editing monitor-nav-across-machines.fish

--- monitor-nav-across-machines.fish
+++ monitor-nav-across-machines.fish
@@ -1,12 +1,12 @@
-# Defined in /home/michael/.config/fish/buffer/monitor-nav-across-machines_mo.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/monitor-nav-across-machines_mo.fish @ line 2
 function monitor-nav-across-machines
 	set window (xdotool getactivewindow)
     i3 focus $argv
     set newwindow (xdotool getactivewindow)
     if [ $window = $newwindow]
         switch-machines
-        i3 focus left
-        i3 focus left
-        i3 focus left
+        remote-i3 focus $argv
+        remote-i3 focus $argv
+        remote-i3 focus $argv
     end
 end
