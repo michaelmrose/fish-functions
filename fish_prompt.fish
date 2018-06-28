a7ce17f3a89e6e7db7c14ac2f12275c220e4dab1
Michael Rose <michael@rosenetwork.net>
2018-06-28 11:21:00 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,14 +1,14 @@
-# Defined in /home/michael/.config/fish/buffer/fish_prompt.fish @ line 3
+# Defined in /home/michael/.config/fish/buffer/fish_prompt_fish_prompt.fish @ line 2
 function fish_prompt
-	set basicinfo (timeout --foreground 0.1 fish -c 'powerline-rs --modules user,host,cwd,ssh,jobs,perms --shell bare')
-set gitinfo (timeout --foreground 0.25 fish -c 'powerline-rs --modules git,gitstage --shell bare')
+	# 	set basicinfo (timeout --foreground 0.1 fish -c 'powerline-rs --modules user,host,cwd,ssh,jobs,perms --shell bare')
+# set gitinfo (timeout --foreground 0.25 fish -c 'powerline-rs --modules git,gitstage --shell bare')
 
-if exists $gitinfo
-    echo $basicinfo$gitinfo
-else
-    echo {$basicinfo}...
-end
+# if exists $gitinfo
+#     echo $basicinfo$gitinfo
+# else
+#     echo {$basicinfo}...
+# end
 
 
-	# timeout 0.25 powerline-rs --shell bare --modules "user,host,cwd,ssh,jobs,perms,git,gitstage";or powerline-rs --shell bare --modules "user,host,cwd,ssh,jobs,perms"
+	timeout 0.25 powerline-rs --shell bare --modules "user,host,cwd,ssh,jobs,perms,git,gitstage";or powerline-rs --shell bare --modules "user,host,cwd,ssh,jobs,perms"
 end
