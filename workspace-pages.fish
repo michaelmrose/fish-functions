5cf63dd4991e7c0143b9bb7794cffd933003b67c
Michael Rose <michael@rosenetwork.net>
2018-07-12 01:48:34 -0700

    editing workspace-pages.fish

--- workspace-pages.fish
+++ workspace-pages.fish
@@ -1,7 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/workspace-pages.fish @ line 2
 function workspace-pages
-	for ws in (i3-msg -t get_workspaces \
+	set workspaces (i3-msg -t get_workspaces \
     | jq -r '.[] | select(.visible) | .name'| sed "s#[0-9]#$WSPAGE#g")
+    for workspace in $workspaces
         echo i3-msg workspace $workspace
     end
 end
