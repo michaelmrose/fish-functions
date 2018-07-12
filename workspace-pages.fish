62c9b10471d71c8ec40bdb0f419bfec2cb4433b2
Michael Rose <michael@rosenetwork.net>
2018-07-12 01:41:11 -0700

    editing workspace-pages.fish

--- workspace-pages.fish
+++ workspace-pages.fish
@@ -1,10 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/nav-workspace-by-page.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/workspace-pages.fish @ line 2
 function workspace-pages
 	#get list of active workspaces
     #each like 1.a 1.d replace leading number. with newnumber.
     #and navigate to all workspaces
     set -U WSPAGE $argv
-    set workspaces (get-actitive-workspaces | sed "s#[0-9]#$WSPAGE#g")
+    set workspaces (get-active-workspaces | sed "s#[0-9]#$WSPAGE#g")
     for workspace in $workspaces
         echo i3-msg workspace $workspace
     end
