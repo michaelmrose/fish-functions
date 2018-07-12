4ba89e22b47c30ec2472267d0703d4781ebe2495
Michael Rose <michael@rosenetwork.net>
2018-07-11 19:19:00 -0700

    editing fix-wp-var.fish

--- fix-wp-var.fish
+++ fix-wp-var.fish
@@ -1,7 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/fix-wp-var.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/fix-wp-var.fish @ line 2
 function fix-wp-var
 	set desktopwallpaperdir /med/images/backgrounds
     if test -d $desktopwallpaperdir
+        echo it lives
         if echo $bgimage |gr $desktopwallpaperdir
             echo fucked
         end
