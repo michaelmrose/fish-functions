3c0829859e43b92fcc6b890b6926e2f016d891c8
Michael Rose <michael@rosenetwork.net>
2018-07-13 12:49:18 -0700

    removed pdfcat5.fish

--- pdfcat5.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/pdfcat5.fish @ line 2
-function pdfcat5
-	set files $argv[1..-2]
-  set out $argv[-1]
-  eval qpdf $files[1] --pages (appendto $files 1-z) -- $out
-end
