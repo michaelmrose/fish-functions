9c51ee77995bab5761b1b73a083ab58fc5d1e0b3
Michael Rose <michael@rosenetwork.net>
2018-07-13 12:49:05 -0700

    removed pdfcat3.fish

--- pdfcat3.fish
+++
@@ -1,9 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/pdfcat3_pdfcat4.fish @ line 2
-function pdfcat3
-	set files $argv[1..-2]
-  set out $argv[-1]
-  for i in $files
-    set acc $acc $i 1-z
-  end
-  qpdf $files[1] --pages $acc -- $out
-end
