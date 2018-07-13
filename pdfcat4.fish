2da16a5b421584e793ca553b7eba49aca1141793
Michael Rose <michael@rosenetwork.net>
2018-07-13 12:36:32 -0700

    editing pdfcat4.fish

--- pdfcat4.fish
+++ pdfcat4.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/pdfcat3_pdfcat4.fish @ line 12
+# Defined in /home/michael/.config/fish/buffer/pdfcat4.fish @ line 2
 function pdfcat4
 	set files $argv[1..-2]
   set out $argv[-1]
-  qdf $files[1] --pages (zip-lists $files  (repeat (count $files) 1-z) | condense_lines) -- $out
+  qpdf $files[1] --pages (zip-lists $files  (repeat (count $files) 1-z) | condense_lines) -- $out
 end
