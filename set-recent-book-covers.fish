879b542db768f47985c0df7729e9a3c2ac8f12e5
Michael Rose <michael@rosenetwork.net>
2018-07-15 17:27:09 -0700

    editing set-recent-book-covers.fish

--- set-recent-book-covers.fish
+++ set-recent-book-covers.fish
@@ -1,3 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/pick-book-by-cover_set-recent-.fish @ line 13
 function set-recent-book-covers
-	set -U RECENT_BOOK_COVERS (apply-to-list get-cover-by-title $recent_reads)
+	set -U RECENT_BOOK_COVERS (apply-to-list get-cover-based-on-title $recent_reads)
 end
