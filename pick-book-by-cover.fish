974ff5343831205ae9bbe499fbfcf65d1d85b835
Michael Rose <michael@rosenetwork.net>
2018-07-16 19:56:01 -0700

    editing pick-book-by-cover.fish

--- pick-book-by-cover.fish
+++ pick-book-by-cover.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/pick-book-by-cover_set-recent-.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/pick-book-by-cover_search-book.fish @ line 2
 function pick-book-by-cover
 	# books $recent_reads[(findindex (sxiv -tbfor $RECENT_BOOK_COVERS) $RECENT_BOOK_COVERS)]
   set-recent-book-covers
