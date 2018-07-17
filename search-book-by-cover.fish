2a1b633bd5a36fcafb63c6095c14cfdcae1014d1
Michael Rose <michael@rosenetwork.net>
2018-07-16 19:56:01 -0700

    editing search-book-by-cover.fish

--- search-book-by-cover.fish
+++ search-book-by-cover.fish
@@ -1,7 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/search-book-by-cover.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/pick-book-by-cover_search-book.fish @ line 13
 function search-book-by-cover
 	set list (calibredb list -s title:$argv -f title| cut -d ' ' -f2-)
 
     set covers (apply-to-list get-cover-based-on-title $list)
-    sxiv -tbfor $covers
+    for choice in $covers
+        set books $books $recent_reads[(findindex $choice $RECENT_BOOK_COVERS)]
+    end
+    p $books | book-list
 end
