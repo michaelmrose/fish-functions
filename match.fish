795dcbba2a3ef3635e0e14dadb155e1a3c5726f8
Michael Rose <michael@rosenetwork.net>
2018-07-19 20:38:22 -0700

    editing match.fish

--- match.fish
+++ match.fish
@@ -1,4 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/match.fish @ line 2
 function match
-	set com echo $argv[1] \| ag -i \'\^$argv[2]\$\'
-  eval $com > /dev/null
+	# set com echo $argv[1] \| ag -i \'\^$argv[2]\$\'
+    # eval $com > /dev/null
+    string match $argv[1] $argv[2]
 end
