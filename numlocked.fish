1cadcaeb02b9d4c1604ff503f2a9f9da303d35dd
Michael Rose <michael@rosenetwork.net>
2018-07-19 20:37:02 -0700

    editing numlocked.fish

--- numlocked.fish
+++ numlocked.fish
@@ -1,5 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/numlocked.fish @ line 2
 function numlocked
-	if match off (xset q | grep 'Num Lock' | awk '{print $8}')
-    xdotool key Num_Lock
+	if string match off (xset q | grep 'Num Lock' | awk '{print $8}')
+        xdotool key Num_Lock
   end
 end
