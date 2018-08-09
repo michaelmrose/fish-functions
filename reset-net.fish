748e41252322813659a6edb44b2badcd6612ce8a
Michael Rose <michael@rosenetwork.net>
2018-08-08 21:25:41 -0700

    editing reset-net.fish

--- reset-net.fish
+++ reset-net.fish
@@ -1,4 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/reset-net.fish @ line 2
 function reset-net
-	s ip link set dev eth1 down
-  s ip link set dev eth1 up
+	s ip link set dev eth0 down
+    s ip link set dev eth0 up
 end
