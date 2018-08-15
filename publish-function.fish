e9c784d02d6fd6c6504c1497a097ce28b2274986
Michael Rose <michael@rosenetwork.net>
2018-08-15 16:55:45 -0700

    editing publish-function.fish

--- publish-function.fish
+++ publish-function.fish
@@ -1,8 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/publish-function.fish @ line 2
 function publish-function
 	switch (hostname)
-        case michael-pc
-            set dest desktop
+      case michael-laptop
+          set dest desktop
         case michael-desktop
             set dest laptop
     end
