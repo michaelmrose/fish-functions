0fa0f4c51cbb24ee8990559521acc2faf43cd355
Michael Rose <michael@rosenetwork.net>
2018-08-15 17:05:58 -0700

    editing both.fish

--- both.fish
+++ both.fish
@@ -4,6 +4,6 @@ function both
 	  set machines $dest
     eval $argv
     for i in $machines
-        echo ssh $i "set -x DISPLAY :0;and $argv"
+        echo ssh $i set -x DISPLAY :0 \; and $argv
     end
 end
