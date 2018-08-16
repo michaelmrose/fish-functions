2851fc54151b60f3054df698c945337f268ed3ab
Michael Rose <michael@rosenetwork.net>
2018-08-15 17:03:40 -0700

    editing both.fish

--- both.fish
+++ both.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/both_get-destination_publish-f.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/both.fish @ line 2
 function both
 	set dest (get-destination)
 	  set machines $dest
     eval $argv
     for i in $machines
-        ssh $i $argv
+        ssh $i "set -x DISPLAY :0;and $argv"
     end
 end
