160f2211218efc652596341af7f483b9ba6ea603
Michael Rose <michael@rosenetwork.net>
2018-08-22 21:18:02 -0700

    editing add-to-recent-reads.fish

--- add-to-recent-reads.fish
+++ add-to-recent-reads.fish
@@ -1,7 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/add-to-recent-reads_open-book.fish @ line 2
 function add-to-recent-reads --description 'keep a list of the 10 most recent unique items opened via this script'
 	if exists $argv
-      msg argv is $argv
       set title $argv
       # set library (get-current-calibre-library)
       # set entry \"$title @$library\"
