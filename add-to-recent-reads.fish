fb00a0a76612e837cbcc92ba80019f38937bb9bb
Michael Rose <michael@rosenetwork.net>
2018-08-22 21:06:17 -0700

    editing add-to-recent-reads.fish

--- add-to-recent-reads.fish
+++ add-to-recent-reads.fish
@@ -1,10 +1,12 @@
+# Defined in /home/michael/.config/fish/buffer/add-to-recent-reads.fish @ line 2
 function add-to-recent-reads --description 'keep a list of the 10 most recent unique items opened via this script'
 	if exists $argv
-        set title $argv
-        # set library (get-current-calibre-library)
-        # set entry \"$title @$library\"
-        set -U recent_reads (take 15 (unique (println "$title" $recent_reads)))
-        echo \"$title\"
+      msg argv is $argv
+      set title $argv
+      # set library (get-current-calibre-library)
+      # set entry \"$title @$library\"
+      set -U recent_reads (take 15 (unique (println "$title" $recent_reads)))
+      echo \"$title\"
     else
         msg could not add to recent reads
         return 1
