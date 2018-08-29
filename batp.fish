a52cabdf56cadab8dc2afe114e9a158d0969f770
Michael Rose <michael@rosenetwork.net>
2018-08-28 22:11:23 -0700

    editing batp.fish

--- batp.fish
+++ batp.fish
@@ -1,11 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/batp.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/batp.fish @ line 2
 function batp
 	set lex (get-ext $argv[1])
     set file $argv[1]
     set res (bat $file)
     if test (count $argv) -gt 1
         for item in $argv[2..-1]
-            set res (p $res | item)
+            set res (p $res | $item)
         end
     end
     p $res | bat -l $lex
