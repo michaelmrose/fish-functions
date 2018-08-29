7a66239f9e9b1fdcece8a47fcb02744876aa42a9
Michael Rose <michael@rosenetwork.net>
2018-08-28 22:12:07 -0700

    editing batp.fish

--- batp.fish
+++ batp.fish
@@ -5,7 +5,7 @@ function batp
     set res (bat $file)
     if test (count $argv) -gt 1
         for item in $argv[2..-1]
-            set res (p $res | $item)
+            eval "set res (p $res | $item)"
         end
     end
     p $res | bat -l $lex
