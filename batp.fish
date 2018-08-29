2e7cd477687c545f6b41531efef2e58c7dd58df1
Michael Rose <michael@rosenetwork.net>
2018-08-28 22:13:32 -0700

    editing batp.fish

--- batp.fish
+++ batp.fish
@@ -2,7 +2,7 @@
 function batp
 	set lex (get-ext $argv[1])
     set file $argv[1]
-    set res (bat $file)
+    set res (p (bat $file))
     if test (count $argv) -gt 1
         for item in $argv[2..-1]
             echo eval "set res (p $res | $item)"
