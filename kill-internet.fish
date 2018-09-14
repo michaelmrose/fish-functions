af93fae090219607f32236bcc8e5d5d23662e777
Michael Rose <michael@rosenetwork.net>
2018-09-13 23:08:31 -0700

    editing kill-internet.fish

--- kill-internet.fish
+++ kill-internet.fish
@@ -3,6 +3,5 @@ function kill-internet
 	s iptables -A INPUT -i lo -j ACCEPT
     s iptables -A INPUT -s 192.168.0.1/16 -j ACCEPT
     s iptables -P INPUT DROP
-    # s systemctl iptables save      
     s systemctl iptables start
 end
