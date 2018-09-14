1bf9525298e971f57d9aec0f52a4079a35e9d146
Michael Rose <michael@rosenetwork.net>
2018-09-13 23:16:48 -0700

    editing kill-internet.fish

--- kill-internet.fish
+++ kill-internet.fish
@@ -1,9 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/kill-internet.fish @ line 2
 function kill-internet
-	# s iptables -A INPUT -i lo -j ACCEPT
-    #   s iptables -A INPUT -s 192.168.0.1/16 -j ACCEPT
-    #   s iptables -P INPUT DROP
-    #   s systemctl start iptables
-    s modprobe ip_tables
+	s modprobe ip_tables
     s modprobe iptable_filter
+	  s iptables -A INPUT -i lo -j ACCEPT
+    s iptables -A INPUT -s 192.168.0.1/16 -j ACCEPT
+    s iptables -P INPUT DROP
+    s systemctl start iptables
 end
