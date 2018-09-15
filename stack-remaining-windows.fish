d2659c4c1bf74a2e5758940b220a89c78442ec9f
Michael Rose <michael@rosenetwork.net>
2018-09-15 12:21:41 -0700

    editing stack-remaining-windows.fish

--- stack-remaining-windows.fish
+++ stack-remaining-windows.fish
@@ -1,9 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/stack-remaining-windows.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/stack-remaining-windows.fish @ line 2
 function stack-remaining-windows
 	i3 focus parent
     i3 focus parent
     i3 focus parent
     i3 layout stacked
+    i3 focus child
+    i3 focus child
     i3 focus child
     i3 move window left
 end
