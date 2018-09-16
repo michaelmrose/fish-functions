35e368a173dfc5809f314439bb131eabc6b27b11
Michael Rose <michael@rosenetwork.net>
2018-09-16 10:14:25 -0700

    editing mu4e.fish

--- mu4e.fish
+++ mu4e.fish
@@ -1,5 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/mu4e.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/mu4e.fish @ line 2
 function mu4e
-	emacsclient -a '' -e '(mu4e-update-mail-and-index t)'
-    emacsclient -a '' -e '(mu4e)'
+	emacsclient -a '' -e '(mu4e)' &
+	  emacsclient -a '' -e '(mu4e-update-mail-and-index t)'&
 end
