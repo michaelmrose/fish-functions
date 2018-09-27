7209848ea1532d6d61380ec8ae96c8b40ccabff4
Michael Rose <michael@rosenetwork.net>
2018-09-26 22:18:57 -0700

    editing lines.fish

---
+++ lines.fish
@@ -0,0 +1,13 @@
+# Defined in /home/michael/.config/fish/buffer/lines.fish @ line 2
+function lines
+	while read -l line
+      set acc $acc $line
+  end
+  set numbers (echo $argv | cut -d - -f1- --output-delimiter= " ")
+  
+  if test (count $numbers) -eq 2
+      p $acc | sed -n $numbers[1],$numbers[2]p
+  else
+      p $acc | sed -n $numbers[1]p
+  end
+end
