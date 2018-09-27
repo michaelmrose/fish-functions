924814462e7f0da5b222523e4cce058fdc122373
Michael Rose <michael@rosenetwork.net>
2018-09-26 22:06:30 -0700

    editing unwrap.fish

--- unwrap.fish
+++ unwrap.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/unwrap.fish @ line 2
 function unwrap
 	set first $argv[1]
-    set second (closeingchar $first)
+    set second (closingchar $first)
     while read -l line
         echo $line | cut -d $first -f2- | cut -d $second -f1
     end
