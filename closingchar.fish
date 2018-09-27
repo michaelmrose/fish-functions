adbbd3cc619cb3fc5c6ce5cb8e58e7877486a45a
Michael Rose <michael@rosenetwork.net>
2018-09-26 22:34:12 -0700

    editing closingchar.fish

--- closingchar.fish
+++ closingchar.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/unwrap.fish @ line 11
+# Defined in /home/michael/.config/fish/buffer/closingchar.fish @ line 2
 function closingchar
 	set first $argv[1]
     switch $first
@@ -8,5 +8,7 @@ function closingchar
             echo ']'
         case '{'
             echo '}'
+        case '*'
+            echo $first
     end
 end
