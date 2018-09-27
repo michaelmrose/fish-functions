16b66901e1e58c5c18a193b69cda54cd6d4784a3
Michael Rose <michael@rosenetwork.net>
2018-09-26 20:32:36 -0700

    editing cuts.fish

---
+++ cuts.fish
@@ -0,0 +1,15 @@
+# Defined in /home/michael/.config/fish/buffer/cuts.fish @ line 1
+function cuts
+	set delimiters (odds $argv)
+    set nums (evens $argv)
+    set paths (zip-lists $delimiters $nums)
+    echo p is
+    p $paths
+    while read -l line
+        # set acc $line
+        # for p in $paths
+        #     set acc (echo $acc | cut -d $p[1] -f $p[2])
+        # end
+
+    end
+end
