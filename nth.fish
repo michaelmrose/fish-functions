85166e8d507bdcbac052c0693cbf85b4ebf29e12
Michael Rose <michael@rosenetwork.net>
2018-09-26 20:13:04 -0700

    editing nth.fish

--- nth.fish
+++ nth.fish
@@ -3,6 +3,8 @@ function nth
 	set segments (foreach $argv 'echo \$_')
 	  while read -l line
         # echo $segments
-        echo $line | awk "{print \$segments}"
+        # echo $line | awk "{print \$segments}"
+        set com 'echo $line | awk '{print $segments}''
+        echo $com
     end
 end
