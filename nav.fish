04337e0e0048a9d155cdb90231a84a0fe1b741c0
Michael Rose <michael@rosenetwork.net>
2018-09-26 22:11:44 -0700

    editing nav.fish

---
+++ nav.fish
@@ -0,0 +1,18 @@
+# Defined in /home/michael/.config/fish/buffer/nav.fish @ line 1
+function nav
+	while read -l line
+        set acc $acc
+    end
+    for field in $argv
+        set c (echo $field | cut -c1)
+        set r (echo $field | cut -c2-)
+        switch $c
+            case l
+                set acc (p $acc | lines $r)
+            case n
+                set acc (p $acc | nth $r)
+            case u
+                set acc (p $acc | unwrap $r)
+        end
+    end
+end
