ec8db295d54247a5033ca23bc1ce705d209e5682
Michael Rose <michael@rosenetwork.net>
2018-09-26 22:13:27 -0700

    editing nav.fish

--- nav.fish
+++ nav.fish
@@ -1,12 +1,12 @@
 # Defined in /home/michael/.config/fish/buffer/nav.fish @ line 2
 function nav
 	while read -l line
-        set acc $acc
-    end
-    for field in $argv
-        set c (echo $field | cut -c1)
-        set r (echo $field | cut -c2-)
-        switch $c
+      set acc $acc $line
+  end
+  for field in $argv
+      set c (echo $field | cut -c1)
+      set r (echo $field | cut -c2-)
+      switch $c
             case l
                 set acc (p $acc | lines $r)
             case n
