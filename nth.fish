eb7e5003fda92a581095be6b5aa640105b422763
Michael Rose <michael@rosenetwork.net>
2018-09-26 20:10:14 -0700

    editing nth.fish

--- nth.fish
+++ nth.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/nth.fish @ line 2
 function nth
-	while read -l line
-        set segments (foreach $line 'echo \$_')
+	set segments (foreach $argv 'echo \$_')
+	  while read -l line
         echo $segments
         # echo $line | awk "{print \$$argv}"
   end
