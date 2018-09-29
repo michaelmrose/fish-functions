a405d99f55ffbe8d6b9c5566a845819226dcc1fa
Michael Rose <michael@rosenetwork.net>
2018-09-29 12:50:41 -0700

    editing show-recent-reads.fish

--- show-recent-reads.fish
+++ show-recent-reads.fish
@@ -1,3 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/show-recent-reads.fish @ line 2
 function show-recent-reads --description 'use rofi to pick one of the items from recent_reads and open it with sopen if it is a file or books if it is a title'
 	set choices (rfi match "choose a book: " $recent_reads | sort -u)
     for choice in $choices
@@ -5,7 +6,8 @@ function show-recent-reads --description 'use rofi to pick one of the items from
           if test -f $choice
               sopen $choice
           else
-              zathura $recent_reads_fnames[(findindex $choice $recent_reads)] &
+              # zathura $recent_reads_fnames[(findindex $choice $recent_reads)] &
+              zathura (get-fname-of-book "$choice") &
               # books $choice
           end
       else
