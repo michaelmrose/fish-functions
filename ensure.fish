7e8086c59d50a3ebbde2e76a028ba44ab433a44a
Michael Rose <michael@rosenetwork.net>
2018-10-02 00:51:48 -0700

    editing ensure.fish

--- ensure.fish
+++ ensure.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/ensure.fish @ line 2
 function ensure
 	set name $argv[1]
-  set com (string lower $argv)
+  set com $argv
   if not pgrep $name
       eval $com &
     end
