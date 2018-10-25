a6c04c712df6945cb986f1b43b32c15aebd64cdf
Michael Rose <michael@rosenetwork.net>
2018-10-25 13:01:39 -0700

    editing nav.fish

--- nav.fish
+++ nav.fish
@@ -9,10 +9,10 @@ function nav
         switch $c
             case /
                 set acc (p $acc | g $r)
-            # case e
-            #     set rest (echo $r | cut -c2-)
-            #     set acc (for i in $acc;eval $rest $i;end)
-            #     set acc (foreach $acc 'eval $rest')
+            case e
+
+                set rest (echo $r | cut -c2-)
+                set acc (p $acc | each $rest)
             case c
                 set second (echo $r | cut -c1)
                 set rest (echo $r | cut -c2-)
