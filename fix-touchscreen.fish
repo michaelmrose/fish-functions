af825bc787bc13ad784f0b3ba2a6d89163f63f02
Michael Rose <michael@rosenetwork.net>
2018-10-25 12:17:10 -0700

    editing fix-touchscreen.fish

--- fix-touchscreen.fish
+++ fix-touchscreen.fish
@@ -1,11 +1,11 @@
 # Defined in /home/michael/.config/fish/buffer/fix-touchscreen.fish @ line 2
 function fix-touchscreen
-	s rmmod wacom
-    s modprobe wacom
-    set finger (xinput list | nav /aten /pointer n6 c=2) 
-    set pen (xinput list | nav /wacom /finger n9 c=2)  
+	# s rmmod wacom
+  #   s modprobe wacom
+    set pen (xinput list | nav /aten /pointer n6 c=2) 
+    set finger (xinput list | nav /wacom /finger n9 c=2)  
     echo p is $pen f is $finger
-    for i in $pen $finger
-        xinput map-to-output $i eDP1
-    end
+    # for i in $pen $finger
+    #     xinput map-to-output $i eDP1
+    # end
 end
