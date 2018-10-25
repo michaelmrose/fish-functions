1b6750c6016f84fe8e7e4db766fa9cb813831659
Michael Rose <michael@rosenetwork.net>
2018-10-25 12:16:13 -0700

    editing fix-touchscreen.fish

--- fix-touchscreen.fish
+++ fix-touchscreen.fish
@@ -2,8 +2,8 @@
 function fix-touchscreen
 	s rmmod wacom
     s modprobe wacom
-    set pen (xinput list | nav /aten /pointer n6 c=2) 
-    set finger (xinput list | nav /wacom /finger n9 c=2)  
+    set finger (xinput list | nav /aten /pointer n6 c=2) 
+    set pen (xinput list | nav /wacom /finger n9 c=2)  
     echo p is $pen f is $finger
     for i in $pen $finger
         xinput map-to-output $i eDP1
