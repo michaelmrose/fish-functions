6fc2c037887c04a976742526b6fb78ea6799041f
Michael Rose <michael@rosenetwork.net>
2018-11-09 14:32:19 -0800

    editing memsize.fish

--- memsize.fish
+++ memsize.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/memsize.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/memsize.fish @ line 2
 function memsize
-	echo (math (ps -C firefox-bin --no-headers -o rss | xargs | sed -e 's/ /+/g' | bc) / 1024)M
+	echo (math (ps -C $argv --no-headers -o rss | xargs | sed -e 's/ /+/g' | bc) / 1024)M
 end
