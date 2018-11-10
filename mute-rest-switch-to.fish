1d25e80d282fefa3525ad540e8c01cfb60b8f016
Michael Rose <michael@rosenetwork.net>
2018-11-09 21:33:37 -0800

    editing mute-rest-switch-to.fish

--- mute-rest-switch-to.fish
+++ mute-rest-switch-to.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/mute-rest-switch-to_mute-rest-.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/mute-rest-switch-to_mute-rest-.fish @ line 2
 function mute-rest-switch-to
 	for i in (seq (ponymix list -t sink --short|wc -l))
         switch (output-type)
-            case headphones
+            case $argv
                 ponymix unmute
             case '*'
                 ponymix mute
