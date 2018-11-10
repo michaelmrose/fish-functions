0c14c95c8291f9d5b63e9a49b0ec060e74149e0d
Michael Rose <michael@rosenetwork.net>
2018-11-09 20:52:40 -0800

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -20,29 +20,38 @@ function focus-app
         case b
             set val Nightly
         case c
+            set val Speedcrunch
         case d
+            set val Qbittorrent
         case e
         case f
         case g
         case h
+            set val Hexchat
         case i
         case j
         case k
         case l
+            set val kitty
         case m
+            set val Clementine
         case n
         case o
         case p
         case q
         case r
         case s
+            set val Steam
         case t
+            set val kitty
         case u
         case v
+            set val mpv
         case w
         case x
         case y
         case z
+            set val Zathura
         case E
             # set criteria title
             # set val '.*mu4e.*'
