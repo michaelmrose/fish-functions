450d52b8b67fcae9f8241d2a2ea9638f4094c57d
Michael Rose <michael@rosenetwork.net>
2018-11-09 20:28:47 -0800

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -1,10 +1,15 @@
+# Defined in /home/michael/.config/fish/buffer/focus-app.fish @ line 2
 function focus-app
 	set criteria (explode-words (return-windowclass (appkey $argv)))
-  switch (count $criteria)
-    case 1
+
+    switch (count $criteria)
+            case 1
       set com i3-msg \'[class=$criteria]\' focus
     case 2
       set com i3-msg \'[class=$criteria[1] title=\"$criteria[2]\"]\' focus
+  end
+  if contains mu4e $argv
+      set com i3-msg \'[title=.*mu4e.*]\' focus
   end
   echo $com
   set res (eval $com)
