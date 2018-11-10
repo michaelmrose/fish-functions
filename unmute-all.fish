2e3db65d7ff3b3c053deb08a36d024253034746c
Michael Rose <michael@rosenetwork.net>
2018-11-09 21:27:21 -0800

    editing unmute-all.fish

--- unmute-all.fish
+++ unmute-all.fish
@@ -1,6 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/mute-all-except-speakers.fish @ line 17
+# Defined in /home/michael/.config/fish/buffer/mute-all-except-speakers_unmut.fish @ line 6
 function unmute-all
 	for i in (seq (ponymix list -t sink --short|wc -l))
         ponymix unmute
-    end
+        fishswitchaudio.fish
+  end
 end
