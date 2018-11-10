a18777231c6573fb57a3cfe92e0e708ed868c51d
Michael Rose <michael@rosenetwork.net>
2018-11-09 21:33:02 -0800

    editing unmute-all.fish

--- unmute-all.fish
+++ unmute-all.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/mute-rest-switch-to-headphones.fish @ line 18
+# Defined in /home/michael/.config/fish/buffer/mute-rest-switch-to_mute-rest-.fish @ line 34
 function unmute-all
 	for i in (seq (ponymix list -t sink --short|wc -l))
         ponymix unmute
