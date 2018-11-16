79d6ab99b423f62bc1994263f3554f9536b12610
Michael Rose <michael@rosenetwork.net>
2018-11-15 17:07:45 -0800

    editing display-number.fish

---
+++ display-number.fish
@@ -0,0 +1,19 @@
+function display-number
+	# set displays xrandr|g " connected"
+    # set names (p $displays | nth 1)
+    # set offsets p $displays | cut -d + -f2
+    set numdisplays (count (get-ws-info get output where visible = true))
+    switch $numdisplays
+        case 1
+            echo 0
+        case '*'
+            switch (get-focused-display)
+                case eDPI
+                    echo 2
+                case DP1
+                    echo 1
+                case HDMI2
+                    echo 0
+                    end
+    end
+end
