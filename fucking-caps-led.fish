ca1107a972813bc244636a506295328cc69a2a35
Michael Rose <michael@rosenetwork.net>
2018-11-16 11:09:22 -0800

    editing fucking-caps-led.fish

--- fucking-caps-led.fish
+++ fucking-caps-led.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/fucking-caps-led.fish @ line 2
 function fucking-caps-led
-	echo $argv | sudo tee /sys/devices/pci0000:00/0000:00:14.0/usb1/1-3/1-3.3/1-3.3.2/1-3.3.2:1.0/0003:24F0:0137.0011/input/input42/input42::capslock/brightness
+	find /sys/devices -type f |g capslock/brightness | each "echo 0 | sudo tee"
 end
