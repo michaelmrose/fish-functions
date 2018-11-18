2e840e3ed81d5c1ab53eed9942a5e8ae898fc3e9
Michael Rose <michael@rosenetwork.net>
2018-11-17 22:13:14 -0800

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -11,25 +11,32 @@ function xrr
           xrr eDP1
       case desktop
           xrr DP1
+      case single
+          xrandr --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75 
+          return 0
       case dual
           # xrr (get-secondary-display) (get-primary-display)
-          xrr DP1 eDP1
-      case '*'
-      set fn xrandr --auto --output $argv[1]
-        set prior $argv[1]
-        if test (count $argv) -gt 1
-          for output in $argv[2..-1]
-              set fn $fn --auto --output $output --right-of $prior
-              set prior $output
-          end
-        end
-        for output in (get-connected-displays)
-          # echo o is $output and a is $argv
-          if not contains $output $argv
-            set fn $fn --output $output --off
-          end
-        end
-        echo $fn;eval $fn
-        wp recall
-      end
+          # xrr DP1 eDP1
+          xrandr --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75 --output DP1 --auto --left-of eDP1
+      case triple
+          xrandr --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75 --output DP1 --auto --left-of eDP1 --output HDMI2 --auto --left-of DP1
+          # case '*'
+          #   set fn xrandr --auto --output $argv[1]
+          # set prior $argv[1]
+          # if test (count $argv) -gt 1
+          #   for output in $argv[2..-1]
+          #       set fn $fn --auto --output $output --right-of $prior
+          #       set prior $output
+          #   end
+          # end
+          # for output in (get-connected-displays)
+          #   # echo o is $output and a is $argv
+          #   if not contains $output $argv
+          #     set fn $fn --output $output --off
+          #   end
+          # end
+          # echo $fn;eval $fn
+          # wp recall
+  end
+  wp recall
 end
