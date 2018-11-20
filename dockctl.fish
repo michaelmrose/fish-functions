4936b722fc06cb892f0ee41a5817a13e8df1ec49
Michael Rose <michael@rosenetwork.net>
2018-11-20 11:09:01 -0800

    editing dockctl.fish

--- dockctl.fish
+++ dockctl.fish
@@ -29,15 +29,16 @@ function dockctl
         fix-touchscreen
       case undock
           ssh desktop xrr triple
-          sudo umount /mnt/michael
-          set -U wallpaperroot /home/michael/backgrounds
+          i3 restart
+          # sudo umount /mnt/michael
+          # set -U wallpaperroot /home/michael/backgrounds
           # sudo ip link set dev wlp2s0 up
-          ssh desktop xrr dual
-          xrandr --output DP1 --off --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75
-          ssh desktop killall synergyc
+          # ssh desktop xrr dual
+          # xrandr --output DP1 --off --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75
+          # ssh desktop killall synergyc
   end
-  setup-keyboard
-for i in (xinput list | gr wacom |nth 10|cut -d = -f2)   
-      xinput map-to-output $i eDP1 2> /dev/null
-  end
+  #   setup-keyboard
+  # for i in (xinput list | gr wacom |nth 10|cut -d = -f2)   
+  #       xinput map-to-output $i eDP1 2> /dev/null
+  #   end
 end
