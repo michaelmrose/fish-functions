0bce096411f296f4697e56d78018a5229c9f39e3
Michael Rose <michael@rosenetwork.net>
2018-11-30 11:40:37 -0800

    editing mute-rest-switch-to.fish

--- mute-rest-switch-to.fish
+++ mute-rest-switch-to.fish
@@ -1,14 +1,20 @@
 # Defined in /home/michael/.config/fish/buffer/mute-rest-switch-to.fish @ line 2
 function mute-rest-switch-to
 	for i in (seq (ponymix list -t sink --short|wc -l))
-        switch (output-type)
-            case $argv
-                ponymix unmute
-                set ndx $i
-            case '*'
-                ponymix mute
-        end
-        fishswitchaudio.fish
+      if string match (output-type) $argv
+          ponymix unmute
+          set ndx $i
+      else
+          ponymix mute
+      end
+      #   switch (output-type)
+      #     case $argv
+      #         ponymix unmute
+      #         set ndx $i
+      #     case '*'
+      #         ponymix mute
+      # end
+      fishswitchaudio.fish
     end
     # while not string match (output-type) $argv
     #     fishswitchaudio.fish
