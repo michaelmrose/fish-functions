604166067918a462675b503bca7b200308e57d23
Michael Rose <michael@rosenetwork.net>
2018-12-01 03:20:12 -0800

    editing from-stdin.fish

--- from-stdin.fish
+++ from-stdin.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/from-stdin.fish @ line 3
+# Defined in /home/michael/.config/fish/buffer/from-stdin.fish @ line 4
 function from-stdin
 	if test -f $tmp
         rm $tmp
