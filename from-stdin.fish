d972008f189d4cfdd5d062545d459932c2a1f396
Michael Rose <michael@rosenetwork.net>
2018-12-01 10:01:16 -0800

    editing from-stdin.fish

--- from-stdin.fish
+++ from-stdin.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/functions/from-stdin.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/from-stdin.fish @ line 2
 function from-stdin
 	#takes a single argument list FN and writes from stdin to a temp file TMP
     #evaluates FN with TMP as an argument.
@@ -6,12 +6,14 @@ function from-stdin
 	  if test -f $tmp
         rm $tmp
     end
-    rm $tmp
     while read -l line
         echo $line >> $tmp
     end
-    # emacsclient -nw $tmp
-    eval $argv $tmp
+    if exists $argv
+        eval $argv $tmp
+    else
+        emacsclient -nw $tmp
+    end
     cat $tmp
     rm $tmp
 end
