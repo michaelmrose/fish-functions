d5b76b2152c55a4b2e614d64106fc2e14296ce0e
Michael Rose <michael@rosenetwork.net>
2018-12-01 10:01:46 -0800

    editing gather.fish

---
+++ gather.fish
@@ -0,0 +1,18 @@
+function gather
+	#takes a single argument list FN and writes from stdin to a temp file TMP
+    #evaluates FN with TMP as an argument.
+    set tmp /tmp/stdin-(uid)
+	  if test -f $tmp
+        rm $tmp
+    end
+    while read -l line
+        echo $line >> $tmp
+    end
+    if exists $argv
+        eval $argv $tmp
+    else
+        emacsclient -nw $tmp
+    end
+    cat $tmp
+    rm $tmp
+end
