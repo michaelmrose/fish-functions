8e7064eebacaf591d8c377317a3aeb883abac80b
Michael Rose <michael@rosenetwork.net>
2018-12-01 23:30:19 -0800

    editing setup-mouse.fish

--- setup-mouse.fish
+++ setup-mouse.fish
@@ -1,8 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/setup-mouse.fish @ line 2
 function setup-mouse
 	mouse-set smooth-scroll on
     mouse-set dpi 900
     xset m 3/2
-    set id (xinput list | gr M510 | nth 5 | cut -d = -f2)
+    # set id (xinput list | gr M510 | nth 5 | cut -d = -f2)
+    set id (xinput list |nav /madcatz n8 c=2)
     xinput --set-prop $id 'Device Accel Adaptive Deceleration' 1.0 
     xinput --set-prop $id 'Device Accel Constant Deceleration' 0.5
     xinput --set-prop $id 'Device Accel Velocity Scaling' 9
