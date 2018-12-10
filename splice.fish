14274afbec3a39c67da980459614ff19cb9ed636
Michael Rose <michael@rosenetwork.net>
2018-12-10 12:39:55 -0800

    editing splice.fish

--- splice.fish
+++ splice.fish
@@ -1,8 +1,14 @@
 # Defined in /home/michael/.config/fish/buffer/splice.fish @ line 2
 function splice
 	set ranges (echo $argv[1] | cut -d , -f1- --output-delimiter=" ")
-    set vals $argv[2..-1]
-    for r in $ranges
-        p $r
+  if test (count $argv) -lt 2
+      while read -l line
+          set vals $vals $line
+      end
+  else
+      set vals $argv[2..-1]
+  end
+  for r in $ranges
+      p $r
     end
 end
