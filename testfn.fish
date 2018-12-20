0fdc8f54e243951d30c5d95f7f1fa962d8e32009
Michael Rose <michael@rosenetwork.net>
2018-12-19 22:18:51 -0800

    editing testfn.fish

--- testfn.fish
+++ testfn.fish
@@ -1,10 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/testfn.fish @ line 2
 function testfn
-	echo $argv
-	  # echo $argv[1]
-	  # msg %self
-    # ps -A | grep %self
-	  # if pstree -s %self | grep 'steam'
-    #   i3-msg move window to workspace r
-    # end
+	switch $argv
+        case bob
+            echo hi bob
+        case '*'
+            echo fu
+    end
 end
