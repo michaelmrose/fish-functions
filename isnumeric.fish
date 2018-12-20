cefeedad3a7467050bd40a441cfa55e5433811f0
Michael Rose <michael@rosenetwork.net>
2018-12-19 22:16:07 -0800

    editing isnumeric.fish

--- isnumeric.fish
+++ isnumeric.fish
@@ -1,5 +1,8 @@
+# Defined in /home/michael/.config/fish/buffer/isnumeric.fish @ line 2
 function isnumeric
 	if exists $argv
         echo $argv | grep -E '^[.0-9]+$' > /dev/null
-    end
+    else
+        return 1
+  end
 end
