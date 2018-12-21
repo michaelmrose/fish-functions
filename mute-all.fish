8ae278458ec97760a557bdae32ab61defa18f24a
Michael Rose <michael@rosenetwork.net>
2018-12-20 22:16:58 -0800

    editing mute-all.fish

---
+++ mute-all.fish
@@ -0,0 +1,8 @@
+# Defined in /home/michael/.config/fish/buffer/mute-all_mute-rest_unmute-all.fish @ line 1
+function mute-all
+	set current (ponymix -t sink defaults --short | awk '{print $2}'|head -1)
+    set sinks (pactl list short sinks | awk '{print $1}')
+    for s in $sinks
+            pactl set-sink-mute $s 1
+    end
+end
