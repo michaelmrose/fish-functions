08820c0c87fb362a702d107e7ac39e67327222b3
Michael Rose <michael@rosenetwork.net>
2018-12-20 22:14:53 -0800

    editing mute-rest.fish

--- mute-rest.fish
+++ mute-rest.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/mute-rest.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/mute-rest.fish @ line 2
 function mute-rest
 	set current (ponymix -t sink defaults --short | awk '{print $2}'|head -1)
     set sinks (pactl list short sinks | awk '{print $1}')
     for s in $sinks
-        if test s -ne $current
+        if test $s -ne $current
             pactl set-sink-mute $s 1
         end
     end
