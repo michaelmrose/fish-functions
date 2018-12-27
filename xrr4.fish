92c98644e967ad81d051ad0e6b807709a5650e6c
Michael Rose <michael@rosenetwork.net>
2018-12-27 10:18:13 -0800

    editing xrr4.fish

--- xrr4.fish
+++ xrr4.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/xrr2_xrr3_xrr4.fish @ line 36
 function xrr4
 	set fn xrandr
-	  set all (xrandr | grep 'connected' | cut -d ' ' -f1)
+	  set all (xrandr | grep ' connected' | cut -d ' ' -f1)
     for display in $argv
         set fn $fn --auto --output $display
         if exists $prior
