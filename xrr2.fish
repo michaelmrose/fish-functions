b8f02de0883a44147420d41651bb99d33b661790
Michael Rose <michael@rosenetwork.net>
2018-12-26 23:56:44 -0800

    editing xrr2.fish

--- xrr2.fish
+++ xrr2.fish
@@ -4,7 +4,7 @@ function xrr2
     set fn xrandr --auto --output $prior
     if test (count $argv) -gt 1
         for display in $argv[2..-1]
-            set fn $fn --auto --output $display --right-of prior 
+            set fn $fn --auto --output $display --right-of $prior 
             set prior output
         end
     end
