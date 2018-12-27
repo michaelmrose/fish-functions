9320b8c75db2608bdbeeb7faca68ec570ec2b7a0
Michael Rose <michael@rosenetwork.net>
2018-12-27 00:08:24 -0800

    editing xrr2.fish

--- xrr2.fish
+++ xrr2.fish
@@ -1,8 +1,10 @@
 # Defined in /home/michael/.config/fish/buffer/xrr2.fish @ line 2
 function xrr2
-	set prior $argv[1]
+	set all (xrandr | grep 'connected' | cut -d ' ' -f1)
+	  set prior $argv[1]
     set fn xrandr --auto --output $prior
     if test (count $argv) -gt 1
+        set rest $argv[2..-1]
         for display in $argv[2..-1]
             set fn $fn --auto --output $display --right-of $prior 
             set prior $display
