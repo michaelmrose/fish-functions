06d809709731bac951a86087a4a11f8026834869
Michael Rose <michael@rosenetwork.net>
2018-12-27 00:15:21 -0800

    editing xrr3.fish

--- xrr3.fish
+++ xrr3.fish
@@ -1,16 +1,16 @@
-# Defined in /home/michael/.config/fish/buffer/xrr2.fish @ line 16
+# Defined in /home/michael/.config/fish/buffer/xrr2_xrr3.fish @ line 17
 function xrr3
 	set all (xrandr | grep 'connected' | cut -d ' ' -f1)
     for display in $all
         if contains $display $argv
             set fn $fn --auto --output $display
             if exists $prior
-                set $fn --right-of $prior
+                set fn $fn --right-of $prior
             else
                 set prior $display
             end
         else
-            set $fn --off --output $display
+            set fn $fn --off --output $display
         end
     end
     echo $fn
