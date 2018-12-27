a4d964c43f2359cfa6c71eee79e424ed625b3ad8
Michael Rose <michael@rosenetwork.net>
2018-12-27 10:52:09 -0800

    editing xrr2.fish

--- xrr2.fish
+++ xrr2.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/xrr2_xrr3_xrr4.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/xrr_xrr2_xrr3_xrr4.fish @ line 47
 function xrr2
 	set all (xrandr | grep 'connected' | cut -d ' ' -f1)
 	  set prior $argv[1]
