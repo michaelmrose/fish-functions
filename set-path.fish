daa67d41de0267c0401e85edacd0abfb6669fbfb
Michael Rose <michael@rosenetwork.net>
2018-12-29 23:25:44 -0800

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -1,10 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/set-path.fish @ line 2
 function set-path
-	# set javapaths /usr/lib/jvm/java-8-oracle/bin /usr/lib/jvm/java-8-oracle/db/bin /usr/lib/jvm/java-8-oracle/jre/bin/
+	set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin ~/scripts
 
-    set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin ~/scripts
-
-    set mypaths /opt/bin /home/michael/bin /opt/android-sdk/platform-tools /home/michael/.cargo/bin /home/michael/extproj/imapnotify/node_modules/imapnotify/bin /home/michael/.gem/ruby/2.5.0/bin /usr/bin/vendor_perl
+    set mypaths /opt/bin /home/michael/bin /opt/android-sdk/platform-tools /home/michael/.cargo/bin /home/michael/extproj/imapnotify/node_modules/imapnotify/bin /home/michael/.gem/ruby/2.5.0/bin /usr/bin/vendor_perl /home/michael/extproj/fish-shell/build
 
     set -xg PATH $mypaths $javapaths $builtinpaths
 end
