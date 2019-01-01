3386edeff52376c1bca4bd1695687a75b7031d4f
Michael Rose <michael@rosenetwork.net>
2019-01-01 10:45:18 -0800

    editing rb.fish

--- rb.fish
+++ rb.fish
@@ -1,5 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/rb.fish @ line 2
 function rb
-	manage-share ensure-unmounted
+	# manage-share ensure-unmounted
   cleanup-desktop
 	sudo shutdown -r now
 end
