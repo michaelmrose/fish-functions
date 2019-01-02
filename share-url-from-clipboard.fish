9384a642a6402c5dca1fa189038227d160de73c9
Michael Rose <michael@rosenetwork.net>
2019-01-02 14:22:00 -0800

    editing share-url-from-clipboard.fish

--- share-url-from-clipboard.fish
+++ share-url-from-clipboard.fish
@@ -8,5 +8,5 @@ function share-url-from-clipboard
     # echo the fucking url is $url
     # p "Subject: $title" "$url" | msmtp $target
     # p "Subject: $title" $url |msmtp $target
-    echo "Subject: $subject" "\n\n" \"$url\"|msmtp $target
+    p "Subject: $subject" " " "$url"|msmtp $target
 end
