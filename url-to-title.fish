11fb231e09be67968399ca9c150f9eb3f17e9000
Michael Rose <michael@rosenetwork.net>
2019-01-02 13:02:12 -0800

    editing url-to-title.fish

--- url-to-title.fish
+++ url-to-title.fish
@@ -1,5 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/url-to-title.fish @ line 2
 function url-to-title
 	set url (string escape $argv)
-	  wget -qO- $argv | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..
+	wget -qO- "$argv" | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..
 end
