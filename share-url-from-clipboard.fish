d01ac2db37c5cd13af418ce03fabcc8c4d7c2533
Michael Rose <michael@rosenetwork.net>
2019-01-02 13:34:35 -0800

    editing share-url-from-clipboard.fish

--- share-url-from-clipboard.fish
+++ share-url-from-clipboard.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/share-url-from-clipboard.fish @ line 2
 function share-url-from-clipboard
-	set url (xclip -o)
+	set url (xclip -selection clipboard -o)
     set target $argv[1]
     set title (url-to-title $url)
     echo sharing url "$url" title "$title" to "$target"
