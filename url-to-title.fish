fabaf02ca18856e7604c0074be830a77d59fdf81
Michael Rose <michael@rosenetwork.net>
2019-01-02 12:59:27 -0800

    editing url-to-title.fish

--- url-to-title.fish
+++ url-to-title.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/url-to-title.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/url-to-title.fish @ line 2
 function url-to-title
-	wget -qO- 'http://www.youtube.com/watch?v=Dd7dQh8u4Hc' | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..
+	set url (string escape $argv)
+	  wget -qO- $url | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..
 end
