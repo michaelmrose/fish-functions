2570cfc97c6c4f088959e894ca498bd1b6e9ac72
Michael Rose <michael@rosenetwork.net>
2019-01-02 13:41:12 -0800

    editing send-email.fish

--- send-email.fish
+++ send-email.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/send-email.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/send-email_share-url-from-clip.fish @ line 2
 function send-email
 	set target $argv[1]
     set sub $argv[2]
