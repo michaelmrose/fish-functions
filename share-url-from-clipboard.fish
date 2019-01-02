5baec5dcf00bdcd58b3b70c9195f7edbfe6acba7
Michael Rose <michael@rosenetwork.net>
2019-01-02 15:11:38 -0800

    editing share-url-from-clipboard.fish

--- share-url-from-clipboard.fish
+++ share-url-from-clipboard.fish
@@ -10,6 +10,7 @@ function share-url-from-clipboard
     # p "Subject: $title" $url |msmtp $target
     rm /tmp/anemail.txt
     echo "Subject: $subject">>/tmp/anemail.txt
+    echo >> /tmp/anemail.txt
     echo "$url" >>/tmp/anemail.txt
     cat /tmp/anemail.txt | msmtp $target
 end
