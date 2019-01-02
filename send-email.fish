2c23a5bb44191e30a795ccc8a29864c5980be7b4
Michael Rose <michael@rosenetwork.net>
2019-01-02 13:24:34 -0800

    editing send-email.fish

--- send-email.fish
+++ send-email.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/send-email.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/send-email.fish @ line 2
 function send-email
-	printf "Subject: {$argv[2]}nn{$argv[3..-1]}" | msmtp $argv[1]
+	printf "Subject: $argv[2]nn$argv[3..-1]" | msmtp $argv[1]
 end
