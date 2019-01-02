6864045f8154b34fbe50a982da3cf5b4b8db7287
Michael Rose <michael@rosenetwork.net>
2019-01-02 14:13:08 -0800

    editing share-url-from-clipboard.fish

--- share-url-from-clipboard.fish
+++ share-url-from-clipboard.fish
@@ -3,9 +3,10 @@ function share-url-from-clipboard
 	set url (xclip -selection clipboard -o)
     set target $argv[1]
     set title (url-to-title $url)
-    echo sharing url "$url" title "$title" to "$target"
+    # echo sharing url "$url" title "$title" to "$target"
     # echo send-email "$target" "$title" "$url"
-    echo the fucking url is $url
+    # echo the fucking url is $url
     # p "Subject: $title" "$url" | msmtp $target
-    p "Subject: $title" $url |msmtp $target
+    # p "Subject: $title" $url |msmtp $target
+    echo "Subject: $subject" \n\n\"$url\"|msmtp $target
 end
