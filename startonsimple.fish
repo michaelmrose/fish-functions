a553822735872173bffd46d8f948107e34c4103a
Michael Rose <michael@rosenetwork.net>
2019-01-03 23:53:36 -0800

    editing startonsimple.fish

--- startonsimple.fish
+++ startonsimple.fish
@@ -1,20 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/starton_startonsimple.fish @ line 27
+# Defined in /home/michael/.config/fish/buffer/starton_startonsimple.fish @ line 28
 function startonsimple
 	set workspace $argv[1]
     set app $argv[2..-1]
     set winclass (return-windowclass $app)
     set layout /tmp/(uuidgen)-layout
     save-workspaces
-    set json '{
-    "swallows": [
-    {
-    "class": "^#winclass$"
-    }
-    ],
-    "type": "con"
-    }'
-
-    # p $json | sed "s/#winclass/$winclass/g" > $layout
+    set json '{"swallows": [{"class": "^#winclass$"}],"type": "con"}'
     p $json | sd \#winclass $winclass > $layout
     i3 workspace $workspace
     i3 append_layout $layout
