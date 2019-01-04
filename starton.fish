b49dfbc4ab85ed3cfa69420041358d4644b8be78
Michael Rose <michael@rosenetwork.net>
2019-01-03 23:26:43 -0800

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/startiton.fish @ line 34
+# Defined in /home/michael/.config/fish/buffer/starton.fish @ line 2
 function starton
 	set visible (i3-msg -t get_workspaces|jq .[]|jq -r "select(.visible == true).name" )
     set current (i3-msg -t get_workspaces|jq .[]|jq -r "select(.focused == true).name" )
@@ -10,4 +10,5 @@ function starton
     ],
     "type": "con"
     }'
+    p $json
 end
