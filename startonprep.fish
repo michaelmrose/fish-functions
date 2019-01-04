ad9c2f1ba9b818c0db6130984300dd666a828d85
Michael Rose <michael@rosenetwork.net>
2019-01-04 13:04:44 -0800

    editing startonprep.fish

---
+++ startonprep.fish
@@ -0,0 +1,13 @@
+# Defined in /home/michael/.config/fish/buffer/starton_startonprep_startonsim.fish @ line 27
+function startonprep
+	set workspace $argv[1]
+    set app $argv[2..-1]
+    set winclass (return-windowclass $app)
+    set layout /tmp/(uuidgen)-layout
+    save-workspaces
+    set json '{"swallows": [{"class": "^#winclass$"}],"type": "con"}'
+    p $json | sd \#winclass $winclass > $layout
+    i3 workspace $workspace
+    i3 append_layout $layout
+    restore-workspaces
+end
