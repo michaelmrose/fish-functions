a32b0a9f9e08ea7f64d6eb325ad03fc1c6a926c3
Michael Rose <michael@rosenetwork.net>
2019-01-03 23:52:11 -0800

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/startiton_starton.fish @ line 35
+# Defined in /home/michael/.config/fish/buffer/starton_startonsimple.fish @ line 2
 function starton
 	set workspace $argv[1]
     set app $argv[2..-1]
