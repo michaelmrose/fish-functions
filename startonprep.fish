5c41070e7e8f2f30adcb1e5428b94f439682de14
Michael Rose <michael@rosenetwork.net>
2019-01-04 13:12:18 -0800

    editing startonprep.fish

--- startonprep.fish
+++ startonprep.fish
@@ -1,13 +1,18 @@
-# Defined in /home/michael/.config/fish/buffer/starton_startonprep_startonsim.fish @ line 27
+# Defined in /home/michael/.config/fish/buffer/starton_startonprep_startonsim.fish @ line 28
 function startonprep
-	set workspace $argv[1]
-    set app $argv[2..-1]
-    set winclass (return-windowclass $app)
-    set layout /tmp/(uuidgen)-layout
-    save-workspaces
+	save-workspaces
+    set workspaces (odds $argv)
+    set apps (evens $argv)
     set json '{"swallows": [{"class": "^#winclass$"}],"type": "con"}'
-    p $json | sd \#winclass $winclass > $layout
-    i3 workspace $workspace
-    i3 append_layout $layout
+    set 
+    for i in (range (count $workspaces))
+        set app $apps[$i]
+        set workspace $workspaces[$i]
+        set winclass (return-windowclass $app)
+        set layout /tmp/(uuidgen)-layout
+        p $json | sd \#winclass $winclass > $layout
+        i3 workspace $workspace
+        i3 append_layout $layout
+    end
     restore-workspaces
 end
