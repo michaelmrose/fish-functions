31048ab2a6bd276d5a249213485cfa17da6cf536
Michael Rose <michael@rosenetwork.net>
2019-01-04 00:02:06 -0800

    editing nothing-urgent.fish

--- nothing-urgent.fish
+++ nothing-urgent.fish
@@ -1,5 +1,9 @@
+# Defined in /home/michael/.config/fish/buffer/nothing-urgent.fish @ line 2
 function nothing-urgent
-	for i in (windows-list class)
-        noturgent $i
+	# for i in (windows-list class)
+  #       noturgent $i
+  #   end
+    for i in (xdotool search --class .\*)
+        xdotool set_window --urgency 0 $i
     end
 end
