6106fedb4403079f2dfb36476ce1d6b9f5a17edb
Michael Rose <michael@rosenetwork.net>
2019-01-03 23:20:56 -0800

    editing nonsense.fish

--- nonsense.fish
+++ nonsense.fish
@@ -1,4 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/nonsense.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/nonsense.fish @ line 2
 function nonsense
-	echo \$ \( \'
+	i3 workspace z
+    kitty -1
+    i3 workspace 3
 end
