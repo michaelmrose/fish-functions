95e4d642abd314a301681c3d63dadecda20bb876
Michael Rose <michael@rosenetwork.net>
2019-01-04 11:57:36 -0800

    editing newfuckingweather.fish

--- newfuckingweather.fish
+++ newfuckingweather.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/newfuckingweather.fish @ line 2
 function newfuckingweather
 	curl wttr.in|pup body > /tmp/weather.out
-  set status (cat /tmp/weather.out | n l4 | cut -c 31-)
+  set weather (cat /tmp/weather.out | n l4 | cut -c 31-)
   set temp (cat /tmp/weather.out | n l5 | cut -c 79-)
-  echo $status $temp
+  echo $weather $temp
 end
