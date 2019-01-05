967c26ae14b1934633312b450c74a57b48910fb3
Michael Rose <michael@rosenetwork.net>
2019-01-04 19:52:23 -0800

    editing initial-apps.fish

--- initial-apps.fish
+++ initial-apps.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/initial-apps.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/initial-apps.fish @ line 2
 function initial-apps
 	startonprep f (appkey e) c (appkey E) v (appkey t) g (appkey b)
-    eval (appkey e)
-    eval (appkey E)
-    eval (appkey t)
-    eval (appkey b)
+  eval (appkey e) &
+  eval (appkey E) &
+  eval (appkey t) &
+  eval (appkey b) &
 end
