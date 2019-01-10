c195cf39925cc4837d364747b7fed3bf88c9a4bb
Michael Rose <michael@rosenetwork.net>
2019-01-09 20:14:41 -0800

    editing weather.fish

--- weather.fish
+++ weather.fish
@@ -1,4 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/geo_newfuckingweather_weather.fish @ line 13
 function weather
-	curl "wttr.in/(geo)$argv"
+	set g (geo)
+	  curl "wttr.in/$g$argv"
 end
