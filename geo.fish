f5853890dcd762e2c3c601dd18c8c6cbfe3eaf63
Michael Rose <michael@rosenetwork.net>
2019-01-09 20:23:17 -0800

    editing geo.fish

--- geo.fish
+++ geo.fish
@@ -1,4 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/geo_newfuckingweather_weather.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/geo.fish @ line 2
 function geo
-	echo "47.56626170000001,-122.6264645"
+	if exists argv
+        ./home/michael/extproj/whereami/node_modules/@rafaelrinaldi/whereami/./cli.js -r |n /'lat|long' c:2 t|condense_lines | sd " " ""
+    else
+	      echo "47.56626170000001,-122.6264645"
+    end
 end
