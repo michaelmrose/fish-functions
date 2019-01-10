2079e1bce8dce5127247bb1280c29e4006886a16
Michael Rose <michael@rosenetwork.net>
2019-01-09 20:23:29 -0800

    editing geo.fish

--- geo.fish
+++ geo.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/geo.fish @ line 2
 function geo
-	if exists argv
+	if exists $argv
         ./home/michael/extproj/whereami/node_modules/@rafaelrinaldi/whereami/./cli.js -r |n /'lat|long' c:2 t|condense_lines | sd " " ""
     else
 	      echo "47.56626170000001,-122.6264645"
