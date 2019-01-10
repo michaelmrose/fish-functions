e553b092fb8da2f9f8189390953309f845ef2d86
Michael Rose <michael@rosenetwork.net>
2019-01-10 15:39:36 -0800

    editing get-focused-display-resolution.fish

--- get-focused-display-resolution.fish
+++ get-focused-display-resolution.fish
@@ -1,10 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/get-focused-display-resolution.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/get-display-position_get-focus.fish @ line 12
 function get-focused-display-resolution
-	# set res (xrandr --verbose | grep " connected" | grep (get-focused-display) | cut -d "x" -f1-2 | cut -d "+" -f1)
-  #   set res (explode $res)
-  #   set res $res[-1]
-  #   echo $res
-	# xrandr --verbose | grep " connected" | grep (get-focused-display) | cut -d "x" -f3 | cut -d "+" -f1
-# string match -r '^[0-9].*' (explode-words (xrandr --verbose | grep ' connected' | grep (get-focused-display))) | grep x | cut -d '+' -f1
-    string match -r '^[0-9].*' (explode-words (xrandr --verbose | grep ' connected' | cut -d ' ' -f1-4 | grep \^(get-focused-display))) | cut -d '+' -f1
+	get-display-position (get-focused-display)
+    #   #   set res (explode $res)
+    #   #   set res $res[-1]
+    #   #   echo $res
+    # 	# xrandr --verbose | grep " connected" | grep (get-focused-display) | cut -d "x" -f3 | cut -d "+" -f1
+    # # string match -r '^[0-9].*' (explode-words (xrandr --verbose | grep ' connected' | grep (get-focused-display))) | grep x | cut -d '+' -f1
+    #     string match -r '^[0-9].*' (explode-words (xrandr --verbose | grep ' connected' | cut -d ' ' -f1-4 | grep \^(get-focused-display))) | cut -d '+' -f1
 end
