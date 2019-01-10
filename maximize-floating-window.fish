43a052a2c1757517e9806470f461c5be2826d0fc
Michael Rose <michael@rosenetwork.net>
2019-01-10 15:50:38 -0800

    editing maximize-floating-window.fish

--- maximize-floating-window.fish
+++ maximize-floating-window.fish
@@ -1,7 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/maximize-floating-window.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/maximize-floating-window.fish @ line 2
 function maximize-floating-window
-	set pos (get-focused-display-position)
-    set size (calculate-available-space)
-    i3 move absolute position $pos
-    i3 resize set $size
+	i3 move absolute position (get-focused-display-position)
+    i3 resize set (calculate-available-space)
 end
