37c90aa0ed36c0169868614c3c8b560b0972171b
Michael Rose <michael@rosenetwork.net>
2019-01-10 15:38:22 -0800

    editing get-display-position.fish

--- get-display-position.fish
+++ get-display-position.fish
@@ -1,3 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/get-display-position.fish @ line 1
 function get-display-position
-	i3-msg -t get_outputs|jq -r '.[] | select(.name == "DP1") | {x: .rect.x, y: .rect.y}|.[]'
+	i3-msg -t get_outputs|jq -r ".[] | select(.name == \"DP1\") | {x: .rect.x, y: .rect.y}|.[]"
 end
