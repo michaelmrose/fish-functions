7a62c5809801f64c4fb91c1dba1d2cbabe6164f0
Michael Rose <michael@rosenetwork.net>
2019-01-09 18:52:46 -0800

    editing weather.fish

--- weather.fish
+++ weather.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/weather.fish @ line 2
 function weather
-	echo curl "wttr.in/47.56626170000001,-122.6264645{$argv}"
+	echo curl "wttr.in/47.56626170000001,-122.6264645$argv"
 end
