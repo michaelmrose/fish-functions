3ce06ba5bf4df2bc869b119c28990798035be0bd
Michael Rose <michael@rosenetwork.net>
2019-01-10 15:39:36 -0800

    editing get-display-position.fish

--- get-display-position.fish
+++ get-display-position.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/get-display-position.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/get-display-position_get-focus.fish @ line 2
 function get-display-position
 	i3-msg -t get_outputs|jq -r ".[] | select(.name == \"$argv\") | {x: .rect.x, y: .rect.y}|.[]"
 end
