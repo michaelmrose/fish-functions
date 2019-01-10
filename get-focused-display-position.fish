8c73c9e0a10fdabb66c36347bca7a027a7d38ae1
Michael Rose <michael@rosenetwork.net>
2019-01-10 15:58:58 -0800

    editing get-focused-display-position.fish

--- get-focused-display-position.fish
+++ get-focused-display-position.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/calculate-available-space_get-.fish @ line 17
 function get-focused-display-position
-	get-display-position (get-focused-display)|condense_lines
+	get-display-position (get-focused-display) |tr '\n' ' '
 end
