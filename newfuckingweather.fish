e87d124af4127c64bd5b3f3523972b795eefe4e0
Michael Rose <michael@rosenetwork.net>
2019-01-09 20:13:53 -0800

    editing newfuckingweather.fish

--- newfuckingweather.fish
+++ newfuckingweather.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/newfuckingweather.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/newfuckingweather_weather.fish @ line 2
 function newfuckingweather
 	curl wttr.in/47.56626170000001,-122.6264645\?0\?T\?Q|cut -c 16- |condense_lines|condense_spaces
     return 0
