f1bb1f12e6d8db36fbb60cb040a6a7d2bf34778a
Michael Rose <michael@rosenetwork.net>
2019-01-10 15:43:52 -0800

    editing calculate-available-space.fish

--- calculate-available-space.fish
+++ calculate-available-space.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/calculate-available-space.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/calculate-available-space_get-.fish @ line 2
 function calculate-available-space
-	i3-msg -t get_workspaces|jq -r ".[] | select(.focused == true)|{w: .rect.width, h: .rect.height}| .[]"
+	i3-msg -t get_workspaces|jq -r ".[] | select(.focused == true)|{w: .rect.width, h: .rect.height}| .[]" |condense_lines
 end
