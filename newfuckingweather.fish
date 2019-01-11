b7a6cd3a40109ff363bda7442b830427effd961d
Michael Rose <michael@rosenetwork.net>
2019-01-10 21:32:15 -0800

    editing newfuckingweather.fish

--- newfuckingweather.fish
+++ newfuckingweather.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/geo_newfuckingweather_weather.fish @ line 7
+# Defined in /home/michael/.config/fish/buffer/newfuckingweather.fish @ line 2
 function newfuckingweather
-	set g (geo)
-	  curl wttr.in/$g\?0\?T\?Q|cut -c 16- |condense_lines|condense_spaces
+	set g (geo find)
+	  curl wttr.in/$g\?0\?T\?Q\?u|cut -c 16- |condense_lines|condense_spaces
     return 0
 end
