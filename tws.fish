0f338708f34b2ba77c5f1e4c7f11e419dd361069
Michael Rose <michael@rosenetwork.net>
2019-01-14 02:55:13 -0800

    editing tws.fish

--- tws.fish
+++ tws.fish
@@ -1,3 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/gws_tws_vi3_get-workspace_vi3_.fish @ line 7
 function tws
-	vi3_take-window-to-workspace $argv; 
+	vi3_take-window-to-workspace $argv;
 end
