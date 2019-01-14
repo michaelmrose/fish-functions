d75a6249b149fd63cf84fefdbe8d6bd359818767
Michael Rose <michael@rosenetwork.net>
2019-01-14 13:16:20 -0800

    editing weather-icon.fish

--- weather-icon.fish
+++ weather-icon.fish
@@ -1,3 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/weather-icon.fish @ line 2
 function weather-icon
-	weather $geo --iconify | rev | cut -c1-3
+	# weather $geo --iconify | rev | cut -c1-3
+/home/michael/extproj/IconicWeather/IconicWeather.sh 'NAM|US|WA|BREMERTON'|nth 1
 end
