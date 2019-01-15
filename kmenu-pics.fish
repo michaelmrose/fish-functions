b412f2be7af928ad26f7c8cd1df18a5be2de90b9
Michael Rose <michael@rosenetwork.net>
2019-01-14 19:37:59 -0800

    editing kmenu-pics.fish

--- kmenu-pics.fish
+++ kmenu-pics.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/kmenu_kmenu-pics.fish @ line 13
+# Defined in /home/michael/.config/fish/buffer/kmenu_kmenu-pics.fish @ line 16
 function kmenu-pics
 	xdotool key Menu
     sleep 0.1
