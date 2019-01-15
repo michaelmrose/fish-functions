e9bbd311484c2fa141c9dd777b97d0167d0c9e98
Michael Rose <michael@rosenetwork.net>
2019-01-14 23:06:56 -0800

    editing wp.fish

---
+++ wp.fish
@@ -0,0 +1,22 @@
+# Defined in /home/michael/.config/fish/buffer/wallpaper_wp.fish @ line 256
+function wp
+	if test -f $argv[1]
+        set-wallpaper $argv[1]
+    else
+        set $rest $argv[2..-1]
+        switch $argv[1]
+            case view
+                pics (get-folder-for-backgrounds $rest)
+            case categories
+                find $wallpaperroot -type d | each cutlast /
+            case edit
+                gimp $bgimage
+                wp $bgimage
+            case recent
+                wp (sxiv -tbfor $recent_backgrounds 2> /dev/null)
+
+                
+
+        end
+    end
+end
