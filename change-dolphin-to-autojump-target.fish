c3df8615903bedeb6287d7d64ec327f9eb791c85
Michael Rose <michael@rosenetwork.net>
2019-01-14 19:59:41 -0800

    editing change-dolphin-to-autojump-target.fish

--- change-dolphin-to-autojump-target.fish
+++ change-dolphin-to-autojump-target.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/change-dolphin-dir_change-dolp.fish @ line 13
 function change-dolphin-to-autojump-target
 	set target (autojump (rfi enter))
-  if test string length $target -gt 1
+  if test (string length $target) -gt 1
       change-dolphin-dir $target
   else
       msg no target
