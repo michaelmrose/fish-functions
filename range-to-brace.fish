0a2c63b253f3df42cb844f1c66dfea8b8f3f271a
Michael Rose <michael@rosenetwork.net>
2019-01-15 10:14:54 -0800

    editing range-to-brace.fish

--- range-to-brace.fish
+++ range-to-brace.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/range-to-brace.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/range-to-brace.fish @ line 2
 function range-to-brace
 	set acc "{"
     for i in (seq $argv[1] $argv[2])
         set acc $i{$acc},
     end
-    echo $acc[1..-2]\}
+    echo $acc[1..-2]
 end
