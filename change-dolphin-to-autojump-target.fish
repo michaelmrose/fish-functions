f8431b7379063fe5bdddf1699003ac8d729e5f10
Michael Rose <michael@rosenetwork.net>
2019-01-14 19:49:51 -0800

    editing change-dolphin-to-autojump-target.fish

---
+++ change-dolphin-to-autojump-target.fish
@@ -0,0 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/change-dolphin-dir_change-dolp.fish @ line 12
+function change-dolphin-to-autojump-target
+	change-dolphin-dir (autojump (rfi enter))
+end
