895435d6ae0684fbff62683d32fed076cd3a50bd
Michael Rose <michael@rosenetwork.net>
2019-01-15 10:16:27 -0800

    editing range-to-brace.fish

--- range-to-brace.fish
+++ range-to-brace.fish
@@ -3,7 +3,7 @@ function range-to-brace
 	set acc "{"
     for i in (seq $argv[1] $argv[2])
         echo acc is $acc
-        set acc $i$acc,
+        set acc $acc$i,
     end
     echo $acc[1..-2]
 end
