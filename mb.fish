499d75c7816d38b8bb536c6c6407e7c366ecfb66
Michael Rose <michael@rosenetwork.net>
2019-01-14 19:30:55 -0800

    editing mb.fish

--- mb.fish
+++ mb.fish
@@ -1,4 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/mb.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/mb.fish @ line 2
 function mb
-	xdotool key Menu a p
+	xdotool key Menu
+    sleep 0.1
+    xdotool key a
+    sleep 0.1
+    xdotool key p
 end
