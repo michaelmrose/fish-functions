5f72bf7f1fc6adb9c40a9b31c270db48ef7cb475
Michael Rose <michael@rosenetwork.net>
2019-01-18 12:21:31 -0800

    editing sus.fish

--- sus.fish
+++ sus.fish
@@ -1,7 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/sus.fish @ line 2
 function sus
-	# manage-share ensure-unmounted
-  # sudo pm-suspend
+	
   # wp recall
   # if [ (whoami) = michael ]
   #   setup-keyboard
@@ -14,17 +13,17 @@ function sus
   #   end
   # end
   # manage-share ensure-mounted
-  sudo rmmod wacom
-  systemctl suspend;
-  sleep 10
-  fix-touchscreen
-  setup-keyboard
-  xrr triple
-  sleep 1
-  i3 restart
-  # signal-i3blocks battery
-  # sleep 10
-  # if docked
-  #     dockctl dock
-  # end
+    # sudo rmmod wacom
+    systemctl suspend;
+    # sleep 10
+    # fix-touchscreen
+    # setup-keyboard
+    # xrr triple
+    # sleep 1
+    # i3 restart
+    # signal-i3blocks battery
+    # sleep 10
+    # if docked
+    #     dockctl dock
+    # end
 end
