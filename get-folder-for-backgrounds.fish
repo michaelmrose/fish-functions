0bdb8e0b3ce6443b5c3c3be883ca7a2ceb3e2690
Michael Rose <michael@rosenetwork.net>
2019-01-23 19:11:39 -0800

    editing get-folder-for-backgrounds.fish

--- get-folder-for-backgrounds.fish
+++ get-folder-for-backgrounds.fish
@@ -1,12 +1,14 @@
+# Defined in /home/michael/.config/fish/buffer/get-folder-for-backgrounds.fish @ line 2
 function get-folder-for-backgrounds
-	if match $argv any
-        echo $wallpaperroot
-        return 0
-    end
-    set res (find $wallpaperroot -type d | grep $argv | head -1)
-    if test (count $res) -ne 1
-        return 1
-    else
-        echo $res
-    end
+	
+    #       echo $wallpaperroot
+    #       return 0
+    #   end
+    # set res (find $wallpaperroot -type d | grep $argv | head -1)
+    # if test (count $res) -ne 1
+    #     return 1
+    # else
+    #     echo $res
+    # end
+    find $wallpaperroot -type d | grep $argv | head -1
 end
