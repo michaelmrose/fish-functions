9d69bf1eda7b351081b845c455765ac63b4cebb9
Michael Rose <michael@rosenetwork.net>
2019-01-24 11:35:28 -0800

    editing songtitle.fish

--- songtitle.fish
+++ songtitle.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/songtitle.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/songtitle.fish @ line 2
 function songtitle
-	playerctl metadata title
+	playerctl metadata title | cut -c1-20
 end
