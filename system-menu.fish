3b24be4a93173f4c192fd19542284e9c30b094c6
Michael Rose <michael@rosenetwork.net>
2019-01-24 12:49:07 -0800

    editing system-menu.fish

--- system-menu.fish
+++ system-menu.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/system-menu.fish @ line 2
 function system-menu
-	switch (p Lightsout Suspend Shutdown Switch Reboot | rofi -dmenu -i -p System:)
+	switch (p Lightsout Suspend Shutdown "Switch User" Reboot | rofi -dmenu -i -p System:)
         case Lightsout
             lightsout
         case Suspend
@@ -9,7 +9,7 @@ function system-menu
           msg shutting down
       case Reboot
           msg rebooting
-      case Switch
+        case "Switch User"
             dm-tool switch-to-greeter
         end
 end
