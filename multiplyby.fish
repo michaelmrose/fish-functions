dd58e377399ebdc387207ad3b289d79c037cc4c8
Michael Rose <michael@rosenetwork.net>
2019-01-23 21:49:34 -0800

    editing multiplyby.fish

--- multiplyby.fish
+++ multiplyby.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/divideby_multiplyby.fish @ line 8
+# Defined in /home/michael/.config/fish/buffer/divideby_multiplyby.fish @ line 9
 function multiplyby
 	while read -l line
-        eval math "$line * $argv"
+        eval math "$line \* $argv"
     end
 end
