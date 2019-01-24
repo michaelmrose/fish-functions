555ea86756a560fb58ce2130c8b3dfc215f75a8b
Michael Rose <michael@rosenetwork.net>
2019-01-23 21:37:30 -0800

    editing meminfo.fish

--- meminfo.fish
+++ meminfo.fish
@@ -1,7 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/meminfo.fish @ line 2
 function meminfo
 	
-    #       set val (math (pmap -x $i | grep total | condense_spaces | cut -d " " -f5) / 1024)
     #       set acc (math "$acc" + "$val")
     #   end
     #   echo {$acc}M
@@ -10,9 +9,9 @@ function meminfo
     #   for i in (pgrep $argv);pmap -x $i | gr total | nth 3;end | sumof
     set res (pgrep $argv |each pmap -x|g total|nth 4 |sumof)
     if test $res -gt 1023
-        set suffix K
-    else
         set suffix M
+    else
+        set suffix K
     end
     echo $res$suffix
 end
