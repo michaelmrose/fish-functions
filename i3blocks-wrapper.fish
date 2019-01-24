2afbc4017d8babdab21bdaa491db9f11b387d6b3
Michael Rose <michael@rosenetwork.net>
2019-01-24 11:04:09 -0800

    editing i3blocks-wrapper.fish

--- i3blocks-wrapper.fish
+++ i3blocks-wrapper.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/i3blocks-wrapper.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/i3blocks-wrapper.fish @ line 2
 function i3blocks-wrapper
 	set click $argv[1]
     set output $argv[2]
     if exists $BLOCK_BUTTON
-        $click
+        $click > /dev/null
     else
         $output
     end
