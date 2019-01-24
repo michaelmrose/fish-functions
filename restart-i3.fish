e687e986570b706e0b29d98cb69eb396027f1522
Michael Rose <michael@rosenetwork.net>
2019-01-23 16:50:50 -0800

    editing restart-i3.fish

--- restart-i3.fish
+++ restart-i3.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/restart-i3.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/restart-i3.fish @ line 2
 function restart-i3
 	killall compton
     i3 restart
     compton &
-    i3 restart
+    sleep 0.5
     wmctrl -l|awk '{print $1}'|each remove-opacity-from-window
 end
