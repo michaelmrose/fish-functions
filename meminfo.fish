a4afd87c64a7d22a42b744031d2d43772e044df1
Michael Rose <michael@rosenetwork.net>
2019-01-23 21:50:16 -0800

    editing meminfo.fish

--- meminfo.fish
+++ meminfo.fish
@@ -1,11 +1,12 @@
 # Defined in /home/michael/.config/fish/buffer/meminfo.fish @ line 2
 function meminfo
-	set res (pgrep $argv |each pmap -x|g total|nth 4 |sumof)
-    if test $res -gt 1023
-        set suffix M
-        set res (echo $res |divideby 1024)
-    else
-        set suffix K
-    end
-    echo $res$suffix
+	
+    #   if test $res -gt 1023
+    #       set suffix M
+    #       set res (echo $res |divideby 1024)
+    #   else
+    #       set suffix K
+    #   end
+    #   echo $res$suffix
+	  human (pgrep $argv |each pmap -x|g total|nth 4 |sumof|multiplyby 1024)
 end
