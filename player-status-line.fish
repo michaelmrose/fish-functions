dc4c9a9738557009a524fb5c3451e857e362150d
Michael Rose <michael@rosenetwork.net>
2019-01-24 12:54:03 -0800

    editing player-status-line.fish

--- player-status-line.fish
+++ player-status-line.fish
@@ -1,10 +1,11 @@
+# Defined in /home/michael/.config/fish/buffer/player-status-line.fish @ line 2
 function player-status-line
 	set playerStatus (apctl status)
 	if contains $playerStatus Playing Paused
-    set title (apctl metadata title)
-    set artist (apctl metadata artist)
-    set symbol (match-lists $playerStatus "Playing Paused" " " "")
-    echo $symbol $title by $artist
+      set title (apctl metadata title | cut -c1-20)
+      set artist (apctl metadata artist)
+      set symbol (match-lists $playerStatus "Playing Paused" " " "")
+      echo $symbol $title by $artist
   else
     echo None
   end
