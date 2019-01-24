e58d7deb304ff5c8858bfc194b46b35257f386c9
Michael Rose <michael@rosenetwork.net>
2019-01-23 20:04:25 -0800

    editing get-image-aspect-ratio-type.fish

--- get-image-aspect-ratio-type.fish
+++ get-image-aspect-ratio-type.fish
@@ -1,5 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/get-image-aspect-ratio-type.fish @ line 2
 function get-image-aspect-ratio-type
 	set ratio (get-aspect-ratio $argv) 
     set integer (truncate-num (wcalc -q "$ratio * 100"))
-    switchonval $integer 1-132 narrow 133-230 wide 231-249 extrawide 250-900 superwide
+    echo $integer
+    # switchonval $integer 1-132 narrow 133-230 wide 231-249 extrawide 250-900 superwide
 end
