f7083bc4204d4da03c4e5975d1b11142f2d63723
Michael Rose <michael@rosenetwork.net>
2019-01-23 21:21:13 -0800

    editing divideby.fish

--- divideby.fish
+++ divideby.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/divideby.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/divideby.fish @ line 2
 function divideby
 	while read -l line
-        math \\ $argv
-    end
+      eval math "$line / $argv"
+  end
 end
