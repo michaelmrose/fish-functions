d839a13b659f7dcc240b002f497e21e80f0925f3
Michael Rose <michael@rosenetwork.net>
2019-01-23 21:40:36 -0800

    editing human-byte-count.fish

---
+++ human-byte-count.fish
@@ -0,0 +1,12 @@
+# Defined in /home/michael/.config/fish/buffer/human-byte-count.fish @ line 1
+function human-byte-count
+	awk 'function human(x) {
+    s=" B   KiB MiB GiB TiB EiB PiB YiB ZiB"
+    while (x>=1024 && length(s)>1) 
+    {x/=1024; s=substr(s,5)}
+    s=substr(s,1,4)
+    xf=(s==" B  ")?"%5d   ":"%8.2f"
+    return sprintf( xf"%s\n", x, s)
+    }
+    {gsub(/^[0-9]+/, human($argv)); print}'
+end
