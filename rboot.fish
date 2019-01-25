f7491c340a68f2ff93740c47129835e274763d85
Michael Rose <michael@rosenetwork.net>
2019-01-25 11:56:40 -0800

    editing rboot.fish

--- rboot.fish
+++ rboot.fish
@@ -1,3 +1,6 @@
+# Defined in /home/michael/.config/fish/buffer/rb_rboot_sdown.fish @ line 9
 function rboot
-	sudo shutdown -r now
+	sudo umount -fl /mnt/michael
+    cleanup-desktop
+	  sudo shutdown -r now
 end
