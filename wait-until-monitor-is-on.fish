92b8d686d96a080d2d5413dcab958b027cce8667
Michael Rose <michael@rosenetwork.net>
2019-01-24 20:18:56 -0800

    editing wait-until-monitor-is-on.fish

--- wait-until-monitor-is-on.fish
+++ wait-until-monitor-is-on.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/wait-until-monitor-is-on.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/lightsout_wait-until-monitor-i.fish @ line 16
 function wait-until-monitor-is-on
 	while true
         if xset -q |g 'monitor is on' > /dev/null
