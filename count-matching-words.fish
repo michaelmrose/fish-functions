6bf4e94eb7235887fe31bf28b10eb77292cefb63
Michael Rose <michael@rosenetwork.net>
2019-01-26 00:49:28 -0800

    editing count-matching-words.fish

--- count-matching-words.fish
+++ count-matching-words.fish
@@ -1,9 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/count-matching-words.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/count-matching-words.fish @ line 2
 function count-matching-words
 	set words (echo $argv | cut -d @ -f1)
     set text (echo $argv | cut -d @ -f2)
     set cnt 0
+    echo words is $words and text is $text
     for word in $words
+        echo word is $word
         if echo $text |g $word > /dev/null
             set cnt (increase $cnt)
         end
