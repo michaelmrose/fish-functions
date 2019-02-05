7e560ba5badff04740b4351032f3b8246a6e0270
Michael Rose <michael@rosenetwork.net>
2019-02-05 01:36:18 -0800

    editing lightsout.fish

--- lightsout.fish
+++ lightsout.fish
@@ -1,13 +1,13 @@
-# Defined in /home/michael/.config/fish/buffer/lightsout_wait-until-monitor-i.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/lightsout.fish @ line 2
 function lightsout
 	sleep 0.5
     xset dpms force off
-	  while true
-        if xset -q |g 'monitor is on' > /dev/null
-            xset -dpms
-            return 0
-        else
-            sleep 1
-        end
-    end
+	  # while true
+    #     if xset -q |g 'monitor is on' > /dev/null
+    #         xset -dpms
+    #         return 0
+    #     else
+    #         sleep 1
+    #     end
+    # end
 end
