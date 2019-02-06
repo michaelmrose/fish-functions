58e8aaae8c3cec2af13bdb2c1bcbbbebd4e1577d
Michael Rose <michael@rosenetwork.net>
2019-02-06 15:00:08 -0800

    editing gargnew.fish

--- gargnew.fish
+++ gargnew.fish
@@ -1,8 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/garg_gargnew.fish @ line 31
 function gargnew
 	set games (endsin z5 z3 gblorb ~/if)
-    switch $argv
-        case select
+  switch $argv[1]
+      case select
             p $games | all select | each gargoyle
         case search
             p $games | all select | g $argv[2..-1]| each gargoyle
