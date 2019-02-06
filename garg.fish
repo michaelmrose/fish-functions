84b1c4a71549f8934839a11cbe8478b1ba16475b
Michael Rose <michael@rosenetwork.net>
2019-02-06 15:04:06 -0800

    editing garg.fish

--- garg.fish
+++ garg.fish
@@ -1,13 +1,20 @@
+# Defined in /home/michael/.config/fish/buffer/garg.fish @ line 1
 function garg
-	set games (endsin z5 z3 gblorb ~/if)
-  switch $argv[1]
-      case select
+	set list $argv
+
+    if test -f $list[1]
+        set list open $argv
+    end
+    
+	  set games (endsin z5 z3 gblorb ~/if)
+    switch $list[1]
+        case select
             p $games | all select | each gargoyle
         case search
-            p $games | g $argv[2..-1] | all select | each gargoyle
-      case open
-            set -U LAST_GAME $argv[2]
-            gargoyle $argv[2]
+            p $games | g $list[2..-1] | all select | each gargoyle
+        case open
+            set -U LAST_GAME $list[2]
+            gargoyle $list[2]
         case last
             gargoyle $LAST_GAME
     end
