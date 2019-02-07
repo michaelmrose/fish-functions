513a86dc79c0c52f933f5240dd44474011dad959
Michael Rose <michael@rosenetwork.net>
2019-02-06 22:48:24 -0800

    editing fix-touchscreen.fish

--- fix-touchscreen.fish
+++ fix-touchscreen.fish
@@ -1,8 +1,12 @@
 # Defined in /home/michael/.config/fish/buffer/fix-touchscreen.fish @ line 2
 function fix-touchscreen
-	
-    #   s modprobe wacom
+	s rmmod wacom
+    s modprobe wacom
     set id (xinput list |n /wacom c=2 /pointer n1)
+    set id (xinput list |n /finger n9 c=2)
     # foreach $ids 'xinput map-to-output _ eDP1'
     xinput map-to-output $id eDP1
+    # for i in (xinput list| n /finger n9 c=2)
+    #     xinput map-to-output $i eDP1
+    # end
 end
