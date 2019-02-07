0c76051e7cd3958686bfea81e5ace61a87d823e4
Michael Rose <michael@rosenetwork.net>
2019-02-07 15:44:08 -0800

    editing find-font.fish

--- find-font.fish
+++ find-font.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/find-font.fish @ line 2
 function find-font
-	fc-list |g $argv|rev|cut -d : -f2|rev|trim|sort -u
+	fc-list |g $argv|rev|cut -d : -f1-2|rev|trim|sort -u
 end
