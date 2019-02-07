09a7a60bf48977bfd4daefd110b4ace5acb7efd1
Michael Rose <michael@rosenetwork.net>
2019-02-06 22:49:54 -0800

    editing fix-touchscreen.fish

--- fix-touchscreen.fish
+++ fix-touchscreen.fish
@@ -3,10 +3,10 @@ function fix-touchscreen
 	s rmmod wacom
     s modprobe wacom
     # set id (xinput list |n /wacom c=2 /pointer n1)
-    set id (xinput list |n /finger n9 c=2)
+    # set id (xinput list |n /finger n9 c=2)
     # foreach $ids 'xinput map-to-output _ eDP1'
-    echo xinput map-to-output $id eDP1
-    # for i in (xinput list| n /finger n9 c=2)
-    #     xinput map-to-output $i eDP1
-    # end
+    # echo xinput map-to-output $id eDP1
+    for i in (xinput list| n /finger n9 c=2)
+        xinput map-to-output $i eDP1
+    end
 end
