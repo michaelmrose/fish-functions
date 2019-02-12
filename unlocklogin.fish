3126bfe121ed7084788e1cebb60816d30e90a8ce
Michael Rose <michael@rosenetwork.net>
2019-02-12 03:15:20 -0800

    editing unlocklogin.fish

--- unlocklogin.fish
+++ unlocklogin.fish
@@ -1,4 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/unlocklogin.fish @ line 2
 function unlocklogin
 	echo -n (rofim -i -password -dmenu) | gnome-keyring-daemon --unlock
+  killall gnome-keyring-daemon
 end
