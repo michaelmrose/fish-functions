272e1187174ba4e752e7c800b2f10636e52b182d
Michael Rose <michael@rosenetwork.net>
2019-02-13 10:47:53 -0800

    editing toggle-easystroke.fish

--- toggle-easystroke.fish
+++ toggle-easystroke.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/toggle-easystroke.fish @ line 2
 function toggle-easystroke
-	if pgrep easystroke
+	if pgrep easystroke > /dev/null
         killall easystroke
         echo off
     else
