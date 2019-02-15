aa1c255330d47a54f28e15ffaf8ad91fcc51442b
Michael Rose <michael@rosenetwork.net>
2019-02-14 18:03:18 -0800

    editing graal.fish

--- graal.fish
+++ graal.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/graal.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/graal.fish @ line 2
 function graal
-	$GRAALVM_HOME/bin/native-image -H:+ReportUnsupportedElementsAtRuntime -J-Xmx3G -J-Xms3G --no-server -jar $argv
+	$GRAALVM_HOME/bin/native-image -H:ReflectionConfigurationFiles=reflectconfig.json -H:+ReportUnsupportedElementsAtRuntime -J-Xmx3G -J-Xms3G --no-server -jar $argv
 end
