4826bef8620d37aad8d163400888fca3af2bf5b3
Michael Rose <michael@rosenetwork.net>
2019-02-20 10:38:48 -0800

    removed run-appkey-as-su.fish

--- run-appkey-as-su.fish
+++
@@ -1,9 +0,0 @@
-function run-appkey-as-su
-	set app (return-program-name (appkey $argv))
-    if [ $app = urxvtc ]
-        set app urxvt
-    end
-    eval sudo $app &
-    er vi3op
-    update-op-status
-end
