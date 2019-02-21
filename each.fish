39131434f3e429d9a783aaaaa3b3c17b8190e9d4
Michael Rose <michael@rosenetwork.net>
2019-02-21 12:32:16 -0800

    editing each.fish

--- each.fish
+++ each.fish
@@ -2,7 +2,7 @@
 function each
 	if string match _ $argv > /dev/null
         while read -l line
-            eval (echo $argv | sd _ $line)
+            echo (echo $argv | sd _ $line)
         end
     else
         while read -l line
