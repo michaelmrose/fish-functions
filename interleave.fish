5b40fbf4f5adac210d2c7d23ac95eee12ecf79e6
Michael Rose <michael@rosenetwork.net>
2019-02-21 12:47:23 -0800

    editing interleave.fish

--- interleave.fish
+++ interleave.fish
@@ -1,7 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/interleave.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/interleave.fish @ line 2
 function interleave
 	while read -l line
-        set acc $acc $arg $line
+        set acc $acc $argv $line
     end
     echo $acc
 end
