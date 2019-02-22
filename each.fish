1f32e74dc8a8be5892aab71569b82cc95b4d153d
Michael Rose <michael@rosenetwork.net>
2019-02-21 21:51:47 -0800

    editing each.fish

--- each.fish
+++ each.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/all_each.fish @ line 17
+# Defined in /home/michael/.config/fish/buffer/each.fish @ line 2
 function each
-	if string match _ $argv > /dev/null
+	if string match _ "$argv" > /dev/null
         while read -l line
             eval (echo $argv | sd _ $line)
         end
