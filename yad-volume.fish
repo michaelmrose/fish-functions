8cbb5f9063da7018c7595b6f9ec388e5c4defd55
Michael Rose <michael@rosenetwork.net>
2019-02-21 16:20:01 -0800

    editing yad-volume.fish

--- yad-volume.fish
+++ yad-volume.fish
@@ -1,5 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/yad-volume.fish @ line 2
 function yad-volume
-	
-    yad --text="Volume" --text-align center --scale --value (ponymix get-volume) --geometry=10x300 --vertical --button done --print-partial --no-close --step 10 --max-value 130 | while read -l line;ponymix set-volume $line;end;and vol set-volume (ponymix get-volume)
+	yad --text="Volume" --text-align center --scale --value (ponymix get-volume) --geometry=10x300 --vertical --button done --print-partial --no-close --step 10 --max-value 130 | while read -l line;ponymix set-volume $line;end;and vol set-volume (ponymix get-volume)
 end
