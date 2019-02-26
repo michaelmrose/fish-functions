2a0266b1f3047e0eed371db3059775f124e40c45
Michael Rose <michael@rosenetwork.net>
2019-02-25 22:13:41 -0800

    editing modm.fish

---
+++ modm.fish
@@ -0,0 +1,18 @@
+# Defined in /home/michael/.config/fish/buffer/modm.fish @ line 1
+function modm
+	switch $argv[1]
+        case ls
+            if test (count $argv) -eq 2
+                lsmod
+            else
+                lsmod |g $argv[2..-1]
+            end
+        case p
+            s modprobe $argv[2..-1] 
+        case t
+            s rmmod $argv[2..-1]
+            s modprobe $argv[2..-1]
+        case r
+            s rmmod $argv[2..-1]
+    end
+end
