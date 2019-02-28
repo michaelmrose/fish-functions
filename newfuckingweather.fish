90f039b024b457c577d8b654c215acefc038aa59
Michael Rose <michael@rosenetwork.net>
2019-02-28 01:11:07 -0800

    editing newfuckingweather.fish

--- newfuckingweather.fish
+++ newfuckingweather.fish
@@ -1,14 +1,15 @@
 # Defined in /home/michael/.config/fish/buffer/newfuckingweather.fish @ line 2
 function newfuckingweather
-	if not areweonline
-        either $priorweather unavailable
-        return 0
-    end
-        
-	  set g (geo find)
-    set icon (weather-icon)
-	  set weather (curl wttr.in/$g\?0\?T\?Q\?u|cut -c 16- | condense_lines |condense_spaces)
-    set -U priorweather $icon $weather
-    echo $icon $weather
-    return 0
+	echo unavailable
+	  # if not areweonline
+    #       either $priorweather unavailable
+    #       return 0
+    #   end
+    
+	  #   set g (geo find)
+    #   set icon (weather-icon)
+	  #   set weather (curl wttr.in/$g\?0\?T\?Q\?u|cut -c 16- | condense_lines |condense_spaces)
+    #   set -U priorweather $icon $weather
+    #   echo $icon $weather
+    #   return 0
 end
