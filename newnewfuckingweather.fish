fba6f0d5c68f946e4ed910b5a36d0f913b061a69
Michael Rose <michael@rosenetwork.net>
2019-03-01 10:34:59 -0800

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 2
 function newnewfuckingweather
 	set icon (weather-icon)
-	  set report (weather-report (geo find) --no-cache)
+	  set report (weather-report (geo find))
     set temp (p $report|n l3 n2)°F
     set conditions ( p $report|n l6 |cut -d : -f2|trim)
     set wind (p $report|n l5 n8-9)
