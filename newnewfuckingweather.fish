ef0691077ff689bd409adc680bfc7b6fc8feb914
Michael Rose <michael@rosenetwork.net>
2019-02-28 20:58:36 -0800

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -1,8 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/newfuckingweather_newnewfuckin.fish @ line 17
 function newnewfuckingweather
 	set icon (weather-icon)
-	  set report (weather-report fips5307695)
-    set temp (p $report|n l3 n2)°F
-    set conditions ( p $report|n l6 |cut -d : -f2|trim)
-    echo $icon $temp $conditions
+	set report (weather-report fips5303590288)
+  set temp (p $report|n l3 n2)°F
+  set conditions ( p $report|n l6 |cut -d : -f2|trim)
+  echo $icon $temp $conditions
 end
