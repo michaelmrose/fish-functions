c311016484b6068fe8b52c5fa0c85487204757fd
Michael Rose <michael@rosenetwork.net>
2019-03-06 12:14:48 -0800

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -4,6 +4,6 @@ function newnewfuckingweather
 	  set report (weather-report (geo find))
     set temp (p $report|n l3 n2)°F
     set conditions ( p $report|n l6 |cut -d : -f2|trim)
-    set wind (p $report|n l5 n8-9)
+    set wind (p $report|n l5|cut -d : -f1-|trim)
     echo $icon $temp $conditions wind: $wind
 end
