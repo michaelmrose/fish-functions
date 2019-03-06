ea1fd068669c42901ff01acc963f6323b8c0ef9c
Michael Rose <michael@rosenetwork.net>
2019-03-06 02:24:07 -0800

    editing fucking-caps-led.fish

--- fucking-caps-led.fish
+++ fucking-caps-led.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/fucking-caps-led.fish @ line 2
 function fucking-caps-led
-	fnd '.*capslock/brightness$' /sys/devices|each "echo $argv | sudo tee" 
+	fnd '.*capslock/brightness$' /sys/devices|each "echo $argv | sudo tee _" 
 end
