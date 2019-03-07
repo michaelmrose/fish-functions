add8451be06f8f034afe9ea4df1dae61fdd2e27c
Michael Rose <michael@rosenetwork.net>
2019-03-06 22:53:51 -0800

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -6,6 +6,6 @@ function newnewfuckingweather
     set conditions ( p $report|n l6 |cut -d : -f2|trim)
     # set wind (p $report|n l5|cut -d : -f1-|trim)
     set windchill (p $report |grep Windchill|trim)
-    set wind (filter-with-expr) isnumeric (explode-words (p $report|n l5))
+    set wind (filter-with-expr isnumeric (explode-words (p $report|n l5)))
     echo $icon $temp $conditions wind: $wind mph $windchill
 end
