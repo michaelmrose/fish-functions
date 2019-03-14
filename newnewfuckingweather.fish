b69ff15d780b6da026eaa3928bd1a75323e29651
Michael Rose <michael@rosenetwork.net>
2019-03-13 23:19:37 -0700

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -1,10 +1,12 @@
 # Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 2
 function newnewfuckingweather
-	set icon (weather-icon)
-	# set report (weather-report (geo find))
-  set report (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial)
-  set report (p $report|n l5-7|cut -d : -f2-|trim |condense_lines) 
-  echo $icon $report
+	
+	# # set report (weather-report (geo find))
+  # set report (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial)
+  # set report (p $report|n l5-7|cut -d : -f2-|trim |condense_lines) 
+  # echo $icon $report
+
+  echo (weather-icon) (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial |n l5-7|cut -d : -f2-|trim |condense_lines)
   # set temp (p $report|n l3 n2)°F
   # set conditions ( p $report|n l6 |cut -d : -f2|trim)
   # # set wind (p $report|n l5|cut -d : -f1-|trim)
