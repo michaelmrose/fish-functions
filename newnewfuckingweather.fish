ce54426f21f4b82265ab7ddca119e84399fb2b29
Michael Rose <michael@rosenetwork.net>
2019-03-13 23:16:44 -0700

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -1,11 +1,14 @@
 # Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 2
 function newnewfuckingweather
 	set icon (weather-icon)
-	  set report (weather-report (geo find))
-    set temp (p $report|n l3 n2)°F
-    set conditions ( p $report|n l6 |cut -d : -f2|trim)
-    # set wind (p $report|n l5|cut -d : -f1-|trim)
-    set windchill (p $report |grep Windchill|trim)
-    set wind (filter-with-expr isnumeric (explode-words (p $report|n l5)))
-    echo $icon $temp $conditions wind: $wind mph $windchill
+	# set report (weather-report (geo find))
+  set report (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial)
+  set report (p $report|n l5-7|cut -d : -f2-|trim |condense_lines) 
+  echo $icon $report
+  # set temp (p $report|n l3 n2)°F
+  # set conditions ( p $report|n l6 |cut -d : -f2|trim)
+  # # set wind (p $report|n l5|cut -d : -f1-|trim)
+  # set windchill (p $report |grep Windchill|trim)
+  # set wind (filter-with-expr isnumeric (explode-words (p $report|n l5)))
+  # echo $icon $temp $conditions wind: $wind mph $windchill
 end
