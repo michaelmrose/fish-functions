2584bb78270b5ee9c581ed04b9dac168a57f466f
Michael Rose <michael@rosenetwork.net>
2019-03-21 11:42:44 -0700

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 2
 function newnewfuckingweather
-	echo (weather-icon) (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | sd 'from the [NESW]{1,3} at' 'wind:' | condense_lines |trim|condense_spaces)
+	echo (weather-icon) (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | condense_lines |trim|condense_spaces| sd 'from the [NESW]{1,3} at' 'wind:' )
 end
