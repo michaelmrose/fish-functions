ded9f84ce0b290ff51f1bcf96a8485e701353d8c
Michael Rose <michael@rosenetwork.net>
2019-03-23 12:06:38 -0700

    removed nthn.fish

--- nthn.fish
+++
@@ -1,9 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/nthn.fish @ line 2
-function nthn
-	while read -l line
-        set acc $acc $line
-    end
-    set com awk \'"{print \$$argv}"\'
-    p $acc | $com
-    # p $acc | awk "{print \${$argv[1]}}"
-end
