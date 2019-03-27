c038d02e270266d130f72411ae0f149b9ce75f33
Michael Rose <michael@rosenetwork.net>
2019-03-27 09:34:11 -0700

    editing bremerton-weather.fish

--- bremerton-weather.fish
+++ bremerton-weather.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 20
+# Defined in /home/michael/.config/fish/buffer/bremerton-weather_format-weath.fish @ line 2
 function bremerton-weather
 	weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial
 end
