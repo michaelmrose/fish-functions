16280689c57a346e77fa09443071ceb43e1b2921
Michael Rose <michael@rosenetwork.net>
2019-03-27 09:34:11 -0700

    editing format-weather-report.fish

--- format-weather-report.fish
+++ format-weather-report.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 24
+# Defined in /home/michael/.config/fish/buffer/bremerton-weather_format-weath.fish @ line 7
 function format-weather-report
 	p $argv | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | condense_lines |trim|condense_spaces
 end
