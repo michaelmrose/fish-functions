b61d43f0546c59392c7e35be8387a02d7fabe402
Michael Rose <michael@rosenetwork.net>
2019-03-27 13:56:32 -0700

    editing reasonable-weather.fish

--- reasonable-weather.fish
+++ reasonable-weather.fish
@@ -1,7 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/bremerton-weather_format-weath.fish @ line 11
+# Defined in /home/michael/.config/fish/buffer/reasonable-weather.fish @ line 2
 function reasonable-weather
 	if set weather (bremerton-weather)
-        set -U priorweather (weather-icon) (format-weather-report weather)
-    end
-    either $priorweather unavailable
+      set -U priorweather (weather-icon) (format-weather-report $weather)
+  end
+  either $priorweather unavailable
 end
