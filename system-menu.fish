ae3814602070f081b9b432f38652ab9e6abb5392
Michael Rose <michael@rosenetwork.net>
2019-04-11 00:06:18 -0700

    editing system-menu.fish

--- system-menu.fish
+++ system-menu.fish
@@ -1,16 +1,19 @@
-# Defined in /home/michael/.config/fish/buffer/system-menu.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/lightsout_rboot_sdown_sus_syst.fish @ line 59
 function system-menu
 	switch (p Lightsout Suspend Shutdown "Restart i3wm" Reboot "Switch User"| rofi -dmenu -i -p System:)
         case Lightsout
-            lightsout
-        case Suspend
-            sus
-        case Shutdown
-            sdown
-        case "Restart i3wm"
+	          sleep 0.5
+            xset dpms force off
+      case Suspend
+          systemctl suspend;
+      case Shutdown
+            cleanup-desktop
+	          sudo shutdown -h now
+      case "Restart i3wm"
             restart-i3
         case Reboot
-            rboot
+            cleanup-desktop
+	          sudo shutdown -r now
         case "Switch User"
             dm-tool switch-to-greeter
     end
