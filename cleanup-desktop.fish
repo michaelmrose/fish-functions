d0d438e04b77dd3062e16fe20a88c0e05ce144c6
Michael Rose <michael@rosenetwork.net>
2019-04-10 23:58:05 -0700

    editing cleanup-desktop.fish

--- cleanup-desktop.fish
+++ cleanup-desktop.fish
@@ -1,9 +1,11 @@
+# Defined in /home/michael/.config/fish/buffer/cleanup-desktop.fish @ line 2
 function cleanup-desktop
-	kill (pgrep emacs)
-  kill (pgrep zeal)
-  sleep 1
-  rm /tmp/ZealLocalServer
-  sleep 1
-	i3-msg [class="."] kill
-  countdown 7
+	sudo umount -fl /mnt/michael
+	  kill (pgrep emacs)
+    kill (pgrep zeal)
+    sleep 1
+    rm /tmp/ZealLocalServer
+    sleep 1
+	  i3-msg [class="."] kill
+    countdown 7
 end
