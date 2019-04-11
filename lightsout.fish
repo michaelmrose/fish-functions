bd1423f67270971d17ab032c21a919bf1c297055
Michael Rose <michael@rosenetwork.net>
2019-04-11 00:06:18 -0700

    editing lightsout.fish

--- lightsout.fish
+++ lightsout.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/lightsout.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/lightsout_rboot_sdown_sus_syst.fish @ line 2
 function lightsout
 	sleep 0.5
     xset dpms force off
