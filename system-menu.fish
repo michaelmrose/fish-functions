e9ec84aefcdf25049a803d83be6a21b607468ad2
Michael Rose <michael@rosenetwork.net>
2019-04-11 00:07:17 -0700

    editing system-menu.fish

--- system-menu.fish
+++ system-menu.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/lightsout_rboot_sdown_sus_syst.fish @ line 59
+# Defined in /home/michael/.config/fish/buffer/cleanup-desktop_lightsout_rboo.fish @ line 70
 function system-menu
 	switch (p Lightsout Suspend Shutdown "Restart i3wm" Reboot "Switch User"| rofi -dmenu -i -p System:)
         case Lightsout
