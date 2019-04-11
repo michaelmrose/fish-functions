d1a1e4459b7db811a20d0b0fca6b81011d23daec
Michael Rose <michael@rosenetwork.net>
2019-04-10 23:41:13 -0700

    editing list-outputs.fish

--- list-outputs.fish
+++ list-outputs.fish
@@ -1,13 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/list-outputs.fish @ line 2
 function list-outputs
-	set outputs (pacmd list-sinks | sed -n -e 's/device.description[[:space:]]=[[:space:]]"\(.*\)"/\1/p')
-    set cnt 0
-    for i in $outputs
-        if [ $cnt = $default_sink ]
-            echo \* $i
-        else
-            echo $i
-        end
-        # inc cnt
-        set cnt (math cnt + 1)
-    end
+	ponymix -t sink --short|cut -d \t -f4
 end
