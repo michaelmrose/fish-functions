af9f19a99530cec99b093acd0b279705ffa5c558
Michael Rose <michael@rosenetwork.net>
2019-04-11 00:06:18 -0700

    editing rboot.fish

--- rboot.fish
+++ rboot.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/rb_rboot_sdown.fish @ line 9
+# Defined in /home/michael/.config/fish/buffer/lightsout_rboot_sdown_sus_syst.fish @ line 16
 function rboot
 	sudo umount -fl /mnt/michael
     cleanup-desktop
