a04135dfab224032fd0f0603405ec6f84d370e6a
Michael Rose <michael@rosenetwork.net>
2019-04-11 00:09:33 -0700

    editing menu.fish

--- menu.fish
+++ menu.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/menu.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/menu_system-menu.fish @ line 2
 function menu
-	
+	p $argv[2..-1] | rofi -dmenu -i -p $argv[1]
 end
