1844ec640b90da3c7126d5858059e9e6ce28593a
Michael Rose <michael@rosenetwork.net>
2019-04-11 02:25:23 -0700

    editing menu.fish

--- menu.fish
+++ menu.fish
@@ -1,4 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/menu_system-menu.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/menu.fish @ line 2
 function menu
-	p $argv[2..-1] | rofi -dmenu -i -p $argv[1]
+	if test (count $argv) -gt 1
+	      p $argv[2..-1] | rofi -dmenu -i -p $argv[1]
+    else
+        while read -l line
+            set acc $acc $line
+        end
+        menu $argv[1] $acc
+    end
 end
