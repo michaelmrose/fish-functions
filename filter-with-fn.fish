f0b2e83d00cef09b202d007658cb588b434c059a
Michael Rose <michael@rosenetwork.net>
2019-04-16 01:40:25 -0700

    editing filter-with-fn.fish

--- filter-with-fn.fish
+++ filter-with-fn.fish
@@ -1,9 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/filter-with_filter-with-fn.fish @ line 11
+# Defined in /home/michael/.config/fish/buffer/filter-with_filter-with-fn.fish @ line 12
 function filter-with-fn
 	set fn $argv[1]
-	  for i in $argv[2..-1]
-        if eval $fn $i
-            echo $i
-        end
-    end
+  set vals $argv[2..-1]
+	for i in $vals
+      if eval (echo $fn | sd _ $i) 
+          echo $i
+      end
+  end
 end
