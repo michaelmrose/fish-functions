b52811c85a28e9992ae5645aeccbc23753441655
Michael Rose <michael@rosenetwork.net>
2019-04-24 14:33:58 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -1,6 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/tdown.fish @ line 2
 function tdown
-	set duration (prompt duration)
-  set title (prompt title)
-  kitty -1 -e termdown -T $title $duration
+	
+    # set title (prompt title)
+    set info (prompt title @ duration)
+    set title (echo $info | cut -d @ -f1)
+    set duration (echo $info | cut -d @ -f2)
+    kitty -1 -e termdown -T $title $duration
 end
