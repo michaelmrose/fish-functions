ed0cfd9fd22bab2b1397968d9f071fc1f8351483
Michael Rose <michael@rosenetwork.net>
2019-04-24 14:54:05 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -3,5 +3,5 @@ function tdown
 	set info (prompt 'title @ duration')
   set title (echo $info | cut -d @ -f1| trim)
   set duration (echo $info | cut -d @ -f2 | trim)
-  kitty -o font_size=50 -e termdown --no-figlet -T "$title" $duration
+  kitty -o font_size=50 -e termdown --no-figlet -v en-us -T "$title" $duration
 end
