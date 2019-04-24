932a7e1a12d9d01872b5091e18878895ee57c254
Michael Rose <michael@rosenetwork.net>
2019-04-24 14:30:54 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -1,9 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/tdown.fish @ line 2
 function tdown
-	if not exists $argv
-      set input (rofim -dmenu -i -p duration:)
-  else
-        set input $argv
-    end
-	  kitty -1 -e termdown $input
+	set duration (prompt duration)
+    set title (promt title)
+    kitty -1 -e termdown -T $title $duration
 end
