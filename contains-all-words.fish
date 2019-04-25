b7cc371cede599acd42b8258728bf3e2719bd9a8
Michael Rose <michael@rosenetwork.net>
2019-04-24 19:40:16 -0700

    removed contains-all-words.fish

--- contains-all-words.fish
+++
@@ -1,9 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/contains-all-words.fish @ line 1
-function contains-all-words
-	while read -l line
-        set res $line
-        for word in $argv
-            set res (echo $res |g $word)
-        end
-    end
-end
