c554ee3bddbd4321b7f3c11ed3ce38be46cff247
Michael Rose <michael@rosenetwork.net>
2019-05-01 22:56:18 -0700

    editing graal.fish

--- graal.fish
+++ graal.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/graal.fish @ line 2
 function graal
-	$GRAALVM_HOME/bin/native-image -H:+ReportUnsupportedElementsAtRuntime -J-Xmx3G -J-Xms3G -Dgraal.CompilerConfiguration=economy --engine.Mode=latency --no-server -jar $argv
+	$GRAALVM_HOME/bin/native-image -H:+ReportUnsupportedElementsAtRuntime -J-Xmx3G -J-Xms3G -Dgraal.CompilerConfiguration=economy --no-server -jar $argv
 end
