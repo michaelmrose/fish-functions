351a410c11185cbc48a24dbf88f0cdc5c766380c
Michael Rose <michael@rosenetwork.net>
2019-06-03 11:13:30 -0700

    editing r.fish

--- r.fish
+++ r.fish
@@ -1,14 +1,16 @@
+# Defined in /home/michael/.config/fish/buffer/r.fish @ line 2
 function r
-	for item in $argv
-    if exists $item
-      echo $item
-      return 0
-    end
-  end
-  
-	# if exists $argv[1]
-  #   echo $argv[1]
-  # else
-  #   echo $argv[2]
-  # end
+	ssh funtoo $argv
+	  # for item in $argv
+    #   if exists $item
+    #     echo $item
+    #     return 0
+    #   end
+    # end
+    
+	  # # if exists $argv[1]
+    # #   echo $argv[1]
+    # # else
+    # #   echo $argv[2]
+    # # end
 end
