0822442d0f5b561b26c9c7f63630a0ef36777846
Michael Rose <michael@rosenetwork.net>
2019-06-17 16:16:22 -0700

    editing wp.fish

--- wp.fish
+++ wp.fish
@@ -141,7 +141,10 @@ function wp
                 echo $pics
                 set -U bgimage $pics
             case clip
-                wp url (cl)
+                wp url (xclip -o -selection clip)
+            case multi
+                feh --bg-max $argv[2..-1]
+                set -U $bgimage $argv[2..-1]
             case randstyle
                 set numdisp (count (get-connected-displays))
                 set style (wp cat ls|shuf|all take 1)   
