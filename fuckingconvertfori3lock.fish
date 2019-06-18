51ca5e63201fe3be7ea0771ac8387a6d36a0d585
Michael Rose <michael@rosenetwork.net>
2019-06-17 17:35:54 -0700

    editing fuckingconvertfori3lock.fish

---
+++ fuckingconvertfori3lock.fish
@@ -0,0 +1,8 @@
+# Defined in /home/michael/.config/fish/buffer/fuckingconvertfori3lock.fish @ line 1
+function fuckingconvertfori3lock
+	set img $argv
+    convert -resize 1440x810 $img /tmp/bg3.png
+    convert -resize 1920x1080 $img /tmp/bg2.png
+    convert -resize 1680x1050 $img /tmp/bg1.png
+    convert /tmp/bg1.png /tmp/bg2.png /tmp/bg3.png +append /tmp/multibg.png
+end
