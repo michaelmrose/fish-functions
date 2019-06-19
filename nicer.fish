4b8ccaddfe40b9b13979e75beb4bbeb4b6b53206
Michael Rose <michael@rosenetwork.net>
2019-06-18 23:08:07 -0700

    editing nicer.fish

--- nicer.fish
+++ nicer.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/nicer.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/nicer.fish @ line 2
 function nicer
-	renice -n -20 -p $argv
+	sudo renice -n -20 -p $argv
+    sudo ionice -c 1 -p $argv
 end
