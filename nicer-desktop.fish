67c2f47bdbc534612b56c4827c5ddda40a164cc3
Michael Rose <michael@rosenetwork.net>
2019-06-18 23:10:11 -0700

    editing nicer-desktop.fish

--- nicer-desktop.fish
+++ nicer-desktop.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/nicer-desktop.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/nicer-desktop.fish @ line 2
 function nicer-desktop
-	for p in (pgrep 'Xorg|i3')
+	for p in (pgrep 'Xorg|i3|emacs')
         nicer $p
     end
 end
