9236e578e2a3b77d0dac67619cc826f243ecdfb7
Michael Rose <michael@rosenetwork.net>
2019-07-03 15:12:12 -0700

    editing wallpaper-status-line.fish

--- wallpaper-status-line.fish
+++ wallpaper-status-line.fish
@@ -1,4 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/wallpaper-status-line.fish @ line 2
 function wallpaper-status-line
-	basename $bgimage[2] |sd - " " | cut -d . -f1
+	if test (count $bgimage) -gt 1
+        set image $bgimage[2]
+    else
+        set image $bgimage
+    end
+	  basename $image |sd - " " | cut -d . -f1
 end
