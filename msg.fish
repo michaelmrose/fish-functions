98e78bebc1646ebb8f4fd303a325eef93aef92c6
Michael Rose <michael@rosenetwork.net>
2019-07-09 11:33:51 -0700

    editing msg.fish

--- msg.fish
+++ msg.fish
@@ -1,4 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/msg.fish @ line 2
 function msg
-	# twmnc --id 1 --content (center-text $argv)
-	twmnc -d 4000 --id 1 --content "$argv"
+	twmnc -d 2000 --id 1 --content "$argv"
 end
