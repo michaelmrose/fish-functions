471afd2df45597acf6e49951c1295c7b9a22436c
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:09:40 -0700

    editing start.fish

--- start.fish
+++ start.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/get-command-for-window_place_s.fish @ line 31
+# Defined in /home/michael/.config/fish/buffer/start.fish @ line 2
 function start
 	set commandname $argv[1]
     set command $argv
     set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
     echo $focused > /tmp/fuckingstart/$commandname
-    eval $command &
+    eval "$command &"
 end
