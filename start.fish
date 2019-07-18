334c335ad4145409b6138bb279d14c23f184148d
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:04:58 -0700

    editing start.fish

--- start.fish
+++ start.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/get-command-for-window_place_s.fish @ line 30
+# Defined in /home/michael/.config/fish/buffer/get-command-for-window_place_s.fish @ line 31
 function start
 	set commandname $argv[1]
     set command $argv
