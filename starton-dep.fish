b6cad65fbe3b9dc733d71cc29bf817507ed0aafd
Michael Rose <michael@rosenetwork.net>
2019-07-17 21:33:49 -0700

    editing starton-dep.fish

---
+++ starton-dep.fish
@@ -0,0 +1,24 @@
+function starton-dep
+	set workspace $argv[1]
+    set app $argv[2..-1]
+    set winclass (return-windowclass $app)
+    set layout /tmp/(uuidgen)-layout
+	  set visible (i3-msg -t get_workspaces|jq .[]|jq -r "select(.visible == true).name" )
+    set current (i3-msg -t get_workspaces|jq .[]|jq -r "select(.focused == true).name" )
+    set json '{
+    "swallows": [
+    {
+    "class": "^#winclass$"
+    }
+    ],
+    "type": "con"
+    }'
+
+    p $json | sed "s/#winclass/$winclass/g" > $layout
+    i3-msg workspace $workspace
+    i3-msg append_layout $layout
+    fish -c "$app" &
+    for ea in $visible $current
+        i3-msg workspace $ea
+    end
+end
