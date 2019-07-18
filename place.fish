daab9bb5f958099cbbe0b21f9f5fb50679a61934
Michael Rose <michael@rosenetwork.net>
2019-07-17 22:35:45 -0700

    editing place.fish

--- place.fish
+++ place.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 2
 function place
-	set wid (dectohex (xdtool getactivewindow))
+	set wid (dectohex (xdotool getactivewindow))
     set command (get-command-for-window $wid)
     set target (cat /tmp/fuckingstart/$command)
     i3 move window to $target
