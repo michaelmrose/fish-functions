ea1fab5457f873df67978afa877583e2fa822fac
Michael Rose <michael@rosenetwork.net>
2019-07-17 22:31:47 -0700

    editing start.fish

---
+++ start.fish
@@ -0,0 +1,8 @@
+# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 8
+function start
+	set commandname $argv[1]
+    set command $argv
+    set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
+    echo focused > /tmp/fuckingstart/commandname
+    eval $command &
+end
