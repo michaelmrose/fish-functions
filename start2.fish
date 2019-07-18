e25ff9ca14283a6a1ff392ede476014673cabe3b
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:54:27 -0700

    editing start2.fish

--- start2.fish
+++ start2.fish
@@ -1,11 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 29
+# Defined in /home/michael/.config/fish/buffer/place_start_start2.fish @ line 23
 function start2
 	set commandname $argv[1]
     set command $argv
     set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
-    echo $focused > /tmp/fuckingstart/$commandname
+    # echo $focused > /tmp/fuckingstart/$commandname
     eval "$command &"
     while not string match commandname (get-command-for-window)
     end
-    place
+    i3 workspace $focused
 end
