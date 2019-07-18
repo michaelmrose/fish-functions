5de1ebdfcceec5619865375d3e03581da732880b
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:54:27 -0700

    editing place.fish

--- place.fish
+++ place.fish
@@ -1,18 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/place_start_start2.fish @ line 2
 function place
-	echo starting
-	  set wid (dectohex (xdotool getactivewindow))
+	set wid (dectohex (xdotool getactivewindow))
     set command (get-command-for-window $wid)
     set target (cat /tmp/fuckingstart/$command)
 	  msg in place fn wid is $wid command is $command and target is $target
-	  echo in place fn wid is $wid command is $command and target is $target
     if exists $target
         i3 move window to workspace $target
         rm /tmp/fuckingstart/$command
-        sleep 10
-        msg removing target
-    else
-        msg target is empty
-        return 0
     end
 end
