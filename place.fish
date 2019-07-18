6951b32ea894f16e0af631e1137c81318e2663b7
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:04:58 -0700

    editing place.fish

--- place.fish
+++ place.fish
@@ -1,6 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/get-command-for-window_place_s.fish @ line 19
 function place
-	set wid (dectohex (xdotool getactivewindow))
+	msg in place fn wid is $wid command is $command and target is $target
+	  set wid (dectohex (xdotool getactivewindow))
     set command (get-command-for-window $wid)
     set target (cat /tmp/fuckingstart/$command)
     if exists $target
