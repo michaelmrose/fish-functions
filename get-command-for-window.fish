027280b1a07eca62a53481dba5cc84698a385620
Michael Rose <michael@rosenetwork.net>
2019-07-17 22:45:47 -0700

    editing get-command-for-window.fish

--- get-command-for-window.fish
+++ get-command-for-window.fish
@@ -1,6 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 16
+# Defined in /home/michael/.config/fish/buffer/get-command-for-window_place_s.fish @ line 2
 function get-command-for-window
-	set wid $argv[1]
+	if exists $argv
+	      set wid $argv[1]
+    else
+        set wid (ensure-hex (xdotool getactivewindow))
+    end
     set pid (wmctrl -lp|grep $wid|awk '{print $3}')
     set com (cat /proc/$pid/comm )
     echo $com
