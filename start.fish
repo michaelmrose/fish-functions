33c8734298d38b140ae2361c408cfeb9c29c7d69
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:21:27 -0700

    editing start.fish

--- start.fish
+++ start.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 16
+# Defined in /home/michael/.config/fish/buffer/place_start.fish @ line 18
 function start
 	set commandname $argv[1]
     set command $argv
