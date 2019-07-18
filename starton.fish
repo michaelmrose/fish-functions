36ff7a6a1c544664081afe1dfdcde0e7a2743479
Michael Rose <michael@rosenetwork.net>
2019-07-17 21:34:07 -0700

    editing starton.fish

---
+++ starton.fish
@@ -0,0 +1,17 @@
+function starton
+	set ws $argv[1]
+    set command $argv[2]
+    set class $argv[3]
+    set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
+    set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
+    set json '{"swallows": [{"class": "^#winclass$"}], "type": "con"}'
+    set layout /tmp/(uuidgen)-layout
+    echo $json  | sed "s/#winclass/$class/g" > $layout
+    i3-msg workspace $ws
+    i3-msg append_layout $layout
+    fish -c $command &
+    for w in $active
+        i3-msg workspace $w
+    end
+    i3-msg workspace $focused
+end
