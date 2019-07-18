320cf2e5a0e3700ff0f2a2fd84652917ebe57115
Michael Rose <michael@rosenetwork.net>
2019-07-17 23:05:47 -0700

    editing place.fish

--- place.fish
+++ place.fish
@@ -1,9 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/get-command-for-window_place_s.fish @ line 19
 function place
-	msg in place fn wid is $wid command is $command and target is $target
-	  set wid (dectohex (xdotool getactivewindow))
+	set wid (dectohex (xdotool getactivewindow))
     set command (get-command-for-window $wid)
     set target (cat /tmp/fuckingstart/$command)
+	  msg in place fn wid is $wid command is $command and target is $target
     if exists $target
         i3 move window to workspace $target
         rm /tmp/fuckingstart/$command
