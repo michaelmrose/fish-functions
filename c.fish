aeba8303b5ea2a40f65206183de5e77b1dc3522c
Michael Rose <michael@rosenetwork.net>
2019-07-18 23:29:27 -0700

    editing c.fish

--- c.fish
+++ c.fish
@@ -1,11 +1,15 @@
+# Defined in /home/michael/.config/fish/buffer/c.fish @ line 2
 function c
-	# if test -d $argv
-        # set dir $argv
+	
     # else if bmark $argv > /dev/null
         # set dir (bmark $argv) > /dev/null
     # else
         # set dir (autojump $argv) > /dev/null
     # end
-    cd (dr $argv)
+    if exists $argv
+        set target (dr $argv)
+        set -U LAST_DIR $target
+    end
+    cd $LAST_DIR
     # pushd $dir
 end
