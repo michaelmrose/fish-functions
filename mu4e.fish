a6459c00f94b82f5d0ec997c386dd4958c839377
Michael Rose <michael@rosenetwork.net>
2019-07-26 01:47:40 -0700

    editing mu4e.fish

--- mu4e.fish
+++ mu4e.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/mu4e.fish @ line 2
 function mu4e
 	emf ce mmr/mu4e-inbox &
-  sleep 0.5
+  sleep 1
   xdotool search --onlyvisible --class Emacs key Return
 end
