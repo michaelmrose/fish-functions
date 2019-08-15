8406d26402043b88397874c3c3d4d8a5ace9ecb1
Michael Rose <michael@rosenetwork.net>
2019-08-15 12:16:51 -0700

    editing lockmeminimal.fish

--- lockmeminimal.fish
+++ lockmeminimal.fish
@@ -1,10 +1,11 @@
+# Defined in /home/michael/.config/fish/buffer/floating_term_focus_focus-dist.fish @ line 64
 function lockmeminimal
 	save-workspaces
     for i in (get-connected-displays)
-        im focus output $i
-        im workspace {$i}_is_locked
+        i3 focus output $i
+        i3 workspace {$i}_is_locked
     end
-    im bar mode invisible
+    i3 bar mode invisible
     set -U locked true
     xdotool key XF86LaunchA
 end
