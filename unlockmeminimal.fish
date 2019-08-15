79c4aab021e5fdd8b06c9a42159636ccfa0fb38b
Michael Rose <michael@rosenetwork.net>
2019-08-15 12:16:51 -0700

    editing unlockmeminimal.fish

--- unlockmeminimal.fish
+++ unlockmeminimal.fish
@@ -1,9 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/floating_term_focus_focus-dist.fish @ line 166
 function unlockmeminimal
 	if [ $locked = "false" ]
         return 0
     else
         restore-workspaces
-        im bar mode dock
+        i3 bar mode dock
         set -U locked false
     end
 end
