79f0c68987b279c8b0581c8f15c16637ed805cfb
Michael Rose <michael@rosenetwork.net>
2019-08-15 00:13:45 -0700

    editing useful-mail.fish

---
+++ useful-mail.fish
@@ -0,0 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/mail_useful.fish @ line 1
+function useful-mail
+	echo \'and not flag:trashed and not maildir:/OldGmail/[Gmail].Spam and not maildir:/OldGmail/techmail and not maildir:/Gmail/[Gmail].Spam\'
+end
