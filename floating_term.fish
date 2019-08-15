3c37899ddb286066a87f15944185862207b93bd9
Michael Rose <michael@rosenetwork.net>
2019-08-15 12:16:50 -0700

    editing floating_term.fish

--- floating_term.fish
+++ floating_term.fish
@@ -1,6 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/floating_term_focus_focus-dist.fish @ line 2
 function floating_term
 	eval $TERMINAL &
     sleep 0.25
     float_fullscreen
-    im move scratchpad
+    i3 move scratchpad
 end
