131b3f33e9887e51ab7318f2f916eb0926dd8355
Michael Rose <michael@rosenetwork.net>
2019-08-21 16:29:47 -0700

    editing mpd-status-line.fish

--- mpd-status-line.fish
+++ mpd-status-line.fish
@@ -4,7 +4,7 @@ function mpd-status-line
   set mpdstatus (p $mpd_info |grep -E 'play|pause'|cut -d \[ -f2|cut -d \] -f1)
   if exists $mpdstatus
         set current (mpc status |head -1)
-        set playtime (mpc status |condense_lines | cut -d\# -f2-|awk '{print $2}')
+        # set playtime (mpc status |condense_lines | cut -d\# -f2-|awk '{print $2}')
         switch $mpdstatus
             case paused
                 echo  $current $playtime
