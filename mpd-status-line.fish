28ce3632852fe90935de135e22bd878412e8ca13
Michael Rose <michael@rosenetwork.net>
2019-08-21 16:28:45 -0700

    editing mpd-status-line.fish

--- mpd-status-line.fish
+++ mpd-status-line.fish
@@ -1,18 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/mpd-status-line.fish @ line 2
 function mpd-status-line
-	
-    #   if not exists $mpdstat
-    #       echo none
-    #       return 0
-    #   end
-    #   switch $mpdstat
-    #       case paused
-    #           echo  (mpc current) $playtime
-    #       case playing
-    #           echo  (mpc current) $playtime
-    #   end
-    set mpd_info (mpc status)
-    set mpdstatus (p $mpd_info |grep -E 'play|pause'|cut -d \[ -f2|cut -d \] -f1)
+	set mpd_info (mpc status)
+    set mpdstatus (p $mpd_info |grep 'play|pause'|cut -d \[ -f2|cut -d \] -f1)
     if exists $mpdstatus
         set current (mpc status |head -1)
         set playtime (mpc status |condense_lines | cut -d\# -f2-|nth 2)
