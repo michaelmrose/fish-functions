843fc00e206cadccd0bf8546bef846b4d3ae478a
Michael Rose <michael@rosenetwork.net>
2019-08-27 19:22:46 -0700

    editing download-clipboard.fish

--- download-clipboard.fish
+++ download-clipboard.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/download-clipboard.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/download-clipboard_download-fr.fish @ line 2
 function download-clipboard
 	set target (xclip -o -sel clip)
   cd ~/youtube-dl
