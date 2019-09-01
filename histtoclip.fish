3ad24339dce69d5740fc6fa59bf105c9713dc0ae
Michael Rose <michael@rosenetwork.net>
2019-08-31 19:03:52 -0700

    editing histtoclip.fish

--- histtoclip.fish
+++ histtoclip.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/histtoclip.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/histtoclip.fish @ line 2
 function histtoclip
-	echo $history[2]
+	echo $history[1]|clipboard
 end
