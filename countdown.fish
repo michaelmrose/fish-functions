35ce9166776d3a5c4e71f6e66eb07e3a07f450e4
Michael Rose <michael@rosenetwork.net>
2019-09-03 16:07:08 -0700

    editing countdown.fish

--- countdown.fish
+++ countdown.fish
@@ -1,7 +1,8 @@
+# Defined in /home/michael/.config/fish/buffer/countdown.fish @ line 2
 function countdown
 	echo going in ....
     for i in (reverse (seq $argv[1]))
-        sendit $i
+        echo $i
         sleep 1
     end
     echo too late!
