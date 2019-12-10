b8363c339e145a8c4874dbed7e9ad3737a6b2f6b
Michael Rose <michael@rosenetwork.net>
2019-12-09 23:59:43 -0800

    editing graal.fish

--- graal.fish
+++ graal.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/graal.fish @ line 2
 function graal
-	$GRAALVM_HOME/bin/native-image -H:+ReportUnsupportedElementsAtRuntime -Dgraal.CompilerConfiguration=economy --no-server --no-fallback -jar $argv
+	$GRAALVM_HOME/bin/native-image -H:+ReportUnsupportedElementsAtRuntime  -H:+initialize-at-build-time -Dgraal.CompilerConfiguration=economy --no-server --no-fallback -jar $argv
 end
