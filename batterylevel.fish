b02eaba26cccbf3ff6343bf746b83c80a16b45e9
Michael Rose <michael@rosenetwork.net>
2019-12-31 22:46:16 -0800

    editing batterylevel.fish

--- batterylevel.fish
+++ batterylevel.fish
@@ -1,5 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/batterylevel.fish @ line 2
 function batterylevel
 	upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|to\ full|percentage"|awk '{print $2}'| condense_lines
-    " " 
 end
