05da9151711e183625b60b2bb501e7d7de5ae935
Michael Rose <michael@rosenetwork.net>
2020-01-20 18:30:37 -0800

    editing dolphin-toggle.fish

--- dolphin-toggle.fish
+++ dolphin-toggle.fish
@@ -1,8 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/dolphin-toggle.fish @ line 2
 function dolphin-toggle
-	if string match (winclass) kitty
+	if string match (winclass) dolphin
         i3 move window to scratchpad
   else
-        i3 '[ class = ^kitty ] scratchpad show;floating disable' 
+        i3 '[ class = ^dolphin ] scratchpad show;floating disable' 
     end
 end
