7a18ef6279e8f92037257c427d4447141f63d4df
Michael Rose <michael@rosenetwork.net>
2020-01-20 18:58:58 -0800

    editing dolphin-toggle.fish

--- dolphin-toggle.fish
+++ dolphin-toggle.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/dolphin-toggle.fish @ line 2
+# Defined in /home/michael/.config/fish/functions/dolphin-toggle.fish @ line 2
 function dolphin-toggle
 	if pgrep dolphin
         if string match (winclass) dolphin
