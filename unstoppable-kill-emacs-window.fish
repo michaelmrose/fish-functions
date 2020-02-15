20c229ab61674925c41cead7e6e4b53a9052ab35
Michael Rose <michael@rosenetwork.net>
2020-02-15 10:56:22 -0800

    editing unstoppable-kill-emacs-window.fish

--- unstoppable-kill-emacs-window.fish
+++ unstoppable-kill-emacs-window.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/unstoppable-kill-emacs-window.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/unstoppable-kill-emacs-window.fish @ line 2
 function unstoppable-kill-emacs-window
 	emf e 'kill-buffer-and-window'
+  emf e 'kill-matching-buffers-without-prompt "*eaf*"'
 end
