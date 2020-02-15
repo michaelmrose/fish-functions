021e50990ff6dc832d524ea5f42512ed2b545e76
Michael Rose <michael@rosenetwork.net>
2020-02-15 10:31:39 -0800

    editing sus.fish

--- sus.fish
+++ sus.fish
@@ -1,26 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/cleanup-desktop_lightsout_rboo.fish @ line 42
+# Defined in /home/michael/.config/fish/buffer/dockctl_sus.fish @ line 49
 function sus
-	
-  # end
-  
-  # if exists $argv
-  #   switch $argv[1]
-  #     case greet
-  #       greeter
-  #   end
-  # end
-  # manage-share ensure-mounted
-    # sudo rmmod wacom
-    systemctl suspend;
-    # sleep 10
-    # fix-touchscreen
-    # setup-keyboard
-    # xrr triple
-    # sleep 1
-    # i3 restart
-    # signal-i3blocks battery
-    # sleep 10
-    # if docked
-    #     dockctl dock
-    # end
+	systemctl suspend;
 end
