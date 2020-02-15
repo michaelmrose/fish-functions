b4e6174e48ffa605fff7dec8376647d9b08a092a
Michael Rose <michael@rosenetwork.net>
2020-02-15 10:30:58 -0800

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,13 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/xrr.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/game_runp2_xrr.fish @ line 28
 function xrr
 	switch $argv[1]
       case laptop
           xrandr --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75 --output DP1 --off
       case desktop
           xrandr --output eDP1 --off --output DP1 --auto
-      case triple
-          xrandr --output eDP1 --size 1920x1080 --output DP1 --size 1920x1080 --right-of eDP1
-      case test
+      case double
           xrandr --output eDP1 --auto --set "scaling mode" "Full aspect" --scale 0.75x0.75 --output DP1 --auto --right-of eDP1
 
   end
