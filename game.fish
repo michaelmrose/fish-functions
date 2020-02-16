c5a1d96fee86ef4a2f18b2e913529eaa716a4ad5
Michael Rose <michael@rosenetwork.net>
2020-02-16 11:43:20 -0800

    editing game.fish

--- game.fish
+++ game.fish
@@ -1,10 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/game_runp2_xrr.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/game.fish @ line 2
 function game
 	xrr desktop
-    killall compton
-    sleep 0.25
-    optirun $argv
-    sleep 1
-    xrr double
-    compton &
+  killall picom
+  sleep 0.25
+  optirun $argv
+  sleep 1
+  xrr double
+  picom &
 end
