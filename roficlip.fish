82d697c83d986198533f78bd6d47c81d93717795
Michael Rose <michael@rosenetwork.net>
2020-03-02 13:00:39 -0800

    editing roficlip.fish

--- roficlip.fish
+++ roficlip.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/roficlip.fish @ line 2
 function roficlip
-	rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
+	rofi -m -1 -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
 end
