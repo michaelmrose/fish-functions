ec764c8ec3244598dae88b8aff3febcdba7856cd
Michael Rose <michael@rosenetwork.net>
2020-03-08 04:04:58 -0700

    editing z.fish

--- z.fish
+++ z.fish
@@ -1,6 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/z.fish @ line 2
 function z
 	if test -d $argv
+        cd $argv
     else
 	      set zres ( zoxide query $argv|cut -c8-)
         if test (count $zres) -gt 0
