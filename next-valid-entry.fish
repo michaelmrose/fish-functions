156ee612e2734daff64d32adc9aedeb9e71336c5
Michael Rose <michael@rosenetwork.net>
2020-03-08 08:14:41 -0700

    editing next-valid-entry.fish

--- next-valid-entry.fish
+++ next-valid-entry.fish
@@ -1,18 +1,16 @@
-# Defined in /home/michael/.config/fish/buffer/next-valid-entry_next-valid-in.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/next-valid-entry.fish @ line 2
 function next-valid-entry
 	set current $argv[1]
   set ndx 3
   for item in $argv[2..-2]
       if [ $current = $item ]
           echo $argv[$ndx]
-          return 0
         else
             set ndx (math $ndx + 1)
         end
     end
     if [ $current = $argv[-1] ]
         echo $argv[2]
-        return 0
     else
         return 1
     end
