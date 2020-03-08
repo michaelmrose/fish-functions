68fd69146150f028268c405ef19788ce4fcad485
Michael Rose <michael@rosenetwork.net>
2020-03-08 11:37:12 -0700

    editing zr.fish

---
+++ zr.fish
@@ -0,0 +1,13 @@
+# Defined in /home/michael/.config/fish/buffer/zr.fish @ line 1
+function zr
+	if test (count $argv) -gt 0
+        set _Z_RESULT (zoxide query $argv)
+        switch "$_Z_RESULT"
+            case 'query: *'
+                cd (string sub -s 8 -- "$_Z_RESULT")
+                commandline -f repaint
+            case '*'
+                echo -n "$_Z_RESULT"
+        end
+    end
+end
