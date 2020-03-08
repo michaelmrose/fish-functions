a2f86200fc923feaa708448a6f456a29063e99ac
Michael Rose <michael@rosenetwork.net>
2020-03-08 07:48:41 -0700

    editing switchaudio.fish

--- switchaudio.fish
+++ switchaudio.fish
@@ -20,7 +20,7 @@ function switchaudio
 
     end
 
-    if not exists $next
+    if not test $next
         echo 'invalid argument please enter next, set [name], or toggle [name] [name] wherein name is a valid substring in the sink description'
         echo 'Sinks:'
         ponymix -t sink list --short
