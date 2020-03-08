b614a7c3dd24bfd9909d388c9f0864bd1b1a8776
Michael Rose <michael@rosenetwork.net>
2020-03-08 08:02:46 -0700

    editing next-valid-entry.fish

--- next-valid-entry.fish
+++ next-valid-entry.fish
@@ -1,9 +1,10 @@
 # Defined in /home/michael/.config/fish/buffer/next-valid-entry_next-valid-in.fish @ line 2
 function next-valid-entry
 	set current $argv[1]
-    set lst $argv[2..-2]
-    set ndx 2
-    for item in $lst
+  set lst $argv[2..-2]
+  set last $argv[-1]
+  set ndx 2
+  for item in $lst
         echo c is $current and i is $item
         if test $current = $item
             echo $lst[$ndx]
@@ -12,8 +13,8 @@ function next-valid-entry
             set ndx (math $ndx + 1)
         end
     end
-    echo c is $current and last is $lst[-1]
-    if test $current = $lst[-1]
+    echo c is $current and last is $last
+    if test $current = $last
         echo last entry
         echo $lst[1]
         return 0
