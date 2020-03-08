55bfa0e500c2328ae7c17a613998e7faa6aa87b3
Michael Rose <michael@rosenetwork.net>
2020-03-08 07:59:27 -0700

    editing next-valid-index.fish

--- next-valid-index.fish
+++ next-valid-index.fish
@@ -1,7 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/next-valid-entry_next-valid-in.fish @ line 22
+# Defined in /home/michael/.config/fish/buffer/next-valid-entry_next-valid-in.fish @ line 24
 function next-valid-index
 	
-    # ternary test $n -ge (count $lst) @ $lst[1] @ $lst[(math $n + 1)]
 	set val $argv[1]
   set lst $argv[2..-1]
   set n (findindex $val $lst)
