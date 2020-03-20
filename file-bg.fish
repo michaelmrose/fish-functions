973ab71ed5f5375e26306873207c150956fe3bd9
Michael Rose <michael@rosenetwork.net>
2020-03-20 15:03:26 -0700

    editing file-bg.fish

--- file-bg.fish
+++ file-bg.fish
@@ -6,17 +6,17 @@ function file-bg
     set category (echo $target | cut -d "/" -f1)
     set name (echo $target | cut -d "/" -f2)
     set dir (get-folder-for-backgrounds $category)
-
+    echo dir is $dir
     if not test -d $dir
         echo $dir does not exist
         return 1
     end
 
-    set location $dir/$name.$ext
-    if test -e $location
-        echo target already exists
-        return 1
-    end
-    mv $file $location
-    wp $location
+    # set location $dir/$name.$ext
+    # if test -e $location
+    #     echo target already exists
+    #     return 1
+    # end
+    # mv $file $location
+    # wp $location
 end
