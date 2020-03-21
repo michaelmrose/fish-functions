5a8387bc0f1724de6a2f6ad3ca54c0face2db9a9
Michael Rose <michael@rosenetwork.net>
2020-03-21 11:24:29 -0700

    editing deprio.fish

--- deprio.fish
+++ deprio.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/deprio.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/deprio_nicer_nicer-desktop.fish @ line 2
 function deprio
     for p in (pgrep $argv)
         s ionice -c 3 -p $p
