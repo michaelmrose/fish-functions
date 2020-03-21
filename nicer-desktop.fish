af8d60dd33389fd679fc009b4711aa542ce7dcd9
Michael Rose <michael@rosenetwork.net>
2020-03-20 17:04:47 -0700

    editing nicer-desktop.fish

--- nicer-desktop.fish
+++ nicer-desktop.fish
@@ -1,4 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/nicer-desktop.fish @ line 2
 function nicer-desktop
 	pgrep 'Xorg|i3|emacs|pulseaudio'|each nicer
+  deprio Web
+  deprio firefox
+  deprio qbittorrent
+  deprio mullvad
+  deprio openvpn
 end
