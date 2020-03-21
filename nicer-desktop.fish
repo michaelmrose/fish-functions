334f06b0fbbb64042b2c3c5b95b4ad028160e165
Michael Rose <michael@rosenetwork.net>
2020-03-21 11:24:29 -0700

    editing nicer-desktop.fish

--- nicer-desktop.fish
+++ nicer-desktop.fish
@@ -1,9 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/nicer-desktop.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/deprio_nicer_nicer-desktop.fish @ line 15
 function nicer-desktop
-	pgrep 'Xorg|i3|emacs|pulseaudio'|each nicer
-  deprio Web
-  deprio firefox
-  deprio qbittorrent
-  deprio mullvad
-  deprio openvpn
+    set high (pgrep 'Xorg|i3|emacs|pulseaudio')
+    set low (pgrep 'firefox|qbittorrent|mullvad|openvpn')
+    sudo renice -n -20 -p $high
+    sudo ionice -c 1 -p $high
+    sudo ionice -c 3 -p $low
 end
