149154749627606f748392d812ae6c83796ed0b2
Michael Rose <michael@rosenetwork.net>
2020-03-22 13:38:33 -0700

    editing dji.fish

--- dji.fish
+++ dji.fish
@@ -1,5 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/dji.fish @ line 2
 function dji
     set -x NO_COLOR 1
-    ticker.sh \^DJI|nth 2|money-format
+    set ndx (ticker.sh \^DJI|nth 2|money-format)
+    msg the dow is now at ... $ndx
+    echo $ndx
 end
