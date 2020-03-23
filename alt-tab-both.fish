1eaa7ee1b919c0da6f73cf0df5874b1e0811f2b1
Michael Rose <michael@rosenetwork.net>
2020-03-23 16:06:43 -0700

    editing alt-tab-both.fish

--- alt-tab-both.fish
+++ alt-tab-both.fish
@@ -1,6 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/alt-tab-both.fish @ line 13
+# Defined in /home/michael/.config/fish/buffer/alt-tab-both.fish @ line 2
 function alt-tab-both
     set active (get-ws-info get name where visible = true)
-    i3-msg workspace (next-valid-entry (odds $active) (odds (seq 9) 0) )
-    i3-msg workspace (next-valid-entry (evens $active) (evens (seq 9) 0))
+    set workspaces (seq 9) 0
+    i3-msg workspace (next-valid-entry (odds $active) (odds $workspaces ))
+    i3-msg workspace (next-valid-entry (evens $active) (evens $workspaces))
 end
