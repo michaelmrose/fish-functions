875bfcf62e173c9bcdf2fd8ac67b1989e6eb53d9
Michael Rose <michael@rosenetwork.net>
2020-03-22 19:12:01 -0700

    editing fw.fish

--- fw.fish
+++ fw.fish
@@ -1,7 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/fw.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/fw.fish @ line 2
 function fw
     set words $argv
     while read -l line
-        echo $line
+        set acc $line
+        for word in $words
+            echo $word
+        end
     end
 end
