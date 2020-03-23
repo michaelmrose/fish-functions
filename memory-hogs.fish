8527500d94590c55e988197f38ffc42c8a2c69b1
Michael Rose <michael@rosenetwork.net>
2020-03-22 21:09:14 -0700

    editing memory-hogs.fish

--- memory-hogs.fish
+++ memory-hogs.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/memory-hogs.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/memory-hogs.fish @ line 2
 function memory-hogs
     set ps (ps -eo pmem,pcpu,rss,pid,cmd | sort -k 1 -nr)
     for p in $ps
-        set mem (echo $p | '{print 3}')
+        set mem (echo $p |awk '{print $3}')
         echo $mem
     end
 end
