2d50bc0d0c29a570d094a045df4c14faedc2233e
Michael Rose <michael@rosenetwork.net>
2020-03-23 15:39:33 -0700

    editing alt-tab-both.fish

--- alt-tab-both.fish
+++ alt-tab-both.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/alt-tab-both.fish @ line 2
 function alt-tab-both
     set active (get-ws-info get name where visible = true)
-    set valid (seq 9) 0
+    set valid (seq 9) 0 # 0 needs to come after 9 contrary to math
     set odds (filter-with-expr is-odd $valid)
     set evens (filter-with-expr is-even $valid)
     set left  (filter-with-expr is-odd $active)
