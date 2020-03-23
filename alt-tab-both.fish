06e79c934a8472fe6d5a6b7a239d9e447af0f9c5
Michael Rose <michael@rosenetwork.net>
2020-03-23 15:57:14 -0700

    editing alt-tab-both.fish

--- alt-tab-both.fish
+++ alt-tab-both.fish
@@ -1,11 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/alt-tab-both.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/alt-tab-both.fish @ line 13
 function alt-tab-both
     set active (get-ws-info get name where visible = true)
-    set valid (seq 9) 0 # 0 needs to come after 9 contrary to math
-    set odds (filter-with-expr is-odd $valid)
-    set evens (filter-with-expr is-even $valid)
-    set left  (filter-with-expr is-odd $active)
-    set right (filter-with-expr is-even $active)
-    i3-msg workspace (next-valid-entry $left $odds)
-    i3-msg workspace (next-valid-entry $right $evens)
+    i3-msg workspace (next-valid-entry (odds $active) (odds (seq 9) 0) )
+    i3-msg workspace (next-valid-entry (evens $active) (evens (seq 9) 0))
 end
