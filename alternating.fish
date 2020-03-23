1da517079ba4c7a57a63b83882037ff2ccf492f4
Michael Rose <michael@rosenetwork.net>
2020-03-23 15:48:12 -0700

    removed alternating.fish

--- alternating.fish
+++
@@ -1,7 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/alternating.fish @ line 2
-function alternating
-    echo a is $argv
-    echo o is (odds $argv)
-    set -x odds (odds $argv)
-    set -x evens (evens $argv)
-end
