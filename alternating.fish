a4fd6ba796d017f33110a54daf828980ee995846
Michael Rose <michael@rosenetwork.net>
2020-03-23 15:45:38 -0700

    editing alternating.fish

--- alternating.fish
+++ alternating.fish
@@ -1,5 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/alternating.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/alternating.fish @ line 2
 function alternating
+    echo a is $argv
+    echo o is (odds $argv)
     set -x odds (odds $argv)
     set -x evens (evens $argv)
 end
