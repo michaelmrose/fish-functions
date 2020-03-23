fad235781a1cb39d8093aecba8b98dd117e90433
Michael Rose <michael@rosenetwork.net>
2020-03-22 18:36:37 -0700

    editing money-format.fish

--- money-format.fish
+++ money-format.fish
@@ -1,7 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/money-format.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/inostd_money-format.fish @ line 13
 function money-format
     if exists $argv
-        printf '$%.2f\n' $argv|sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
+        printf '$%.2f ' $argv|sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
     else
         while read -l line
             money-format $line
