14965638c2a91533886560a64828fe0f3662ffd5
Michael Rose <michael@rosenetwork.net>
2020-03-23 15:32:35 -0700

    editing alt-tab-both.fish

---
+++ alt-tab-both.fish
@@ -0,0 +1,11 @@
+# Defined in /home/michael/.config/fish/buffer/alt-tab-both.fish @ line 1
+function alt-tab-both
+    set active (get-ws-info get name where visible = true)
+    set valid (seq 10)
+    set odds (filter-with-expr is-odd $valid)
+    set evens (filter-with-expr is-even $valid)
+    set left  (filter-with-expr is-odd $active)
+    set right (filter-with-expr is-even $active)
+    i3-msg workspace (next-valid-entry $left $odds)
+    i3-msg workspace (next-valid-entry $ight $evens)
+end
