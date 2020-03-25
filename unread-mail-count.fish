ac9b372e7c4ae8df593fecd0cab61b8f0560f475
Michael Rose <michael@rosenetwork.net>
2020-03-25 13:42:22 -0700

    editing unread-mail-count.fish

--- unread-mail-count.fish
+++ unread-mail-count.fish
@@ -1,7 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/unread-mail-count.fish @ line 2
 function unread-mail-count
-  set spam maildir:/OldGmail/[Gmail].Spam
-  set tech maildir:/OldGmail/techmail
-  set morespam maildir:/Gmail/[Gmail].Spam
-  mu find "date:7D..now and flag:unread and not flag:trashed and not $spam and not $morespam and not $tech"|wc -l
+    # set spam maildir:/OldGmail/[Gmail].Spam
+    # set tech maildir:/OldGmail/techmail
+    # set morespam maildir:/Gmail/[Gmail].Spam
+    mu find flag:unread and date:7d..now and not maildir:/OldGmail/techmail
+    # mu find "date:7D..now and flag:unread and not flag:trashed and not $spam and not $morespam and not $tech"|wc -l
 end
