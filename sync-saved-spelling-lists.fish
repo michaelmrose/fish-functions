2bcb7c71dbbe286b4d38ae98892f260d3cfe1b27
Michael Rose <michael@rosenetwork.net>
2020-03-29 22:20:26 -0700

    editing sync-saved-spelling-lists.fish

--- sync-saved-spelling-lists.fish
+++ sync-saved-spelling-lists.fish
@@ -1,5 +1,13 @@
-# Defined in /home/michael/.config/fish/buffer/sync-saved-spelling-lists.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/sync-saved-spelling-lists.fish @ line 2
 function sync-saved-spelling-lists
-    set words (p (cat ~/.aspell.en.pws)[2..-1] $FIREFOX_WORDS_FILE | sort -u) 
+    set aspell ~/.aspell.en.pws
+    set aspellcontents (cat $aspell)
+    set header $aspellcontents[1]
+    set aspellwords $aspellcontents[2..-1]
+    set firefoxwords (cat $FIREFOX_WORDS_FILE)
+    set combined (p $aspellwords $firefoxwords | sort -u)
+
+    p $header > $aspell
+    p $combined >> $aspell
     p $words > $FIREFOX_WORDS_FILE
 end
