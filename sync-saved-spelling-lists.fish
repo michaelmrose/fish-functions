3df8f00d68744bd756d6e86da7dffa6800ba34b2
Michael Rose <michael@rosenetwork.net>
2020-03-29 22:38:03 -0700

    editing sync-saved-spelling-lists.fish

--- sync-saved-spelling-lists.fish
+++ sync-saved-spelling-lists.fish
@@ -2,14 +2,22 @@
 function sync-saved-spelling-lists
     set aspell ~/.aspell.en.pws
     set hunspell ~/.hunspell_en_US
+    set enchant ~/.config/enchant/en_US.dic
+    set firefox $FIREFOX_WORDS_FILE
+
     set aspellcontents (cat $aspell)
     set header $aspellcontents[1]
     set aspellwords $aspellcontents[2..-1]
-    set firefoxwords (cat $FIREFOX_WORDS_FILE)
-    set combined (p $aspellwords $firefoxwords | sort -u)
+
+    set firefoxwords (cat $firefox)
+    set enchantwords (cat $enchant)
+    set hunspellwords (cat $hunspell)
+
+    set combined (p $aspellwords $firefoxwords $enchantwords $hunspellwords | sort -u)
 
     p $header > $aspell
     p $combined >> $aspell
-    p $combined > $FIREFOX_WORDS_FILE
+    p $combined > $firefox
     p $combined > $hunspell
+    p $combined > $enchant
 end
