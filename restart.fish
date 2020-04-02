4d8993e18c68d219d76d6b54f312d35f1661523d
Michael Rose <michael@rosenetwork.net>
2020-04-02 16:17:22 -0700

    editing restart.fish

--- restart.fish
+++ restart.fish
@@ -1,6 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/restart.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/restart.fish @ line 2
 function restart
 	killall $argv
     sleep 1
     $argv &
+    daj
 end
