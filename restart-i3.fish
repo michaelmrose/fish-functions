730d4cc1c7a0861b1837b525af3bccfa2751cccb
Michael Rose <michael@rosenetwork.net>
2020-04-02 16:16:12 -0700

    editing restart-i3.fish

--- restart-i3.fish
+++ restart-i3.fish
@@ -2,10 +2,12 @@
 function restart-i3
 	set wid (xdotool getactivewindow)
 	  i3 restart
-	  killall compton
+    picom
     sleep 0.25
     compton &
+    picom &
     sleep 0.5
     wmctrl -l|awk '{print $1}'|each remove-opacity-from-window
     eval "i3 [id=$wid] focus" 
+    daj
 end
