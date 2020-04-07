f40ab05f4ab189f2da835bd20702bf27adcf4088
Michael Rose <michael@rosenetwork.net>
2020-04-07 14:21:06 -0700

    editing decorate-workspacename.fish

--- decorate-workspacename.fish
+++ decorate-workspacename.fish
@@ -1,5 +1,62 @@
-# Defined in /home/michael/.config/fish/buffer/decorate-workspacename_workspa.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/decorate-workspacename_ws_ws2.fish @ line 2
 function decorate-workspacename
-	# echo "   $argv   "
-    echo $argv
+    switch $argv
+        case a
+            set target 1
+        case b
+            set target 2
+        case c
+            set target 3
+        case d
+            set target 4
+        case e
+            set target 5
+        case f
+            set target 6
+        case g
+            set target 7
+        case h
+            set target 8
+        case i
+            set target 9
+        case j
+            set target 10
+        case k
+            set target 11
+        case l
+            set target 12
+        case m
+            set target 13
+        case n
+            set target 14
+        case o
+            set target 15
+        case p
+            set target 16
+        case q
+            set target 17
+        case r
+            set target 18
+        case s
+            set target 19
+        case t
+            set target 20
+        case u
+            set target 21
+        case v
+            set target 22
+        case w
+            set target 23
+        case x
+            set target 24
+        case y
+            set target 25
+        case z
+            set target 26
+        case "*"
+
+        echo $argv
+        return 0
+    end
+    echo {$target}:{$argv}
 end
