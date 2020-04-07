e086b283649510e2926ba9039b7e284d2f5b923b
Michael Rose <michael@rosenetwork.net>
2020-04-07 13:52:08 -0700

    editing decorate-workspacename.fish

--- decorate-workspacename.fish
+++ decorate-workspacename.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/decorate-workspacename.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/decorate-workspacename_workspa.fish @ line 2
 function decorate-workspacename
 	# echo "   $argv   "
     echo $argv
