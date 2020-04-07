29b216044a59428d76a4ee29b73e067c42c3492a
Michael Rose <michael@rosenetwork.net>
2020-04-07 15:04:03 -0700

    editing keyboardordering.fish

---
+++ keyboardordering.fish
@@ -0,0 +1,82 @@
+# Defined in /home/michael/.config/fish/buffer/alphaordering_decorate-workspa.fish @ line 148
+function keyboardordering
+    switch $argv
+        case a
+            set target 1
+        case b
+            set target 2
+        case c
+            set target 3
+        case d
+            set target 4
+        case e
+            set target 5
+        case f
+            set target 6
+        case g
+            set target 7
+        case h
+            set target 8
+        case i
+            set target 9
+        case j
+            set target 10
+        case k
+            set target 11
+        case l
+            set target 12
+        case m
+            set target 13
+        case n
+            set target 14
+        case o
+            set target 15
+        case p
+            set target 16
+        case q
+            set target 17
+        case r
+            set target 18
+        case s
+            set target 19
+        case t
+            set target 20
+        case u
+            set target 21
+        case v
+            set target 22
+        case w
+            set target 23
+        case x
+            set target 24
+        case y
+            set target 25
+        case z
+            set target 26
+        case 1
+            set target 27
+        case 2
+            set target 28
+        case 3
+            set target 29
+        case 4
+            set target 30
+        case 5
+            set target 31
+        case 6
+            set target 32
+        case 7
+            set target 33
+        case 8
+            set target 34
+        case 9
+            set target 35
+        case 0
+            set target 36
+        case "*"
+
+        echo $argv
+        return 0
+    end
+    echo {$target}:{$argv}
+end
