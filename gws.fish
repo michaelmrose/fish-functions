7e23a784d325fa613eb742b5d00d0a9e8268798b
Michael Rose <michael@rosenetwork.net>
2020-04-07 14:24:37 -0700

    editing gws.fish

--- gws.fish
+++ gws.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/gws_vi3_get-workspace.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/gws.fish @ line 2
 function gws
 	save-workspaces
-    ws $argv
-    vi3_select-all-in-workspace
-    vi3_take-back
-    restore-workspaces
+  ws (decorate-workspacename $argv)
+  vi3_select-all-in-workspace
+  vi3_take-back
+  restore-workspaces
 end
