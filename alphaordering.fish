919100657859201009f17d2401b6758c521325ef
Michael Rose <michael@rosenetwork.net>
2020-04-07 15:04:03 -0700

    editing alphaordering.fish

--- alphaordering.fish
+++ alphaordering.fish
@@ -1,78 +1,58 @@
-# Defined in /home/michael/.config/fish/buffer/alphaordering_decorate-workspa.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/alphaordering_decorate-workspa.fish @ line 2
 function alphaordering
     switch $argv
         case a
             set target 1
-        case b
-            set target 2
-        case c
-            set target 3
-        case d
-            set target 4
-        case e
-            set target 5
-        case f
-            set target 6
-        case g
-            set target 7
-        case h
-            set target 8
-        case i
-            set target 9
-        case j
-            set target 10
-        case k
-            set target 11
-        case l
-            set target 12
-        case m
-            set target 13
-        case n
-            set target 14
-        case o
-            set target 15
-        case p
-            set target 16
-        case q
-            set target 17
-        case r
-            set target 18
         case s
-            set target 19
-        case t
-            set target 20
-        case u
-            set target 21
-        case v
-            set target 22
-        case w
-            set target 23
-        case x
-            set target 24
-        case y
-            set target 25
+            set target 2
+        case d
+            set target 3
+        case f
+            set target 4
+        case g
+            set target 5
         case z
-            set target 26
+            set target 6
+        case x
+            set target 7
+        case c
+            set target 8
+        case v
+            set target 9
+        case b
+            set target 10
         case 1
-            set target 27
+            set target 11
         case 2
-            set target 28
+            set target 12
         case 3
-            set target 29
+            set target 13
         case 4
-            set target 30
+            set target 14
         case 5
-            set target 31
+            set target 15
+        case h
+            set target 16
+        case j
+            set target 17
+        case k
+            set target 18
+        case l
+            set target 19
+        case n
+            set target 20
+        case m
+            set target 21
         case 6
-            set target 32
+            set target 22
         case 7
-            set target 33
+            set target 23
         case 8
-            set target 34
+            set target 24
         case 9
-            set target 35
+            set target 25
         case 0
-            set target 36
+            set target 26
         case "*"
 
         echo $argv
