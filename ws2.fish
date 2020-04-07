1123a309185157e2aaac3c352df4399cc71e9745
Michael Rose <michael@rosenetwork.net>
2020-04-07 14:01:36 -0700

    editing ws2.fish

--- ws2.fish
+++ ws2.fish
@@ -5,7 +5,55 @@ function ws2
             set target 1
         case b
             set target 2
+        case c
+            set target 3
+        case d
+            set target 4
+        case e
+            set target 5
+        case f
+            set target 6
+        case g
+            set target 7
+        case h
+            set target 8
+        case i
+            set target 9
+        case j
+            set target 10
+        case k
+            set target 11
+        case l
+            set target 12
+        case m
+            set target 13
+        case n
+            set target 14
+        case o
+            set target 15
+        case p
+            set target 16
+        case q
+            set target 17
+        case r
+            set target 18
+        case s
+            set target 19
+        case t
+            set target 20
+        case u
+            set target 21
+        case v
+            set target 22
+        case w
+            set target 23
+        case x
+            set target 24
+        case y
+            set target 25
+        case z
+            set target 26
     end
 
-    echo $target: $argv
+    i3-msg $target: $argv
 end
