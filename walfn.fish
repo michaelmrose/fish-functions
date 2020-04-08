491fbc54a16d595680a01c5e25f346903f9d2519
Michael Rose <michael@rosenetwork.net>
2020-04-07 22:17:41 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,5 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/walfn.fish @ line 2
 function walfn
-    echo setting wal
-    wal -n --saturate 1.0 -i $bgimage[1]
+    # echo setting wal
+    # wal -n --saturate 1.0 -i $bgimage[1]
 end
