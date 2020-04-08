580bae4318680675b1ad542fc58d6c1b1cb88bdf
Michael Rose <michael@rosenetwork.net>
2020-04-08 14:26:05 -0700

    editing colorize-emacs.fish

--- colorize-emacs.fish
+++ colorize-emacs.fish
@@ -1,8 +1,14 @@
-# Defined in /home/michael/.config/fish/buffer/colorize-emacs.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/colorize-emacs_walfn.fish @ line 2
 function colorize-emacs
-    set acc $argv[1]
-    set fg $argv[2]
-    set bg $argv[3]
+    if not exists $argv
+        set accent (jq -r .colors.color2 ~/.cache/wal/colors.json)
+        set fg (jq -r .colors.color0 ~/.cache/wal/colors.json)
+        set bg (jq -r .colors.color15 ~/.cache/wal/colors.json)
+    else
+        set acc $argv[1]
+        set fg $argv[2]
+        set bg $argv[3]
+    end
     set com emf e \'mmr/pywal-colors \"$acc\" \"$fg\" \"$bg\"\'
     eval $com
 end
