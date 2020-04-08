3d2ab62ac947876a14ec58ae68ea53a08a1a09f8
Michael Rose <michael@rosenetwork.net>
2020-04-08 15:58:12 -0700

    editing process-colors-to-hexchat.fish

--- process-colors-to-hexchat.fish
+++ process-colors-to-hexchat.fish
@@ -1,5 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/get-wal-rgb-colors_process-col.fish @ line 7
 function process-colors-to-hexchat
     set colors (get-wal-rgb-colors)
-    p $colors
+    set ndx 0
+    for c in $colors
+        echo color_$ndx = $c
+        set ndx (increase $ndx)
+    end
 end
