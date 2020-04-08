cbdb80117be219392ef3fd2094ec572ea156a7d2
Michael Rose <michael@rosenetwork.net>
2020-04-08 15:53:00 -0700

    editing process-colors-to-hexchat.fish

--- process-colors-to-hexchat.fish
+++ process-colors-to-hexchat.fish
@@ -1,11 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/hexcolor-to-hexchatcolor_proce.fish @ line 10
 function process-colors-to-hexchat
-    set colors (cat ~/.cache/wal/colors)
-    set numbers (seq 1 (count $colors))
-    set ndx 1
-    for c in $colors
-        echo c is $c
-        echo  colors_$ndx = (hexcolor-to-hexchatcolor $c)
-        set ndx (increase $ndx)
-    end
+    set colors (process-colors-to-hexchat)
 end
