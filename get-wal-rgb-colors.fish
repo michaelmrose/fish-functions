a15c75c871273cbb88f6d69d1b5f067536c52506
Michael Rose <michael@rosenetwork.net>
2020-04-08 15:54:50 -0700

    editing get-wal-rgb-colors.fish

--- get-wal-rgb-colors.fish
+++ get-wal-rgb-colors.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/get-wal-rgb-colors.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/get-wal-rgb-colors_process-col.fish @ line 2
 function get-wal-rgb-colors
     cat ~/.cache/wal/colors-konsole.colorscheme|g color|cut -d = -f2-|g '^[0-9]'|sd , " "
 end
