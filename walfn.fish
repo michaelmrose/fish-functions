e11cd280e7b0900fffbaed2c0ce6ff1bd6f15408
Michael Rose <michael@rosenetwork.net>
2020-04-08 10:26:12 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/inject-rounded_walfn.fish @ line 15
+# Defined in /home/michael/.config/fish/buffer/walfn.fish @ line 2
 function walfn
     if test (count $bgimage) -gt 1
         montage $bgimage[1] $bgimage[2] -geometry +0+0 /tmp/montage.jpg
@@ -6,6 +6,6 @@ function walfn
     else
         set target $bgimage
     end
-    wal -n --saturate 1.0 -i $target
+    wal -n --saturate 0.7 -i $target
     inject-rounded
 end
