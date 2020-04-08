b7c4a98409f80ca9c61c170c929fb123b5e6d33f
Michael Rose <michael@rosenetwork.net>
2020-04-08 12:20:38 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,5 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/walfn.fish @ line 2
 function walfn
+    set accent (jq -r .colors.color2 ~/.cache/wal/colors.json)
+    replace-color-in-i3 $accent
     if test (count $bgimage) -gt 1
         montage $bgimage[1] $bgimage[2] -geometry +0+0 /tmp/montage.jpg
         set target /tmp/montage.jpg
@@ -8,4 +10,7 @@ function walfn
     end
     wal -n --saturate 0.7 -i $target
     inject-rounded
+    set accent (jq -r .colors.color2 ~/.cache/wal/colors.json)
+    replace-color-in-i3 $accent
+    restart-i3
 end
