20e5173d9ea34fe678d6488c43ff293ab2d5199b
Michael Rose <michael@rosenetwork.net>
2020-04-08 15:54:50 -0700

    editing process-colors-to-hexchat.fish

--- process-colors-to-hexchat.fish
+++ process-colors-to-hexchat.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/hexcolor-to-hexchatcolor_proce.fish @ line 10
+# Defined in /home/michael/.config/fish/buffer/get-wal-rgb-colors_process-col.fish @ line 7
 function process-colors-to-hexchat
-    set colors (process-colors-to-hexchat)
+    set colors (get-wal-rgb-colors)
+    p $colors
 end
