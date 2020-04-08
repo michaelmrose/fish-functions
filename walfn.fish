cb1c3c5f2067fdd46e7751425ed08fa6e425062e
Michael Rose <michael@rosenetwork.net>
2020-04-08 16:04:28 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/colorize-emacs_walfn.fish @ line 17
+# Defined in /home/michael/.config/fish/buffer/walfn.fish @ line 2
 function walfn
 
 if test (count $bgimage) -gt 1
@@ -8,7 +8,7 @@ else
     set target $bgimage
 end
 
-wal -n --saturate 1.0 -i $target
+wal -n --saturate 0.7 -i $target
 
 if exists $argv
     set accent $argv[1]
