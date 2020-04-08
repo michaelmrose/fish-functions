2cec205d7af3e5634ce6acb600d8e0269d8ec01f
Michael Rose <michael@rosenetwork.net>
2020-04-08 14:30:51 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/colorize-emacs_walfn.fish @ line 19
+# Defined in /home/michael/.config/fish/buffer/colorize-emacs_walfn.fish @ line 23
 function walfn
 
 wal -n --saturate 1.0 -i $target
