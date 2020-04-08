5c825e9db275cc0890bdf9590a1cf5b0c414a598
Michael Rose <michael@rosenetwork.net>
2020-04-08 15:41:08 -0700

    editing process-colors-to-hexchat.fish

--- process-colors-to-hexchat.fish
+++ process-colors-to-hexchat.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/hexcolor-to-hexchatcolor_proce.fish @ line 9
+# Defined in /home/michael/.config/fish/buffer/hexcolor-to-hexchatcolor_proce.fish @ line 10
 function process-colors-to-hexchat
     set colors (cat ~/.cache/wal/colors)
     set numbers (seq 1 (count $colors))
     for c in $colors
         set acc $acc 
     end
-    echo numbers
+    echo $numbers
 end
