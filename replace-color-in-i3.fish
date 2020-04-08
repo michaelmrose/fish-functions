4b59dd468802238cd5a8f1cd4ec7b14b8ad9f0db
Michael Rose <michael@rosenetwork.net>
2020-04-08 12:16:03 -0700

    editing replace-color-in-i3.fish

--- replace-color-in-i3.fish
+++ replace-color-in-i3.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/replace-color-in-i3.fish @ line 2
 function replace-color-in-i3
-    sd -i "focused_workspace  #[a-fA-F0-9]{6} #[a-fA-F0-9]{6} #[a-fA-F0-9]{6}" "focused_workspace  #111111 $argv #000000" ~/.config/i3/config
+    sd -i "focused_workspace  #[a-fA-F0-9]{6} #[a-fA-F0-9]{6} #[a-fA-F0-9]{6}" "focused_workspace  #111111 #{$argv} #000000" ~/.config/i3/config
 end
