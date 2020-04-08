3af9d7d71e49e7840c28cff8a323ccfec352d44c
Michael Rose <michael@rosenetwork.net>
2020-04-08 14:30:50 -0700

    editing colorize-emacs.fish

--- colorize-emacs.fish
+++ colorize-emacs.fish
@@ -4,12 +4,16 @@ function colorize-emacs
         set accent (jq -r .colors.color2 ~/.cache/wal/colors.json)
         set fg (jq -r .colors.color0 ~/.cache/wal/colors.json)
         set bg (jq -r .colors.color15 ~/.cache/wal/colors.json)
-        echo a f b is $accent $fg $bg
+        echo a is $accent
+        echo fg is $fg
+        echo bg is $bg
     else
         set acc $argv[1]
         set fg $argv[2]
         set bg $argv[3]
-        echo a f b is $accent $fg $bg
+        echo a is $accent
+        echo fg is $fg
+        echo bg is $bg
     end
     set com emf e \'mmr/pywal-colors \"$acc\" \"$fg\" \"$bg\"\'
     eval $com
