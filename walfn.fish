80c9901f6f1ffabe27e0b8c40a9745bbb0345e6d
Michael Rose <michael@rosenetwork.net>
2020-04-08 13:05:24 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,6 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/replace-color-in-i3_walfn.fish @ line 7
 function walfn
     set accent (jq -r .colors.color2 ~/.cache/wal/colors.json)
+    set fg (jq -r .colors.color0 ~/.cache/wal/colors.json)
+    set bg (jq -r .colors.color15 ~/.cache/wal/colors.json)
     replace-color-in-i3 $accent
     if test (count $bgimage) -gt 1
         montage $bgimage[1] $bgimage[2] -geometry +0+0 /tmp/montage.jpg
