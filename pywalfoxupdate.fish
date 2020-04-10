f4578b72c1e892b8564c8806cdd0b4b38b4b2fba
Michael Rose <michael@rosenetwork.net>
2020-04-10 12:24:51 -0700

    editing pywalfoxupdate.fish

--- pywalfoxupdate.fish
+++ pywalfoxupdate.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/pywalfoxupdate.fish @ line 2
 function pywalfoxupdate
-    if pgrep firefox
+    if pgrep firefox > /dev/null
         echo updating firefox
         python /home/michael/extproj/Pywalfox/daemon/pywalfox.py update
     end
