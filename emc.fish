457f508f65adcc911b312dd5a0acb3515995a745
Michael Rose <michael@rosenetwork.net>
2020-04-14 12:21:47 -0700

    editing emc.fish

--- emc.fish
+++ emc.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/emc.fish @ line 2
 function emc
-    if not pgrep emacs > /dev/null
-        emd
-    end
+    # if not pgrep emacs > /dev/null
+    #     emd
+    # end
     emacsclient -c --alternate-editor='' $argv &
 end
