ce39050b0c0c2abcb72a2ed469dd0c6a57c22a3c
Michael Rose <michael@rosenetwork.net>
2020-04-14 12:28:52 -0700

    editing emc.fish

--- emc.fish
+++ emc.fish
@@ -1,6 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/emc.fish @ line 2
 function emc
     if not emacsclient -c $argv
+        msg daemon not running... starting now
         emd
         emacsclient -c $argv
     end
