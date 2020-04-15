01a5b4d2425e75aa6db6e689d9cd4fea4896084a
Michael Rose <michael@rosenetwork.net>
2020-04-15 12:13:30 -0700

    editing meminfo.fish

--- meminfo.fish
+++ meminfo.fish
@@ -4,7 +4,7 @@ function meminfo
         human (pgrep $argv |each pmap -x|g total|nth 5 |sumof|multiplyby 1024)
     else
         while read -l line
-            pgrep $line|each pmap -x|g total|nth 5|sumof|multiplyby 1024|each human
+            pmap -x $line |g total|nth 5|sumof|multiplyby 1024|each human
         end
     end
 end
