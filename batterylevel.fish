b0e1cf5b848133fbcd38169a8a07333d618b670d
Michael Rose <michael@rosenetwork.net>
2020-04-17 12:19:28 -0700

    editing batterylevel.fish

--- batterylevel.fish
+++ batterylevel.fish
@@ -4,7 +4,7 @@ function batterylevel
     set state $text[1]
     set percentage $text[2]
     set numeric (echo $percentage |rev |cut -c 2- |rev)
-    if [ state = "charging"]
+    if [ state = "charging" ]
         echo charging
     else
         echo not
