1a518423b2a4036293acbb647f232ff82ed84a86
Michael Rose <michael@rosenetwork.net>
2020-04-17 12:30:48 -0700

    editing batterylevel.fish

--- batterylevel.fish
+++ batterylevel.fish
@@ -9,6 +9,6 @@ function batterylevel
     else
         set start F00C
     end
-    set hex $start(hexdigit (math 5 / 10))
+    set hex $start(hexdigit (math 50 / 10))
     printf "\U$hex  $state $numeric"
 end
