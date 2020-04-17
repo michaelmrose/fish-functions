45735ec082738ea909f5825946860abb6cb21605
Michael Rose <michael@rosenetwork.net>
2020-04-17 12:36:29 -0700

    editing batterylevelicon.fish

--- batterylevelicon.fish
+++ batterylevelicon.fish
@@ -1,4 +1,13 @@
-# Defined in /home/michael/.config/fish/buffer/batterylevel_batterylevelicon.fish @ line 16
+# Defined in /home/michael/.config/fish/buffer/batterylevel_batterylevelicon.fish @ line 18
 function batterylevelicon
-
+    set state $argv[1]
+    set numeric $argv[2]
+    if [ $state = charging ]
+        set start F00B
+    else
+        set start F00C
+    end
+    set hex $start(hexdigit (math $numeric / 10))
+    printf "\U$hex  $state $numeric%%"|tr -d '
+'
 end
