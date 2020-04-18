c2f1225502b760c51b4a4bf0852bd3e3df51c23d
Michael Rose <michael@rosenetwork.net>
2020-04-17 19:44:47 -0700

    editing batterylevel.fish

--- batterylevel.fish
+++ batterylevel.fish
@@ -1,13 +1,13 @@
-# Defined in /home/michael/.config/fish/functions/batterylevel.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/batterylevel.fish @ line 2
 function batterylevel
     set text (	upower -i /org/freedesktop/UPower/devices/battery_$argv | grep -E "state|to\ full|percentage"|awk '{print $2}' |tr -d %)
     set state $text[1..-2]
     set percentage $text[-1]
-    if [ $state[1] = charging ]
+    if [ $state[1] = cha ]
         set start F00B
     else
         set start F00C
     end
-    set hex $start(printf '%x' (echo "$percentage / 10"|bc))
+    set hex $start(printf '%x' (echo "50 / 10"|bc))
     printf "\U$hex  $state $percentage%%" | cut -c1-90
 end
