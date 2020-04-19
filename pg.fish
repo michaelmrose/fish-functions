e76bdbe88bb24edf5ecdffcccfa0afebedf127af
Michael Rose <michael@rosenetwork.net>
2020-04-19 11:09:19 -0700

    editing pg.fish

--- pg.fish
+++ pg.fish
@@ -1,8 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/pg.fish @ line 1
 function pg
     set tmp /tmp/pager-(uid)
     while read -l line
         echo $line >> $tmp
     end
     emc $tmp
+    echo removing $tmp
     rm $tmp
 end
