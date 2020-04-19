fa1428a48848e8acc6428fd3523577078cbaca92
Michael Rose <michael@rosenetwork.net>
2020-04-19 11:08:38 -0700

    removed pager.fish

--- pager.fish
+++
@@ -1,9 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/pager.fish @ line 1
-function pager
-    set tmp /tmp/pager-(uid)
-    while read -l line
-        echo $line >> $tmp
-    end
-    emc $tmp
-    rm $tmp
-end
