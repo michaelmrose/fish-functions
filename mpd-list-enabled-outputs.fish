856be5b6454ed7b676511d61b481adcaf25aca2b
Michael Rose <michael@rosenetwork.net>
2020-04-20 03:41:24 -0700

    editing mpd-list-enabled-outputs.fish

--- mpd-list-enabled-outputs.fish
+++ mpd-list-enabled-outputs.fish
@@ -1,5 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/mpd-list-enabled-outputs_mpd-s.fish @ line 2
 function mpd-list-enabled-outputs
-    mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|tr '
-    ' ' '
+    # mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|tr '
+    # ' ' '
+    mpd outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|cut -c1-90
 end
