8aca92cfc6d9b76a1d6764cb4270dd37b9bbdacb
Michael Rose <michael@rosenetwork.net>
2020-04-20 03:42:47 -0700

    editing mpd-list-enabled-outputs.fish

--- mpd-list-enabled-outputs.fish
+++ mpd-list-enabled-outputs.fish
@@ -2,5 +2,10 @@
 function mpd-list-enabled-outputs
     # mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|tr '
     # ' ' '
-    mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|cut -c1-90
+    set outputs (mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|cut -c1-90)
+    if test (count $outputs) -gt 1
+        echo $outputs[1] and $outputs[2]
+    else
+        echo $outputs
+    end
 end
