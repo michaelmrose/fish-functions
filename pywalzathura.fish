f2d1506e2dcb7692ec2c303808de1231d11a1a6e
Michael Rose <michael@rosenetwork.net>
2020-04-22 02:23:52 -0700

    editing pywalzathura.fish

---
+++ pywalzathura.fish
@@ -0,0 +1,5 @@
+# Defined in /home/michael/.config/fish/buffer/pywalzathura.fish @ line 1
+function pywalzathura
+    # cat ~/.config/zathura/zathurarc.primary > ~/.config/zathura/zathurarc
+    # gen
+end
