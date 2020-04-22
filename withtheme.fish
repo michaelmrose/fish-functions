908cc75b8a68497c4a118743fe6ae82081321f5d
Michael Rose <michael@rosenetwork.net>
2020-04-21 21:42:38 -0700

    editing withtheme.fish

--- withtheme.fish
+++ withtheme.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/withtheme.fish @ line 2
 function withtheme
-    if string match '*:*' $argv[1]
+    if string match '*:*' $argv[1]>/dev/null
         set theme $argv[1]
     else
         set theme $argv[1]:light
