8a3321f4b50075b8c3f9b6a57e235678562eee27
Michael Rose <michael@rosenetwork.net>
2020-04-21 21:42:11 -0700

    editing withtheme.fish

--- withtheme.fish
+++ withtheme.fish
@@ -1,9 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/withtheme.fish @ line 2
 function withtheme
     if string match '*:*' $argv[1]
-        set theme $argv[1]:light
-    else
         set theme $argv[1]
+    else
+        set theme $argv[1]:light
     end
 
     # env GTK_THEME=$argv[1] $argv[2..-1]
