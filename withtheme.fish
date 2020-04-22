65e4cf926c208c0cb84440a6e777848437479956
Michael Rose <michael@rosenetwork.net>
2020-04-21 21:35:20 -0700

    editing withtheme.fish

--- withtheme.fish
+++ withtheme.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/withtheme.fish @ line 2
 function withtheme
-    set name (echo $argv[1]| cut -d : -f1)
-    set style (echo $argv[1]| cut -d : -f2)
+    set name (echo $argv[1]  | cut -d : -f1)
+    set style (echo $argv[1] | cut -d : -f2)
     echo n is $name s is $style
     if not exists $style
         set style light
