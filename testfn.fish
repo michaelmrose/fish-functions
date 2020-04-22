ae7e9ac38d5ef864e73d0c4701d7962dc8577ce9
Michael Rose <michael@rosenetwork.net>
2020-04-21 20:47:25 -0700

    editing testfn.fish

--- testfn.fish
+++ testfn.fish
@@ -1,9 +1,8 @@
 # Defined in /home/michael/.config/fish/buffer/testfn.fish @ line 2
 function testfn
-	switch $argv
-        case bob
-            echo hi bob
-        case '*'
-            echo fu
+    if status is-interactive
+        echo $argv
+    else
+        msg $argv
     end
 end
