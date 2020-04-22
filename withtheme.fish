401cb774a699b3d7cdc7e4de2e5906dad05e2510
Michael Rose <michael@rosenetwork.net>
2020-04-21 21:34:00 -0700

    editing withtheme.fish

--- withtheme.fish
+++ withtheme.fish
@@ -5,7 +5,7 @@ function withtheme
     if not exists $style
         set style light
     end
-    set theme $name:$style
+    set theme {$name}:$style
     # env GTK_THEME=$argv[1] $argv[2..-1]
     echo env GTK_THEME=$theme $argv[2..-1]
 end
