d3370bf12fdf9ae459017af9f5c94b5d993f34f7
Michael Rose <michael@rosenetwork.net>
2020-04-21 23:25:44 -0700

    editing kitty-pick-with-z.fish

--- kitty-pick-with-z.fish
+++ kitty-pick-with-z.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/kitty-pick-with-z.fish @ line 2
 function kitty-pick-with-z
-    if in-terminal
+    if status is-interactive
         spacefm -s set current_dir (zoxide query -i|cut -d ' ' -f2)
     else
         kitty -1 ff kitty-pick-with-z
