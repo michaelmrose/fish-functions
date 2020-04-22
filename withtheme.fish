7e7087725f66d00ac92caea32a35a89d7ad557e3
Michael Rose <michael@rosenetwork.net>
2020-04-21 21:33:15 -0700

    editing withtheme.fish

--- withtheme.fish
+++ withtheme.fish
@@ -1,4 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/withtheme.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/withtheme.fish @ line 2
 function withtheme
-	env GTK_THEME=$argv[1] $argv[2..-1]
+    set name (echo $argv[1]| cut -d : -f1)
+    set style (echo $argv[1]| cut -d : -f2)
+    if not exists $style
+        set style light
+    end
+    set theme $name:$style
+    # env GTK_THEME=$argv[1] $argv[2..-1]
+    echo env GTK_THEME=$theme $argv[2..-1]
 end
