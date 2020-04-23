c5b658a518faf7b83d5943601111ea912e988ac8
Michael Rose <michael@rosenetwork.net>
2020-04-23 15:44:12 -0700

    editing top-memory-hogs.fish

--- top-memory-hogs.fish
+++ top-memory-hogs.fish
@@ -1,14 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/top-memory-hogs.fish @ line 2
 function top-memory-hogs
     set hogs (ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head | awk '{print $3}'|sort -u|g --invert-match CMD)
-    for h in $hogs
-        if test -f $h
-            set item $h
-        else
-            set item (which $h)
-        end
-        set acc $acc \n $item
-    end
 
-    echo $acc
+
+for h in $hogs
+    if-set h (path-of-binary $h) meminfo --full $h
+end
 end
