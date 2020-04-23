e55efb8e1e6e2561afb520012e48d10b002b9f7a
Michael Rose <michael@rosenetwork.net>
2020-04-23 15:40:30 -0700

    editing if-set.fish

--- if-set.fish
+++ if-set.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/if-set.fish @ line 2
 function if-set
     set original $$argv[1]
-    set new (eval $argv[2])
+    set new (eval $argv[2..-1])
     echo $new
     # set $argv[1] 
 end
