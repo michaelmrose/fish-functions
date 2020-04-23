bf15d7d49df36b864c56fb69ef9d915e438aeee0
Michael Rose <michael@rosenetwork.net>
2020-04-23 15:34:11 -0700

    editing path-of-binary.fish

--- path-of-binary.fish
+++ path-of-binary.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/path-of-binary.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/path-of-binary.fish @ line 2
 function path-of-binary
     if test -f $argv
         echo $argv
     else
-        if which $argv
+        if which $argv 2> /dev/null
         else
             return 1
         end
