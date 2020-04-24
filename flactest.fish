149f70e25e0019d99d5f7fe0988f342fdae79a4b
Michael Rose <michael@rosenetwork.net>
2020-04-24 15:06:45 -0700

    editing flactest.fish

--- flactest.fish
+++ flactest.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/flactest_oggtest.fish @ line 2
 function flactest
     for f in (fd -e flac)
-        if not flac --test $f >/dev/null
+        if not flac --test $f 2>/dev/null
             echo (fullpath $f)
         end
     end
