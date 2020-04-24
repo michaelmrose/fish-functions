418b151ca74626ae41ca0e0ba67afcb717e8b8c3
Michael Rose <michael@rosenetwork.net>
2020-04-24 11:52:46 -0700

    editing albumfetch.fish

--- albumfetch.fish
+++ albumfetch.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/albumfetch.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/albumfetch.fish @ line 2
 function albumfetch
     while true;
-        mpc idleloop player|albumart.sh
+        mpc idleloop player|albumart
     end
 end
