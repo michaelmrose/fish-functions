523910b8b02f8e9adf6e669165c6802cffad3784
Michael Rose <michael@rosenetwork.net>
2020-04-24 15:10:23 -0700

    editing oggtest.fish

--- oggtest.fish
+++ oggtest.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/flactest_mp3test_oggtest.fish @ line 19
+# Defined in /home/michael/.config/fish/buffer/flactest_mp3test_oggtest.fish @ line 20
 function oggtest
             for f in (fd -e ogg)
         if not ogginfo -q $f > /dev/null
