117d1aee73086b9e807da367e62888fb734bc591
Michael Rose <michael@rosenetwork.net>
2020-04-24 11:44:55 -0700

    editing albumdir.fish

--- albumdir.fish
+++ albumdir.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/albumdir.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/albumart_albumdir.fish @ line 6
 function albumdir
     echo $MUSIC_DIR(mpc current -f %file%)|rev | cut -d / -f2-|rev
 end
