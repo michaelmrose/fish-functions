78811863ab318ed1bd8737b4ea786cbd89591489
Michael Rose <michael@rosenetwork.net>
2020-04-24 14:54:44 -0700

    editing flactest.fish

--- flactest.fish
+++ flactest.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/flactest.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/flactest_oggtest.fish @ line 2
 function flactest
     flac --test --silent (fd -e flac) 2>| cut -d : -f1|g 'flac$'
 end
