d39f04346e6cfc504762d90d7054d92abaa5dc03
Michael Rose <michael@rosenetwork.net>
2020-04-23 17:39:23 -0700

    editing albumart.fish

---
+++ albumart.fish
@@ -0,0 +1,7 @@
+# Defined in /home/michael/.config/fish/buffer/albumart.fish @ line 1
+function albumart
+    set COVER /tmp/kunst.jpg
+    set connected true
+    set artless false
+    # API_URL="https://api.deezer.com/search/autocomplete?q=$(mpc current)" && API_URL=${API_URL//' '/'%20'}
+end
