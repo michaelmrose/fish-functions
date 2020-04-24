009d1a60925935f883035315680ce56a30e49e3e
Michael Rose <michael@rosenetwork.net>
2020-04-24 15:35:38 -0700

    editing mp3test.fish

--- mp3test.fish
+++ mp3test.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/flactest_mp3test_oggtest.fish @ line 11
 function mp3test
     for f in (fd -e mp3)
-        if not mp3val -si $f &>/dev/null
+        if mp3val -si $f &>/dev/null
             echo (fullpath $f)
         end
     end
