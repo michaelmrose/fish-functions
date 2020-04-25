877998bedf57c208d996423b35d77703909aa373
Michael Rose <michael@rosenetwork.net>
2020-04-25 16:34:51 -0700

    editing racket-suicide.fish

--- racket-suicide.fish
+++ racket-suicide.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/racket-suicide.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/racket-suicide.fish @ line 2
 function racket-suicide
     while true
         if meminfo --full /usr/sbin/racket|g Gib
-            echo true
+            pgrep --full /usr/sbin/racket|each kill -9
         end
         sleep 1
     end
