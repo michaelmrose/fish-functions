dbbfd784bd257c6ace92f10d3f5012da208864c5
Michael Rose <michael@rosenetwork.net>
2020-04-26 17:53:31 -0700

    editing measure-app-startup.fish

--- measure-app-startup.fish
+++ measure-app-startup.fish
@@ -3,6 +3,7 @@ function measure-app-startup
     set app $argv[1]
     set class $argv[2]
     set start date +%s
+    $app &
     while true
         if window-exists $class
             set end date +%s
