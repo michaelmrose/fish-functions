c62974fb677f9239ca351d7e984509c24042c2fc
Michael Rose <michael@rosenetwork.net>
2020-04-26 17:58:14 -0700

    editing measure-app-startup.fish

--- measure-app-startup.fish
+++ measure-app-startup.fish
@@ -1,7 +1,12 @@
 # Defined in /home/michael/.config/fish/buffer/measure-app-startup_window-exi.fish @ line 2
 function measure-app-startup
-    set app $argv[1]
-    set class $argv[2]
+    if test (count $argv) -gt 1
+        set app $argv[1]
+        set class $argv[2]
+    else
+        set app $argv[1]
+        set class $argv[1]
+    end
     set start (date +%s)
     $app >/dev/null &
     while true
