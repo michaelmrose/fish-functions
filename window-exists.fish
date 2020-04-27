1a9d2ca531d20ed4f1a920ed87ba285f8b7c69a1
Michael Rose <michael@rosenetwork.net>
2020-04-26 17:53:31 -0700

    editing window-exists.fish

--- window-exists.fish
+++ window-exists.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/measure-app-startup_window-exi.fish @ line 17
+# Defined in /home/michael/.config/fish/buffer/measure-app-startup_window-exi.fish @ line 18
 function window-exists
     wmctl -lxp | awk '{print $4}' |g  $argv[1]
 end
