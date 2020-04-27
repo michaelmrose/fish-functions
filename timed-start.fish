bdae6640c33c3a867f2991ad71afc793c7ad394a
Michael Rose <michael@rosenetwork.net>
2020-04-26 18:26:06 -0700

    editing timed-start.fish

---
+++ timed-start.fish
@@ -0,0 +1,16 @@
+function timed-start
+    if test (count $argv) -gt 1
+        set app $argv[1]
+        set class $argv[2]
+    else
+        set app $argv[1]
+        set class $argv[1]
+    end
+    fish -c $app >/dev/null &
+    while true
+        if window-exists $class
+            return 0
+
+        end
+    end
+end
