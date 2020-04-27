1cb7e02c0a64001cd41b6de0b84e3a298aa7f522
Michael Rose <michael@rosenetwork.net>
2020-04-26 17:52:41 -0700

    editing measure-app-startup.fish

--- measure-app-startup.fish
+++ measure-app-startup.fish
@@ -1,5 +1,14 @@
-# Defined in /home/michael/.config/fish/buffer/measure-app-startup.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/measure-app-startup_window-exi.fish @ line 2
 function measure-app-startup
+    set app $argv[1]
+    set class $argv[2]
+    set start date +%s
     while true
-    end
+        if window-exists $class
+            set end date +%s
+        echo (math $end - $start)
+    return 0
+
+end
+end
 end
