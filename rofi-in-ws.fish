d6728473c3fdfe537012b0d971833eb0152d32ff
Michael Rose <michael@rosenetwork.net>
2020-04-28 14:41:28 -0700

    editing rofi-in-ws.fish

--- rofi-in-ws.fish
+++ rofi-in-ws.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/rofi-in-ws.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/rofi-in-ws.fish @ line 2
 function rofi-in-ws
-    i3-msg workspace rofi;rofi $argv -m -1;i3-msg workspace back_and_forth
+    i3-msg workspace rofi & rofi $argv -m -1;i3-msg workspace back_and_forth
 end
