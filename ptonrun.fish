e7927ecdf0571b42712deb50b2299053e3985e2b
Michael Rose <michael@rosenetwork.net>
2020-04-30 21:44:37 -0700

    editing ptonrun.fish

---
+++ ptonrun.fish
@@ -0,0 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/ptonrun.fish @ line 1
+function ptonrun
+    STEAM_COMPAT_DATA_PATH=~/.proton/ optirun ~/.steam/steam/steamapps/common/Proton\ 5.0/proton run $argv
+end
