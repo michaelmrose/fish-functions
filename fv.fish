cac77e7efe83d16c6890bb4d9bb9df3618c5939e
Michael Rose <michael@rosenetwork.net>
2020-05-01 19:01:16 -0700

    editing fv.fish

--- fv.fish
+++ fv.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/fv.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/f_fv.fish @ line 6
 function fv
     fd -e mp4 -e avi -e mkv |select|all mpv
 end
