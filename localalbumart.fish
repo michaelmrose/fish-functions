a2828a86b344763df1142836b952984a02d44bf6
Michael Rose <michael@rosenetwork.net>
2020-05-02 22:10:44 -0700

    editing localalbumart.fish

--- localalbumart.fish
+++ localalbumart.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/localalbumart.fish @ line 2
 function localalbumart
-    set localart (albumdir)/cover.*
+    set localart "(albumdir)/cover.*"
     if exists $localart
         cp "$localart" /tmp/kunst.jpg
         echo "$localart"
