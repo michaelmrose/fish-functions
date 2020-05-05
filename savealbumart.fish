ddda80c63c66905640cbce5cdbd540ff6a12c2e5
Michael Rose <michael@rosenetwork.net>
2020-05-05 14:10:41 -0700

    editing savealbumart.fish

--- savealbumart.fish
+++ savealbumart.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/albumdir_savealbumart.fish @ line 7
+# Defined in /home/michael/.config/fish/buffer/albumdir_removealbumart_saveal.fish @ line 11
 function savealbumart
     cp /tmp/kunst.jpg (albumdir)/cover.jpg
 end
