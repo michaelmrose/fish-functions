c5e444f1d82b8b0ae9b3acbc09d31eb75edf8b97
Michael Rose <michael@rosenetwork.net>
2020-05-08 14:54:29 -0700

    editing repeatedly.fish

--- repeatedly.fish
+++ repeatedly.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/repeatedly.fish @ line 2
 function repeatedly
     argparse 't/time=+' -- $argv
-    echo time is $t
+    increase $t
     # while true
     #     $argv[2..-1]
     #     sleep $argv[1]
