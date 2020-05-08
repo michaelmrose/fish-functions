bc529933d0d095ed0bcb2cd35dec11749e3ffb5d
Michael Rose <michael@rosenetwork.net>
2020-05-08 14:42:27 -0700

    editing repeatedly.fish

--- repeatedly.fish
+++ repeatedly.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/repeatedly.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/repeatedly.fish @ line 2
 function repeatedly
     argparse 't/time' --$argv
     echo time is $time
-    while true
-        $argv[2..-1]
-        sleep $argv[1]
-    end
+    # while true
+    #     $argv[2..-1]
+    #     sleep $argv[1]
+    # end
 end
