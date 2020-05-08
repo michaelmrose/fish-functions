caa6692e99851246b70c446c4c56167684f4e8b5
Michael Rose <michael@rosenetwork.net>
2020-05-08 14:58:55 -0700

    editing repeatedly.fish

--- repeatedly.fish
+++ repeatedly.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/repeatedly.fish @ line 2
 function repeatedly
-    set -l t
+    set -l t fucker
     set -l options 't/time'
     argparse $options -- $argv
     echo a is $argv
