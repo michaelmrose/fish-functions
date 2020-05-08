1dffa0b92799207d0dd86d494056b1d1bc0abdac
Michael Rose <michael@rosenetwork.net>
2020-05-07 23:37:09 -0700

    editing timed-start2.fish

--- timed-start2.fish
+++ timed-start2.fish
@@ -1,17 +1,17 @@
-# Defined in /home/michael/.config/fish/buffer/timed-start_timed-start2.fish @ line 19
+# Defined in /home/michael/.config/fish/buffer/timed-start_timed-start2.fish @ line 20
 function timed-start2
     function lamb
 
     if test (count $argv) -gt 1
-        set app $argv[1]
-        set class $argv[2]
+        set app '$argv[1]'
+        set class '$argv[2]'
     else
-        set app $argv[1]
-        set class $argv[1]
+        set app '$argv[1]'
+        set class '$argv[1]'
     end
-    fish -c $app >/dev/null &
+    fish -c '$app >/dev/null &'
     while true
-        if window-exists $class
+        if window-exists '$class'
             return 0
 
         end
