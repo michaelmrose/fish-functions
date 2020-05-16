38805f9c8a19a369970c4a620e1e463efae48ec2
Michael Rose <michael@rosenetwork.net>
2020-05-16 01:43:16 -0700

    editing netflix.fish

--- netflix.fish
+++ netflix.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/netflix.fish @ line 2
 function netflix
-    chrome --app=http://www.netflix.com
+    chrome --enable-gpu-rasterization --ignore-gpu-blacklist --disable-gpu-driver-workarounds --use-gl=desktop --app=http://www.netflix.com
 end
