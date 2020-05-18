61a204c0666d7af6808de3ab8386657bd31f145a
Michael Rose <michael@rosenetwork.net>
2020-05-18 03:27:15 -0700

    editing ms.fish

--- ms.fish
+++ ms.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/msg.fish @ line 6
+# Defined in /home/michael/.config/fish/buffer/ms.fish @ line 2
 function ms
-	twmnc -d 2000 --id 1 --icon info_icon --title $argv[1] --content "$argv[2..-1]"
+	twmnc -d 4000 --id 1 --icon info_icon --title $argv[1] --content "$argv[2..-1]"
 end
