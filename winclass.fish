0be667948811d3dcddf62e2dfa80c50841a1c30b
Michael Rose <michael@rosenetwork.net>
2020-05-27 12:07:31 -0700

    editing winclass.fish

--- winclass.fish
+++ winclass.fish
@@ -1,5 +1,5 @@
 # Defined in /home/michael/.config/fish/buffer/winclass.fish @ line 2
 function winclass
 	# xprop -id (wininfo id dec) | grep WM_CLASS | cut -d '"' -f4
-xprop -id (xdotool getactivewindow) WM_CLASS|awk '{print $4}'|sed 's#"##'
+xprop -id (xdotool getactivewindow) WM_CLASS|awk '{print $4}'| sed 's#"##g'
 end
