c0835970be6ba1eb1ce4cb55a3f52533ff35bc46
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:44:25 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -3,6 +3,10 @@ function m
     if test (count $argv) -gt 1
         set name (prompt 'name?')
         if exists $name
+            for file in $argv
+                set files $files \"$file\"
+            end
+            
             p $argv |each fullpath > ~/playlists/mpv/$name.m3u
         end
     end
