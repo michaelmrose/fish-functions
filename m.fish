63c9f609db1057fc8396bfca3ef17d734faa0209
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:09:31 -0700

    editing m.fish

---
+++ m.fish
@@ -0,0 +1,10 @@
+# Defined in /home/michael/.config/fish/buffer/m.fish @ line 1
+function m
+    if test (count $argv) -gt 1
+        set name (prompt 'name?: ')
+        if exists $name
+            p $argv > ~/playlists/mpv/$name.m3u
+        end
+    end
+    mpv $argv &
+end
