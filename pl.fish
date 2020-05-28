40a4ec1aa3e1d1bdeb0dbf334d866bd2feaadb21
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:39:12 -0700

    editing pl.fish

--- pl.fish
+++ pl.fish
@@ -1,13 +1,12 @@
-# Defined in /home/michael/.config/fish/buffer/pl.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/pick-playlist_pl.fish @ line 15
 function pl
     if exists $argv
-        if test -f ~/playlists/mpv/$argv
-            set playlist ~/playlists/mpv/$argv
-        end
+        set selection $argv
+    else
+        set selection ''
     end
-
+    
     set playlists (p ~/playlists/mpv/*.m3u)
-    set choice (p $playlists|each cutlast /|cut -d . -f1|select)
-    set playlist (p $playlists | grep $choice)
-    echo $playlist
+    set choice (p $playlists|g $selection|each cutlast /|cut -d . -f1|select)
+    mpv (p $playlists | grep $choice) &
 end
