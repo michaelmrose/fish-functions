be54b36dc5e22ec272a6168072988a18ab1aeb92
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:45:43 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -4,10 +4,10 @@ function m
         set name (prompt 'name?')
         if exists $name
             for file in $argv
-                set files $files \"$file\"
+                set files $files (fullpath $file)
             end
             
-            p $argv |each fullpath > ~/playlists/mpv/$name.m3u
+            p $files > ~/playlists/mpv/$name.m3u
         end
     end
     mpv $argv &
