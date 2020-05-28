553fb26449bdb9f66e8d8dd9401680df57b21b84
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:22:38 -0700

    editing pl.fish

--- pl.fish
+++ pl.fish
@@ -8,6 +8,5 @@ function pl
     set playlists (p ~/playlists/mpv/*.m3u)
     set choice (p $playlists|each cutlast /|cut -d . -f1|select)
     echo p is $playlists c is $choice
-    set path (findindex "$choice" $playlists)
-    echo $path
+    echo $choice
 end
