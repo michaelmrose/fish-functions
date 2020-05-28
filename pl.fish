114de6af750ee83e78ba3ba166f0ebe9f53f1c9d
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:33:17 -0700

    editing pl.fish

--- pl.fish
+++ pl.fish
@@ -2,11 +2,12 @@
 function pl
     if exists $argv
         if test -f ~/playlists/mpv/$argv
-            mpv ~/playlists/mpv/$argv
+            set playlist ~/playlists/mpv/$argv
         end
     end
+
     set playlists (p ~/playlists/mpv/*.m3u)
     set choice (p $playlists|each cutlast /|cut -d . -f1|select)
-    set path (p $playlists | grep $choice)
-    echo $path
+    set playlist (p $playlists | grep $choice)
+    echo $playlist
 end
