4d114a5701e5b8a73163be33f858e8190016c1db
Michael Rose <michael@rosenetwork.net>
2020-05-28 16:42:02 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/m.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/m_pl.fish @ line 2
 function m
     if test (count $argv) -gt 1
         set name (prompt 'name?')
         if exists $name
-            p $argv > ~/playlists/mpv/$name.m3u
+            p $argv |each fullpath > ~/playlists/mpv/$name.m3u
         end
     end
     mpv $argv &
