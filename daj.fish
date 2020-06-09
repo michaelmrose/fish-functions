10b03da2ce147080ff4147f9ec2829e704873f72
Michael Rose <michael@rosenetwork.net>
2020-06-09 00:03:44 -0700

    editing daj.fish

--- daj.fish
+++ daj.fish
@@ -1,6 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/bga_daj.fish @ line 8
+# Defined in /home/michael/.config/fish/buffer/daj.fish @ line 2
 function daj
-	for job in (jobs | awk '{print $1}')
+	for job in (jobs | choose 0)
     disown %$job
   end
 end
