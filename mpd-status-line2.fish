a43c9c622eca1c17841b64dc1a17ae92f2a297e0
Michael Rose <michael@rosenetwork.net>
2020-06-09 01:18:25 -0700

    editing mpd-status-line2.fish

--- mpd-status-line2.fish
+++ mpd-status-line2.fish
@@ -1,6 +1,6 @@
 # Defined in /home/michael/.config/fish/buffer/mpd-status-line_mpd-status-lin.fish @ line 21
 function mpd-status-line2
-    set mpdstatus (mpc status)
+    set mpdstatus (p mpc status)
     set playorpaused (p $mpdstatus| grep -E 'play|pause'|choose 0|str \[)
     echo m is $mpdstatus
     echo p is $playorpaused
