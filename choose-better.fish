37fbc3e9c6720e6b79ef3f036883abd76aad42f2
Michael Rose <michael@rosenetwork.net>
2020-06-09 00:31:45 -0700

    editing choose-better.fish

--- choose-better.fish
+++ choose-better.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/choose-better.fish @ line 2
 function choose-better
     if not isnumeric $argv[1]
-        set delim "-f (singlequote $argv[1])"
+        set delim -f (singlequote $argv[1])
         set targets $argv[2..-1]
     else
         set delim "-f ' '" 
