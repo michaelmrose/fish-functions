e1bb2e8a79d564e57dee2122491575ca795e9259
Michael Rose <michael@rosenetwork.net>
2020-06-09 00:58:59 -0700

    editing str.fish

--- str.fish
+++ str.fish
@@ -11,5 +11,5 @@ function str
     while read -l line
         set acc $acc $line
     end
-    echo p $acc \| cut -d $argv[1] -f 2 \|cut -d $close -f 1
+    p $acc | cut -d $argv[1] -f 2 |cut -d $close -f 1
 end
