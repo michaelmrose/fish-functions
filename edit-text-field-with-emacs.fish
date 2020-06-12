246c3f8c85ff38465764547854daa09c6e514890
Michael Rose <michael@rosenetwork.net>
2020-06-12 16:16:19 -0700

    editing edit-text-field-with-emacs.fish

--- edit-text-field-with-emacs.fish
+++ edit-text-field-with-emacs.fish
@@ -1,6 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/edit-text-field-with-emacs.fish @ line 2
 function edit-text-field-with-emacs
     set tmp /tmp/(uid)-edit-text-field
+    sleep 0.1
     xdotool key Control+a
     xdotool key Control+c
     # xclip -o -s clip > $tmp  
