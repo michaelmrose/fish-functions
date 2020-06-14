416219d599c2362f97f475cf7fc8b76e154d71ae
Michael Rose <michael@rosenetwork.net>
2020-06-14 14:15:45 -0700

    editing mpd-status-line.fish

--- mpd-status-line.fish
+++ mpd-status-line.fish
@@ -1,15 +1,15 @@
-# Defined in /home/michael/.config/fish/buffer/mpd-status-line_mpd-status-lin.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/mpd-status-line.fish @ line 2
 function mpd-status-line
 	  set mpd_info (mpc status)
     set mpdstatus (p $mpd_info |grep -E 'play|pause'|cut -d \[ -f2|cut -d \] -f1)
     if exists $mpdstatus
         set current (mpc status |head -1)
-        set playtime (mpc status |condense_lines | cut -d\# -f2-|awk '{print $2}')
+        # set playtime (mpc status |condense_lines | cut -d\# -f2-|awk '{print $2}')
         switch $mpdstatus
             case paused
-                echo  $current $playtime via (mpd-list-enabled-outputs)
+                echo  $current via (mpd-list-enabled-outputs)
             case playing
-                echo  $current $playtime via (mpd-list-enabled-outputs)
+                echo  $current via (mpd-list-enabled-outputs)
         end
     else
         echo none
