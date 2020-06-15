45b16d35f5a536ef611a66c5fd219799b2948fac
Michael Rose <michael@rosenetwork.net>
2020-06-15 14:37:44 -0700

    editing walfn-selection.fish

--- walfn-selection.fish
+++ walfn-selection.fish
@@ -1,25 +1,6 @@
-# Defined in /home/michael/.config/fish/buffer/walfn.fish @ line 33
+# Defined in /home/michael/.config/fish/buffer/walfn-selection.fish @ line 2
 function walfn-selection
-
-if test (count $argv) -gt 1
-    montage $argv[1] $argv[2] -geometry +0+0 /tmp/montage.jpg
-    set target /tmp/montage.jpg
-else
-    set target $argv
-end
-
-wal -n --saturate 0.7 -i $target
-
-    set accent (jq -r .colors.color3 ~/.cache/wal/colors.json)
-    set fg (jq -r .colors.color0 ~/.cache/wal/colors.json)
-    set bg (jq -r .colors.color15 ~/.cache/wal/colors.json)
-
-replace-color-in-i3 $accent
-inject-rounded
-replace-color-in-i3 $accent
-colorize-emacs $accent $fg $bg
-restart-i3
-sleep 1
-pywalfoxupdate
-pywalzathura
+    set current $bgimage
+    wp $argv
+    feh --bg-fill $current
 end
