5aabee53d98cfa1963ea02d0045feafdf409e57b
Michael Rose <michael@rosenetwork.net>
2020-06-16 21:13:25 -0700

    removed agenda-to-html.fish

--- agenda-to-html.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/agenda-to-html.fish @ line 1
-function agenda-to-html
-    emacs -Q --batch --load ~/.emacs.d/elpa/htmlize-20191111.2130/htmlize.el --load ~/.emacs.d/michael/agenda-to-html.el
-end
