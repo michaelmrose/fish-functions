3fc52298ad552c794f0ba90c87c6dd43df34babc
Michael Rose <michael@rosenetwork.net>
2020-06-21 16:30:55 -0700

    editing going-to-rain?.fish

--- going-to-rain?.fish
+++ going-to-rain?.fish
@@ -1,7 +1,7 @@
 # Defined in /home/michael/.config/fish/buffer/going-to-rain?.fish @ line 2
 function going-to-rain?
     if http get https://www.accuweather.com/en/us/bremerton-wa/98337/minute-weather-forecast/331425 |pup |g 'No precipitation for at least 120 min' > /dev/null
-        return 0
+        return 1
     else
         echo rain inc
     end
