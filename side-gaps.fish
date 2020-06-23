c549c0c7d638a6efa8e95bd14cc96f374584a096
Michael Rose <michael@rosenetwork.net>
2020-06-23 13:01:19 -0700

    editing side-gaps.fish

--- side-gaps.fish
+++ side-gaps.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/side-gaps.fish @ line 2
 function side-gaps
-    i3-msg "gaps right current set $argv px\;gaps left current set $argv px"
+    i3-msg "gaps right current set $argv ; gaps left current set $argv px"
 end
