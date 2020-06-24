fe816bf50b4e75ce60dbc465c5a0c9401e90d9a5
Michael Rose <michael@rosenetwork.net>
2020-06-23 19:43:56 -0700

    editing set-lightdm-bg.fish

--- set-lightdm-bg.fish
+++ set-lightdm-bg.fish
@@ -1,4 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/set-lightdm-bg.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/set-lightdm-bg.fish @ line 2
 function set-lightdm-bg
+    if exists $argv
+        set img $argv
+    else
+        set img $bgimage
+    end
     s cp $img /etc/lightdm/bg.jpg
 end
