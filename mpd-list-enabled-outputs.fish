3f5da13e41de85f49622eb1217f4ae0538bd4c3a
Michael Rose <michael@rosenetwork.net>
2020-06-23 19:53:09 -0700

    editing mpd-list-enabled-outputs.fish

--- mpd-list-enabled-outputs.fish
+++ mpd-list-enabled-outputs.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/mpd-list-enabled-outputs_mpd-s.fish @ line 2
+# Defined in /home/michael/.config/fish/buffer/mpd-list-enabled-outputs.fish @ line 2
 function mpd-list-enabled-outputs
     # mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|tr '
     # ' ' '
-    set outputs (mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|cut -c1-90)
+    set outputs (mpc outputs|grep enabled|cut -d '(' -f2|cut -d ')' -f1|cut -c1-90)|sd speakers (output-type)
     if test (count $outputs) -gt 1
         echo $outputs[1] and $outputs[2]
     else
