99b14701c7b195ed58ccf783f17c75e3e9653164
Michael Rose <michael@rosenetwork.net>
2020-06-27 01:11:05 -0700

    editing mpd-status-line4.fish

--- mpd-status-line4.fish
+++ mpd-status-line4.fish
@@ -1,16 +1,29 @@
-# Defined in /home/michael/.config/fish/buffer/mpd-status-line_mpd-status-lin.fish @ line 60
+# Defined in /home/michael/.config/fish/buffer/mpd-status-line4.fish @ line 2
 function mpd-status-line4
 	  set mpd_info (mpc status)
     set mpdstatus (p $mpd_info |grep -E 'play|pause'|cut -d \[ -f2|cut -d \] -f1)
     if exists $mpdstatus
         set current (mpc status |head -1)
-        # set playtime (mpc status |condense_lines | cut -d\# -f2-|awk '{print $2}')
         switch $mpdstatus
             case paused
-                echo  $current via (mpd-list-enabled-outputs) at(getvolume)
+                set symbol  
             case playing
-                echo  $current via (mpd-list-enabled-outputs) at(getvolume)
+                set symbol 
         end
+        set tail via (mpd-list-enabled-outputs) at(getvolume)
+        if exists $argv #we have a max size to respect
+            if test (echo $current |wc -c) -gt $argv
+                set current (echo $current | choose -f ' - ' 1)
+                if test (echo $current |wc -c) -gt $argv
+                    set current (truncate $argv right $current) 
+                end
+                
+            end
+        end
+        
+            
+
+
     else
         output-status-line
         return 0
