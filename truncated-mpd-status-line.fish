525cbed0618765d88b3a28540e667b750640264c
Michael Rose <michael@rosenetwork.net>
2020-06-27 00:44:25 -0700

    editing truncated-mpd-status-line.fish

--- truncated-mpd-status-line.fish
+++ truncated-mpd-status-line.fish
@@ -1,4 +1,4 @@
 # Defined in /home/michael/.config/fish/buffer/truncated-mpd-status-line.fish @ line 2
 function truncated-mpd-status-line
-    truncate 40 right (mpd-status-line)
+    truncate 40 right (mpd-status-line) | iconv -f utf-8 -t utf-8
 end
