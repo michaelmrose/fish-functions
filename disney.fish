0d7b14b16f5e55c5474a42c99af18c517c04ca22
Michael Rose <michael@rosenetwork.net>
2020-07-02 21:26:55 -0700

    editing disney.fish

---
+++ disney.fish
@@ -0,0 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/disney_netflix.fish @ line 1
+function disney
+    chromium --enable-gpu-rasterization --ignore-gpu-blacklist --disable-gpu-driver-workarounds --use-gl=desktop --app=https://www.disneyplus.com
+end
