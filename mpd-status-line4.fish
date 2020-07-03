796c8b7232cd8ca27a607bc5c3075e8a68437386
Michael Rose <michael@rosenetwork.net>
2020-07-02 22:38:42 -0700

    editing mpd-status-line4.fish

--- mpd-status-line4.fish
+++ mpd-status-line4.fish
@@ -10,7 +10,7 @@ function mpd-status-line4
             case playing
                 set symbol 
         end
-        set tail via (mpd-list-enabled-outputs) at(getvolume)
+        set tail via (mpd-list-enabled-outputs) at (getvolume)
         if exists $argv #we have a max size to respect
             if test (echo $symbol $current $tail |wc -c) -gt $argv
                 set current (echo $current | choose -f ' - ' 1)
