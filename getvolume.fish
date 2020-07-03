2c87c5563072e26e9be9ad28e1eaf3317aed4dd0
Michael Rose <michael@rosenetwork.net>
2020-07-02 23:08:07 -0700

    editing getvolume.fish

--- getvolume.fish
+++ getvolume.fish
@@ -1,9 +1,9 @@
 # Defined in /home/michael/.config/fish/buffer/getvolume.fish @ line 2
 function getvolume
 	  set vol (ponymix get-volume 2> /dev/null)
-    if test $vol -lt 100
-        set vol " "{$vol}
-  end
+  #   if test $vol -lt 100
+  #       set vol " "{$vol}
+  # end
   
   set result {$vol}%
   if ponymix is-muted 2>/dev/null
