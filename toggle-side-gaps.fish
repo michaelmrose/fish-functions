41955a778bfce7bba0c55d1ca51b260587826ba2
Michael Rose <michael@rosenetwork.net>
2020-07-06 20:21:14 -0700

    editing toggle-side-gaps.fish

--- toggle-side-gaps.fish
+++ toggle-side-gaps.fish
@@ -5,7 +5,6 @@ function toggle-side-gaps
         i3 gaps horizontal current set 5
     else
         set size (math (math $width - 1250 ) / 2)
-        echo $size
-        # i3 gaps horizontal current set 300
+        i3 gaps horizontal current set $size
     end
 end
