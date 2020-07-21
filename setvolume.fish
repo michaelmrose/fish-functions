39e976387b00d442818c34c734cef62a47f8112d
Michael Rose <michael@rosenetwork.net>
2020-07-21 12:58:56 -0700

    editing setvolume.fish

--- setvolume.fish
+++ setvolume.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/setvolume.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/setvolume.fish @ line 2
 function setvolume
-	ponymix set-volume $argv
+	# ponymix set-volume $argv
+    pamixer --set-volume $argv
 end
