99dbb94e2174e165e58189eb99b4330db5984f4d
Michael Rose <michael@rosenetwork.net>
2020-07-21 14:56:01 -0700

    editing typewriter-notification.fish

--- typewriter-notification.fish
+++ typewriter-notification.fish
@@ -4,9 +4,11 @@ function typewriter-notification
     #     ms $argv[1..$i]
     #     sleep 0.2
     # end
-   set length (echo $argv|wc -c)
+   set title $argv[1]
+   set message $argv[2..-1]
+   set length (echo $message|wc -c)
    for i in (seq 1 $length)
-       ms (echo $argv |cut -c 1-$i)
+twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content (echo $message|cut -c 1-$i)
        sleep 0.02
    end
 end
