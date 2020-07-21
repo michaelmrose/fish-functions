f236dda338c7c114c6c18a12d0cdf00da4041f3c
Michael Rose <michael@rosenetwork.net>
2020-07-21 14:43:50 -0700

    editing typewriter-notification.fish

--- typewriter-notification.fish
+++ typewriter-notification.fish
@@ -1,7 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/typewriter-notification.fish @ line 1
+# Defined in /home/michael/.config/fish/buffer/typewriter-notification.fish @ line 2
 function typewriter-notification
     for i in (seq 1 (count $msg))
-        ms $msg[1..$argv]
+        ms $argv[1..$i]
         sleep 0.2
     end
 end
