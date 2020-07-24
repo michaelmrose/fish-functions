c7bb2ff4c9069e5edb18c8609245a5e4b71925a8
Michael Rose <michael@rosenetwork.net>
2020-07-23 19:09:06 -0700

    editing rootsmagic.fish

---
+++ rootsmagic.fish
@@ -0,0 +1,4 @@
+# Defined in /home/michael/.config/fish/buffer/rootsmagic.fish @ line 1
+function rootsmagic
+    env WINEPREFIX="/home/michael/.wine" wine C:\\\\Program\\ Files\\ \\(x86\\)\\\\RootsMagic\\ 7\\\\RootsMagic.exe 
+end
