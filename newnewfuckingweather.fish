c071d84dec89cf30ee7d2b96dde453227328027d
Michael Rose <michael@rosenetwork.net>
2020-08-03 10:33:48 -0700

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -1,21 +1,14 @@
 # Defined in /home/michael/.config/fish/buffer/newnewfuckingweather.fish @ line 2
 function newnewfuckingweather
-	if not areweonline
+    if not online?
         either $priorweather unavailable
         return 0
     end
-	  set weather (weather-icon) (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | condense_lines |trim|condense_spaces)
-    set -U priorweather $weather
+    set weather (weather-icon) (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | condense_lines |trim|condense_spaces)
     if going-to-rain?
         set weather $weather rain inc
     end
-    
+
+    set -U priorweather $weather
     echo $weather
-
-
-    # if areweonline
-	  #     set weather (weather-icon) (weather-report fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | condense_lines |trim|condense_spaces)
-    # else
-    #     set weather (either $priorweather unavailable)
-    # end
 end
