ce3232e4a25f085c3676f1a3c2c2cdfd6dc1bb8a
Michael Rose <michael@rosenetwork.net>
2020-08-23 02:12:00 -0400

    editing open-app.fish

--- open-app.fish
+++ open-app.fish
@@ -1,6 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/open-app.fish @ line 2
 function open-app
-	# set target (appkey $argv) \&
-    # eval $target
-    $argv &
+	eval (appkey $argv) \&
 end
