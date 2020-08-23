7ca86333e894a3ebe7503ca77d306101eba29a20
Michael Rose <michael@rosenetwork.net>
2020-08-23 03:15:22 -0400

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/set-path.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/set-path.fish @ line 2
 function set-path
 	set builtinpaths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin ~/scripts
 
-  set mypaths /opt/bin /home/michael/bin /opt/android-sdk/platform-tools /home/michael/.cargo/bin /home/michael/extproj/imapnotify/node_modules/imapnotify/bin /home/michael/.gem/ruby/2.5.0/bin /usr/bin/vendor_perl /opt/android-sdk/tools/bin/ $GERBIL_HOME/bin
+  set mypaths /opt/bin /home/michael/bin /opt/android-sdk/platform-tools /home/michael/.cargo/bin /home/michael/extproj/imapnotify/node_modules/imapnotify/bin /home/michael/.gem/ruby/2.5.0/bin /usr/bin/vendor_perl /opt/android-sdk/tools/bin/ $GERBIL_HOME/bin /home/michael/.local/bin
 
   set -xg PATH $mypaths $javapaths $builtinpaths
 end
