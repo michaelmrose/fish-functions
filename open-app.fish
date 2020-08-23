2ad93bdeafc566d435752daf783845628e75d53f
Michael Rose <michael@rosenetwork.net>
2020-08-23 02:11:18 -0400

    editing open-app.fish

--- open-app.fish
+++ open-app.fish
@@ -1,14 +1,6 @@
+# Defined in /usr/home/michael/.config/fish/buffer/open-app.fish @ line 2
 function open-app
-	set target (appkey $argv) \&
-  # echo $target
-  # set rorrlist inbox googlecal
-  if contains $target $rorrlist
-    raiseorrun-appkey $argv
-  else
-    eval $target
-  end
-  
-  # er vi3op
-  # update-op-status
-  signal-i3blocks pages
+	# set target (appkey $argv) \&
+    # eval $target
+    $argv &
 end
