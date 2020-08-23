d8e242beba9b4d1121f771591d2bf9a628de24ed
Michael Rose <michael@rosenetwork.net>
2020-08-23 02:56:35 -0400

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -13,7 +13,7 @@ function fish_prompt
   # powerline-rs --shell bare $status
   # powerline-rs --modules user,host,cwd,ssh,jobs,perms,git,gitstage --shell bare $status
   # powerline-rs --modules ssh,cwd,perms,git,gitstage,root --shell bare $status
-  # timeout 0.25 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status
-  powerline-rs --shell bare $status
+  timeout 0.25 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status
+  # powerline-rs --shell bare $status
   # echo (pwd) '$ ' 
 end
