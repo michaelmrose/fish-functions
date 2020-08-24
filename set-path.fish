1a6620326ce7e516e1815233a2a4daf34aa00f09
Michael Rose <michael@rosenetwork.net>
2020-08-23 19:01:03 -0700

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -6,7 +6,7 @@ function set-path
 
   set builtinpaths  /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
 
-  set mypaths /usr/home/michael/.cargo/bin /usr/home/michael/.local/bin /usr/home/michael/scripts /usr/home/michael/bin /opt/gargoyle
+  set mypaths /usr/home/michael/.cargo/bin /usr/home/michael/.local/bin /usr/home/michael/scripts /usr/home/michael/bin 
 
   set -xg PATH $mypaths  $builtinpaths
 end
