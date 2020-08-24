d5528f2fb26d4dc6ad1bb926fdc7407ac2f69976
Michael Rose <michael@rosenetwork.net>
2020-08-23 19:53:49 -0700

    editing xcape-file.fish

--- xcape-file.fish
+++ xcape-file.fish
@@ -1,3 +1,4 @@
+# Defined in /usr/home/michael/.config/fish/buffer/xcape-file.fish @ line 2
 function xcape-file
 	killall xcape
 
@@ -9,5 +10,6 @@ function xcape-file
   end
 
   set timeout (optvalue t 500 $argv) #val default
-	eval xcape -t $timeout -e \'(cat $file | condense_lines | sed 's/ /;/g')\'
+	# eval xcape -t $timeout -e \'(cat $file | condense_lines | sed 's/ /;/g')\'
+	eval xcape -t 500 -e \'(cat $file | condense_lines | sed 's/ /;/g')\'
 end
