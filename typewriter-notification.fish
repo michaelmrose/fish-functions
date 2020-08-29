d075e01a3f0b49997b7857b32e34c808b6ce9339
Michael Rose <michael@rosenetwork.net>
2020-08-29 13:08:37 -0700

    editing typewriter-notification.fish

--- typewriter-notification.fish
+++ typewriter-notification.fish
@@ -8,8 +8,9 @@ function typewriter-notification
    set message $argv[2..-1]
    set length (echo $message|wc -c)
    for i in (seq 1 $length)
-       twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content (echo $message|cut -c 1-$i)
-       echo foo
+       set current (echo $message|cut -c 1-$i)
+       echo c is $current
+       twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content 
        sleep 0.02
    end
 end
