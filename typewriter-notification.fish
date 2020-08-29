ea2d58999f85bdb980aae591d4d2b463036d1850
Michael Rose <michael@rosenetwork.net>
2020-08-29 13:09:13 -0700

    editing typewriter-notification.fish

--- typewriter-notification.fish
+++ typewriter-notification.fish
@@ -10,7 +10,7 @@ function typewriter-notification
    for i in (seq 1 $length)
        set current (echo $message|cut -c 1-$i)
        echo c is $current
-       twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content 
+       twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content $current
        sleep 0.02
    end
 end
