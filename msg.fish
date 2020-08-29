3772442c2c38bca5f3dcc55bd98536362fc008e8
Michael Rose <michael@rosenetwork.net>
2020-08-29 04:17:14 -0700

    editing msg.fish

--- msg.fish
+++ msg.fish
@@ -1,5 +1,5 @@
 # Defined in /usr/home/michael/.config/fish/buffer/msg.fish @ line 2
 function msg
-	twmnc -d 2000 --id 1 --content "$argv"
-    notify-send -d 3000 $argv
+	# twmnc -d 2000 --id 1 --content "$argv"
+    notify-send -t 3000 $argv
 end
