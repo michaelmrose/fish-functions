ad78f9cdd8f9364054c6ea3ec2c7421418da4151
Michael Rose <michael@rosenetwork.net>
2020-08-29 13:07:40 -0700

    editing typewriter-notification.fish

--- typewriter-notification.fish
+++ typewriter-notification.fish
@@ -1,15 +1,15 @@
 # Defined in /usr/home/michael/.config/fish/buffer/typewriter-notification.fish @ line 2
 function typewriter-notification
-    for i in (seq 1 (count $argv))
-        ms $argv[1..$i]
-        sleep 0.2
-    end
-   # set title $argv[1]
-   # set message $argv[2..-1]
-   # set length (echo $message|wc -c)
-   # for i in (seq 1 $length)
-   #     twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content (echo $message|cut -c 1-$i)
-   #     echo foo
-   #     sleep 0.02
-   # end
+    # for i in (seq 1 (count $argv))
+    #     ms $argv[1..$i]
+    #     sleep 0.2
+    # end
+   set title $argv[1]
+   set message $argv[2..-1]
+   set length (echo $message|wc -c)
+   for i in (seq 1 $length)
+       twmnc -d 4000 --id 1 --icon ~/images/icons/xkill.png --title $title --content (echo $message|cut -c 1-$i)
+       echo foo
+       sleep 0.02
+   end
 end
