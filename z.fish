2ec10d0b8341aefc7bf28846a0ccd13c699a640f
Michael Rose <michael@rosenetwork.net>
2020-08-30 07:12:52 -0700

    editing z.fish

--- z.fish
+++ z.fish
@@ -1,10 +1,10 @@
-# Defined in /home/michael/.config/fish/buffer/z.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/z.fish @ line 2
 function z
 	if test -d $argv
       cd "$argv"
       zoxide add
     else
-	      set zres (zoxide query "$argv"|cut -c8-)
+	      set zres (zoxide query "$argv")
         if test (count $zres) -gt 0
             cd "$zres"
             zoxide add
