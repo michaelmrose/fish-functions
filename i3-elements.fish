03b308c06a6e6d8b9c0ff3fdfdbcc2dad3e31129
Michael Rose <michael@rosenetwork.net>
2020-08-31 15:25:01 -0700

    editing i3-elements.fish

--- i3-elements.fish
+++ i3-elements.fish
@@ -1,11 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/i3-elements.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/i3-elements.fish @ line 2
 function i3-elements
 	set i3_bar_ids (xwininfo -all -root |grep i3bar | awk '{print $1}')
     set i3_frame_ids (xwininfo -all -root |grep i3-frame |grep 'no name' |awk '{print $1}')
     set ids $i3_bar_ids $i3_frame_ids
     switch $argv[1]
     case trans
-        set val 0.76
+        set val 0.5
       case hide
       set val 0
     case show
