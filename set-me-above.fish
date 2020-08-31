6c9cea51a808d0bb754ec3de4430a1869c3bc0d0
Michael Rose <michael@rosenetwork.net>
2020-08-30 20:43:14 -0700

    editing set-me-above.fish

--- set-me-above.fish
+++ set-me-above.fish
@@ -1,4 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/set-me-above.fish @ line 2
 function set-me-above
-    xprop -id (xdotool getactivewindow) -set  _NET_WM_SET_ABOVE
+    xprop -id (xdotool getactivewindow) -set   _NET_WM_STATE _NET_WM_SET_ABOVE
 end
