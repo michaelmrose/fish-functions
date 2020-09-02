5a219e12431558701f528bad496f03b987788efa
Michael Rose <michael@rosenetwork.net>
2020-09-02 15:47:46 -0700

    editing find-disk-id.fish

--- find-disk-id.fish
+++ find-disk-id.fish
@@ -1,4 +1,5 @@
-# Defined in /usr/home/michael/.config/fish/buffer/find-disk-id.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/find-disk-id.fish @ line 2
 function find-disk-id
+    s fdisk -l /dev/$argv
 ls -alF /dev/disk/by-id/|g $argv
 end
