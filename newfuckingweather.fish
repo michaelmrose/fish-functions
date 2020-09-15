37e0178f6c93f3315a240fab817708729c754055
Michael Rose <michael@rosenetwork.net>
2020-09-15 14:17:27 -0700

    editing newfuckingweather.fish

--- newfuckingweather.fish
+++ newfuckingweather.fish
@@ -9,6 +9,7 @@ function newfuckingweather
   set icon (weather-icon)
 	set weather (curl wttr.in/$g\?0\?T\?Q\?u|cut -c 16- | condense_lines |condense_spaces)
   set -U priorweather $icon $weather
-  echo $icon $weather
+  set aqi (aqi)
+  echo $icon $weather aqi: $aqi
   return 0
 end
