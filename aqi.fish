32b759fae949499a216d03dbb70ea0c37d0b3fa0
Michael Rose <michael@rosenetwork.net>
2020-09-15 14:25:19 -0700

    editing aqi.fish

--- aqi.fish
+++ aqi.fish
@@ -1,4 +1,5 @@
-# Defined in /usr/home/michael/.config/fish/buffer/aqi.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/aqi.fish @ line 2
 function aqi
 http 'https://aqicn.org/city/usa/washington/bremerton/spruce-ave/'|pup |g 'fine particulate matter)  AQI is'|choose 5
+return 0
 end
