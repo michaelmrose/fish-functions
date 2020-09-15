78f9e0081fbd7b7063f84c75bc13c134262a988e
Michael Rose <michael@rosenetwork.net>
2020-09-15 14:11:20 -0700

    editing newnewfuckingweather.fish

--- newnewfuckingweather.fish
+++ newnewfuckingweather.fish
@@ -5,9 +5,9 @@ function newnewfuckingweather
         return 0
     end
     set weather (weather-icon) (/usr/bin/weather  fips5303590288 --no-cache --headers=Temperature,Wind,'Sky conditions' --imperial | sd '\(.*\)' '' |n l5-7|cut -d : -f2- | condense_lines |trim|condense_spaces)
-    if going-to-rain?
-        set weather $weather rain inc
-    end
+    # if going-to-rain?
+    #     set weather $weather rain inc
+    # end
 
     set -U priorweather $weather
     echo $weather
