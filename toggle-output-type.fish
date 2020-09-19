c67082b0da32858e1540f1aada276d590aba545b
Michael Rose <michael@rosenetwork.net>
2020-09-19 12:41:48 -0700

    editing toggle-output-type.fish

--- toggle-output-type.fish
+++ toggle-output-type.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/toggle-output-type.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/toggle-output-type.fish @ line 2
 function toggle-output-type
 	switch (output-type)
         case headphones
-            switchaudio set dock
+            switchaudio set pci
         case speakers
-            switchaudio set headset
+            switchaudio set usb
     end
 end
