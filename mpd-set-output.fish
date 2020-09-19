dc5d1b7a27ac3f76eef047db70de8db56a61c100
Michael Rose <michael@rosenetwork.net>
2020-09-19 12:37:22 -0700

    editing mpd-set-output.fish

--- mpd-set-output.fish
+++ mpd-set-output.fish
@@ -1,18 +1,18 @@
-# Defined in /home/michael/.config/fish/buffer/mpd-set-output.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/mpd-list-enabled-outputs_mpd-s.fish @ line 15
 function mpd-set-output
 	switch $argv
         case computer
-            mpc enable only speakers
+            mpc enable only local
         case speakers
-            mpc enable only speakers
-            switchaudio set dock
+            mpc enable only local
+            switchaudio set pci
         case headphones
-            mpc enable only speakers
-            switchaudio set headset
+            mpc enable only local
+            switchaudio set usb
         case http
             mpc enable only http
         case both
             mpc enable http
-            mpc enable speakers
+            mpc enable local
     end
 end
