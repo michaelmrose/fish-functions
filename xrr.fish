6cc637a905cad504e06ed8d12ac24f31ed5349ee
Michael Rose <michael@rosenetwork.net>
2020-09-25 19:48:44 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,8 +1,8 @@
-# Defined in /usr/home/michael/.config/fish/buffer/xrr.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/xrr_xrrlist.fish @ line 2
 function xrr
     switch $argv[1]
         case single
-            xrrlist HDMI-0
+            xrr HDMI-0
         case left
            # add argv[2] to existing layout on left side
            xrr $argv[2] $ENABLED_DISPLAYS
