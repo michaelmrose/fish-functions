e03e11f01f099be3345d168de7e83cd90d313379
Michael Rose <michael@rosenetwork.net>
2020-09-25 19:34:11 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,18 +1,21 @@
 # Defined in /usr/home/michael/.config/fish/buffer/xrr.fish @ line 2
 function xrr
-    if exists $argv
-        switch $argv[1]
-            case single
-                xrrlist HDMI-0
-            case triple
+    switch $argv[1]
+        case single
+            xrrlist HDMI-0
+        case triple
             xrrlist DVI-D-0 HDMI-0 DP-5
-            case '*'
-                xrrlist $argv
-        end
-    else
-        xrrswap
+        case toggle
+            if test (count (get-connected-displays)) -gt 1
+                xrr single
+            else
+                xrr triple
+            end
+            
+        case '*'
+            xrrlist $argv
     end
     
-  wp recall
-  restart picom
+    wp recall
+    restart picom
 end
