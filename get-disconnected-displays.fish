27bd97b9a360eedadf4f2d59bc05b67e3854ed6c
Michael Rose <michael@rosenetwork.net>
2020-09-25 23:33:47 -0700

    editing get-disconnected-displays.fish

--- get-disconnected-displays.fish
+++ get-disconnected-displays.fish
@@ -1,4 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/get-disconnected-displays.fish @ line 2
 function get-disconnected-displays
-    xrandr | grep ' connected' |grep -E --invert-match '[[:print:]]{3,7} [[:print:]]{9} [0-9]{3,4}'
+    xrandr | grep ' connected' |grep -E --invert-match '[[:print:]]{3,7} [[:print:]]{9} [0-9]{3,4}'|choose 0
 end
