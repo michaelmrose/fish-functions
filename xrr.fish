072f67bd536683170a2706459e105b13808870df
Michael Rose <michael@rosenetwork.net>
2020-09-25 19:44:11 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -5,11 +5,15 @@ function xrr
             xrrlist HDMI-0
         case left
            # add argv[2] to existing layout on left side
-           set com $LASTXRR --output --auto --left-of $ENABLED_DISPLAYS[1]
+           set com $LASTXRR --output $argv --auto --left-of $ENABLED_DISPLAYS[1]
+           set -U LASTXRR $com
+           set -U ENABLED_DISPLAYS $argv[2] $ENABLED_DISPLAYS
            eval $com
         case right
            # add argv[2] to existing layout on right side
-           set com $LASTXRR --output --auto --right-of $ENABLED_DISPLAYS[-1]
+           set com $LASTXRR --output $argv[2] --auto --right-of $ENABLED_DISPLAYS[-1]
+           set -U LASTXRR $com
+           set -U ENABLED_DISPLAYS  $ENABLED_DISPLAYS $argv[2]
            eval $com
         case triple
             xrrlist DVI-D-0 HDMI-0 DP-5
