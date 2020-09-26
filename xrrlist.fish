382b1aecda9a66f6fd2434c47aec3ca1454c60ea
Michael Rose <michael@rosenetwork.net>
2020-09-25 19:17:11 -0700

    editing xrrlist.fish

--- xrrlist.fish
+++ xrrlist.fish
@@ -1,10 +1,10 @@
-# Defined in /usr/home/michael/.config/fish/buffer/xrrlist.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/xrrlist.fish @ line 2
 function xrrlist
     set prior $argv[1]
-    set com "xrandr --output $prior --auto " 
+    set com "xrandr --output $prior --auto" 
     if test (count $argv) -gt 1
         for d in $argv[2..-1]
-            set com "$com --output $d --auto --right-of $prior " 
+            set com "$com --output $d --auto --right-of $prior" 
         end
     end
     echo $com
