14ef6c1aa7ebbd84e6c78952e65683d2c9681b71
Michael Rose <michael@rosenetwork.net>
2020-09-25 19:30:21 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,12 +1,18 @@
 # Defined in /usr/home/michael/.config/fish/buffer/xrr.fish @ line 2
 function xrr
-	switch $argv[1]
-        case single
-            xrrlist HDMI-0
-        case triple
-          xrrlist DVI-D-0 HDMI-0 DP-5
-        case '*'
-            xrrlist $argv
-  end
+    if exists $argv
+        switch $argv[1]
+            case single
+                xrrlist HDMI-0
+            case triple
+            xrrlist DVI-D-0 HDMI-0 DP-5
+            case '*'
+                xrrlist $argv
+        end
+    else
+        xrrswap
+
+    end
+    
   wp recall
 end
