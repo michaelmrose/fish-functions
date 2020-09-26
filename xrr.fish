c57b44ae91c6af03522886677ca7a99f2278ed8c
Michael Rose <michael@rosenetwork.net>
2020-09-25 19:42:08 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -5,8 +5,12 @@ function xrr
             xrrlist HDMI-0
         case left
            # add argv[2] to existing layout on left side
+           set com $LASTXRR --output --auto --left-of $ENABLED_DISPLAYS[1]
+           eval $com
         case right
            # add argv[2] to existing layout on right side
+           set com $LASTXRR --output --auto --right-of $ENABLED_DISPLAYS[-1]
+           eval $com
         case triple
             xrrlist DVI-D-0 HDMI-0 DP-5
         case toggle
