cb368260008e6a67ddd892020f0b849744cf3712
Michael Rose <michael@rosenetwork.net>
2020-09-26 17:13:04 -0700

    editing get-disconnected-displays.fish

--- get-disconnected-displays.fish
+++ get-disconnected-displays.fish
@@ -1,4 +1,5 @@
 # Defined in /usr/home/michael/.config/fish/buffer/get-disconnected-displays.fish @ line 2
 function get-disconnected-displays
-    xrandr | grep ' connected' |grep -E --invert-match '[[:print:]]{3,7} [[:print:]]{9} [0-9]{3,4}'|choose 0
+    # xrandr | grep ' connected' |grep -E --invert-match '[[:print:]]{3,7} [[:print:]]{9} [0-9]{3,4}'|choose 0
+    xrandr | grep ' connected' |grep -E --invert-match  '[0-9]{3,4}'
 end
