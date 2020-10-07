7f51c74f4474557e805f3ac47c54199b6058b54b
Michael Rose <michael@rosenetwork.net>
2020-10-07 11:01:30 -0700

    editing fuckingdbus.fish

--- fuckingdbus.fish
+++ fuckingdbus.fish
@@ -1,6 +1,6 @@
-# Defined in /usr/home/michael/.config/fish/functions/fuckingdbus.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/emd_fuckingdbus.fish @ line 10
 function fuckingdbus
-    set dbusvals (dbus-launch|sd " " \n|choose -f = 1:)
-    set -Ux DBUS_SESSION_BUS_ADDRESS $dbusvals[1]
-    set -Ux DBUS_SESSION_BUS_PID $dbusvals[2]
+    set dbusvals (dbus-launch|sd " " |choose -f = 1:)
+    set -x DBUS_SESSION_BUS_ADDRESS $dbusvals[1]
+    set -x DBUS_SESSION_BUS_PID $dbusvals[2]
 end
