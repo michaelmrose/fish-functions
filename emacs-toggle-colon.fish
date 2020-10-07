e154aa0ef45b0b4d5c74b0ad749b2e2ff68ba437
Michael Rose <michael@rosenetwork.net>
2020-10-07 11:17:38 -0700

    editing emacs-toggle-colon.fish

--- emacs-toggle-colon.fish
+++ emacs-toggle-colon.fish
@@ -1,5 +1,5 @@
-# Defined in /usr/home/michael/.config/fish/buffer/emacs-toggle-colon.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/emacs-toggle-colon.fish @ line 2
 function emacs-toggle-colon
     xdotool key colon
-    xdotool Escape
+    xdotool escape
 end
