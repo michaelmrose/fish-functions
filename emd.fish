8c8e3599b7043190bccbd0e42f99e8a414c8cde5
Michael Rose <michael@rosenetwork.net>
2020-10-07 11:01:30 -0700

    editing emd.fish

--- emd.fish
+++ emd.fish
@@ -1,5 +1,7 @@
-# Defined in /usr/home/michael/.config/fish/buffer/emd.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/emd_fuckingdbus.fish @ line 2
 function emd
-    fuckingdbus
+    set dbusvals (dbus-launch|sd " " |choose -f = 1:)
+    set -x DBUS_SESSION_BUS_ADDRESS $dbusvals[1]
+    set -x DBUS_SESSION_BUS_PID $dbusvals[2]
 	  emacs --daemon
 end
