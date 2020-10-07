c277d17a703fe5e9a047e603d4c4fbc67d4d463c
Michael Rose <michael@rosenetwork.net>
2020-10-07 11:55:06 -0700

    editing emd.fish

--- emd.fish
+++ emd.fish
@@ -1,7 +1,7 @@
 # Defined in /usr/home/michael/.config/fish/buffer/emd.fish @ line 2
 function emd
-    # set dbusvals (dbus-launch|sd " " |choose -f = 1:)
-    # set -x DBUS_SESSION_BUS_ADDRESS $dbusvals[1]
-    # set -x DBUS_SESSION_BUS_PID $dbusvals[2]
+    set dbusvals (dbus-launch|sd " " |choose -f = 1:)
+    set -x DBUS_SESSION_BUS_ADDRESS $dbusvals[1]
+    set -x DBUS_SESSION_BUS_PID $dbusvals[2]
 	  emacs --daemon
 end
