29c1a52913b997aa55aaa43c42cac3c7d3451c43
Michael Rose <michael@rosenetwork.net>
2020-10-11 14:47:33 -0700

    editing fm.fish

--- fm.fish
+++ fm.fish
@@ -1,8 +1,8 @@
-# Defined in /home/michael/.config/fish/buffer/fm.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/fm.fish @ line 2
 function fm
 	if not exists $argv
-      spacefm (pwd)
+      dolphin (pwd) &
   else
-      spacefm $argv
+      dolphin $argv &
   end
 end
