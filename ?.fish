ec6359c99f69852e80bff53620c07b34d21db9c6
Michael Rose <michael@rosenetwork.net>
2020-10-12 19:20:05 -0700

    editing ?.fish

--- ?.fish
+++ ?.fish
@@ -1,8 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/?.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/?.fish @ line 2
 function ?
-    if test -d ~/service/$argv
-        sv status $argv
-    else
-        sudo sv status $argv
-    end
+    service status $argv
 end
