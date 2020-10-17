1ea8eec6b855a4a1868dbb49665541bcdceae61d
Michael Rose <michael@rosenetwork.net>
2020-10-17 13:51:52 -0700

    removed winclass.fish

--- winclass.fish
+++
@@ -1,5 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/winclass.fish @ line 2
-function winclass
-	# xprop -id (wininfo id dec) | grep WM_CLASS | cut -d '"' -f4
-xprop -id (xdotool getactivewindow) WM_CLASS|awk '{print $4}'| sed 's#"##g'
-end
