2146a731392201565a76ee61013840f3d2893d11
Michael Rose <michael@rosenetwork.net>
2020-10-21 15:40:13 -0700

    editing is-a-video?.fish

--- is-a-video?.fish
+++ is-a-video?.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/is-a-video?_m.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/is-a-video?_m.fish @ line 2
 function is-a-video?
         test -f $argv && string match --regex 'video/.*$' (file --mime-type $argv)
 end
