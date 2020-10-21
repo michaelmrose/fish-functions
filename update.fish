042484a40f952cf6416fbf6789744716926994ab
Michael Rose <michael@rosenetwork.net>
2020-10-21 15:23:16 -0700

    editing update.fish

--- update.fish
+++ update.fish
@@ -1,5 +1,5 @@
-# Defined in /usr/home/michael/.config/fish/buffer/update.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/update.fish @ line 2
 function update
-    echo sudo zfs snapshot -r trident/ROOT@(preferred-date)
-    echo sudo xbps-install -Su
+    sudo zfs snapshot -r trident/ROOT@(preferred-date)
+    sudo xbps-install -Su
 end
