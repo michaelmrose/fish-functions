0c0ab7ed26bec06a0ecdd8cffbeabfec5c9f98fc
Michael Rose <michael@rosenetwork.net>
2020-10-21 16:20:38 -0700

    editing v.fish

--- v.fish
+++ v.fish
@@ -1,4 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/v.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/v.fish @ line 2
 function v
-	find-video $argv | select | all mpv &
+	# find-video $argv | select | all mpv &
+    select (cat ~/playlists/recent.m3u)|all o
 end
