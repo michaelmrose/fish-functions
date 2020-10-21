9e5ebb741fead22edb513be835e4714df4d5f264
Michael Rose <michael@rosenetwork.net>
2020-10-21 15:40:13 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/is-a-video?_m.fish @ line 6
+# Defined in /usr/home/michael/.config/fish/buffer/is-a-video?_m.fish @ line 7
 function m
     if test (count $argv) -gt 1
         set name (prompt 'name?')
@@ -10,8 +10,8 @@ function m
             p $files > ~/playlists/mpv/$name.m3u
         end
     else
-        if test -f $argv && string match --regex 'video/.*$' (file --mime-type $argv)
-            echo (fullpath $argv) > ~/playlists/recent.m3u
+        if is-a-video? $argv
+            echo (fullpath $argv) >> ~/playlists/mpv/recent.m3u
         end
     end
     mpv $argv &
