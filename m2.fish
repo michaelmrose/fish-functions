50c21d5c8f085a2bc2bccc9590faa06e03b82c2e
Michael Rose <michael@rosenetwork.net>
2020-10-21 17:55:28 -0700

    editing m2.fish

--- m2.fish
+++ m2.fish
@@ -1,33 +1,10 @@
+# Defined in /usr/home/michael/.config/fish/buffer/m.fish @ line 56
 function m2
-	if exists $argv
-    switch $argv[1]
-      case list-playlists
-        println (ls ~/playlists)
-        return 0
-      case ls
-        cat ~/playlists/$argv[2]
-        return 0
-      case play
-        m2 (cat ~/playlists/$argv[2])
-        return 0
+    switch (count $argv)
+        case 1
+            add-file-to-recent-playlist $argv
+        case '*'
+            save-list-of-files-to-playlist $argv
     end
-  end
-	set vals (vals 1..-1 $argv)
-  set arguments (filter-with-expr "not startswith @" $vals)
-  if not exists $arguments or get-tag play
-    while read -l line
-      set arguments $arguments $line
-    end
-  end
-  if get-tag replace $vals
-    for p in (get-tag replace $vals)
-      println $arguments > ~/playlists/$p
-    end
-  end
-  if get-tag append $vals
-    for p in (get-tag append $vals)
-      println $arguments >> ~/playlists/$p
-    end
-  end
-  umpv $arguments &
+   mpv $argv & 
 end
