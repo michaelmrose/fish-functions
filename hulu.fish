c8a941a35cccc6c203911dac88d0995054121705
Michael Rose <michael@rosenetwork.net>
2020-10-21 22:18:42 -0700

    editing hulu.fish

---
+++ hulu.fish
@@ -0,0 +1,4 @@
+# Defined in /usr/home/michael/.config/fish/buffer/hulu_netflix.fish @ line 1
+function hulu
+    chromium --enable-gpu-rasterization --ignore-gpu-blacklist --disable-gpu-driver-workarounds --use-gl=desktop --app=http://www.hulu.com
+end
