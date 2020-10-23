bea6c94a28ded24e211e97fc319c05519b839a7f
Michael Rose <michael@rosenetwork.net>
2020-10-23 16:06:59 -0700

    editing clipboard.fish

--- clipboard.fish
+++ clipboard.fish
@@ -1,4 +1,13 @@
-# Defined in /usr/home/michael/.config/fish/buffer/clip_clipboard.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/clip_clipboard_clipboard-old.fish @ line 2
 function clipboard
-    xclip -selection clipboard $argv
+	if exists $argv
+        switch $argv
+            case  'o'
+                xclip  -selection clipboard -o
+            case '*'
+                eval $argv | clipboard
+        end
+    else
+        xclip -selection clipboard -i
+    end
 end
