c069ebe5164fa1b72617667672345c6128eac9e5
Michael Rose <michael@rosenetwork.net>
2020-11-05 18:49:53 -0800

    editing georgia.fish

--- georgia.fish
+++ georgia.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/georgia.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/georgia.fish @ line 2
 function georgia
-    https://www.nytimes.com/interactive/2020/11/03/us/elections/results-georgia.html
+    http 'https://www.nytimes.com/interactive/2020/11/03/us/elections/results-georgia.html'|pup|g 'currently up'|choose 7:
 end
