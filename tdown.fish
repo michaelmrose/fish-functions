ed3acbb1df59ba72c475ef2df68db92755eb5178
Michael Rose <michael@rosenetwork.net>
2020-11-22 13:10:56 -0800

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -1,6 +1,10 @@
 # Defined in /usr/home/michael/.config/fish/buffer/tdown.fish @ line 2
 function tdown
 		set info (prompt 'title @ duration')
+		if not exists $info
+				last-timer
+				return 0
+		end
 		set title  (echo $info | cut -d @ -f1| trim)
 		set duration (echo $info | cut -d @ -f2 | trim)
 		eval kitty -o font_size=50 -e termdown --no-figlet -v en-us -T (quote $title) $duration
