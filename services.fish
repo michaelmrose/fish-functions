1320f873c468a855c0a341e8328c5d355adb0114
Michael Rose <michael@rosenetwork.net>
2020-11-22 12:14:04 -0800

    editing services.fish

---
+++ services.fish
@@ -0,0 +1,24 @@
+# Defined in /usr/home/michael/.config/fish/buffer/services.fish @ line 1
+function services
+		if not exists $argv
+				echo                  ENABLED
+				echo ===================
+				p /run/runit/runsvdir/current/*|each basename
+				echo                  USER
+				echo ===================
+				p ~/service/enabled/*|each basename
+				echo                  ALL
+				echo ===================
+				p /etc/sv/*|each basename
+				return 0
+		end
+		switch $argv[1]
+				case enabled
+						p /run/runit/runsvdir/current/*|each basename
+				case user
+						p ~/service/enabled/*|each basename
+				case all
+						p /etc/sv/*|each basename
+				case '*'
+		end
+end
