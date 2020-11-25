3b05a1c654e9998ca63f4074e90d94d6a5d8ca14
Michael Rose <michael@rosenetwork.net>
2020-11-25 14:48:49 -0800

    editing lookup-saved-colors-for-wp.fish

--- lookup-saved-colors-for-wp.fish
+++ lookup-saved-colors-for-wp.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/save-colors-for-wp-based-on-ha.fish @ line 8
+# Defined in /usr/home/michael/.config/fish/buffer/lookup-saved-colors-for-wp.fish @ line 2
 function lookup-saved-colors-for-wp
-		echo $wallpaperroot/saved-colors/(md5sum $argv)
+		cat $wallpaperroot/saved-colors/(md5sum $argv)
 end
