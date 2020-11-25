44c8bb7ef052916922da9f3fdc033a3622a81c05
Michael Rose <michael@rosenetwork.net>
2020-11-25 15:05:08 -0800

    editing save-colors-for-wp-based-on-hash.fish

--- save-colors-for-wp-based-on-hash.fish
+++ save-colors-for-wp-based-on-hash.fish
@@ -1,7 +1,7 @@
 # Defined in /usr/home/michael/.config/fish/buffer/save-colors-for-wp-based-on-ha.fish @ line 2
 function save-colors-for-wp-based-on-hash
-		set colorsfrom $argv[2]
-		set image $argv[1]
+		set colorsfrom $argv[1]
+		set image $argv[2]
 		set hash (md5sum $image)
-		echo $colorsfrom > $wallpaperroot/saved-colors/$hash
+		echo '$colorsfrom > $wallpaperroot/saved-colors/$hash'
 end
