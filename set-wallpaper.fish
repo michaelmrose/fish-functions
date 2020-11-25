9a8f2c608dfab6b2404a03a77fce643a7bb1b951
Michael Rose <michael@rosenetwork.net>
2020-11-25 14:30:43 -0800

    editing set-wallpaper.fish

--- set-wallpaper.fish
+++ set-wallpaper.fish
@@ -1,23 +1,12 @@
-# Defined in /home/michael/.config/fish/buffer/set-wallpaper.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/get-feh-format_set-wallpaper.fish @ line 18
 function set-wallpaper
-	set img (pathof $argv[1])
-    # echo i is $img
-    set ratio (get-image-aspect-ratio-type $img)
-    switch $ratio
-      case "narrow"
-          set format max
-      case "wide"
-          set format fill
-      case "extrawide"
-          set format max
-      case "superwide"
-          set format max
-  end
-  feh --bg-{$format} $img
-  add-to-recent-backgrounds $img
-  set -U bgstyle (cutlastn "/" 2 $img)
-  set-lightdm-bg $img
-  set -U bgimage $img
-  walfn
-  signal-i3blocks 12
+		set img (pathof $argv[1])
+		set format (get-feh-format $img)
+		feh --bg-{$format} $img
+		add-to-recent-backgrounds $img
+		set -U bgstyle (cutlastn "/" 2 $img)
+		set-lightdm-bg $img
+		set -U bgimage $img
+		walfn
+		signal-i3blocks 12
 end
