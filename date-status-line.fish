51d0f6011020cac6c21aa93e6f7e2029ca1bcf64
Michael Rose <michael@rosenetwork.net>
2020-12-20 14:17:54 -0800

    editing date-status-line.fish

--- date-status-line.fish
+++ date-status-line.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/date-status-line_unread-mail-c.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/date-status-line_output-icon_u.fish @ line 2
 function date-status-line
 		i3status-rs-colorize Info (date '+%A %d %b %Y %l:%M %p')
 end
