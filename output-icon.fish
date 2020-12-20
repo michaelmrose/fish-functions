3ebd6f992daaea99f74ef86673a751a6f93903ce
Michael Rose <michael@rosenetwork.net>
2020-12-20 13:57:33 -0800

    editing output-icon.fish

--- output-icon.fish
+++ output-icon.fish
@@ -12,5 +12,5 @@ function output-icon
   end
   # echo $symbol
 
-	echo "{\"state\":\"Alternating_Ting\", \"text\": \"$symbol\"}"
+	echo "{\"state\":\"Alternating_Tint\", \"text\": \"$symbol\"}"
 end
