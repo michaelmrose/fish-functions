26c5979eab97e02397541a2807f452230a360c81
Michael Rose <michael@rosenetwork.net>
2020-12-20 14:03:51 -0800

    editing i3status-rs-colorize.fish

--- i3status-rs-colorize.fish
+++ i3status-rs-colorize.fish
@@ -1,6 +1,6 @@
-# Defined in /usr/home/michael/.config/fish/buffer/output-icon.fish @ line 17
+# Defined in /usr/home/michael/.config/fish/buffer/i3status-rs-colorize.fish @ line 2
 function i3status-rs-colorize
-		set color $argv[1] # One of idle good warning critical
+		set color $argv[1] # One of Idle Info Good Warning
 		set txt $argv[2..-1]
 		echo "{\"state\":\"$color\", \"text\": \"$txt\"}"
 end
