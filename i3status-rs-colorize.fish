d238365b90166157945f09b1751fa7d05e6fd638
Michael Rose <michael@rosenetwork.net>
2020-12-20 14:09:52 -0800

    editing i3status-rs-colorize.fish

--- i3status-rs-colorize.fish
+++ i3status-rs-colorize.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/i3status-rs-colorize.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/i3status-rs-colorize_unread-ma.fish @ line 2
 function i3status-rs-colorize
 		set color $argv[1] # One of Idle Info Good Warning
 		set txt $argv[2..-1]
