608120eb7ad21c0e224fa3c035b52020d62b7d4e
Michael Rose <michael@rosenetwork.net>
2020-12-20 14:11:39 -0800

    editing date-status-line.fish

--- date-status-line.fish
+++ date-status-line.fish
@@ -1,4 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/date-status-line.fish @ line 2
 function date-status-line
-	i3status-rs-colorize Warning date '+%A %d %b %Y %l:%M %p'
+	i3status-rs-colorize Info date '+%A %d %b %Y %l:%M %p'
 end
