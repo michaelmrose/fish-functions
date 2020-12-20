27a423c615ed8e40ebd51083e492fa8772923e18
Michael Rose <michael@rosenetwork.net>
2020-12-20 12:53:48 -0800

    editing signal-i3blocks.fish

--- signal-i3blocks.fish
+++ signal-i3blocks.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/signal-i3blocks.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/signal-i3blocks.fish @ line 2
 function signal-i3blocks
 	switch $argv
         case vi3
@@ -28,4 +28,5 @@ function signal-i3blocks
     end
     # pkill -RTMIN+$val i3blocks
     pkill -SIGRTMIN+$val i3blocks
+    pkill -SIGRTMIN+$val i3status-rs
 end
