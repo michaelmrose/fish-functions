581fbc00753cab04cb4402df042abd684a0ee86d
Michael Rose <michael@rosenetwork.net>
2020-12-20 14:01:48 -0800

    editing output-icon.fish

--- output-icon.fish
+++ output-icon.fish
@@ -1,16 +1,16 @@
 # Defined in /usr/home/michael/.config/fish/buffer/output-icon.fish @ line 2
 function output-icon
-	switch (output-type)
-    case headphones
-      set symbol 
-    case speakers
-      set symbol 
-      case built-in
-          set symbol 
-    case "*"
-      set symbol 
-  end
-  # echo $symbol
+		switch (output-type)
+				case headphones
+						set symbol 
+				case speakers
+						set symbol 
+				case built-in
+						set symbol 
+				case "*"
+						set symbol 
+		end
+		# echo $symbol
 
-	echo "{\"state\":\"Warning\", \"text\": \"$symbol\"}"
+		echo "{\"state\":\"Warning\", \"text\": \"$symbol\"}"
 end
