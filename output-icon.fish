8c06e577fae52a2447671da2f6a8ceb815e76bf0
Michael Rose <michael@rosenetwork.net>
2020-12-20 13:52:59 -0800

    editing output-icon.fish

--- output-icon.fish
+++ output-icon.fish
@@ -12,5 +12,5 @@ function output-icon
   end
   # echo $symbol
 
-	echo '{\"icon\":\"mail\",\"state\":\"Critical\", \"text\": \"Danger!\"}'
+	echo '{"icon":"mail","state":"Critical", "text": "Danger!"}'
 end
