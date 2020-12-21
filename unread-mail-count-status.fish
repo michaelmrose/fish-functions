39006f841418a8167b8d6e26b119e7be4f4d0059
Michael Rose <michael@rosenetwork.net>
2020-12-20 17:45:42 -0800

    editing unread-mail-count-status.fish

--- unread-mail-count-status.fish
+++ unread-mail-count-status.fish
@@ -1,5 +1,5 @@
 # Defined in /usr/home/michael/.config/fish/buffer/date-status-line_output-icon_u.fish @ line 24
 function unread-mail-count-status
 		set mail_count (unread-mail-count)
-		i3status-rs-colorize Warning "     $mail_count"
+		i3status-rs-colorize Warning "   $mail_count"
 end
