f20239ffee6f64f489111375142defa919d34557
Michael Rose <michael@rosenetwork.net>
2020-12-20 18:00:35 -0800

    editing app-status.fish

--- app-status.fish
+++ app-status.fish
@@ -1,11 +1,13 @@
 # Defined in /usr/home/michael/.config/fish/buffer/app-status_calculator-status_i.fish @ line 2
 function app-status
-		set style $arg[1]
+		set style $argv[1]
 		set app $argv[2]
 		switch $app
-				case calculator
+				case calc
 						set symbol 
 		end
-		echo symbol is $symbol
+		if exists $symbol
+				echo it is real
+		end
 		i3status-rs-colorize $style " $symbol "
 end
