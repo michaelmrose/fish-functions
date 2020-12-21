43bbefef7688ac6e0dda53be4d98a312b8377ab7
Michael Rose <michael@rosenetwork.net>
2020-12-20 17:51:05 -0800

    editing calculator-status.fish

--- calculator-status.fish
+++ calculator-status.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/date-status-line_output-icon_u.fish @ line 29
+# Defined in /usr/home/michael/.config/fish/buffer/calculator-status.fish @ line 2
 function calculator-status
-		i3status-rs-colorize Info 🖩
+		i3status-rs-colorize Info"   "
 end
