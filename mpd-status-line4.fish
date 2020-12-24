8bddbe1f3abd96f8deb8e10f9eb3f507ad56509c
Michael Rose <michael@rosenetwork.net>
2020-12-23 19:14:57 -0800

    editing mpd-status-line4.fish

--- mpd-status-line4.fish
+++ mpd-status-line4.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-info_m.fish @ line 44
+# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-info_m.fish @ line 47
 function mpd-status-line4
 		if set playback (mpd-info status)
 				echo (? "string match playing $playback"  ) (mpd-info metadata) via (mpd-info outputs)
