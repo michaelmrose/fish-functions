56b72f82f2ede41b920942171e706c46a5a3e137
Michael Rose <michael@rosenetwork.net>
2020-12-23 16:15:48 -0800

    editing either-of.fish

--- either-of.fish
+++ either-of.fish
@@ -1,4 +1,7 @@
-# Defined in /usr/home/michael/.config/fish/buffer/either-of.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/either-of.fish @ line 2
 function either-of
-		 if string match foo foo > /dev/null;echo a; else; echo b;end;
+		# if string match foo foo > /dev/null;echo a; else; echo b;end;
+		set fn $argv[1]
+		optiona $argv[2]
+		optionb $argv[3]
 end
