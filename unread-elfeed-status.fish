5694c1c533b62b9d7717a3f7cfee2facee186305
Michael Rose <michael@rosenetwork.net>
2020-12-24 04:14:50 -0800

    editing unread-elfeed-status.fish

---
+++ unread-elfeed-status.fish
@@ -0,0 +1,4 @@
+# Defined in /usr/home/michael/.config/fish/buffer/unread-elfeed-status_unread-ma.fish @ line 1
+function unread-elfeed-status
+		i3status-rs-colorize Warning" emf e mmr/elfeed-count-status"
+end
