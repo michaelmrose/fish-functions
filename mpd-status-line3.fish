bbbedc255878e0d464a7b1b278972e24457e58bf
Michael Rose <michael@rosenetwork.net>
2020-12-23 16:45:29 -0800

    editing mpd-status-line3.fish

--- mpd-status-line3.fish
+++ mpd-status-line3.fish
@@ -1,9 +1,10 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-list-e.fish @ line 22
+# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-list-e.fish @ line 23
 function mpd-status-line3
 		if set playback (mpc status|sed -n 2p|choose -f '\[|\]' 0|grep -E 'playing|paused')
 				set symbol (? "string match playing $playback"  )
-				set metadata (mpd status |head -1)
-				set outputs (mpc outputs | choose -f '\(|\)' 1|sd '\n' ' ')
+				set metadata (mpc status |head -1)
+				set outputs (mpc outputs | choose -f '\(|\)' 1|sd '
+' ' ')
 				echo $symbol $metadata via $outputs
 		end
 end
