10311813261ff3a94cb58cfdeff00f37c586d4a7
Michael Rose <michael@rosenetwork.net>
2020-12-23 19:14:33 -0800

    editing mpd-list-enabled-outputs.fish

--- mpd-list-enabled-outputs.fish
+++ mpd-list-enabled-outputs.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-list-e.fish @ line 7
+# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-info_m.fish @ line 18
 function mpd-list-enabled-outputs
 		mpc outputs | choose -f '\(|\)' 1|sd '
 ' ' '
