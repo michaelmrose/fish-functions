3961a2313ad1407d530dc9b14a6752246c2890ed
Michael Rose <michael@rosenetwork.net>
2020-12-23 16:20:06 -0800

    editing mpd-status-line2.fish

--- mpd-status-line2.fish
+++ mpd-status-line2.fish
@@ -7,11 +7,12 @@ function mpd-status-line2
 				# 		case playing
 				# 				set symbol 
 				# end
-				if [ $playback = playing ]
-								set symbol 
-						else
-								set symbol 
-				end
+				# if [ $playback = playing ]
+				# 				set symbol 
+				# 		else
+				# 				set symbol 
+				# end
+				set symbol (either-of "string match playing $playback"  )
 				echo $symbol (mpc status |head -1) via (mpd-list-enabled-outputs)
 		end
 end
