5b322b0346ea5864d41a2eac0e04ad6320866716
Michael Rose <michael@rosenetwork.net>
2020-12-23 19:14:57 -0800

    editing mpd-info.fish

--- mpd-info.fish
+++ mpd-info.fish
@@ -1,10 +1,11 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-info_m.fish @ line 6
+# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-info_m.fish @ line 7
 function mpd-info
 		switch argv[1]
 				case metadata
-						mpd status |head -1
+						mpc status |head -1
 				case outputs
-						mpc outputs | choose -f '\(|\)' 1|sd ' \n' ' '
+						mpc outputs | choose -f '\(|\)' 1|sd ' 
+' ' '
 				case status
 						mpc status|sed -n 2p|choose -f '\[|\]' 0|grep -E 'playing|paused'
 		end
