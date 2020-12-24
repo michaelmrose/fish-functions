50802b3612cd8d9e3eacf5c67898ffe31653e839
Michael Rose <michael@rosenetwork.net>
2020-12-23 19:32:15 -0800

    removed mpd-status-line4.fish

--- mpd-status-line4.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-info_m.fish @ line 47
-function mpd-status-line4
-		if set playback (mpd-info status)
-				echo (? "string match playing $playback"  ) (mpd-info metadata) via (mpd-info outputs)
-		end
-end
