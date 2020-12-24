0cc920f98c9a85581135228ddedbf3209a120e38
Michael Rose <michael@rosenetwork.net>
2020-12-23 16:42:40 -0800

    removed ?.fish

--- =.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /home/michael/.config/fish/buffer/=.fish @ line 1
-function =
-	string match -r $argv[1] $argv[2] > /dev/null
-end
--- ?.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /usr/home/michael/.config/fish/buffer/?.fish @ line 2
-function ?
-    service status $argv
-end
