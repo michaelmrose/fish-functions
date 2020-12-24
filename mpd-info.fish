017a49db5c91d5c126e6a1e7436d1e0c5e6ae626
Michael Rose <michael@rosenetwork.net>
2020-12-23 19:26:48 -0800

    editing mpd-info.fish

--- mpd-info.fish
+++ mpd-info.fish
@@ -5,8 +5,7 @@ function mpd-info
 						echo metadata
 						mpc status |head -1
 				case outputs
-						mpc outputs | choose -f '\(|\)' 1|sd ' 
-' ' '
+						mpc outputs | choose -f '\(|\)' 1|sd ' ' ' '
 				case status
 						mpc status|sed -n 2p|choose -f '\[|\]' 0|grep -E 'playing|paused'
 		end
