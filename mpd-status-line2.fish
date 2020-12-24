61a71ad9ee1be4db89f96aae2b3357387359f64b
Michael Rose <michael@rosenetwork.net>
2020-12-23 16:44:08 -0800

    editing mpd-status-line2.fish

--- mpd-status-line2.fish
+++ mpd-status-line2.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpd-status-line2.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/mpc-playback-status_mpd-list-e.fish @ line 13
 function mpd-status-line2
 		if set playback (mpc-playback-status)
 				set symbol (either-of "string match playing $playback"  )
