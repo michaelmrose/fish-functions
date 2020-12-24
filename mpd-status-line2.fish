9cba41aa2161f2ac18a431bb0a002562f7d7e70f
Michael Rose <michael@rosenetwork.net>
2020-12-23 16:01:46 -0800

    editing mpd-status-line2.fish

--- mpd-status-line2.fish
+++ mpd-status-line2.fish
@@ -1,12 +1,13 @@
 # Defined in /usr/home/michael/.config/fish/buffer/mpd-status-line2.fish @ line 2
 function mpd-status-line2
 		if set playback (mpc-playback-status)
-				switch $playback
-						case paused
-								set symbol 
-						case playing
-								set symbol 
-				end
+				# switch $playback
+				# 		case paused
+				# 				set symbol 
+				# 		case playing
+				# 				set symbol 
+				# end
+				set symbol (match-lists $playback "playing paused" )
 				echo $symbol (mpc status |head -1) via (mpd-list-enabled-outputs)
 		end
 end
