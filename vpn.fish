58f030b0b43dc15a7e02e856ddd64ba6fe967374
Michael Rose <michael@rosenetwork.net>
2020-12-28 12:48:03 -0800

    editing vpn.fish

--- vpn.fish
+++ vpn.fish
@@ -1,10 +1,15 @@
 # Defined in /usr/home/michael/.config/fish/buffer/vpn.fish @ line 2
 function vpn
-    switch $argv[1]
-        case -d
-            volemad-cli -d
-        case  -c
-            volemad-cli  -p 'USA' -s 'Los Angeles, CA'
+		switch $argv[1]
+				case -d
+						volemad-cli -d
+						for i in (seq 10)
+								signal-i3blocks 12
+								sleep 1
+						end
+
+				case  -c
+						volemad-cli  -p 'USA' -s 'Los Angeles, CA'
 						for i in (seq 10)
 								signal-i3blocks 12
 								sleep 1
