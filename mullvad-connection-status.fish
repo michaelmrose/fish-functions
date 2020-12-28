3c14650b0072094fc924820bbf54f705cf56250d
Michael Rose <michael@rosenetwork.net>
2020-12-28 12:50:14 -0800

    editing mullvad-connection-status.fish

--- mullvad-connection-status.fish
+++ mullvad-connection-status.fish
@@ -1,5 +1,8 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mullvad-status.fish @ line 10
+# Defined in /usr/home/michael/.config/fish/buffer/mullvad-connection-status.fish @ line 2
 function mullvad-connection-status
 		if set name (mullvad-connection-name); and  ip link show $name|head -1|choose 8|g UP > /dev/null
+				return 0
+		else
+				return 1
 		end
 end
