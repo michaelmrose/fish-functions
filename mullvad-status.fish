2c9ca01ee63c306ed744065f0cef63b9db7aa210
Michael Rose <michael@rosenetwork.net>
2020-12-28 12:42:18 -0800

    editing mullvad-status.fish

--- mullvad-status.fish
+++ mullvad-status.fish
@@ -1,8 +1,8 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mullvad-connection-name_mullva.fish @ line 8
+# Defined in /usr/home/michael/.config/fish/buffer/mullvad-status.fish @ line 2
 function mullvad-status
 		if set name (mullvad-connection-name); and  ip link show $name|head -1|choose 8|g UP
 				i3status-rs-colorize Warning vpn: up
 		else
-				i3status-rs-colorize Info vpn: down
+				i3status-rs-colorize Idle vpn: down
 		end
 end
