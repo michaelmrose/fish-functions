698f77f6dddfd3753aae24fe00f8dd1c37c2d1a3
Michael Rose <michael@rosenetwork.net>
2020-12-28 12:26:21 -0800

    editing vpn.fish

--- vpn.fish
+++ vpn.fish
@@ -4,10 +4,6 @@ function vpn
         case -d
             volemad-cli -d
         case  -c
-            if not pgrep volemad\$
-                volemad &
-                sleep 0.5
-            end
             volemad-cli  -p 'USA' -s 'Los Angeles, CA'
     end
 end
