4ecd9c3fe5c65ca4a8ffe1cf2629eb9766078f84
Michael Rose <michael@rosenetwork.net>
2020-12-28 12:39:13 -0800

    editing mullvad-connection-name.fish

--- mullvad-connection-name.fish
+++ mullvad-connection-name.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/functions/mullvad-connection-name.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/mullvad-connection-name_mullva.fish @ line 2
 function mullvad-connection-name
 		odds (ip link show up)|choose -f ' |:' 1|g mullvad
 end
