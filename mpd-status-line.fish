633ef05e36f04d6533cc2b59294e9ab3c3917a4f
Michael Rose <michael@rosenetwork.net>
2020-12-29 22:12:02 -0800

    editing mpd-status-line.fish

--- mpd-status-line.fish
+++ mpd-status-line.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpd-status-line.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/firefoxPlayerMeta_mpd-status-l.fish @ line 10
 function mpd-status-line
 		if set playback (mpd-info status)
 				echo (? "string match playing $playback" " " " ") (mpd-info metadata) via (mpd-info outputs)
