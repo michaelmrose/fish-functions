1c06bb85e04448dc46ecdbb5b086756b639b7db7
Michael Rose <michael@rosenetwork.net>
2020-12-29 22:12:02 -0800

    editing firefoxPlayerMeta.fish

--- firefoxPlayerMeta.fish
+++ firefoxPlayerMeta.fish
@@ -1,6 +1,7 @@
-# Defined in /usr/home/michael/.config/fish/buffer/firefoxPlayerMeta.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/firefoxPlayerMeta_mpd-status-l.fish @ line 2
 function firefoxPlayerMeta
 		if set firefoxplayer (playerctl -l | g firefox)
-				playerctl -p $firefoxplayer metadata title
+				set playback (playerctl -p $firefoxplayer status)
+				echo (? "string match playing $playback" " " " ") playerctl -p $firefoxplayer metadata title
 		end
 end
