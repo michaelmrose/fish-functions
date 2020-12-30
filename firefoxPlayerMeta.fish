715146cf491b87bff04d9e237eb2175f6d158b1d
Michael Rose <michael@rosenetwork.net>
2020-12-29 22:10:25 -0800

    editing firefoxPlayerMeta.fish

--- firefoxPlayerMeta.fish
+++ firefoxPlayerMeta.fish
@@ -1,6 +1,6 @@
-# Defined in /usr/home/michael/.config/fish/buffer/firefoxPlayerMeta.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/firefoxPlayerMeta.fish @ line 2
 function firefoxPlayerMeta
 		if set firefoxplayer (playerctl -l | g firefox)
-				playerctl -p $firefoxplayer metadata
+				playerctl -p $firefoxplayer metadata title
 		end
 end
