79dc052b7180e228a4deb8082e3883b4e9a47aa6
Michael Rose <michael@rosenetwork.net>
2021-01-03 15:52:17 -0800

    editing mpd-status-line.fish

--- mpd-status-line.fish
+++ mpd-status-line.fish
@@ -1,9 +1,9 @@
 # Defined in /usr/home/michael/.config/fish/buffer/mpd-status-line.fish @ line 2
 function mpd-status-line
-		if set playback (mpd-info status)
-				echo (? "string match playing $playback" " " " ") (mpd-info metadata) via (mpd-info outputs)
-		else
-				echo fuckyou
-		end
+		# if set playback (mpd-info status)
+		# 		echo (? "string match playing $playback" " " " ") (mpd-info metadata) via (mpd-info outputs)
+		# else
+		# 		echo fuckyou
+		# end
 				echo (? "string match playing $playback" " " " ") (mpd-info metadata) via (mpd-info outputs)
 end
