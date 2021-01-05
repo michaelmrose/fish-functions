289ab75956a8d3e4b6baafaacdc7ca22dce8f6bb
Michael Rose <michael@rosenetwork.net>
2021-01-05 11:01:59 -0800

    editing set-path.fish

--- set-path.fish
+++ set-path.fish
@@ -6,7 +6,7 @@ function set-path
 
   set builtinpaths  /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
 
-  set mypaths /usr/home/michael/.cargo/bin /usr/home/michael/.local/bin /usr/home/michael/scripts /usr/home/michael/bin /opt/clojure/bin
+  set mypaths /usr/home/michael/.cargo/bin /usr/home/michael/.local/bin /usr/home/michael/scripts /usr/home/michael/bin /opt/clojure/bin /usr/home/michael/scripts/clj
 
   set -xg PATH $mypaths  $builtinpaths
 end
