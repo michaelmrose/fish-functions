09b134d604e5068075b9ec5af1567bc16babc7f8
Michael Rose <michael@rosenetwork.net>
2021-01-18 14:06:24 -0800

    editing hours-of-trump.fish

---
+++ hours-of-trump.fish
@@ -0,0 +1,4 @@
+# Defined in /usr/home/michael/.config/fish/buffer/hours-of-trump.fish @ line 1
+function hours-of-trump
+http 'https://www.timeanddate.com/countdown/to?msg=Countdown%20Clock%20to%20the%20End%20of%20Trump%27s%20Reign%20of%20Terror&p0=263&year=2021&month=1&day=20&hour=12&min=0&sec=0&fromtheme=generic'|pup 'td#el_h2 text{}'
+end
