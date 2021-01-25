55c5260e30b0b38dcf49c77c72558ecf8eee5573
Michael Rose <michael@rosenetwork.net>
2021-01-24 20:16:04 -0800

    editing toggle-sound-port.fish

---
+++ toggle-sound-port.fish
@@ -0,0 +1,17 @@
+# Defined in /usr/home/michael/.config/fish/buffer/toggle-sound-port.fish @ line 1
+function toggle-sound-port
+		set active (pactl list sinks|grep "Active Port"|choose 2)
+		switch $active
+				case analog-output-headphones
+						# disable automute so the fucking sound can come out of line out
+						amixer -c 1 sset "Auto-Mute Mode" Disabled
+						pact set-sink-port SINK PORT
+						# switch to line out
+						pactl set-sink-port 1 analog-output-lineout
+				case analog-output-lineout
+						# enable automute so that sound doesn't come out of both
+						amixer -c 1 sset "Auto-Mute Mode" Enabled
+						# switch to headphones
+						pactl set-sink-port 1 analog-output-headphones
+		end
+end
