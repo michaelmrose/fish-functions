670bd110dfe0207fa2249a6f6690370124c84092
Michael Rose <michael@rosenetwork.net>
2021-01-27 21:26:32 -0800

    editing toggle-alsa.fish

--- toggle-alsa.fish
+++ toggle-alsa.fish
@@ -1,6 +1,6 @@
 # Defined in /usr/home/michael/.config/fish/buffer/toggle-alsa.fish @ line 2
 function toggle-alsa
-		switch (amixer -c 1 get Headphone | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]')
+		switch (amixer -c 1 get Headphone | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]|' |sort -u)
 				case '[on]'
 						echo on
 				case '[off]'
