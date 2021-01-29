377976e0c33863bc30b7e2b9e9b865c5fd493d8b
Michael Rose <michael@rosenetwork.net>
2021-01-28 16:25:32 -0800

    editing adjust.fish

--- adjust.fish
+++ adjust.fish
@@ -1,4 +1,5 @@
-# Defined in /usr/home/michael/.config/fish/buffer/adjust.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/adjust.fish @ line 2
 function adjust
 		sndio.clj adjust $argv[1] $argv[2]
+		signal-i3blocks 2
 end
