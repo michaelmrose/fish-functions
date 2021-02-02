0bffaf2c157e4dea96ec3a3e9836a35a13d85915
Michael Rose <michael@rosenetwork.net>
2021-02-01 22:46:43 -0800

    editing emf.fish

--- emf.fish
+++ emf.fish
@@ -1,4 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/emf.fish @ line 2
 function emf
-		eval emacsclient -$argv[1] \'\($argv[2..-1]\)\' &
+		eval emacsclient -$argv[1] \'\($argv[2..-1]\)\' 
 end
