1ac12473d41bd68bd79ebf5cc6940807e317afbf
Michael Rose <michael@rosenetwork.net>
2021-02-01 22:43:12 -0800

    editing tunes.fish

--- tunes.fish
+++ tunes.fish
@@ -1,4 +1,7 @@
+# Defined in /usr/home/michael/.config/fish/buffer/tunes.fish @ line 2
 function tunes
-	clementine &
-	update-now-playing &
+		emf ec mmr/tunes
+		kitty -1 -e cava &
+		sleep 0.5
+		i3-msg resize shrink up 120px
 end
