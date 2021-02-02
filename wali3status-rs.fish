daad648b228b2eb10429c77c308dfd90f53b6429
Michael Rose <michael@rosenetwork.net>
2021-02-02 15:05:59 -0800

    editing wali3status-rs.fish

--- wali3status-rs.fish
+++ wali3status-rs.fish
@@ -1,4 +1,5 @@
 # Defined in /usr/home/michael/.config/fish/buffer/wali3status-rs.fish @ line 2
 function wali3status-rs
-		sd '^warning_bg.*' "warning_bg = \"$argv\"" ~/.config/i3status-rust/config.toml
+		sd '^warning_bg.*' "warning_bg = \"$argv[1]\"" ~/.config/i3status-rust/config.toml
+		sd '^warning_bg.*' "info_bg = \"$argv[1]\"" ~/.config/i3status-rust/config.toml
 end
