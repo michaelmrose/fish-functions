0e0ef16982ccbfa29d2ee37529a017f7eaf10e53
Michael Rose <michael@rosenetwork.net>
2021-02-03 21:16:57 -0800

    editing darkerhex.fish

--- darkerhex.fish
+++ darkerhex.fish
@@ -1,9 +1,9 @@
-# Defined in /usr/home/michael/.config/fish/buffer/darkerhex_darker-hex-channel.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/darkerhex.fish @ line 2
 function darkerhex
 		set start \#
 		set left (echo $argv|cut -c2-3)
 		set middle (echo $argv|cut -c4-5)
-		set right (echo $argv|cut -c5-6)
+		set right (echo $argv|cut -c6-7)
 
 		echo (darker-hex-channel $left)(darker-hex-channel $middle)(darker-hex-channel $right)
 end
