f38f2bc37d066b8ea950e62936261c6f745798fe
Michael Rose <michael@rosenetwork.net>
2021-02-03 20:57:58 -0800

    editing darker-accent.fish

--- darker-accent.fish
+++ darker-accent.fish
@@ -1,6 +1,6 @@
-# Defined in /usr/home/michael/.config/fish/buffer/darker-accent.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/darker-accent.fish @ line 2
 function darker-accent
 		set accent (jq -r .colors.color3 ~/.cache/wal/colors.json)
-		sd \#$accent \#(darkerhex "#9ec5ed") ~/.cache/wal/colors.json
+		sd $accent \#(darkerhex "#9ec5ed") ~/.cache/wal/colors.json
 		walfnadd
 end
