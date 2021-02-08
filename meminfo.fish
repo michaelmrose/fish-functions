fcb47702d8fb8476a930b3d1e0902f149aa80f59
Michael Rose <michael@rosenetwork.net>
2021-02-08 12:43:34 -0800

    editing meminfo.fish

--- meminfo.fish
+++ meminfo.fish
@@ -1,7 +1,7 @@
-# Defined in /home/michael/.config/fish/buffer/meminfo.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/meminfo.fish @ line 2
 function meminfo
     if exists $argv
-        human (pgrep $argv |each pmap -x|g total|nth 5 |sumof|multiplyby 1024)
+        human (pgrep $argv |each s pmap -x|g total|nth 5 |sumof|multiplyby 1024)
     else
         while read -l line
             pmap -x $line |g total|nth 5|sumof|multiplyby 1024|each human
