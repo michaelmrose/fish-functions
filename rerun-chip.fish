11007daef9664724580224f23f789c2850de9fd5
Michael Rose <michael@rosenetwork.net>
2021-02-08 20:52:26 -0800

    editing rerun-chip.fish

--- rerun-chip.fish
+++ rerun-chip.fish
@@ -11,7 +11,9 @@ function rerun-chip
 		# xdotool mousemove 125 50
 		# xdotool click 1
 		xdotool mousemove 175 50
-	 xdotool click 1
+		sleep 0.5
+		xdotool click 1
 	 xdotool mousemove 125 50
+	 sleep 0.5
 	 xdotool click 1
 end
