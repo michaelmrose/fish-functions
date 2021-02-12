3255d4c3ad6d27be2ef35be9d0e34eb0fdf1d015
Michael Rose <michael@rosenetwork.net>
2021-02-12 01:53:33 -0800

    editing e.fish

--- e.fish
+++ e.fish
@@ -1,6 +1,6 @@
 # Defined in /usr/home/michael/.config/fish/buffer/e_emf.fish @ line 2
 function e
-		set com \'\($argv\)\'
+		set com \\'\($argv\)\\'
 		echo $com
 		# echo emacsclient -e "$com"
 end
