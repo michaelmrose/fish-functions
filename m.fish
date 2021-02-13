8db65226450b367b9ce0b6b650ebc97bc99af602
Michael Rose <michael@rosenetwork.net>
2021-02-13 00:55:37 -0800

    removed m.fish

--- m.fish
+++
@@ -1,9 +0,0 @@
-function m
-    switch (count $argv)
-        case 1
-            add-file-to-recent-playlist $argv
-        case '*'
-            save-list-of-files-to-playlist $argv
-    end
-   mpv $argv & 
-end
