177cb316e5cf154c79734d5c02bf265ef3bf7626
Michael Rose <michael@rosenetwork.net>
2021-02-13 00:55:45 -0800

    editing pl.old.fish

---
+++ pl.old.fish
@@ -0,0 +1,18 @@
+function pl.old
+    if exists $argv
+            set selection $argv
+    else
+        set selection ''
+    end
+    
+    set playlists (p ~/playlists/mpv/*.m3u)
+    set choice (p $playlists|g $selection|each cutlast /|cut -d . -f1|select)
+    if exists $choice
+        set playlist (p $playlists | g $choice)
+        if exists $playlist
+                mpv $playlist &
+        end
+    else
+        echo aborted
+    end
+end
