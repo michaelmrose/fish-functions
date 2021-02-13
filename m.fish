d61aad3ca78db5ef23fa469c5942dd3c2b3c0fa0
Michael Rose <michael@rosenetwork.net>
2021-02-13 00:59:22 -0800

    editing m.fish

--- m.fish
+++ m.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/m_pl.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/m_pl.fish @ line 2
 function m
-		videos.clj -o $argv
+		videos.clj -o $argv &
 end
