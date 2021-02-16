fb95dfbe90096085b0cbe13d458e6f8ca1c1497b
Michael Rose <michael@rosenetwork.net>
2021-02-16 13:34:47 -0800

    editing last-email-subject.fish

--- last-email-subject.fish
+++ last-email-subject.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/mu-last-message-subject.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/last-email-subject.fish @ line 2
 function last-email-subject
-	mu find date:7d..now --sortfield=date --reverse --fields=s|head -1
+	mu find flag:unread date:7d..now --sortfield=date --reverse --fields=s|head -1
 end
