ee060e9d8d10684c71a06575a701f2932da89322
Michael Rose <michael@rosenetwork.net>
2021-02-16 14:38:14 -0800

    editing unread-mail-count.fish

--- unread-mail-count.fish
+++ unread-mail-count.fish
@@ -1,5 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/unread-mail-count.fish @ line 2
 function unread-mail-count
-		typewriter-notification "email: " (last-email-subject)
 		mu find flag:unread and date:7d..now and not maildir:/OldGmail/techmail 2>/dev/null|wc -l
 end
