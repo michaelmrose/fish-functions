85694ed91c9204254dfe7b96aa9a87f3ace0bc27
Michael Rose <michael@rosenetwork.net>
2021-02-16 14:47:04 -0800

    editing email-notification.fish

--- email-notification.fish
+++ email-notification.fish
@@ -1,8 +1,8 @@
-# Defined in /usr/home/michael/.config/fish/buffer/email-notification.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/email-notification.fish @ line 2
 function email-notification
 		set email_subject (last-email-subject)
 		if not string match $email_subject $LAST_EMAIL_SUBJECT
-				typewriter-notification "email: " (email-subject)
+				typewriter-notification "email: " $email_subject
 				set -U LAST_EMAIL_SUBJECT $email_subject
 		end
 end
