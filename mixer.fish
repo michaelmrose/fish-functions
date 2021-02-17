37e8704baced07035a8c93e9c50df4e167059692
Michael Rose <michael@rosenetwork.net>
2021-02-16 19:30:22 -0800

    editing mixer.fish

--- mixer.fish
+++ mixer.fish
@@ -1,6 +1,10 @@
 # Defined in /usr/home/michael/.config/fish/buffer/mixer.fish @ line 2
 function mixer
-		kitty -1 -e alsamixer -c 1
+		# kitty -1 -e alsamixer -c 1
+		# waituntilfocused kitty
+		# i3 fullscreen
+		cmixer
 		waituntilfocused kitty
 		i3 fullscreen
+		xdotool key Next
 end
