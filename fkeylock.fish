1edc9ecc935729522c89694d53c94c0c61cbc368
Michael Rose <michael@rosenetwork.net>
2021-02-18 14:15:05 -0800

    editing fkeylock.fish

--- fkeylock.fish
+++ fkeylock.fish
@@ -2,13 +2,13 @@
 function fkeylock
 		set state (cat /tmp/fkeylock)
 		if string match true $state
-				echo true >> /tmp/fkeylock
+				echo true > /tmp/fkeylock
 				xmodmap -e 'keycode 71 = F5'
 				xmodmap -e 'keycode 72 = F6'
 				xmodmap -e 'keycode 73 = F7'
 				xmodmap -e 'keycode 74 = F8'
 		else
-				echo false >> /tmp/fkeylock
+				echo false > /tmp/fkeylock
 				xmodmap -e 'keycode 71 = XF86AudioPlay'
 				xmodmap -e 'keycode 72 = XF86AudioStop'
 				xmodmap -e 'keycode 73 = XF86AudioPrev'
