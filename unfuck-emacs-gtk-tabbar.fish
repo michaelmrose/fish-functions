0d187f3d854fc49b6451a1a6b12bff3a63028648
Michael Rose <michael@rosenetwork.net>
2021-02-19 14:46:20 -0800

    editing unfuck-emacs-gtk-tabbar.fish

--- unfuck-emacs-gtk-tabbar.fish
+++ unfuck-emacs-gtk-tabbar.fish
@@ -1,7 +1,7 @@
 # Defined in /usr/home/michael/.config/fish/buffer/unfuck-emacs-gtk-tabbar.fish @ line 2
 function unfuck-emacs-gtk-tabbar
 		waituntilfocused Emacs
-		sleeo 0.1
+		sleeo 0.3
 		i3 fullscreen
 		i3 fullscreen
     # emf e mmr/distraction-free
