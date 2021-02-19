c8cd3f262009c01a75da1089a7237f1807891663
Michael Rose <michael@rosenetwork.net>
2021-02-19 14:01:50 -0800

    editing vpn.fish

--- vpn.fish
+++ vpn.fish
@@ -1,23 +1,23 @@
 # Defined in /usr/home/michael/.config/fish/buffer/vpn.fish @ line 2
 function vpn
-	switch $argv[1]
-		case -d
-			olemad-cli -d
-		case  -c
-			volemad-cli  -p 'USA' -s 'Los Angeles, CA'
-		case -t
-			set mullvad (mullvad-status)
-			if mullvad-connection-status
-				vpn -d
-			else
-				vpn -c
-			end
-			for i in (seq  40)
-				if not [ mullvad = (mullvad-status) ]
-						signal-i3blocks 12
-						return 0
-				end
-				sleep 0.25
-			end
-	end
+		switch $argv[1]
+				case -d
+						olemad-cli -d
+				case	-c
+						volemad-cli	 -p 'USA' -s 'Los Angeles, CA'
+				case -t
+						set mullvad (mullvad-status)
+						if mullvad-connection-status
+								vpn -d
+						else
+								vpn -c
+						end
+						for i in (seq	 40)
+								if not [ mullvad = (mullvad-status) ]
+										signal-i3blocks 12
+										return 0
+								end
+								sleep 0.25
+						end
+		end
 end
