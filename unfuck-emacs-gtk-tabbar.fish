d6aac217b6bffd1aca3e41a3f3df9de286504d04
Michael Rose <michael@rosenetwork.net>
2021-02-18 19:54:58 -0800

    editing unfuck-emacs-gtk-tabbar.fish

--- unfuck-emacs-gtk-tabbar.fish
+++ unfuck-emacs-gtk-tabbar.fish
@@ -1,6 +1,6 @@
 # Defined in /usr/home/michael/.config/fish/buffer/unfuck-emacs-gtk-tabbar.fish @ line 2
 function unfuck-emacs-gtk-tabbar
-		sleep 0.1
+		sleep 0.2
 		i3 fullscreen
 		i3 fullscreen
     # emf e mmr/distraction-free
