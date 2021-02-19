153969cecd5179166ecaed951eb0e17a020b3f1c
Michael Rose <michael@rosenetwork.net>
2021-02-19 14:48:50 -0800

    editing unfuck-emacs-gtk-tabbar.fish

--- unfuck-emacs-gtk-tabbar.fish
+++ unfuck-emacs-gtk-tabbar.fish
@@ -1,6 +1,7 @@
 # Defined in /usr/home/michael/.config/fish/buffer/unfuck-emacs-gtk-tabbar.fish @ line 2
 function unfuck-emacs-gtk-tabbar
 		waituntilfocused Emacs
+		sleep 1
 		i3 focus right
 		i3 focus left
 end
