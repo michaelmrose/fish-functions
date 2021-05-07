913e491dd34653be738464f8e478e020d3bc8d3f
Michael Rose <michael@rosenetwork.net>
2021-05-06 19:35:50 -0700

    editing mullvad-status-official.fish

--- mullvad-status-official.fish
+++ mullvad-status-official.fish
@@ -1,6 +1,6 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mullvad-status-official.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/mullvad-status-official.fish @ line 2
 function mullvad-status-official
-		if mullvad status|grep Connected
+		if mullvad status|grep Connected > /dev/null
 				i3status-rs-colorize Warning "  "
 		else
 				i3status-rs-colorize Info "    "
