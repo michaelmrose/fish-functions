4d02a04aaa9fe97ad9dcc64de4466c4aa38661ac
Michael Rose <michael@rosenetwork.net>
2021-05-12 11:12:58 -0700

    editing system-menu.fish

--- system-menu.fish
+++ system-menu.fish
@@ -1,22 +1,28 @@
-# Defined in /home/michael/.config/fish/buffer/menu_system-menu.fish @ line 7
+# Defined in /usr/home/michael/.config/fish/buffer/system-menu.fish @ line 2
 function system-menu
-	
-    switch (menu System: Lightsout Suspend Shutdown "Restart i3wm" Reboot "Switch User")
-      
-        case Lightsout
-	          sleep 0.5
-            xset dpms force off
-      case Suspend
-          systemctl suspend;
-      case Shutdown
-            cleanup-desktop
-	          sudo shutdown -h now
-      case "Restart i3wm"
-            restart-i3
-        case Reboot
-            cleanup-desktop
-	          sudo shutdown -r now
-        case "Switch User"
-            dm-tool switch-to-greeter
-    end
+		switch (menu System: Lightsout Logout Suspend Shutdown "Restart i3wm" Reboot "Switch User")
+				case Lock
+						dm-tool switch-to-greeter
+						sleep 0.5
+						xset dpms force off
+				case Lightsout
+						sleep 0.5
+						xset dpms force off
+				case Suspend
+						sudo zzz
+				case Logout
+						cleanup-desktop
+						i3 exit
+
+				case Shutdown
+						cleanup-desktop
+						sudo shutdown -h now
+				case "Restart i3wm"
+						restart-i3
+				case Reboot
+						cleanup-desktop
+						sudo shutdown -r now
+				case "Switch User"
+						dm-tool switch-to-greeter
+		end
 end
