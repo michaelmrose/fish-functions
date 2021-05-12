3d0b2c63b1bdb0f3d40615fe0212d7849e0e81df
Michael Rose <michael@rosenetwork.net>
2021-05-12 11:14:42 -0700

    editing system-menu.fish

--- system-menu.fish
+++ system-menu.fish
@@ -2,7 +2,7 @@
 function system-menu
 		switch (menu System: Lock Lightsout Logout Suspend Shutdown "Restart i3wm" Reboot "Switch User")
 				case Lock
-						dm-tool switch-to-greeter
+						dm-tool switch-to-greeter &
 						sleep 3
 						xset dpms force off
 				case Lightsout
