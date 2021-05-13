193a48dfcd62cae6eb0c3a2b9896ec3a04f90bb1
Michael Rose <michael@rosenetwork.net>
2021-05-12 18:53:12 -0700

    editing replace-color-in-i3.fish

--- replace-color-in-i3.fish
+++ replace-color-in-i3.fish
@@ -2,5 +2,5 @@
 function replace-color-in-i3
     sd  "focused_workspace  #[a-fA-F0-9]{6} #[a-fA-F0-9]{6} #[a-fA-F0-9]{6}" "focused_workspace  #111111 $argv #000000" ~/.config/i3/config
     sd  "client.focused_inactive.*" "client.focused_inactive  #111111 $argv $argv $argv" ~/.config/i3/config
-    sd  " selected-normal-background: #*" " selected-normal-background: $argv;" ~/.config/rofi/themes/colors-rofi-dark.rasi
+    sd  " selected-normal-background: #.*" " selected-normal-background: $argv;" ~/.config/rofi/themes/colors-rofi-dark.rasi
 end
