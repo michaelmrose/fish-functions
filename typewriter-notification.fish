fef683cfd70361d7b1b131a9fba312cf249e4a01
Michael Rose <michael@rosenetwork.net>
2021-05-15 01:38:06 -0700

    editing typewriter-notification.fish

--- typewriter-notification.fish
+++ typewriter-notification.fish
@@ -13,7 +13,7 @@ function typewriter-notification
     #     sleep 0.3
     # end
    set title $argv[1]
-   set message $argv[2..-1]
+   set message "$argv[2..-1] "
    set length (echo $message|wc -c)
    for i in (seq 1 $length)
        set current (echo $message|cut -c 1-$i)
