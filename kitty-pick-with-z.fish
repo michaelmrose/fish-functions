377ae9356b86e4548feeeb649fb388e7e5c9eefc
Michael Rose <michael@rosenetwork.net>
2021-05-16 12:23:19 -0700

    editing kitty-pick-with-z.fish

--- kitty-pick-with-z.fish
+++ kitty-pick-with-z.fish
@@ -1,7 +1,8 @@
 # Defined in /usr/home/michael/.config/fish/buffer/kitty-pick-with-z.fish @ line 2
 function kitty-pick-with-z
 if in-terminal
-		spacefm -s set current_dir (quote (zoxide query -i|cut -d ' ' -f2))
+		set dir (zoxide query -i|cut -d ' ' -f2)
+		spacefm -s set current_dir "$dir"
 else
 kitty -1 ff kitty-pick-with-z
 end
