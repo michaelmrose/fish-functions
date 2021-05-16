7815646db9f7a7e93e4f96627842039ef68ec7e6
Michael Rose <michael@rosenetwork.net>
2021-05-15 23:21:21 -0700

    editing rerun-chip.fish

--- rerun-chip.fish
+++ rerun-chip.fish
@@ -1,8 +1,8 @@
 # Defined in /usr/home/michael/.config/fish/buffer/rerun-chip.fish @ line 2
 function rerun-chip
-		xdotool mousemove 200 50
+		xdotool mousemove 400 100
 		xdotool click 1
-	 xdotool mousemove 125 50
+	 xdotool mousemove 250 100
 	 xdotool click 1
 	 i3 focus right
 end
