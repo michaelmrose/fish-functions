f8814e1daa2acf19710883a0862f2786f8da5af1
Michael Rose <michael@rosenetwork.net>
2021-05-22 12:06:26 -0700

    editing clipboard-to-note.fish

---
+++ clipboard-to-note.fish
@@ -0,0 +1,10 @@
+# Defined in /usr/home/michael/.config/fish/buffer/clipboard-to-note.fish @ line 1
+function clipboard-to-note
+		set uid (uid)
+		set dest/usr/home/michael/org/roam/images/$uid.png
+
+		xclip -o -selection clipboard -target image/png > /tmp/$uid.png
+		convert /tmp/$uid.png -resize 50% $dest
+		echo "[[$dest]]"|clipboard
+		rm /tmp/$uid.png
+end
