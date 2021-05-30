517b520cf7b139966404f71f35788dc9958b3d15
Michael Rose <michael@rosenetwork.net>
2021-05-29 23:00:38 -0700

    editing mountdroid.fish

--- mountdroid.fish
+++ mountdroid.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mountdroid.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/mountdroid_mountuser.fish @ line 2
 function mountdroid
 		s mount -t exfat -o umask=0022,gid=(id -g),uid=(id -u) $argv
 end
