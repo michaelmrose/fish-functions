4f19658c1de6f91b36956377a573abac0e04959e
Michael Rose <michael@rosenetwork.net>
2021-05-31 13:29:18 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -1,8 +1,16 @@
 # Defined in /usr/home/michael/.config/fish/buffer/m.fish @ line 2
 function m
 		if exists $argv
-				videos.clj $argv
+				switch $argv[1]
+						case recent
+								select (cat ~/playlists/recent.m3u)|all o
+						case playlists
+								p ~/playlists/mpv/*|select|each mpv
+						case '*'
+								videos.clj $argv
+				end
 		else
-				select (cat ~/playlists/recent.m3u)|all o
+				# select (cat ~/playlists/recent.m3u)|all o
+				p ~/playlists/mpv/*|select|each mpv
 		end
 end
