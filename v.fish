e6246d18e01c707bd17b1cd0324c82865ab9d897
Michael Rose <michael@rosenetwork.net>
2021-05-31 13:17:46 -0700

    removed v.fish

--- v.fish
+++
@@ -1,5 +0,0 @@
-# Defined in /usr/home/michael/.config/fish/buffer/m_pl_v_vids.fish @ line 10
-function v
-	# find-video $argv | select | all mpv &
-    select (cat ~/playlists/recent.m3u)|all o
-end
