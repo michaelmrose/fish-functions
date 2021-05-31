d7c150a15caf99fc38932269f2c1f156315c34e2
Michael Rose <michael@rosenetwork.net>
2021-05-31 13:40:55 -0700

    editing m.fish

--- m.fish
+++ m.fish
@@ -1,11 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/m.fish @ line 2
 function m
-		switch $argv[1]
-				case recent
-						select (cat ~/playlists/recent.m3u)|all o
-				case playlists
-						p ~/playlists/mpv/*|select|each mpv
-				case '*'
-						videos.clj $argv
-		end
+		videos.clj $argv
 end
