c990e95a89544038078da5c5ce11bd4b08ac88fe
Michael Rose <michael@rosenetwork.net>
2021-06-08 13:17:25 -0700

    editing toggle-side-gaps.fish

--- toggle-side-gaps.fish
+++ toggle-side-gaps.fish
@@ -1,10 +1,11 @@
-# Defined in /home/michael/.config/fish/buffer/toggle-side-gaps.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/toggle-side-gaps.fish @ line 2
 function toggle-side-gaps
     set width (window-width)
-    if test $width -lt 1300
+    if test $width -lt 3700
         i3 gaps horizontal current set 5
     else
-        set size (math (math $width - 1200 ) / 2)
-        i3 gaps horizontal current set $size
+        # set size (math (math $width - 1200 ) / 2)
+        i3 gaps horizontal current set 300
     end
+
 end
