ccf2a5f63b54f21d04ea56c4fc0dcd34d9b55c43
Michael Rose <michael@rosenetwork.net>
2021-06-08 11:19:49 -0700

    editing pics.fish

--- pics.fish
+++ pics.fish
@@ -1,16 +1,12 @@
 # Defined in /usr/home/michael/.config/fish/buffer/pics.fish @ line 2
 function pics
-		set arguments $argv
-		# while read -l line
-		# 		set arguments $arguments $line
-		# end
-		if exists $arguments[1]
-				if test -d $arguments[1]
-						set target $arguments[1]
-				else if test -f $arguments
-						sxiv -bo $arguments
+		if exists $argv[1]
+				if test -d $argv[1]
+						set target $argv[1]
+				else if test -f $argv
+						sxiv -bo $argv
 				else
-						set target (get-folder-for-backgrounds $arguments[1])
+						set target (get-folder-for-backgrounds $argv[1])
 				end
 		else
 				pics (pwd)
@@ -18,7 +14,7 @@ function pics
 		# set pictures (findall-list dirs=$target types=jpg)
 		
 		#write something more complicated later
-		switch (count $arguments)
+		switch (count $argv)
 				case 1
 						sxiv -tbfor $target 2> /dev/null
 				case 2
