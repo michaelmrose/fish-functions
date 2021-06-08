75eba5135573cd8487d225454f565186e82129d1
Michael Rose <michael@rosenetwork.net>
2021-06-08 11:32:21 -0700

    editing pics.fish

--- pics.fish
+++ pics.fish
@@ -1,10 +1,15 @@
 # Defined in /usr/home/michael/.config/fish/buffer/pics.fish @ line 2
 function pics
+		# rewrite this whole thing is garbage
 		if exists $argv[1]
 				if test -d $argv[1]
 						set target $argv[1]
 				else if test -f $argv[1]
-						sxiv -tbfor $argv
+						if test (count $argv) -gt 1
+								sxiv -tbfor $argv
+						else
+								sxiv $argv
+						end
 				else
 						set target (get-folder-for-backgrounds $argv[1])
 				end
