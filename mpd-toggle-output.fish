f1c31fe820bfe9f2b2277c1e6541c29c404424e8
Michael Rose <michael@rosenetwork.net>
2021-06-13 14:03:49 -0700

    editing mpd-toggle-output.fish

--- mpd-toggle-output.fish
+++ mpd-toggle-output.fish
@@ -1,5 +1,11 @@
-# Defined in /usr/home/michael/.config/fish/buffer/mpd-toggle-output.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/mpd-toggle-output.fish @ line 2
 function mpd-toggle-output
-		mpc toggleoutput http local
+		# mpc toggleoutput http local
+		set enabled (mpc outputs|g enabled|choose -f '\(|\)' 1)
+		if contains http $enabled
+				mpc enable only local
+		else
+				mpc enable only http
+		end
 		signal-i3blocks 4
 end
