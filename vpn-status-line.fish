67b02963acda2062c2255f5afc7efb9addf4134a
Michael Rose <michael@rosenetwork.net>
2021-06-15 22:27:26 -0700

    editing vpn-status-line.fish

--- vpn-status-line.fish
+++ vpn-status-line.fish
@@ -1,10 +1,13 @@
+# Defined in /usr/home/michael/.config/fish/buffer/vpn-status-line.fish @ line 2
 function vpn-status-line
-	set vpnstatus (vpn show)
-    switch $vpnstatus
-        case none
-            set symbol  
-        case "*"
-            set symbol  
-    end
-    echo $symbol"  "$vpnstatus
+		set vpnstatus (vpn show)
+		switch $vpnstatus
+				case none
+						# set symbol  
+						set symbol 🔓
+				case "*"
+						# set symbol  
+						set symbol 🔒
+		end
+		echo $symbol"  "$vpnstatus
 end
