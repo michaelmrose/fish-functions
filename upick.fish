df22ee2e135ec6e19d98f3644a33881224fd4e55
Michael Rose <michael@rosenetwork.net>
2021-06-27 17:05:27 -0700

    editing upick.fish

--- upick.fish
+++ upick.fish
@@ -1,5 +1,5 @@
 # Defined in /usr/home/michael/.config/fish/buffer/upick.fish @ line 2
 function upick
 	cd /usr/home/michael/extproj/unipicker
-	./unipicker --command "rofi -dmenu"|xargs xdotool type
+	./unipicker --command "rofi -dmenu -m -1"|xargs xdotool type
 end
