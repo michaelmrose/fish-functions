b2d03085ad99cd935153dcb7c116f91bfb940a07
Michael Rose <michael@rosenetwork.net>
2021-07-01 00:42:47 -0700

    editing playtime-status.fish

--- playtime-status.fish
+++ playtime-status.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/playtime-status.fish @ line 1
+# Defined in /usr/home/michael/.config/fish/buffer/playtime-status.fish @ line 2
 function playtime-status
-	mpc status |condense_lines | cut -d\# -f2-|nth 2
+	mpc current -f %time%
 end
