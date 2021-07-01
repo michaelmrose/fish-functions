4a9b9b451253c705954e99754ddb7b2544e83577
Michael Rose <michael@rosenetwork.net>
2021-06-30 21:38:58 -0700

    editing dolphin-pick-with-z.fish

--- dolphin-pick-with-z.fish
+++ dolphin-pick-with-z.fish
@@ -1,6 +1,5 @@
 # Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path_dolphin-open.fish @ line 9
 function dolphin-pick-with-z
 	set pid (xprop -id (xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
-	kitty -1 -e ff zquery
-	dolphin-open-path $pid (cat /tmp/zoxideres)
+	kitty -1 -e ff zquery $pid
 end
