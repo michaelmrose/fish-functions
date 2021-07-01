5a7931db0e803b28e0879e1f1625844257e694e1
Michael Rose <michael@rosenetwork.net>
2021-06-30 21:38:58 -0700

    editing zquery.fish

--- zquery.fish
+++ zquery.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path_dolphin-open.fish @ line 13
+# Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path_dolphin-open.fish @ line 15
 function zquery
-	zoxide query -i > /tmp/zoxideres
+	zoxide query -i| dolphin-open-path $argv[1]
 end
