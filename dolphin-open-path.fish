aa634e6d8f1629944590ef442180d7ce9d1618b7
Michael Rose <michael@rosenetwork.net>
2021-06-30 21:24:11 -0700

    editing dolphin-open-path.fish

--- dolphin-open-path.fish
+++ dolphin-open-path.fish
@@ -1,5 +1,4 @@
 # Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path.fish @ line 2
 function dolphin-open-path
-	set pid (xprop -id (xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$') 
-	qdbus org.kde.dolphin-$pid /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://$argv false
+	qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://$argv[2..-1] false
 end
