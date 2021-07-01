0f1fb30871a0606ec78fdb25e60672ed6cd09c04
Michael Rose <michael@rosenetwork.net>
2021-06-30 21:26:37 -0700

    editing dolphin-open-path.fish

--- dolphin-open-path.fish
+++ dolphin-open-path.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path_dolphin-open.fish @ line 2
 function dolphin-open-path
 	qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://$argv[2..-1] false
 end
