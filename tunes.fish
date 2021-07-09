7761af3e8b3637ce80496605332216915a44b818
Michael Rose <michael@rosenetwork.net>
2021-07-08 23:44:27 -0700

    editing tunes.fish

--- tunes.fish
+++ tunes.fish
@@ -2,7 +2,8 @@
 function tunes
 		i3 splitv
 		emf ecn mmr/tunes &
-		kitty -1 -e cava &
-		sleep 0.5
+		# kitty -1 -e cava &
+		# sleep 0.5
+		starton (get-focused-workspace) cava-fn kitty
 		i3-msg resize shrink up 400px
 end
