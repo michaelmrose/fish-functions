7bbac2d3a08ce562bba8e1505ea40fab4f61f6c2
Michael Rose <michael@rosenetwork.net>
2021-07-09 20:35:49 -0700

    editing tunes.fish

--- tunes.fish
+++ tunes.fish
@@ -1,11 +1,6 @@
 # Defined in /usr/home/michael/.config/fish/buffer/tunes.fish @ line 2
 function tunes
-		# i3 splitv
-		# emf ecn mmr/tunes &
-		# # kitty -1 -e cava &
-		# starton (get-focused-workspace) cava-fn kitty
-		# sleep 0.5
-		# i3-msg resize shrink up 400px
+		i3 splitv
 		i3-msg append_layout ~/.config/i3/layouts/tunes.json
 		emf ecn mmr/tunes &
 		cava-fn &
