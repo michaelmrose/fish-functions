e97e17eff78fd6ee26b9fd00c0a5dc6169c99c74
Michael Rose <michael@rosenetwork.net>
2021-07-17 22:14:21 -0700

    editing startjack.fish

--- startjack.fish
+++ startjack.fish
@@ -1,7 +1,7 @@
-# Defined in /usr/home/michael/.config/fish/buffer/startjack_stopjack.fish @ line 2
+# Defined in /usr/home/michael/.config/fish/buffer/audacity-jack_startjack_stopja.fish @ line 10
 function startjack
 	stop sndiod
 	sleep 1
-	jackd -R -d alsa --device=hw:(cat /etc/sv/sndiod/card),0
+	jackd -R -d alsa --device=hw:(cat /etc/sv/sndiod/card),0 &
 
 end
