f463af65b0ddd8e94b315aa5e4eb3285017b4359
Michael Rose <michael@rosenetwork.net>
2021-07-24 14:56:57 -0700

    editing player-status.fish

--- player-status.fish
+++ player-status.fish
@@ -1,7 +1,7 @@
 # Defined in /usr/home/michael/.config/fish/buffer/player-status.fish @ line 2
 function player-status
 	if string match Playing (playerctl status -p firefox) > /dev/null
-		playerctl metadata title
+		i3status-rs-colorize Info (playerctl metadata title)
 	else
 		mpd-status-line.clj
 	end
