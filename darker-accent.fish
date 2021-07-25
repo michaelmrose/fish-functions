21930204abea37c0023b116fefb2594e48beda4e
Michael Rose <michael@rosenetwork.net>
2021-07-24 20:41:22 -0700

    editing darker-accent.fish

--- darker-accent.fish
+++ darker-accent.fish
@@ -1,4 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/compute-brightness_darker-acce.fish @ line 11
+# Defined in /usr/home/michael/.config/fish/buffer/compute-brightness_darker-acce.fish @ line 12
 function darker-accent
 		set accent (jq -r .colors.color3 ~/.cache/wal/colors.json)
 		sd $accent \#(darkerhex $accent) ~/.cache/wal/colors.json
