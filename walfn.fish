602a781ef8689dd2ba781fec581d651bf0fba9c2
Michael Rose <michael@rosenetwork.net>
2021-07-24 20:49:53 -0700

    editing walfn.fish

--- walfn.fish
+++ walfn.fish
@@ -1,31 +1,38 @@
 # Defined in /usr/home/michael/.config/fish/buffer/walfn.fish @ line 2
 function walfn
 
-if test (count $bgimage) -gt 1
+	if test (count $bgimage) -gt 1
 		montage $bgimage[1] $bgimage[2] -geometry +0+0 /tmp/montage.jpg
 		set target /tmp/montage.jpg
-else
+	else
 		set target $bgimage
-end
+	end
 
-wal -n --saturate 0.7 -i $target
+	wal -n --saturate 0.7 -i $target
 
-if exists $argv
+	if exists $argv
 		set accent $argv[1]
 		set fg $argv[2]
 		set bg $argv[3]
-else
+	else
 		set accent (jq -r .colors.color3 ~/.cache/wal/colors.json)
+		set oldaccent $accent
+		set accent \#(darkerhex $accent)
+		if test (compute-brightness $accent) -gt 100
+			# sd $accent \#(darkerhex $accent) ~/.cache/wal/colors.json
+			sd $oldaccent \#$accent ~/.cache/wal/colors.json
+		end
 		set fg (jq -r .colors.color0 ~/.cache/wal/colors.json)
 		set bg (jq -r .colors.color15 ~/.cache/wal/colors.json)
 		set stat (jq -r .colors.color0 ~/.cache/wal/colors.json)
-end
-wali3status-rs $accent $stat
-replace-color-in-i3 $accent
-replace-color-in-i3 $accent
-colorize-emacs $accent $fg $bg
-restart-i3
-sleep 1
-pywalfox update
-pywalzathura
+	end
+
+	wali3status-rs $accent $stat
+	replace-color-in-i3 $accent
+	replace-color-in-i3 $accent
+	colorize-emacs $accent $fg $bg
+	restart-i3
+	sleep 1
+	pywalfox update
+	pywalzathura
 end
