89a999d85b7823238027afd87b02ae596c544811
Michael Rose <michael@rosenetwork.net>
2021-07-24 20:41:57 -0700

    editing compute-brightness.fish

--- compute-brightness.fish
+++ compute-brightness.fish
@@ -1,9 +1,9 @@
 # Defined in /usr/home/michael/.config/fish/buffer/compute-brightness_darker-acce.fish @ line 2
 function compute-brightness
 	set color $argv
-	set rc (math 0.7 \* (hextodec (echo $color | cut -c2-3)))
-	set gc (math 0.2 \*(hextodec (echo $color | cut -c4-5)))
-	set bc (math 0.07 \*(hextodec (echo $color | cut -c6-7)))
+	set rc (math 0.7 \* (hextodec 0x(echo $color | cut -c2-3)))
+	set gc (math 0.2 \*(hextodec 0x(echo $color | cut -c4-5)))
+	set bc (math 0.07 \*(hextodec 0x(echo $color | cut -c6-7)))
 	echo values are $rc $gc $bc
 	math $rc + $gc + $bc
 end
