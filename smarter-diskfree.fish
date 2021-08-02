21ee36ba7c5da41fb74d7512cfd774ffafa3cd40
Michael Rose <michael@rosenetwork.net>
2021-08-01 19:42:49 -0700

    editing smarter-diskfree.fish

--- smarter-diskfree.fish
+++ smarter-diskfree.fish
@@ -1,7 +1,7 @@
 # Defined in /usr/home/michael/.config/fish/buffer/smarter-diskfree.fish @ line 2
 function smarter-diskfree
 	if mount| awk '{print $3}' | grep $argv > /dev/null
-		df -h $argv|n l2 n3
+		echo (df -h $argv)[2]|awk '{print $2}'
 	else
 		return 1
 	end
