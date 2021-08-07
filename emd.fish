f5ded9e7bb707ec1433bc2aaff375a21fff59c1c
Michael Rose <michael@rosenetwork.net>
2021-08-06 21:41:23 -0700

    editing emd.fish

--- emd.fish
+++ emd.fish
@@ -1,9 +1,10 @@
 # Defined in /usr/home/michael/.config/fish/buffer/emd.fish @ line 2
 function emd
-emacs --bg-daemon
-sleep 3
-	emf ecn mmr/after-initial-setup &
-for p in (pgrep emacs)
-	mullvad split-tunnel pid add $p
+	emacs --bg-daemon
+	sleep 3
+	emf e mmr/after-initial-setup &
+	for p in (pgrep emacs)
+		mullvad split-tunnel pid add $p
 	end
+	emacsclient -c &
 end
