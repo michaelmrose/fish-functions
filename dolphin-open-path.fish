d5edc4893fd9f23982cedc8522a4b58bb6ab1fa1
Michael Rose <michael@rosenetwork.net>
2021-08-13 21:17:03 -0700

    editing dolphin-open-path.fish

--- dolphin-open-path.fish
+++ dolphin-open-path.fish
@@ -3,7 +3,9 @@ function dolphin-open-path
 	while read -l line
 		# qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://$line false
 		qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1/actions/replace_location org.qtproject.Qt.QAction.trigger
+		sleep 0.1
 		xdotool type $line
+		sleep 0.1
 		xdotool key Return
 
 	end
