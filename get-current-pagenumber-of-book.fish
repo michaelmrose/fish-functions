d51813d4582c697fcf258d5c08fbda57edd4a0d5
Michael Rose <michael@rosenetwork.net>
2021-08-14 20:28:41 -0700

    editing get-current-pagenumber-of-book.fish

--- get-current-pagenumber-of-book.fish
+++ get-current-pagenumber-of-book.fish
@@ -1,5 +1,5 @@
 function get-current-pagenumber-of-book
 set pid (xprop -id (xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
-qdbus org.pwmt.zathura.PID-$pid /org/pwmt/zathura org.pwmt.zathura.pagenumber
+increase (qdbus org.pwmt.zathura.PID-$pid /org/pwmt/zathura org.pwmt.zathura.pagenumber)
 
 end
