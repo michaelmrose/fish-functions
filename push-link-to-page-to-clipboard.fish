6d068c96270931a87bfca9e6b12d181f2f91a450
Michael Rose <michael@rosenetwork.net>
2021-08-14 22:08:10 -0700

    editing push-link-to-page-to-clipboard.fish

---
+++ push-link-to-page-to-clipboard.fish
@@ -0,0 +1,6 @@
+function push-link-to-page-to-clipboard
+	set pid (xprop -id (xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
+	set page (increase (qdbus org.pwmt.zathura.PID-$pid /org/pwmt/zathura org.pwmt.zathura.pagenumber))
+	set document (qdbus org.pwmt.zathura.PID-$pid /org/pwmt/zathura org.pwmt.zathura.filename)
+	set title (basename $document)
+end
