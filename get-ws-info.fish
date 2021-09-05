2cfb874d28e0463e2c25962792b24d3762adee91
Michael Rose <michael@rosenetwork.net>
2021-09-05 11:14:38 -0700

    editing get-ws-info.fish

--- get-ws-info.fish
+++ get-ws-info.fish
@@ -1,4 +1,3 @@
-# Defined in /home/michael/.config/fish/buffer/get-ws-info.fish @ line 2
 function get-ws-info
 	set desired $argv[2]
     set var $argv[4]
@@ -7,9 +6,6 @@ function get-ws-info
     if contains $desired x y width height
         set desired rect.$desired
     end
-    i3-msg -t get_workspaces|jq .[]|jq -r "select(.$var == $val).$desired"
-    # i3-msg -t get_workspaces|jq ".[]| select(.$var == $val).$desired"
-    # set com 'i3-msg -t get_workspaces|jq \'.[]| select(.$var == $val).$desired\''
-    # eval $com
-    # eval i3-msg -t get_workspaces|jq '.[]| select(.focused == true).name'
+    # i3-msg -t get_workspaces|jq .[]|jq -r "select(.$var == $val).$desired"
+    i3-msg -t get_workspaces|jq -r  '.[]|select(.$var == $val).$desired'
 end
