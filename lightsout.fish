c243a5eb41cba58064f39417680eadf1b4563647
Michael Rose <michael@rosenetwork.net>
2021-09-26 20:30:54 -0700

    editing lightsout.fish

--- lightsout.fish
+++ lightsout.fish
@@ -1,6 +1,5 @@
-# Defined in /home/michael/.config/fish/buffer/cleanup-desktop_lightsout_rboo.fish @ line 14
 function lightsout
-	sleep 0.5
+	sleep 2
     xset dpms force off
 	  # while true
     #     if xset -q |g 'monitor is on' > /dev/null
