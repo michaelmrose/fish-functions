915fbfd1f229f814245c0cfc58db98a153cb77ab
Michael Rose <michael@rosenetwork.net>
2021-10-10 11:43:39 -0700

    editing menu.fish

--- menu.fish
+++ menu.fish
@@ -1,6 +1,6 @@
 function menu
 	if test (count $argv) -gt 1
-	    p $argv[2..-1] | rofi -dmenu -i -lines (min (decrease (count $argv)) 20) -no-fixed-num-lines -p $argv[1] -m -1
+	    p $argv[2..-1] | rofi -dmenu -i -lines (min (decrease (count $argv)) 20) -no-fixed-num-lines -location 2 -p $argv[1] -m -1
   else
         while read -l line
             set acc $acc $line
