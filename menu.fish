ce17482d7f12f9f9ff6ebe5576ceed8de6d7b06a
Michael Rose <michael@rosenetwork.net>
2021-10-10 11:43:24 -0700

    editing menu.fish

--- menu.fish
+++ menu.fish
@@ -1,7 +1,6 @@
-# Defined in /usr/home/michael/.config/fish/buffer/dolphin-open-path_dolphin-open.fish @ line 2
 function menu
 	if test (count $argv) -gt 1
-	    p $argv[2..-1] | rofi -dmenu -i -lines (min (decrease (count $argv)) 20) -p $argv[1] -m -1
+	    p $argv[2..-1] | rofi -dmenu -i -lines (min (decrease (count $argv)) 20) -no-fixed-num-lines -p $argv[1] -m -1
   else
         while read -l line
             set acc $acc $line
