e2ad72551e53dd54534314efe675c1d7d6b52834
Michael Rose <michael@rosenetwork.net>
2021-10-23 13:37:57 -0700

    editing unread-mail-count.fish

--- unread-mail-count.fish
+++ unread-mail-count.fish
@@ -1,4 +1,3 @@
-# Defined in /usr/home/michael/.config/fish/buffer/unread-mail-count.fish @ line 2
 function unread-mail-count
-		mu find flag:unread and date:7d..now and not maildir:/OldGmail/techmail 2>/dev/null|wc -l
+		mu find flag:unread and date:3d..now and not maildir:/OldGmail/techmail 2>/dev/null|wc -l
 end
