78f3f235f31bcb2d9d3e316c5ad078947f6f0bf2
Michael Rose <michael@rosenetwork.net>
2021-10-23 13:38:14 -0700

    editing unread-mail-count.fish

--- unread-mail-count.fish
+++ unread-mail-count.fish
@@ -1,3 +1,3 @@
 function unread-mail-count
-		mu find flag:unread and date:3d..now and not maildir:/OldGmail/techmail 2>/dev/null|wc -l
+		mu find flag:unread and date:1d..now and not maildir:/OldGmail/techmail 2>/dev/null|wc -l
 end
