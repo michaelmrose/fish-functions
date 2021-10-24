dbf5fb80b5afd8197e4f05a2913bfc1565319426
Michael Rose <michael@rosenetwork.net>
2021-10-23 18:02:44 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -1,4 +1,3 @@
-# Defined in /usr/home/michael/.config/fish/buffer/tdown.fish @ line 2
 function tdown
 		set info (prompt 'title @ duration')
 		if not exists $info
@@ -8,6 +7,8 @@ function tdown
 		set title  (echo $info | cut -d @ -f1| trim)
 		set duration (echo $info | cut -d @ -f2 | trim)
 		eval kitty -o font_size=100 -e termdown --no-figlet -v en-us -T (quote $title) $duration
-		echo $title completed at (date) > /tmp/timers-complete.txt
-		mpv --no-video ~/sounds/alarm.m4a
+		if [ $status = 0 ]
+			echo $title completed at (date) > /tmp/timers-complete.txt
+			mpv --no-video ~/sounds/alarm.m4a
+		end
 end
