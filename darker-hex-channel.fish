f7cf3ccccad7f076eda84e84598bfacb75b096e3
Michael Rose <michael@rosenetwork.net>
2021-10-23 20:30:19 -0700

    editing darker-hex-channel.fish

--- darker-hex-channel.fish
+++ darker-hex-channel.fish
@@ -1,5 +1,4 @@
-# Defined in /usr/home/michael/.config/fish/buffer/darker-hex-channel.fish @ line 2
 function darker-hex-channel
 		# echo (math  3 * (math (hextodec 0x$argv) / 4))
-		dectohex (math 1 \* (math (hextodec 0x$argv) /  2))| cut -c 4-
+		printf "%02d" (dectohex (math 1 \* (math (hextodec 0x$argv) /  2))| cut -c 4-)
 end
