4524aff1f324c791ae7242072a51525ec2e416d9
Michael Rose <michael@rosenetwork.net>
2021-11-15 12:02:17 -0800

    editing focus-pane-or-window.fish

---
+++ focus-pane-or-window.fish
@@ -0,0 +1,12 @@
+function focus-pane-or-window
+    switch (window-class)
+	case Emacs
+	    if emf e windmove-$argv
+		nil
+	    else
+		i3-msg focus $argv
+	    end
+	case '.*'
+	    i3-msg focus $argv
+    end
+end
