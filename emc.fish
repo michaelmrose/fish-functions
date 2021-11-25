564d5d37346b05263a6ef2ee4b7dd04468f56dcf
Michael Rose <michael@rosenetwork.net>
2021-11-25 14:49:53 -0800

    editing emc.fish

--- emc.fish
+++ emc.fish
@@ -1,13 +1,7 @@
 function emc
-    emacsclient -cn $argv &
-    sleep 3
     emf e mmr/after-initial-setup &
     for p in (pgrep emacs)
 	mullvad split-tunnel pid add $p
     end
-    # if not emacsclient -c $argv
-    #     msg daemon not running... starting now
-    #     emd
-    #     emacsclient -c $argv
-    # end
+    emacsclient -cn $argv &
 end
