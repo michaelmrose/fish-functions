726a070a85752b021dc9f41c2a5126cbfaf2146f
Michael Rose <michael@rosenetwork.net>
2021-12-08 20:02:07 -0800

    editing ebook-open.fish

--- ebook-open.fish
+++ ebook-open.fish
@@ -1,4 +1,4 @@
 function ebook-open
-    zathura $argv &
-    java -jar ~/proj/clojure/rdr/target/rdr-0.1.0-SNAPSHOT-standalone.jar -a $argv &
+    zathura "$argv" &
+    java -jar ~/proj/clojure/rdr/target/rdr-0.1.0-SNAPSHOT-standalone.jar -a "$argv" &
 end
