987762acb1de0e54286b2e08d81ca33d6c5636dd
Michael Rose <michael@rosenetwork.net>
2022-01-13 22:14:50 -0800

    removed fuckingdbus.fish

--- fuckingdbus.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /usr/home/michael/.config/fish/buffer/emd_fuckingdbus.fish @ line 10
-function fuckingdbus
-    set dbusvals (dbus-launch|sd " " |choose -f = 1:)
-    set -x DBUS_SESSION_BUS_ADDRESS $dbusvals[1]
-    set -x DBUS_SESSION_BUS_PID $dbusvals[2]
-end
