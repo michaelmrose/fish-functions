f86be228045ffdb86b1728d39703df40cad8e941
Michael Rose <michael@rosenetwork.net>
2022-01-25 21:05:10 -0800

    editing toggle-side-gaps.fish

--- toggle-side-gaps.fish
+++ toggle-side-gaps.fish
@@ -1,11 +1,13 @@
-# Defined in /usr/home/michael/.config/fish/buffer/toggle-side-gaps.fish @ line 2
 function toggle-side-gaps
     set width (window-width)
-    if test $width -lt 3700
+    set monitor_width (get-focused-display-resolution)
+    set target_width (math $monitor_width - 140)
+    set gap_width (math $monitor_with / 12.8)
+    if test $width -lt $target_width
         i3 gaps horizontal current set 0
     else
         # set size (math (math $width - 1200 ) / 2)
-        i3 gaps horizontal current set 300
+        i3 gaps horizontal current set $gap_width
     end
 
 end
