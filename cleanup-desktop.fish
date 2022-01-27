9a00d45debe24cede6c8d5240b24b16ed1ce6f35
Michael Rose <michael@rosenetwork.net>
2022-01-27 14:46:44 -0800

    editing cleanup-desktop.fish

--- cleanup-desktop.fish
+++ cleanup-desktop.fish
@@ -2,12 +2,6 @@ function cleanup-desktop
     for service in ~/service/enabled/*
 	stop $service
     end
-	sudo umount -fl /mnt/michael
-	  kill (pgrep emacs)
-    kill (pgrep zeal)
-    sleep 1
-    rm /tmp/ZealLocalServer
-    sleep 1
-	  i3-msg [class="."] kill
+    i3-msg [class="."] kill
     countdown 3
 end
