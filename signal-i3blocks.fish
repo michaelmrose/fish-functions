f3b0e5790ee789eace20d45e3b6ede566a32ed1d
Michael Rose <michael@rosenetwork.net>
2022-01-29 18:31:20 -0800

    editing signal-i3blocks.fish

--- signal-i3blocks.fish
+++ signal-i3blocks.fish
@@ -26,6 +26,6 @@ function signal-i3blocks
             set val $argv
     end
     # pkill -RTMIN+$val i3blocks
-    pkill -SIGRTMIN+$val i3status-rs
+    # pkill -SIGRTMIN+$val i3status-rs
     pkill -SIGRTMIN+$val swaystatus
 end
