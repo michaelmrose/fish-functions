12e4ac567712b6a994829b3bd3f216765a9ec635
Michael Rose <michael@rosenetwork.net>
2022-02-19 15:38:47 -0800

    editing kitty-zoxide-query.fish

--- kitty-zoxide-query.fish
+++ kitty-zoxide-query.fish
@@ -1,5 +1,5 @@
 function kitty-zoxide-query
 	set pid (xprop -id (xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
-	kitty -1 -e "zoxide query -i| dolphin-open-path $pid"
+	kitty -1 -e fish -c "zoxide query -i| dolphin-open-path $pid"
 
 end
