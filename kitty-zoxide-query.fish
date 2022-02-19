341bfce113c55253d5d8cf9cbe7573ad34945ca3
Michael Rose <michael@rosenetwork.net>
2022-02-19 15:34:33 -0800

    editing kitty-zoxide-query.fish

--- kitty-zoxide-query.fish
+++ kitty-zoxide-query.fish
@@ -1,3 +1,3 @@
 function kitty-zoxide-query
-	qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://(zoxide query -i) false
+	kitty -1 ff qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://(zoxide query -i) false
 end
