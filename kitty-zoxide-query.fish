ca7994a211fce292273cb5ca7e3081fc36365b12
Michael Rose <michael@rosenetwork.net>
2022-02-19 15:41:30 -0800

    editing kitty-zoxide-query.fish

--- kitty-zoxide-query.fish
+++ kitty-zoxide-query.fish
@@ -1,5 +1,5 @@
 function kitty-zoxide-query
 	set pid (xprop -id (xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
-	kitty -1 -e fish -c "zoxide query -i| dolphin-open-path $pid"
+	kitty -1 -e fish -c "qdbus org.kde.dolphin-$pid /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://(zoxide query -i) false"
 
 end
