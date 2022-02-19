e7f6eda8aebe286b20b22f44fa21884867fe9d46
Michael Rose <michael@rosenetwork.net>
2022-02-19 15:30:01 -0800

    editing kitty-pick-with-z.fish

--- kitty-pick-with-z.fish
+++ kitty-pick-with-z.fish
@@ -1,10 +1,10 @@
-# Defined in /usr/home/michael/.config/fish/buffer/kitty-pick-with-z.fish @ line 2
 function kitty-pick-with-z
-if in-terminal
-		set dir (zoxide query -i)
-		# spacefm -s set current_dir "$dir"
-		dolphin-open-path $dir
-else
-kitty -1 ff kitty-pick-with-z
-end
+    if in-terminal
+	set dir (zoxide query -i)
+	# spacefm -s set current_dir "$dir"
+	# dolphin-open-path $dir
+	qdbus org.kde.dolphin-$argv[1] /dolphin/Dolphin_1 org.kde.dolphin.MainWindow.openDirectories file://$dir false
+    else
+	kitty -1 ff kitty-pick-with-z
+    end
 end
