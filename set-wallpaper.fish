4c9546417f69a8b1c7984b70bc8121cfc457d63c
Michael Rose <michael@rosenetwork.net>
2022-04-12 03:06:29 -0700

    editing set-wallpaper.fish

--- set-wallpaper.fish
+++ set-wallpaper.fish
@@ -1,4 +1,3 @@
-# Defined in /usr/home/michael/.config/fish/buffer/get-feh-format_set-wallpaper.fish @ line 18
 function set-wallpaper
 		set img (pathof $argv[1])
 		set format (get-feh-format $img)
@@ -7,6 +6,6 @@ function set-wallpaper
 		set -U bgstyle (cutlastn "/" 2 $img)
 		set-lightdm-bg $img
 		set -U bgimage $img
-		walfn
-		signal-i3blocks 12
+        xrdb ~/.cache/wal/colors-i3.conf
+        restart-i3
 end
