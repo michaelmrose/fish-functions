755e32f9be091a6e341008ef49ebf948dc1d36ca
Michael Rose <michael@rosenetwork.net>
2022-05-02 18:34:37 -0700

    editing time-to-new-window.fish

--- time-to-new-window.fish
+++ time-to-new-window.fish
@@ -1,6 +1,6 @@
 function time-to-new-window
     set num_windows (wmctrl -l|wc -l)
-    fish -c $argv > /dev/null &
+    fish -c "$argv" > /dev/null &
     while test $num_windows -eq (wmctrl -l|wc -l)
     end
 end
