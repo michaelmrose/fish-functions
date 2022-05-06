e5dcdfca417c05ebb16679ec8ba75269c89cfe91
Michael Rose <michael@rosenetwork.net>
2022-05-05 23:01:30 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,19 +1,7 @@
-# Defined in /usr/home/michael/.config/fish/buffer/fish_prompt.fish @ line 2
 function fish_prompt
-	# if not timeout 0.5 /opt/bin/powerlineshell.py $status --shell bare ^/dev/null
-  #   echo (pwd) \$: 
-  # end
-  # echo $USER:\$
-	# /opt/bin/powerlineshell.py $status --shell bare ^/dev/null
-  
-  # timeout 0.5 powerline-rs --shell bare;or echo $USER:(pwd) \$
-  # timeout 0.25 f fish_prompt_git f fish_prompt_light
-  # timeout 0.25 powerline-rs --modules "user,host,cwd,ssh,jobs,perms,git,gitstage" --shell bare $status;or powerline-rs --shell bare --modules "user,host,cwd,ssh,jobs,perms" $status
-  # powerline-rs --modules "user,host,cwd,ssh,jobs,perms,git,gitstage" --shell bare $status
-  # powerline-rs --shell bare $status
-  # powerline-rs --modules user,host,cwd,ssh,jobs,perms,git,gitstage --shell bare $status
-  # powerline-rs --modules ssh,cwd,perms,git,gitstage,root --shell bare $status
-  timeout 0.25 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status
-  # powerline-rs --shell bare $status
-  # echo (pwd) '$ ' 
+		set myprompt ( timeout 0.25 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root,jobs --shell bare $status)
+		echo $myprompt ↷  
+➥" "
+		# echo ┏╸{$myprompt}
+		# echo ┗╸❯❯❯
 end
