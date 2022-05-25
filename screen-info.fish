ed9a00817348ac8295c1efec846caf1ef4a82ba2
Michael Rose <michael@rosenetwork.net>
2022-05-25 14:30:27 -0700

    editing screen-info.fish

--- screen-info.fish
+++ screen-info.fish
@@ -1,3 +1,4 @@
 function screen-info
-    p (xrandr --props|g $argv[1] -A20)[6..21]|trim|sed 's/\t|^ //g'|xxd -r -p|parse-edid
+    xrandr |g $argv
+    p (xrandr --props|g $argv[1] -A20)[6..21]|trim|sed 's/	|^ //g'|xxd -r -p|parse-edid
 end
