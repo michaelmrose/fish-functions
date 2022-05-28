e21464662c8009354ac3bdff6698d62d5ba194cb
Michael Rose <michael@rosenetwork.net>
2022-05-27 18:54:05 -0700

    editing share.fish

---
+++ share.fish
@@ -0,0 +1,10 @@
+function share
+    if test (count $argv) -eq 1
+	set sharepath $argv[1]
+	set options \'rw,insecure\'
+    else
+	set sharepath $argv[1]
+	set options \'$argv[2]\'
+    end
+    echo sudo zfs set sharenfs=$options $sharepath
+end
