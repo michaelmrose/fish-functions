a9cfe07569b7f188c8c1a358a6f15293e051cc9d
Michael Rose <michael@rosenetwork.net>
2022-05-31 10:48:42 -0700

    editing unfuck-emacs.fish

--- unfuck-emacs.fish
+++ unfuck-emacs.fish
@@ -1,5 +1,5 @@
 function unfuck-emacs
-    i3-msg floating enable
+    # i3-msg floating enable
     emf e unfuck-emacs-tab-bar-display
-    i3-msg floating disable
+    # i3-msg floating disable
 end
