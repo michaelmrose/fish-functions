59f774be1091ae5c36cfc00e1c3c6a2d22827229
Michael Rose <michael@rosenetwork.net>
2022-05-30 23:38:56 -0700

    editing animate-window-grow.fish

--- animate-window-grow.fish
+++ animate-window-grow.fish
@@ -2,6 +2,7 @@ function animate-window-grow
     # need to move window progressively up/left or do something to make this look better
     i3-msg floating enable
     sleep 0.1
+
     # set monitor (get-ws-info get output where focused is true)
     # switch $monitor
     # 	case HDMI-0
@@ -16,14 +17,12 @@ function animate-window-grow
     # 	end
     # set sizes 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
 
-    # set size 100
-    # while test $size -lt 
-    # 	i3-msg resize set width $size
-    # 	# i3-msg move left 200 px
-    # 	# i3-msg move up 200 px
-    # 	set size (math $size + 50)
-    # 	sleep 0.01
-    # end
+    set size 100
+    while test $size -lt 1500
+	i3-msg resize set width $size
+	set size (math $size + 50)
+	sleep 0.01
+    end
     i3-msg floating disable
 
 end
