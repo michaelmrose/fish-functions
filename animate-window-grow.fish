3fc6a93aed59ea10dfe28b7f46c6b35a96b1d806
Michael Rose <michael@rosenetwork.net>
2022-05-30 18:50:57 -0700

    editing animate-window-grow.fish

--- animate-window-grow.fish
+++ animate-window-grow.fish
@@ -1,11 +1,11 @@
 function animate-window-grow
-    # i3-msg floating enable
+    i3-msg floating enable
+    i3-msg floating disable
     set sizes 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
     for size in $sizes
 	i3-msg resize set height $size
 	i3-msg resize set width $size
 	sleep 0.001
     end
-    # i3-msg floating disable
 
 end
