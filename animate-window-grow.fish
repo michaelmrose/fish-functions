07826be5e30aad4e97ff692a7ba1c76f9175e349
Michael Rose <michael@rosenetwork.net>
2022-05-30 23:18:15 -0700

    editing animate-window-grow.fish

--- animate-window-grow.fish
+++ animate-window-grow.fish
@@ -15,13 +15,15 @@ function animate-window-grow
 	end
     set sizes 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
 
-    set target_width (math $total_width / 4)
-    set target_height (math $total_height / 2)
+    # set target_width (math $total_width / 4)
+    # set target_height (math $total_height / 2)
 
-    i3-msg resize set height $target_height
     set size 100
-    while test $size -lt $target_width
+    while test $size -lt $total_height
 	i3-msg resize set width $size
+	i3-msg resize set height $size
+	i3-msg move left 50 px
+	i3-msg move up 50 px
 	set size (math $size + 50)
 	sleep 0.0001
     end
