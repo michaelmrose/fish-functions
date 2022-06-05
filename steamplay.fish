122393d227feefe8e4478ba95f9774cca37fcf23
Michael Rose <michael@rosenetwork.net>
2022-06-05 15:46:14 -0700

    editing steamplay.fish

--- steamplay.fish
+++ steamplay.fish
@@ -1,3 +1,3 @@
 function steamplay
-    eval (steam srg Exec (rg steam ~/.local/share/applications/ -l|g $argv|select)|choose -f = 1:)
+    eval (rg Exec (rg steam ~/.local/share/applications/ -l|g $argv|select)|choose -f = 1:)
 end
