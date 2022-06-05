1c2900fc56f72822cb4f7ef5b75caf329a49604b
Michael Rose <michael@rosenetwork.net>
2022-06-05 15:50:55 -0700

    editing steamplay.fish

--- steamplay.fish
+++ steamplay.fish
@@ -1,3 +1,8 @@
 function steamplay
-    eval (rg Exec (rg steam ~/.local/share/applications/ -l|g $argv|select)|choose -f = 1:)
+    if exists $argv
+	eval (rg Exec (rg steam ~/.local/share/applications/ -l|g $argv|select)|choose -f = 1:)
+    else
+	# pick a steam game to play
+    end
+
 end
