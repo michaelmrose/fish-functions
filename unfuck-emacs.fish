902bee8a728ff3a1aab4241b3d9b0126d180e1c6
Michael Rose <michael@rosenetwork.net>
2022-06-05 20:27:04 -0700

    editing unfuck-emacs.fish

--- unfuck-emacs.fish
+++ unfuck-emacs.fish
@@ -1,7 +1,7 @@
 function unfuck-emacs
     sleep 0.2
-    i3-msg fullscreen enable
-    # emf en unfuck-emacs-tab-bar-display
+    # i3-msg fullscreen enable
+    emf en unfuck-emacs-tab-bar-display
     # sleep 0.01
     # i3-msg fullscreen disable
 end
