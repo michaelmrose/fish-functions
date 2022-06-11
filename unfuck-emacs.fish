ab4fa86735284b029fe92667df7cab221ef27a2d
Michael Rose <michael@rosenetwork.net>
2022-06-11 13:53:10 -0700

    editing unfuck-emacs.fish

--- unfuck-emacs.fish
+++ unfuck-emacs.fish
@@ -3,6 +3,7 @@ function unfuck-emacs
     # i3-msg fullscreen enable
     emf en unfuck-emacs-tab-bar-display
     i3-msg gaps inner current plus 5
+    sleep 0.1
     i3-msg gaps inner current minus 5
     sleep 0.01
     # i3-msg fullscreen disable
