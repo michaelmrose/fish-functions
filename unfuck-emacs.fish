7ef1e7530040f81530d599ca2bf7083a44aaefe8
Michael Rose <michael@rosenetwork.net>
2022-06-11 13:53:56 -0700

    editing unfuck-emacs.fish

--- unfuck-emacs.fish
+++ unfuck-emacs.fish
@@ -2,6 +2,6 @@ function unfuck-emacs
     sleep 0.2
     emf en unfuck-emacs-tab-bar-display
     i3-msg gaps inner current plus 5
-    sleep 0.01
+    sleep 0.03
     i3-msg gaps inner current minus 5
 end
