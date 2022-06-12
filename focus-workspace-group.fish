3b32cead4e77fade02e35540ae06f96e9d8aa99d
Michael Rose <michael@rosenetwork.net>
2022-06-11 20:59:25 -0700

    editing focus-workspace-group.fish

--- focus-workspace-group.fish
+++ focus-workspace-group.fish
@@ -3,5 +3,5 @@ function focus-workspace-group
     for w in $workspace_group_$n
 	i3-msg workpace $w
     end
-    i3-msg workspace $workspace-group_$n_focused
+    # i3-msg workspace $workspace-group_$n_focused
 end
