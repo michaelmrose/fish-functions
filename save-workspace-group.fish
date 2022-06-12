cacbe52981de363ee97f62a12488ca71906e7c00
Michael Rose <michael@rosenetwork.net>
2022-06-11 21:10:49 -0700

    editing save-workspace-group.fish

--- save-workspace-group.fish
+++ save-workspace-group.fish
@@ -4,5 +4,6 @@ function save-workspace-group
     set focused (get-ws-info get name where focused is true)
     set -U workspace_group_$n $workspaces
     set val workspace_group_$n_focused
+    echo val is $val
     echo set -U  $val $focused
 end
