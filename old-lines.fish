dc77fe5b502bd14797e74cb77bd9af52f35a5415
Michael Rose <michael@rosenetwork.net>
2022-06-12 11:54:14 -0700

    editing old-lines.fish

---
+++ old-lines.fish
@@ -0,0 +1,12 @@
+function old-lines
+	while read -l line
+      set acc $acc $line
+  end
+  set numbers (echo $argv | cut -d - -f1- --output-delimiter=" " | all explode-words)
+  
+  if test (count $numbers) -eq 2
+      p $acc | sed -n $numbers[1],$numbers[2]p
+  else
+      p $acc | sed -n $numbers[1]p
+  end
+end
