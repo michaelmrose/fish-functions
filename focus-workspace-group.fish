816bb34814168dffa8225f2b92b35fb4e0b82e91
Michael Rose <michael@rosenetwork.net>
2022-06-11 21:18:04 -0700

    editing focus-workspace-group.fish

--- focus-workspace-group.fish
+++ focus-workspace-group.fish
@@ -4,6 +4,7 @@ function focus-workspace-group
 	i3-msg workpace $w
     end
     set val workspace_group_{$n}_focused
-    echo v is $$val
+    echo v1 is $val
+    echo v2 is $$val
     # i3-msg workspace $$val
 end
