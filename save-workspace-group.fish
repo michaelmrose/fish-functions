f6feee27a6095011aa3cd5661bb54a36fa47cb4d
Michael Rose <michael@rosenetwork.net>
2022-06-11 21:39:25 -0700

    editing save-workspace-group.fish

--- save-workspace-group.fish
+++ save-workspace-group.fish
@@ -1,6 +1,6 @@
 function save-workspace-group
     set n $argv[1]
-    set focused (get-ws-info get name where focused is true|quote)
+    set focused (get-ws-info get name where focused is true| quote)
     set other_workspaces (get-ws-info get name where visible is true|quote|grep --invert-match $focused)
     echo $other_workspaces $focused > ~/.config/i3/groups/$n
 end
