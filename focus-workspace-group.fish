c0d1dc87e85b79e52d32a3d85f2fef3d422c5c6f
Michael Rose <michael@rosenetwork.net>
2022-06-11 22:42:29 -0700

    editing focus-workspace-group.fish

--- focus-workspace-group.fish
+++ focus-workspace-group.fish
@@ -1,8 +1,3 @@
 function focus-workspace-group
-    # set n $argv[1]
     ws (cat ~/.config/i3/groups/$argv|xargs -n1)
-    # set workspaces (explode-words (cat ~/.config/i3/groups/$n))
-    # for w in $workspaces
-    # 	i3-msg workspace $w
-    # end
 end
