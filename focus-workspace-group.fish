1f012f58ad48b544f7dc7245c7984e306a244b94
Michael Rose <michael@rosenetwork.net>
2022-06-11 21:07:22 -0700

    editing focus-workspace-group.fish

--- focus-workspace-group.fish
+++ focus-workspace-group.fish
@@ -2,7 +2,7 @@ function focus-workspace-group
     set n $argv[1]
     set group workspace_group_$n
     for w in $$group
-	echo i3-msg workpace $w
+	i3-msg workpace $w
     end
     # i3-msg workspace $workspace-group_$n_focused
 end
