80172e50e0ccbfe64cc0180828ee9b61febdf7a5
Michael Rose <michael@rosenetwork.net>
2022-06-11 21:02:02 -0700

    editing save-workspace-group.fish

--- save-workspace-group.fish
+++ save-workspace-group.fish
@@ -2,6 +2,6 @@ function save-workspace-group
     set n $argv[1]
     set workspaces (get-ws-info get name where visible is true|quote)
     set focused (get-ws-info get name where focused is true)
-    set -U workspace_group_$n $workspaces
+    set -Ux workspace_group_$n $workspaces
     # set -U workspace_group_$n_focus $focused
 end
