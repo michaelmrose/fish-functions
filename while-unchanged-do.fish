248a469c6b5841965c9073c056de77e61188d72c
Michael Rose <michael@rosenetwork.net>
2022-06-27 19:14:37 -0700

    removed while-unchanged-do.fish

--- while-unchanged-do.fish
+++
@@ -1,8 +0,0 @@
-function while-unchanged-do
-	set initial (eval $argv[2..-1])
-	eval $argv[1] &
-	echo initial is $initial
-	while string match $initial (eval $argv[2..-1]) -q
-		sleep 0.1
-	end
-end
