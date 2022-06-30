10240ee5475b5254c5fa06984ca5a45793a40594
Michael Rose <michael@rosenetwork.net>
2022-06-29 23:09:29 -0700

    editing repeatedly.fish

--- repeatedly.fish
+++ repeatedly.fish
@@ -1,10 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/repeatedly.fish @ line 2
 function repeatedly
     if isnumeric $argv[1]
         set t $argv[1]
         set c $argv[2..-1]
     else
-        set t 1
+        set t 0.1
         set c $argv
     end
     
