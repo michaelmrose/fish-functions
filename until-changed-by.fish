38c238932e295f495c78446ab2b5e7a56ee550ee
Michael Rose <michael@rosenetwork.net>
2022-06-29 23:28:10 -0700

    editing until-changed-by.fish

--- until-changed-by.fish
+++ until-changed-by.fish
@@ -2,10 +2,10 @@ function until-changed-by
 	set last ""
 	echo we are going to eval $argv
 	$argv
-	while read -l line
-		echo line and last are $line and $last
-		if not string match $line $last
-			return 0
-		end
-	end
+	# while read -l line
+	# 	echo line and last are $line and $last
+	# 	if not string match $line $last
+	# 		return 0
+	# 	end
+	# end
 end
