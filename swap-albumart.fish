3669d691e0b8415d14db1ec1ce0dfb8fb2f58208
Michael Rose <michael@rosenetwork.net>
2022-07-02 10:57:04 -0700

    editing swap-albumart.fish

--- swap-albumart.fish
+++ swap-albumart.fish
@@ -1,5 +1,9 @@
 function swap-albumart
 	set albumart_path (playerctl -p mpd metadata mpris:artUrl|cut -c 8-) 
-	curl (clip -o) > "$albumart_path"
+	if exists $argv
+		cp $argv "albumart_path"
+	else
+		curl (clip -o) > "$albumart_path"
+	end
 	emf e mmr/update-album-art
 end
