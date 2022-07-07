fb8b7280fa7defc867abff40bb4037727ac3d5f7
Michael Rose <michael@rosenetwork.net>
2022-07-06 18:20:20 -0700

    editing vi3_take-window-to-workspace.fish

--- vi3_take-window-to-workspace.fish
+++ vi3_take-window-to-workspace.fish
@@ -1,9 +1,9 @@
-# Defined in /home/michael/.config/fish/buffer/gws_tws_vi3_get-workspace_vi3_.fish @ line 25
 function vi3_take-window-to-workspace
 	# er vi3op
     # update-op-status
     # set w $argv(getwsstack $argv)
-    i3-msg move container to workspace (decorate-workspacename $argv)
+    # i3-msg move container to workspace (decorate-workspacename $argv)
+    i3-msg move container to workspace $argv
     ws $argv
     # vi3_workspace $argv
     # signal-i3blocks pages
