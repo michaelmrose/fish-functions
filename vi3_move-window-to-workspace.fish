a5767c632ee5574bd67cda9e1371fc3d5d2af290
Michael Rose <michael@rosenetwork.net>
2022-07-06 18:23:43 -0700

    editing vi3_move-window-to-workspace.fish

--- vi3_move-window-to-workspace.fish
+++ vi3_move-window-to-workspace.fish
@@ -1,4 +1,4 @@
-# Defined in /home/michael/.config/fish/buffer/vi3_move-window-to-workspace.fish @ line 2
 function vi3_move-window-to-workspace
-	  i3-msg move container to workspace (decorate-workspacename $argv)
+	  # i3-msg move container to workspace (decorate-workspacename $argv)
+	  i3-msg move container to workspace $argv
 end
