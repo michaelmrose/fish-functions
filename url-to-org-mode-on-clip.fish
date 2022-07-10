81c44e96902d5cb3308fab069cad1f5042ddd159
Michael Rose <michael@rosenetwork.net>
2022-07-10 10:09:03 -0700

    editing url-to-org-mode-on-clip.fish

--- url-to-org-mode-on-clip.fish
+++ url-to-org-mode-on-clip.fish
@@ -1,4 +1,4 @@
 function url-to-org-mode-on-clip
-	readable -q (clip -o|quote)| pandoc --from=html --to=org |clipboard
+	readable -q (clip -o)| pandoc --from=html --to=org |clipboard
 	msg processed url with readable and pandoc
 end
