d00bca88d9c627b8ed2356593b21ab9a4e9837b6
Michael Rose <michael@rosenetwork.net>
2022-07-24 10:16:39 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -3,6 +3,9 @@ function xrr
 		case left
 			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 --output HDMI-0 --off --output DVI-D-0 --off
 		case all
+			for d in (get-disconnected-displays)
+				xrandr --output $d --auto
+			end
 			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DVI-D-0 --pos 7200x0 --scale 1.75x1.75
 		case '*'
 			if test (count (get-connected-displays)) -eq 3
