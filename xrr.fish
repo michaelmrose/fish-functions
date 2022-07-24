d2433e05eb75cc08d86aee56d37cf5c088b72e1a
Michael Rose <michael@rosenetwork.net>
2022-07-24 10:13:09 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,5 +1,17 @@
 function xrr
-    xrr.clj HDMI-0 320 DP-4 -530 DP-1
-    wp recall
-    restart picom
+	xrr.clj HDMI-0 320 DP-4 -530 DP-1
+	switch $argv
+		case left
+			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 - -output HDMI-0 --off --output DVI-D-0 --off
+		case all
+			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 - -output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DV I-D-0 --pos 7200x0 --scale 1.75x1.75
+		case '*'
+			if test (count (get-connected-displays)) -eq 3
+				xrr left
+			else
+				xrr all
+			end
+	end
+	wp recall
+	restart picom
 end
