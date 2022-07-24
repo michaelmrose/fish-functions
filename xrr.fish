bb110fa8c69226132b887f5d10a5923c73aaf23a
Michael Rose <michael@rosenetwork.net>
2022-07-24 10:14:28 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -1,9 +1,9 @@
 function xrr
 	switch $argv
 		case left
-			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 - -output HDMI-0 --off --output DVI-D-0 --off
+			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 --output HDMI-0 --off --output DVI-D-0 --off
 		case all
-			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 - -output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DV I-D-0 --pos 7200x0 --scale 1.75x1.75
+			xrandr --dpi 163 --output DP-0 --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DV I-D-0 --pos 7200x0 --scale 1.75x1.75
 		case '*'
 			if test (count (get-connected-displays)) -eq 3
 				xrr left
