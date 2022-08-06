822ef51b3bfd29ba775e2056d62f548667ba09c4
Michael Rose <michael@rosenetwork.net>
2022-08-05 17:19:04 -0700

    editing nicer-desktop.fish

--- nicer-desktop.fish
+++ nicer-desktop.fish
@@ -1,6 +1,6 @@
 function nicer-desktop
-    set highest (pgrep '^i3$|xcape|sndiod')
-    set high (pgrep 'Xorg|emacs')
+    set highest (pgrep 'xcape')
+    set high (pgrep 'Xorg|emacs|i3$|sndiod')
     set ffp (ps --ppid (pgrep firefox) -o pid=)
     set low $ffp (pgrep 'qbittorrent|mullvad')
 
