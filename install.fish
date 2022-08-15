be765962430b77b0206db235f4bf0980751b7575
Michael Rose <michael@rosenetwork.net>
2022-08-14 20:57:22 -0700

    editing install.fish

--- install.fish
+++ install.fish
@@ -1,4 +1,8 @@
-# Defined in /usr/home/michael/.config/fish/buffer/i_install.fish @ line 6
 function install
-  sudo xbps-install $argv; 
+	switch $argv[1]
+		case -r
+			sudo xbps-install (select-package "$argv[2]")
+		case '*'
+			sudo xbps-install $argv; 
+	end
 end
