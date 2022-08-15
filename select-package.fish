944c7a73c73e8e611a52cf9e175e3ee8b54db1b6
Michael Rose <michael@rosenetwork.net>
2022-08-14 20:52:28 -0700

    editing select-package.fish

--- select-package.fish
+++ select-package.fish
@@ -1,3 +1,3 @@
 function select-package
-	xbps-query -Rs $argv|rofi -dmenu -multi-select|choose 1|rev |choose -f - 1: -o -|rev|condense_lines
+	xbps-query -Rs "$argv" |rofi -dmenu -multi-select|choose 1|rev |choose -f - 1: -o -|rev|condense_lines
 end
