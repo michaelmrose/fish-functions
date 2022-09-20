59a167ae661e0b46d3df7d227bf3b66f436d26f7
Michael Rose <michael@rosenetwork.net>
2022-09-20 01:38:48 -0700

    editing monitors.fish

--- monitors.fish
+++ monitors.fish
@@ -8,4 +8,5 @@ function monitors
 			xrandr --output DVI-D-0 --auto
 			xrandr --dpi 163 --output DP-0 --auto --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DVI-D-0 --pos 7200x0 --scale 1.75x1.75
     end
+	wp recall
 end
