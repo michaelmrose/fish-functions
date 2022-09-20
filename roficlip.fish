17220ddfad95794b0d25537cdf4814d69a8801b6
Michael Rose <michael@rosenetwork.net>
2022-09-20 01:45:34 -0700

    editing roficlip.fish

--- roficlip.fish
+++ roficlip.fish
@@ -1,5 +1,6 @@
 function roficlip
 	# rofi -m -1 -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
-	set text (greenclip print|select)
-	xdotool type --delay 1 "$text"
+	# set text (greenclip print|select)
+	# xdotool type --delay 1 "$text"
+	greenclip print|select|clipboard
 end
