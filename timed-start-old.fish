4c19d65088e3bb4e96a75c654d28e709f288b9e8
Michael Rose <michael@rosenetwork.net>
2022-10-06 08:05:37 -0700

    editing timed-start-old.fish

---
+++ timed-start-old.fish
@@ -0,0 +1,15 @@
+function timed-start-old
+    if test (count $argv) -gt 1
+        set app $argv[1..-2]
+        set class $argv[-1]
+    else
+        set app $argv[1]
+        set class $argv[1]
+    end
+    fish -c $app >/dev/null &
+    while true
+        if window-exists $class
+            return 0
+        end
+    end
+end
