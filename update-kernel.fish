458c9e8abe578e36286696b73b35cf23c78618ff
Michael Rose <michael@rosenetwork.net>
2022-10-28 02:31:59 -0700

    editing update-kernel.fish

--- update-kernel.fish
+++ update-kernel.fish
@@ -3,7 +3,8 @@ function update-kernel
 	cd /usr/home/michael/extproj/voidxanmodK
 	git pull
 	cd linux-gcc
-	set kernel (fd 'linux.\...-xanmod$'|cut -c3-18|select)
+	# set kernel (fd 'linux.\...-xanmod$'|cut -c3-18|select)
+	set kernel (fd 'linux(5|6)\.[0-9]{1,2}-xanmod$'|select)
 	echo Do you want to install the new kernel?
 	if read_confirm
 		set headers $kernel-headers
