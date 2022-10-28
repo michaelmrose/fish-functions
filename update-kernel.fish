ba6783452fcb43b598bc15a77d8f0824a3f4182d
Michael Rose <michael@rosenetwork.net>
2022-10-28 02:36:14 -0700

    editing update-kernel.fish

--- update-kernel.fish
+++ update-kernel.fish
@@ -5,20 +5,18 @@ function update-kernel
 	cd linux-gcc
 	# set kernel (fd 'linux.\...-xanmod$'|cut -c3-18|select)
 	if set kernel (fd 'linux(5|6)\.[0-9]{1,2}-xanmod$'| choose -f / 1| select)
-		echo Do you want to install the new kernel?
-		else
-			echo no decision
-	end
-	if read_confirm
-		set headers $kernel-headers
-		set dbg $kernel-dbg
-		set voidpkgs ~/extproj/void-packages
-		cp -R $kernel $voidpkgs/srcpkgs
-		cp -R $kernel $voidpkgs/srcpkgs
-		cp -R $dbg $voidpkgs/srcpkgs
-		cd $voidpkgs
-		./xbps-src pkg $kernel $headers -j6
-		sudo xbps-install --repository hostdir/binpkgs $kernel $headers
+		echo Do you want to install kernel $kernel
+		if read_confirm
+			set headers $kernel-headers
+			set dbg $kernel-dbg
+			set voidpkgs ~/extproj/void-packages
+			cp -R $kernel $voidpkgs/srcpkgs
+			cp -R $kernel $voidpkgs/srcpkgs
+			cp -R $dbg $voidpkgs/srcpkgs
+			cd $voidpkgs
+			./xbps-src pkg $kernel $headers -j6
+			sudo xbps-install --repository hostdir/binpkgs $kernel $headers
+		end
 	end
 	cd "$target"
 end
