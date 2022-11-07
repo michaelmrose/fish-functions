1cef39be57d46bf95505b41c726702f7c46f9afb
Michael Rose <michael@rosenetwork.net>
2022-11-07 08:45:23 -0800

    editing e.fish

--- e.fish
+++ e.fish
@@ -1,8 +1,9 @@
 function e
-		if stat -c %U $argv &> /dev/null
-			emacsclient -cn $argv
-		else
-			echo opening as root
-			emacsclient -cn "/doas::$argv"
-		end
+	emacsclient -cn $argv
+		# if stat -c %U $argv &> /dev/null;and test -d (dirname $argv)
+		# 	emacsclient -cn $argv
+		# else
+		# 	echo opening as root
+		# 	emacsclient -cn "/doas::$argv"
+		# end
 end
