1d060be3ce76443fed40cac484eb0441c9cefa75
Michael Rose <michael@rosenetwork.net>
2022-11-17 11:28:58 -0800

    editing return-vpn-status-line-text.fish

--- return-vpn-status-line-text.fish
+++ return-vpn-status-line-text.fish
@@ -7,8 +7,10 @@ function return-vpn-status-line-text
 		switch $line
 			case $vpn_connected_string
 				echo $locked
+				signal-i3blocks 12
 			case $vpn_disconnected_string
 				echo $unlocked
+				signal-i3blocks 12
 		end
 	end
 end
