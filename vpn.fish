3e3536b65cda1737baefa272711c19c475df71e0
Michael Rose <michael@rosenetwork.net>
2022-11-17 03:37:38 -0800

    editing vpn.fish

--- vpn.fish
+++ vpn.fish
@@ -2,11 +2,13 @@ function vpn
 	switch $argv[1]
 		case -d
 			# volemad-cli -d
-			mullvad disconnect
+			# mullvad disconnect
+			nmcli connection down mullvad_us_sea
 			signal-i3blocks 12
 		case	-c
 			# volemad-cli	 -p 'USA' -s 'Los Angeles, CA'
-			mullvad connect
+			# mullvad connect
+			nmcli connection up mullvad_us_sea
 				for attempt in (seq 1 15)
 				    signal-i3blocks 12
 				    sleep 1
@@ -24,19 +26,20 @@ function vpn
 				mullvad split-tunnel pid delete $p
 			end
 		case -t
-			if mullvad status | grep Connected
-				mullvad disconnect
+			if nmcli -g GENERAL.STATE c s tun0|grep -q 'activ' 
+				# mullvad disconnect
+				nmcli connection down mullvad_us_sea
 				signal-i3blocks 12
 				msg disconnecting...
 			else
-				mullvad connect
+				nmcli connection up mullvad_us_sea
+				# mullvad connect
 				signal-i3blocks 12
 				msg connecting...
 				for attempt in (seq 1 15)
 				    signal-i3blocks 12
 				    sleep 1
 				end
-				
 			end
 	end
 end
