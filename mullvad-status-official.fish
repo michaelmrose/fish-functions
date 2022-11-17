8a38ddf6f4d904a20fc114dba0591f3da8109577
Michael Rose <michael@rosenetwork.net>
2022-11-17 03:49:33 -0800

    editing mullvad-status-official.fish

--- mullvad-status-official.fish
+++ mullvad-status-official.fish
@@ -1,5 +1,5 @@
 function mullvad-status-official
-		if nmcli -g GENERAL.STATE c s tun0|grep -q 'activ' > /dev/null
+		if nmcli -g GENERAL.STATE c s tun0|grep -q 'activ' &> /dev/null
 				i3status-rs-colorize Warning "  "
 				# i3status-rs-colorize Warning "  🔒 "
 		else
