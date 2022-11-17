6d559ec4dbe668d7e913feb0d1751d25d8483c77
Michael Rose <michael@rosenetwork.net>
2022-11-17 11:38:59 -0800

    editing return-vpn-status-line-text.fish

--- return-vpn-status-line-text.fish
+++ return-vpn-status-line-text.fish
@@ -1,8 +1,8 @@
 function return-vpn-status-line-text
 	set locked '{"state":"Warning", "text": "  "}'
 	set unlocked '{"state":"Info", "text": "    "}'
-	set vpn_connected_string '\'Wired connection 1\' is now the primary connection'
-	set vpn_disconnected_string '\'mullvad_us_sea\' is now the primary connection'
+	set vpn_disconnected_string '\'Wired connection 1\' is now the primary connection'
+	set vpn_connected_string '\'mullvad_us_sea\' is now the primary connection'
 	while read -l line
 		switch $line
 			case $vpn_connected_string
