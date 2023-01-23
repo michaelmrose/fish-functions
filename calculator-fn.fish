69135a7380c1dc29dcbbf428629de65c83889348
Michael Rose <michael@rosenetwork.net>
2023-01-22 21:42:07 -0800

    editing calculator-fn.fish

--- calculator-fn.fish
+++ calculator-fn.fish
@@ -2,5 +2,5 @@ function calculator-fn
 	emf ecn full-calc
 	# waituntilfocused Emacs
 	sleep 0.25
-	i3-msg resize shrink left
+	i3-msg resize shrink left (math (get-focused-display-width) / 10)px
 end
