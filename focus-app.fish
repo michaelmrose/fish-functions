21236a8e13174ecc781dd62dbe5fabf8254e2d1b
Michael Rose <michael@rosenetwork.net>
2023-01-25 13:45:59 -0800

    editing focus-app.fish

--- focus-app.fish
+++ focus-app.fish
@@ -1,60 +1,15 @@
 function focus-app
 	
-    #     set com i3-msg \'[class=$criteria[1] title=\"$criteria[2]\"]\' focus
-    # end
-    # echo com is $com
-    # set res (eval $com)
-    # er vi3op
-    # update-op-status
-    # echo $res | grep true
-    # signal-i3blocks pages
     set criteria class
-    switch $argv
-        case a
-        case b
-            set val Firefox
-        case c
-            set val Speedcrunch
-        case d
-            set val Qbittorrent
-        case e
-            set val Emacs
-        case f
-        case g
-        case h
-            set val Hexchat
-        case i
-        case j
-        case k
-        case l
-            set val kitty
-        case m
-            set val Clementine
-        case n
-        case o
-        case p
-        case q
-        case r
-        case s
-            set val Steam
-        case t
-            set val kitty
-        case u
-        case v
-            set val mpv
-        case w
-        case x
-        case y
-        case z
-            set val Zathura
-        case E
-            # set criteria title
-            # set val '.*mu4e.*'
-            i3 workspace c
-            return 0
-    end
-    # set com i3 \'[{$criteria}={$val}]\' focus
-    # echo $com
-    # eval $com >&2 /dev/null
+	set appkeyValue (appkey $argv|choose 0)
+	switch $appkeyValue
+		case emacsclient
+			set val Emacs
+		case kitty
+			set val kitty
+		case '*'
+			set val (capitalize $appkeyValue)
+	end
+
 	i3-msg "[$criteria=$val]" focus
 end
