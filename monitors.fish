3ee741053872938a6d11487a1a4542019fbe074f
Michael Rose <michael@rosenetwork.net>
2023-02-26 22:59:10 -0800

    editing monitors.fish

--- monitors.fish
+++ monitors.fish
@@ -2,19 +2,19 @@ function monitors
 	switch $argv
         case one
 			xrandr --output DP-0 --off
-			xrandr --output DVI-D-0 --off
+			xrandr --output DP-2 --off
         case all
 			xrandr --output DP-0 --auto
 			xrandr --output DVI-D-0 --auto
-			xrandr --dpi 163 --output DP-0 --auto --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DVI-D-0 --pos 7200x0 --scale 1.75x1.75
+			xrandr --dpi 163 --output DP-0 --auto --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DP-2 --pos 7200x0 --scale 1.75x1.75
 		case '*'
 			if test  (get-connected-displays|wc -l) -gt 1
 				xrandr --output DP-0 --off
-				xrandr --output DVI-D-0 --off
+				xrandr --output DP-2 --off
 			else
 				xrandr --output DP-0 --auto
 				xrandr --output DVI-D-0 --auto
-				xrandr --dpi 163 --output DP-0 --auto --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DVI-D-0 --pos 7200x0 --scale 1.75x1.75
+				xrandr --dpi 163 --output DP-0 --auto --scale 1.75x1.75 --output HDMI-0 --pos 3360x0 --mode 3840x2160 --output DP-2 --pos 7200x0 --scale 1.75x1.75
 			end
 	end
 	wp recall
