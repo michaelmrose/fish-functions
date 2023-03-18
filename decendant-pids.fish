34145911806ab19daf01ace03ad76bfeb4ca6a86
Michael Rose <michael@rosenetwork.net>
2023-03-18 13:42:38 -0700

    editing decendant-pids.fish

--- decendant-pids.fish
+++ decendant-pids.fish
@@ -1,10 +1,8 @@
 function decendant-pids
 	set pids (ps -o pid= --ppid $argv)
 	set collect $pids
-	echo c is now $collect
 	for pid in $pids
 		set collect $collect (decendant-pids $pid)
-	echo c is now $collect
 	end
 	p $collect
 end
