c92ffd2464f9913471d1aee957ce13958e537188
Michael Rose <michael@rosenetwork.net>
2023-03-18 10:09:52 -0700

    editing monitors.fish

--- monitors.fish
+++ monitors.fish
@@ -6,11 +6,11 @@ function monitors
 			xrandr --output DP-2 --off
         case all
 			xrandr --dpi 163 --output DP-0 --pos 0x0 --mode 1920x1080 --scale 1.75x1.75
-			sleep 1
+			sleep 2
 			xrandr --dpi 163 --output HDMI-0 --pos 3360x0 --mode 3840x2160
-			sleep 1
+			sleep 2
 			xrandr --dpi 163 --output DP-2 --pos 7200x0 --scale 1.75x1.75
-			sleep 1
+			sleep 2
 			ddcutil -d 3 setvcp 60 0x0f
 		case work
 			ddcutil -d 3 setvcp 60 0x11
