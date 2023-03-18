5f777c4673698d3127f89c7ac175f78bfd5b52aa
Michael Rose <michael@rosenetwork.net>
2023-03-18 13:45:26 -0700

    editing decendant-pids.fish

--- decendant-pids.fish
+++ decendant-pids.fish
@@ -1,12 +1,10 @@
 function decendant-pids
-	# echo called with $argv
-	if not exists $argv
-		return 0
+	if test (count $argv) -gt 0
+	  set pids (ps -o pid= --ppid $argv)
+	  set collect $pids
+	  for pid in $pids
+		  set collect $collect (decendant-pids $pid)
+	  end
+	  p $collect
 	end
-	set pids (ps -o pid= --ppid $argv)
-	set collect $pids
-	for pid in $pids
-		set collect $collect (decendant-pids $pid)
-	end
-	p $collect
 end
