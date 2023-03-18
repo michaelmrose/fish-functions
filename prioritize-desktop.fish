d452166a153613ec3fe86bbad1914bbefadb082c
Michael Rose <michael@rosenetwork.net>
2023-03-18 13:53:39 -0700

    editing prioritize-desktop.fish

---
+++ prioritize-desktop.fish
@@ -0,0 +1,19 @@
+function prioritize-desktop
+    set highest (pgrep 'xcape')
+    set high (pgrep 'Xorg|emacs|i3$|sndiod')
+
+	set current (decendant-pids (pid-of-active-window))
+
+    set ffp (ps --ppid (pgrep firefox) -o pid=)
+    set low $ffp (pgrep 'qbittorrent|mullvad')
+
+    sudo renice -n -20 -p $highest
+    sudo renice -n -10 -p $high
+    sudo renice -n -10 -p $current
+
+    sudo ionice -c 1 -p $highest
+    sudo ionice -c 1 -p $high
+    sudo ionice -c 1 -p $current
+    sudo ionice -c 3 -p $low
+
+end
