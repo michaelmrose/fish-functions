20af7ec8bb488b9a34547a52247dfd26dcac2ce5
Michael Rose <michael@rosenetwork.net>
2023-03-22 11:31:34 -0700

    editing roam-from-clipboard.fish

--- roam-from-clipboard.fish
+++ roam-from-clipboard.fish
@@ -1,5 +1,5 @@
 function roam-from-clipboard
-	set url (xclip -o -selection clipboard)
+	set url (xclip -o -selection clipboard) >/dev/null
 	set title http "url"|pup title text{}
 	echo $url $title
 end
