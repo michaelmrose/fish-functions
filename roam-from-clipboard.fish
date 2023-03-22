4effedb467131f6178352c3b9cffd2caecc08f3a
Michael Rose <michael@rosenetwork.net>
2023-03-22 11:57:29 -0700

    editing roam-from-clipboard.fish

--- roam-from-clipboard.fish
+++ roam-from-clipboard.fish
@@ -3,5 +3,5 @@ function roam-from-clipboard
 	set encoded (node -e "console.log(encodeURIComponent('$url'))")
 	set title (http "$url"|pup title text{})
 	set link "org-protocol://roam-ref?template=r&ref={$encoded}&title={$title}"
-	jaro "$link"
+	echo "$link"
 end
