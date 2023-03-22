141f4947f847938982a34fe70d3c07e4a05281b6
Michael Rose <michael@rosenetwork.net>
2023-03-22 11:37:34 -0700

    editing roam-from-clipboard.fish

--- roam-from-clipboard.fish
+++ roam-from-clipboard.fish
@@ -2,5 +2,5 @@ function roam-from-clipboard
 	set url (xclip -o -selection clipboard)
 	set title (http "$url"|pup title text{})
 	set link "org-protocol://roam-ref?template=r&ref=$url&title=$title"
-	echo $link
+	jaro "$link"
 end
