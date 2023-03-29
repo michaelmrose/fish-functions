f54d14680f2eff31e1ffe6f69b04501ebef3dfef
Michael Rose <michael@rosenetwork.net>
2023-03-29 00:53:33 -0700

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -1,16 +1,13 @@
 function starton --argument ws command class
   set json "{\"swallows\": [{\"class\": \"^$class\$\"}], \"type\": \"con\"}"
-  set layout /tmp/layout-(uuidgen)
-  echo $json > $layout
   set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
   set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
   i3-msg workspace $ws
-  # i3-msg append_layout $layout
-  i3-msg append_layout (echo $json|psub)
+  # i3-msg append_layout (echo $json|psub)
+  i3-msg append_layout (echo "{\"swallows\": [{\"class\": \"^$class\$\"}], \"type\": \"con\"}"|psub)
   fish -c $command &
   for w in $active
         i3-msg workspace $w
     end
     i3-msg workspace $focused
-  # 	echo additional is $additional
 end
