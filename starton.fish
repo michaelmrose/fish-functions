059e59bbd25ce05720b3d2e43dce4e6f69ed7c46
Michael Rose <michael@rosenetwork.net>
2023-03-29 00:33:45 -0700

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -3,20 +3,16 @@ function starton
   set ws $argv[1]
   set command $argv[2]
   set class $argv[3]
-  set json $argv[4]
-  set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
-  set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
-  if not exists $json
-	  set json '{"swallows": [{"class": "^#winclass$"}], "type": "con"}'
-  end
-  set layout /tmp/(uuidgen)-layout
-  echo $json  | sed "s/#winclass/$class/g" > $layout
-  i3-msg workspace $ws
-  i3-msg append_layout $layout
-  fish -c $command &
-  for w in $active
-        i3-msg workspace $w
-    end
-    i3-msg workspace $focused
-	echo additional is $additional
+  set json "{\"swallows\": [{\"class\": \"^$class\$\"}], \"type\": \"con\"}"
+  echo $json
+  # set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
+  # set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
+  # i3-msg workspace $ws
+  # i3-msg append_layout $layout
+  # fish -c $command &
+  # for w in $active
+  #       i3-msg workspace $w
+  #   end
+  #   i3-msg workspace $focused
+  # 	echo additional is $additional
 end
