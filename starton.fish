295f4aa6e7d6aff58b7ad7e95148579170a0ff2c
Michael Rose <michael@rosenetwork.net>
2023-03-29 11:50:09 -0700

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -1,13 +1,11 @@
 function starton --argument ws command class
   set json "{\"swallows\": [{\"class\": \"^$class\$\"}], \"type\": \"con\"}"
-  set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
-  set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
-  # i3-msg workspace $ws
-  # i3-msg append_layout (echo $json|psub)
-  # $command &
+  # set workspaces (i3-msg -t get_workspaces)
+  # set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
+  # set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
+  # set active (get-ws-info get name where visible = true) 
+  # set focused (get-ws-info get name where focused = true) 
+  set workspaces (get-ws-info get name where focused = true) (get-ws-info get name where visible = true) 
   i3-msg workspace $ws, append_layout (echo $json|psub), exec "fish -c $command"
-  map ws $active $focused
-  # for w in $active $focused
-  #       i3-msg workspace $w
-  #   end
+  map ws $workspaces
 end
