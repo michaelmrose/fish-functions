3139411af0c709aa7123d6d811ab1d9d8e2315eb
Michael Rose <michael@rosenetwork.net>
2023-03-29 00:34:43 -0700

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -5,14 +5,14 @@ function starton
   set class $argv[3]
   set json "{\"swallows\": [{\"class\": \"^$class\$\"}], \"type\": \"con\"}"
   echo $json
-  # set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
-  # set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
-  # i3-msg workspace $ws
-  # i3-msg append_layout $layout
-  # fish -c $command &
-  # for w in $active
-  #       i3-msg workspace $w
-  #   end
-  #   i3-msg workspace $focused
+  set active ( i3-msg -t get_workspaces|jq -r '.[]| select(.visible == true).name')
+  set focused ( i3-msg -t get_workspaces|jq -r '.[]| select(.focused == true).name')
+  i3-msg workspace $ws
+  i3-msg append_layout $layout
+  fish -c $command &
+  for w in $active
+        i3-msg workspace $w
+    end
+    i3-msg workspace $focused
   # 	echo additional is $additional
 end
