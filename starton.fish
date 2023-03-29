5dfc1defa1257207f29b6f1e60fcbaef9ffaa104
Michael Rose <michael@rosenetwork.net>
2023-03-29 16:14:26 -0700

    editing starton.fish

--- starton.fish
+++ starton.fish
@@ -1,6 +1,6 @@
 function starton --argument ws command class
 	set json "{\"swallows\": [{\"class\": \"^$class\$\"}], \"type\": \"con\"}"
-	set workspaces (get-ws-info get name where focused = true) (get-ws-info get name where visible = true) 
+	set workspaces  (get-ws-info get name where visible = true) (get-ws-info get name where focused = true)
 	i3-msg workspace $ws, append_layout (echo $json|psub), exec "fish -c $command"
 	map ws $workspaces
 end
