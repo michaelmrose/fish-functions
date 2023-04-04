b50c0410d3a067308bd7928e03680c75d0f514d8
Michael Rose <michael@rosenetwork.net>
2023-04-04 10:33:28 -0700

    editing set-wallpaper.fish

--- set-wallpaper.fish
+++ set-wallpaper.fish
@@ -7,9 +7,9 @@ function set-wallpaper
 		set-lightdm-bg $img
 		set -U bgimage $img
         walfn
-		convert $img -resize 2000x2000 ~/.cache/wal/rofi.jpg
-		pywalfox update
-		betterlockscreen -u $bgimage --fx
+		convert $img -resize 2000x2000 ~/.cache/wal/rofi.jpg &
+		pywalfox update &
+		betterlockscreen -u $bgimage --fx &
         # wal -n --saturate 0.7 -i $img
         # xrdb ~/.cache/wal/colors-i3.conf
         # restart-i3
