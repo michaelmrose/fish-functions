2d9ace9271cd69a4db964446bf30c480fb0656a8
Michael Rose <michael@rosenetwork.net>
2023-04-09 09:23:11 -0700

    editing jfin.fish

--- jfin.fish
+++ jfin.fish
@@ -1,6 +1,6 @@
 function jfin
 	chromium --app=http://localhost:8096 &
-	# killall jellyfin-mpv-shim
-	# killall jellyfin-mpv-sh
-	# jellyfin-mpv-shim &
+	killall jellyfin-mpv-shim
+	killall jellyfin-mpv-sh
+	jellyfin-mpv-shim &
 end
