643aa51e6133d3eb1b828b4be9212252ccd36f9e
Michael Rose <michael@rosenetwork.net>
2023-04-19 16:07:44 -0700

    editing edit.fish

--- edit.fish
+++ edit.fish
@@ -1,17 +1,8 @@
-# Defined in /usr/home/michael/.config/fish/buffer/edit_emd_emf.fish @ line 2
 function edit
-	# if not exists $argv
-  #   while read -l line
-  #     set acc $acc $line
-  #   end
-  # else
-  #   set acc $argv
-  # end
-  # # set tmp /tmp/(uid)
-  # set tmp /tmp/edited
-  # println $acc > $tmp   
-  # # nvim $tmp
-  # eval $EDITOR $tmp
-  # cat $tmp
-	emacsclient -cn $argv
+	switch $argv[1]
+		case -h
+			emacsclient -c $argv
+			case '*'
+				emacsclient -cn $argv
+	end
 end
