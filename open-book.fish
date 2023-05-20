75727e5b0c3ea626953383645c45e7462478405d
Michael Rose <michael@rosenetwork.net>
2023-05-20 01:16:23 -0700

    editing open-book.fish

--- open-book.fish
+++ open-book.fish
@@ -1,17 +1,17 @@
-# Defined in /home/michael/.config/fish/buffer/add-to-recent-reads_open-book.fish @ line 16
 function open-book
-	set fullpath (pathof $argv)
-  set ext (cutlast "." $argv)
-  set library $ebook_library
-  if substr $fullpath $library #if path is in $library
-    set title (query-calibre-title title (escape-chars (extract-title $fullpath)))
-    add-to-recent-reads "$title"
-    add-to-recent-reads-fnames "$argv"
-    # set-recent-book-covers
-  end
-  if contains $ext epub pdf djvu
-    zathura "$argv"
-  else
-    ebook-viewer "$argv"
-  end
+	rdr -o (pathof $argv)
+  # 	set fullpath (pathof $argv)
+  # set ext (cutlast "." $argv)
+  # set library $ebook_library
+  # if substr $fullpath $library #if path is in $library
+  #   set title (query-calibre-title title (escape-chars (extract-title $fullpath)))
+  #   add-to-recent-reads "$title"
+  #   add-to-recent-reads-fnames "$argv"
+  #   # set-recent-book-covers
+  # end
+  # if contains $ext epub pdf djvu
+  #   zathura "$argv"
+  # else
+  #   ebook-viewer "$argv"
+  # end
 end
