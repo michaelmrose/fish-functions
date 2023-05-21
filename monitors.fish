b89c1e7d608e46ee107997a639ae1d76091ca3b3
Michael Rose <michael@rosenetwork.net>
2023-05-21 12:19:55 -0700

    editing monitors.fish

--- monitors.fish
+++ monitors.fish
@@ -1,20 +1,14 @@
 function monitors
 	switch $argv
         case one
-			# ddcutil -d 3 setvcp 60 0x0f
 			xrandr --output DP-0 --off
-			xrandr --output DP-2 --off
+			xrandr --output HDMI-0 --off
         case all
 			xrandr --dpi 163 --output DP-0 --pos 0x0 --mode 3840x2160
 			sleep 2
-			xrandr --dpi 163 --output HDMI-0 --pos 3840x0 --mode 3840x2160
+			xrandr --dpi 163 --output DP-2 --pos 3840x0 --mode 3840x2160
 			sleep 2
-			xrandr --dpi 163 --output DP-2 --pos 7680x0 --scale 1.75x1.75
-			sleep 2
-			# ddcutil -d 3 setvcp 60 0x0f
-		case work
-			# ddcutil -d 3 setvcp 60 0x11
-			xrandr --dpi 163 --output DP-0 --mode 3840x2160 --pos 0x0  --output HDMI-0  --off --output DP-2 --off
+			xrandr --dpi 163 --output HDMI-0 --pos 7680x0 --scale 1.75x1.75
 		case '*'
 			if test  (get-connected-displays|wc -l) -gt 1
 				monitors one
