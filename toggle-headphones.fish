bef0dcd2b979b6265db46b48ad17b214bca5e2e7
Michael Rose <michael@rosenetwork.net>
2023-05-28 22:58:30 -0700

    editing toggle-headphones.fish

--- toggle-headphones.fish
+++ toggle-headphones.fish
@@ -1,9 +1,9 @@
 function toggle-headphones
 	if [  $SOUND_PORT = headphones  ]
-		pactl set-sink-port $SOUND_CARD analog-output-headphones
-		set -U SOUND_PORT headphones
-	else
 		pactl set-sink-port $SOUND_CARD analog-output-lineout
 		set -U SOUND_PORT speakers
+	else
+		pactl set-sink-port $SOUND_CARD analog-output-headphones
+		set -U SOUND_PORT headphones
 	end
 end
