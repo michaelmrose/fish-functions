6542fb302cdc4a7ef85c05afc52cee2d61860cc3
Michael Rose <michael@rosenetwork.net>
2023-05-28 22:53:30 -0700

    editing toggle-headphones.fish

--- toggle-headphones.fish
+++ toggle-headphones.fish
@@ -1,9 +1,7 @@
 function toggle-headphones
-	
-	amixer set -c 1 Front mute
-	amixer set -c 1 Headphone unmute
-	
-	amixer set -c 1 Front unmute
-	amixer set -c 1 Headphone mute
-
+	if [  $SOUND_PORT == headphones]
+		pactl set-sink-port $SOUND_CARD analog-output-headphones
+	else
+		pactl set-sink-port $SOUND_CARD analog-output-lineout
+	end
 end
