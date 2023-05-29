b5f12678c888b25a468567ed7eb2cf874dd146ab
Michael Rose <michael@rosenetwork.net>
2023-05-29 11:19:20 -0700

    editing get-active-audio-port.fish

--- get-active-audio-port.fish
+++ get-active-audio-port.fish
@@ -1,4 +1,4 @@
 function get-active-audio-port
 	set card (pactl list sinks short|g analog-stereo |choose 0)
-	pactl list sinks|g $card -A30|g "Active Port"| choose 2
+	pactl list sinks|g $card -A25|g "Active Port"| choose 2
 end
