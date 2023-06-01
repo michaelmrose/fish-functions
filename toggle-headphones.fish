ba52dde46b6f523cb3a9c7a86458fc844df86ad6
Michael Rose <michael@rosenetwork.net>
2023-06-01 15:02:18 -0700

    editing toggle-headphones.fish

--- toggle-headphones.fish
+++ toggle-headphones.fish
@@ -1,6 +1,6 @@
 function toggle-headphones
 	set port (get-active-audio-port)
-	set card (pactl list sinks short|g analog-stereo |choose 0)  
+	set card (pactl list sinks short|g 'pci.*analog-stereo' |choose 0)
 	if [  $port = analog-output-headphones  ]
 		pactl set-sink-port $card analog-output-lineout
 	else
