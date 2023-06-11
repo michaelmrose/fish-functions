482079fbfd1a5db31d9fb504eb01da70dc831546
Michael Rose <michael@rosenetwork.net>
2023-06-11 13:50:59 -0700

    editing open-app.fish

--- open-app.fish
+++ open-app.fish
@@ -1,5 +1,9 @@
-# Defined in /usr/home/michael/.config/fish/buffer/open-app.fish @ line 2
 function open-app
 	set target (appkey $argv) \&
     eval $target
+	if exists $UNIVERSAL_ARGUMENT
+		waituntilfocused (class-of-appkey $argv)
+		i3 resize set $NEXT_WINDOW_ORIENTATION $UNIVERSAL_ARGUMENT ppt
+	end
+	erase_variables_for_next_window
 end
