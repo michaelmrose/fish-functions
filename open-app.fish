b878746e3830d635dcf92e5ae6b6bbf07495259c
Michael Rose <michael@rosenetwork.net>
2023-06-11 14:07:54 -0700

    editing open-app.fish

--- open-app.fish
+++ open-app.fish
@@ -1,9 +1,9 @@
 function open-app
 	set target (appkey $argv) \&
     eval $target
-	if exists $UNIVERSAL_ARGUMENT
-		waituntilfocused (class-of-appkey $argv)
-		i3 resize set $NEXT_WINDOW_ORIENTATION $UNIVERSAL_ARGUMENT ppt
-	end
-	erase_variables_for_next_window
+	# if exists $UNIVERSAL_ARGUMENT
+	# 	waituntilfocused (class-of-appkey $argv)
+	# 	i3 resize set $NEXT_WINDOW_ORIENTATION $UNIVERSAL_ARGUMENT ppt
+	# end
+	# erase_variables_for_next_window
 end
