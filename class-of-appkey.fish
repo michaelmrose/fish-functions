549404fb6346172c9df35feb71af118cbe8600cc
Michael Rose <michael@rosenetwork.net>
2023-06-11 13:50:59 -0700

    editing class-of-appkey.fish

---
+++ class-of-appkey.fish
@@ -0,0 +1,21 @@
+function class-of-appkey
+    set criteria class
+	set appkeyValue (appkey $argv|choose 0) > /dev/null
+	switch $appkeyValue
+		case code-oss
+			set val "Code - OSS"
+		case emacsclient
+			set val Emacs
+		case qbittorrent
+			set val qBittorrent
+		case mpv
+			set val mpv
+		case kitty
+			set val kitty
+		case jfin
+			set val mpv
+		case '*'
+			set val (capitalize $appkeyValue)
+	end
+	echo $val
+end
