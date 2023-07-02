135eeb3c7cf9f1b06e7181d258c7d07e4f279000
Michael Rose <michael@rosenetwork.net>
2023-07-02 15:02:28 -0700

    editing google-calendar.fish

--- google-calendar.fish
+++ google-calendar.fish
@@ -1,3 +1,6 @@
 function google-calendar
-	firefox ---kiosk --new-window calendar.google.com
+	firefox ---kiosk --new-window calendar.google.comr
+	waituntilfocused Firefox
+	xdotool key F1
+	i3 fullscreen disable
 end
