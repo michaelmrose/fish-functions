7a8b663ea04e341074930076ecd9797e09d90dbb
Michael Rose <michael@rosenetwork.net>
2023-07-04 09:22:19 -0700

    editing prior.fish

--- prior.fish
+++ prior.fish
@@ -2,6 +2,7 @@ function prior
 	set prev (echo (history)[1]|choose 1:)
 	set n (echo $prev|wc -w)
 	for i in  (seq 1 $n)
-		echo $prev|choose $i:
-	end|fzf
+		set res $res ($prev|choose $i:)
+	end
+	p $res|fzf
 end
