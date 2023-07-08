2fbfdae3ef8aae1784b3bbb1b117d1cb70528309
Michael Rose <michael@rosenetwork.net>
2023-07-08 11:01:39 -0700

    editing youtubetv.fish

--- youtubetv.fish
+++ youtubetv.fish
@@ -1,7 +1,7 @@
 function youtubetv
-	# chrome --app-id=nlmaamaoahjiilibgbafebhafkeccjac
-	firefox --kiosk --new-window 'ext+container:name=work&url=tv.youtube.com'
-	waituntilfocused Firefox
-	sleep 0.5
-	xdotool key F1
+	chrome --app-id=nlmaamaoahjiilibgbafebhafkeccjac
+	# firefox --kiosk --new-window 'ext+container:name=work&url=tv.youtube.com'
+	# waituntilfocused Firefox
+	# sleep 0.5
+	# xdotool key F1
 end
