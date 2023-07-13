55885e6a80ea1ef76abff994830accb3e41fae5c
Michael Rose <michael@rosenetwork.net>
2023-07-13 11:03:57 -0700

    editing cr.fish

--- cr.fish
+++ cr.fish
@@ -1,4 +1,4 @@
 function cr
-	mkdir -p $PWD(echo $argv|choose -f / 0:|choose 0:-2|sd ' ' '/'echo $PWD|choose -f / 0:|choose 0:-2|sd ' ' '/')
+	mkdir -p  /(echo $PWD/$apath|choose -f / 0:|choose 0:-2|sd ' ' '/')
 	touch (echo $argv|choose -f / -1)
 end
