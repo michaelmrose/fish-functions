c116c8cea55239e5ed557d29c0afe14c0db35d4c
Michael Rose <michael@rosenetwork.net>
2023-07-13 11:07:12 -0700

    removed cr.fish

--- cr.fish
+++
@@ -1,6 +0,0 @@
-function cr
-	set thePath /(echo $PWD/$apath|choose -f / 0:|choose 0:-2|sd ' ' '/')
-	set theFile (echo (echo $argv|choose -f / -1))
-	mkdir -p $thePath
-	touch $thePath/$theFile
-end
