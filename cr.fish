db334d2e59b8de0be933b17b30f6928d809e4107
Michael Rose <michael@rosenetwork.net>
2023-07-13 11:06:50 -0700

    editing cr.fish

--- cr.fish
+++ cr.fish
@@ -1,4 +1,6 @@
 function cr
-	mkdir -p  /(echo $PWD/$apath|choose -f / 0:|choose 0:-2|sd ' ' '/')
-	echo (echo $argv|choose -f / -1)
+	set thePath /(echo $PWD/$apath|choose -f / 0:|choose 0:-2|sd ' ' '/')
+	set theFile (echo (echo $argv|choose -f / -1))
+	mkdir -p $thePath
+	touch $thePath/$theFile
 end
