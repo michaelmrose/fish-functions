d225c15fd64af9e4b8645b2831787d79cfb73a7a
Michael Rose <michael@rosenetwork.net>
2023-08-01 20:40:27 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,6 +1,7 @@
 function fish_prompt
 	if exists $VIRTUAL_ENV
 		fix_ve_shell_prompt
+		fish_prompt
 	  end
 		set myprompt ( timeout 0.5 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root,virtualenv --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status)
 		# echo $myprompt ➥
