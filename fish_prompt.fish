b236576a5942c28872f61a52b4e5a770296ca3c6
Michael Rose <michael@rosenetwork.net>
2023-08-01 19:25:07 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-		set myprompt ( timeout 0.5 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root,virutalenv --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status)
+		set myprompt ( timeout 0.5 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root,virtualenv --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status)
 		# echo $myprompt ➥
 		echo ┏╸{$myprompt}
 		echo "┗╸❯❯❯ "
