69d36336ce205af18cbbd80cc37d3d137af0cffd
Michael Rose <michael@rosenetwork.net>
2023-08-01 19:59:57 -0700

    editing ve.fish

--- ve.fish
+++ ve.fish
@@ -2,7 +2,7 @@ function ve
 	if exists $argv
 		python3 -m virtualenv $argv
 	end
-	source env/bin/activate.fish
+	source .venv/bin/activate.fish
 	functions -e fish_prompt
 	functions -c _old_fish_prompt fish_prompt
 end
