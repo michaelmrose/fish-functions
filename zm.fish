f365a9af86e81e65b3f5f271986cae8585e622df
Michael Rose <michael@rosenetwork.net>
2023-08-08 13:37:20 -0700

    editing zm.fish

---
+++ zm.fish
@@ -0,0 +1,9 @@
+function zm
+         switch $argv
+                case v
+                     zoom_send_key 'alt+v'
+                case a
+                     zoom_send_key 'alt+a'
+        end
+
+end
