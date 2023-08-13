6ebd8c2060ab0b87f0c89b4310577cd102e81335
Michael Rose <michael@rosenetwork.net>
2023-08-13 12:28:53 -0700

    editing dj.fish

--- dj.fish
+++ dj.fish
@@ -4,6 +4,10 @@ function dj
             cd ..
          end
         fd manage.py|each dirname|each cd
-        python manage.py $argv
+        if match $argv[1] run
+           echo python manage.py runserver (either $argv[2..1] 7000)
+        else
+            echo python manage.py $argv
+        end
         cd $prior
 end
