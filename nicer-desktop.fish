a45962ce321c21f563e7f4781ead737412c3a6d5
Michael Rose <michael@rosenetwork.net>
2023-08-13 11:22:36 -0700

    editing nicer-desktop.fish

--- nicer-desktop.fish
+++ nicer-desktop.fish
@@ -2,13 +2,10 @@ function nicer-desktop
     set highest (pgrep 'xcape')
     set high (pgrep 'Xorg|emacs|i3$|sndiod')
     # set ffp (ps --ppid (pgrep firefox) -o pid=)
-    # set low $ffp (pgrep 'qbittorrent|mullvad')
-	set low (pgrep mullvad)
 
     sudo renice -n -20 -p $highest
     sudo renice -n -10 -p $high
 
     sudo ionice -c 1 -p $highest
     sudo ionice -c 1 -p $high
-    sudo ionice -c 3 -p $low
 end
