1f8dafb47b3b0a1d325d9a217c98c617cab50d07
Michael Rose <michael@rosenetwork.net>
2023-08-14 15:33:43 -0700

    editing dj.fish

--- dj.fish
+++ dj.fish
@@ -1,10 +1,10 @@
 function dj
 
-         set prior "$PWD"
-         while not test -d .git
-            cd ..
-         end
-        fd manage.py|each dirname|each cd
+        #  set prior "$PWD"
+        #  while not test -d .git
+        #     cd ..
+        #  end
+        # fd manage.py|each dirname|each cd
         if match $argv[1] run
            python manage.py runserver (either $argv[2..-1] 7000)
         else
