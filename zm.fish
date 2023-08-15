e1d6d0d37f2abdb18a2725d5f6239b9a96dfb5d3
Michael Rose <michael@rosenetwork.net>
2023-08-15 11:15:36 -0700

    editing zm.fish

--- zm.fish
+++ zm.fish
@@ -4,6 +4,10 @@ function zm
                      zoom_key_send.sh 'alt+v'
                 case a
                      zoom_key_send.sh 'alt+a'
+                case class
+                    xdg-open "https://generalassembly.zoom.us/j/98267579615?pwd=UC9QQm0rR2VVZ21DalZCQ0YrSGdIUT09"
+                case outcomes
+                     xdg-open "https://generalassembly.zoom.us/j/98569949670?pwd=WjV4WDJBTEIzeVB0YkRCcVJISEc0QT09"
         end
 
 end
