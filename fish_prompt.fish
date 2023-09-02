e816c388e89c88a6c8df991c05bf6d71fb4f130d
Michael Rose <michael@rosenetwork.net>
2023-09-01 22:14:06 -0700

    editing fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-        set myprompt ( timeout 0.5 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root,virtualenv --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare $status)
+        set myprompt ( timeout 0.5 powerline-rs --modules ssh,user,host,cwd,perms,git,gitstage,root,virtualenv --shell bare $status;or powerline-rs --modules ssh,user,host,cwd,perms,root --shell bare --theme ~/.config/powerline-rs/themes/michael.theme $status)
         echo ┏╸{$myprompt}
         echo "┗╸❯❯❯ "
         # starship prompt
