c17b793de6c2bdb13d3300f5587a64a0aa8e6e76
Michael Rose <michael@rosenetwork.net>
2023-09-25 13:10:44 -0700

    editing update-kernel.fish

--- update-kernel.fish
+++ update-kernel.fish
@@ -1,22 +1,22 @@
 function update-kernel
-	set target (pwd)
-	cd /usr/home/michael/extproj/voidxanmodK
-	git pull
-	cd linux-gcc
-	# set kernel (fd 'linux.\...-xanmod$'|cut -c3-18|select)
-	if set kernel (fd 'linux(5|6)\.[0-9]{1,2}-xanmod$'| choose -f / 0| select)
-		echo Preparing to install new kernel version $kernel
-		if read_confirm
-			set headers $kernel-headers
-			set dbg $kernel-dbg
-			set voidpkgs ~/extproj/void-packages
-			cp -R $kernel $voidpkgs/srcpkgs
-			cp -R $kernel $voidpkgs/srcpkgs
-			cp -R $dbg $voidpkgs/srcpkgs
-			cd $voidpkgs
-			./xbps-src pkg $kernel $headers -j6
-			sudo xbps-install --repository hostdir/binpkgs $kernel $headers
-		end
-	end
-	cd "$target"
+    set target (pwd)
+    cd /usr/home/michael/extproj/voidxanmodK
+    # git pull
+    cd linux-gcc
+    # set kernel (fd 'linux.\...-xanmod$'|cut -c3-18|select)
+    if set kernel (fd 'linux(5|6)\.[0-9]{1,2}-xanmod$'| choose -f / 0| select)
+        echo Preparing to install new kernel version $kernel
+        if read_confirm
+            set headers $kernel-headers
+            set dbg $kernel-dbg
+            set voidpkgs ~/extproj/void-packages
+            cp -R $kernel $voidpkgs/srcpkgs
+            cp -R $kernel $voidpkgs/srcpkgs
+            cp -R $dbg $voidpkgs/srcpkgs
+            cd $voidpkgs
+            ./xbps-src pkg $kernel $headers -j6
+            sudo xbps-install --repository hostdir/binpkgs $kernel $headers
+        end
+    end
+    cd "$target"
 end
