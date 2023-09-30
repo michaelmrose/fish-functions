03be0ceac4805fe3c284cb445c066e67bea5f65e
Michael Rose <michael@rosenetwork.net>
2023-09-30 13:45:32 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -3,7 +3,7 @@ function tdown
         set title  (echo $info | cut -d @ -f1| trim)
         set duration (echo $info | cut -d @ -f2 | trim)
         mkdir -p /tmp/timerslist
-        ffmpeg -f lavfi -i color=c=black:s=640x480:d=5 -i ~/sounds/alarm.mp3 -vf "drawtext=fontfile=/usr/share/fonts/TTF/DejaVuSans-Bold.ttf:text='Hello, World!':fontsize=24:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2" -shortest ~/sounds/alarmvids/"$title"
+        ffmpeg -f lavfi -i color=c=black:s=640x480:d=5 -i ~/sounds/alarm.mp3 -vf "drawtext=fontfile=/usr/share/fonts/TTF/DejaVuSans-Bold.ttf:text='Hello, World!':fontsize=24:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2" -shortest ~/sounds/alarmvids/"$title".mp4
         eval kitty  --class timer -o font_size=100 -e termdown --no-figlet -v en-us -T "$title" $duration -o /tmp/timerslist/"$title"
 
         msg Times up! Time for $title
@@ -11,5 +11,5 @@ function tdown
             if [ $status = 0 ]
                 echo "$title" completed at (date) > /tmp/timers-complete.txt
             end
-        
+
 end
