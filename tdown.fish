223716bcc25bc5fd4abd134511012c43c6dc7a87
Michael Rose <michael@rosenetwork.net>
2023-09-30 14:15:31 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -6,10 +6,10 @@ function tdown
     if not test -f ~/sounds/alarmvids/"$title".mp4
         ffmpeg -f lavfi -i color=c=black:s=1920x1080:d=5 -i ~/sounds/alarm.mp3 -vf "drawtext=fontfile=/usr/share/fonts/TTF/DejaVuSans-Bold.ttf:text=\'$title\':fontsize=100:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2" -shortest ~/sounds/alarmvids/"$title".mp4
     end
-    eval kitty  --class timer -o font_size=50 -e termdown --no-figlet -v en-us -T "$title" $duration -o /tmp/timerslist/"$title"
+    eval kitty  --class timer -o font_size=50 -e termdown -s --no-figlet -v en-us -T "$title" $duration -o /tmp/timerslist/"$title"
 
-    msg Times up! Time for $title
         if [ $status = 0 ]
+            msg Times up! Time for $title
             echo "$title" completed at (date) > /tmp/timers-complete.txt
             mpv --force-window --loop-file=inf ~/sounds/alarmvids/"$title".mp4
         end
