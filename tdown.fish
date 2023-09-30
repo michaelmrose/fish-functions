ea97776645e16b2b65fa10592f84ccaf94402336
Michael Rose <michael@rosenetwork.net>
2023-09-30 13:10:13 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -3,7 +3,7 @@ function tdown
         set title  (echo $info | cut -d @ -f1| trim|quote)
         set duration (echo $info | cut -d @ -f2 | trim)
         mkdir -p /tmp/timerslist
-        eval kitty  --class timer -o font_size=100 -e termdown --no-figlet -v en-us -T (quote $title) $duration -o /tmp/timerslist/$title
+        eval kitty  --class timer -o font_size=100 -e termdown --no-figlet -v en-us -T $title $duration -o /tmp/timerslist/$title
         if [ $status = 0 ]
             echo $title completed at (date) > /tmp/timers-complete.txt
         end
