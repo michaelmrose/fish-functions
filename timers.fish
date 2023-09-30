1d715e05e44f40e2c2fc94c53bf25f7b49c389b0
Michael Rose <michael@rosenetwork.net>
2023-09-30 12:40:58 -0700

    editing timers.fish

--- timers.fish
+++ timers.fish
@@ -1,8 +1,10 @@
 function timers
-    echo -n "⏰ "
-    for timer in /tmp/timerslist/*
-        echo ⏺  (basename $timer):(sed -n 1p $timer)|string pad -w 20
-    end|condense_lines
+    if exists /tmp/timerslist/*
+        echo -n "⏰ "
+        for timer in /tmp/timerslist/*
+            echo ⏺  (basename $timer):(sed -n 1p $timer)|string pad -w 20
+        end|condense_lines
+    end
 
 
 
