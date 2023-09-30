26b5ba752a4383189d808a8a8e163bf074d926b0
Michael Rose <michael@rosenetwork.net>
2023-09-29 18:26:27 -0700

    editing tdown.fish

--- tdown.fish
+++ tdown.fish
@@ -1,14 +1,14 @@
 function tdown
-		set info (prompt 'title @ duration')
-		if not exists $info
-				last-timer
-				return 0
-		end
-		set title  (echo $info | cut -d @ -f1| trim)
-		set duration (echo $info | cut -d @ -f2 | trim)
-		eval kitty -o font_size=100 -e termdown --no-figlet -v en-us -T (quote $title) $duration
-		if [ $status = 0 ]
-			echo $title completed at (date) > /tmp/timers-complete.txt
-			# mpv --no-video ~/sounds/alarm.m4a
-		end
+        set info (prompt 'title @ duration')
+        if not exists $info
+                last-timer
+                return 0
+        end
+        set title  (echo $info | cut -d @ -f1| trim)
+        set duration (echo $info | cut -d @ -f2 | trim)
+        mkdir -p /tmp/timerslist
+        eval kitty -o font_size=100 -e termdown --no-figlet -v en-us -T (quote $title) $duration -o /tmp/timerlist/$title
+        if [ $status = 0 ]
+            echo $title completed at (date) > /tmp/timers-complete.txt
+        end
 end
