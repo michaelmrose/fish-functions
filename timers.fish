e89cc150ab840ebc353322b37a3713b366e57646
Michael Rose <michael@rosenetwork.net>
2023-09-30 12:14:45 -0700

    editing timers.fish

--- timers.fish
+++ timers.fish
@@ -1,7 +1,7 @@
 function timers
     echo -n "⏱ "
     for timer in /tmp/timerslist/*
-        echo  (basename $timer):(cat $timer|head -1)
+        echo ⏺  (basename $timer):(cat $timer|head -1)
     end|condense_lines
 
 
