c1951d08df818c89185257bbf46995ab7cfd8706
Michael Rose <michael@rosenetwork.net>
2023-10-07 18:08:57 -0700

    editing volume-status-line.fish

--- volume-status-line.fish
+++ volume-status-line.fish
@@ -1,9 +1,10 @@
 function volume-status-line
-	switch (get-active-audio-port)
-		case analog-output-headphones
-			set icon 
-		case analog-output-lineout
-			set icon 
-	end
-	i3status-rs-colorize Warning $icon (ponymix get-volume)%
+    switch (get-active-audio-port)
+        case analog-output-headphones
+            set icon 
+        case analog-output-lineout
+            set icon 
+    end
+    set volume (amixer -c 2 get Master | grep -oP '\[\d+%\]' | awk -F"[" '{gsub(/%|\]/,""); print $2}')
+    i3status-rs-colorize Warning $icon $volume%
 end
