ba17ffc546741810899e5e8935ca7f5e17714f7e
Michael Rose <michael@rosenetwork.net>
2023-11-19 21:50:19 -0800

    editing volume-status-line.fish

--- volume-status-line.fish
+++ volume-status-line.fish
@@ -5,6 +5,6 @@ function volume-status-line
         case analog-output-lineout
             set icon 
     end
-    set volume (amixer -c 2 get Master | grep -oP '\[\d+%\]' | awk -F"[" '{gsub(/%|\]/,""); print $2}')
+    set volume (amixer -c 1 get Master | grep -oP '\[\d+%\]' | awk -F"[" '{gsub(/%|\]/,""); print $2}')
     i3status-rs-colorize Warning $icon $volume%
 end
