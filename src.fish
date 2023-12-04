998faa2f8a25ecd84ef925fa4b42b66cc0fdf5da
Michael Rose <michael@rosenetwork.net>
2023-12-04 14:27:21 -0800

    editing src.fish

--- src.fish
+++ src.fish
@@ -1,4 +1,17 @@
-# Defined in /home/michael/.config/fish/buffer/src.fish @ line 1
 function src
-	bat (which $argv)
+    set dir $PWD
+
+    if test (count $argv) -eq 2
+        set subdir $argv[1]
+        set app $argv[2]
+    else
+        set app $argv[1]
+    end
+
+    cd ~/extproj/void-packages/
+    ./xbps-src pkg $argv
+    sudo xbps-install -R hostdir/binpkgs/$subdir $app
+    set res $status
+    cd $dir
+    return $res
 end
