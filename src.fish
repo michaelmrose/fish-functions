b1387c2d71daf3b4d9ffd07ea23a27e717fd6f70
Michael Rose <michael@rosenetwork.net>
2023-12-04 15:06:49 -0800

    editing src.fish

--- src.fish
+++ src.fish
@@ -1,6 +1,6 @@
 function src
     set dir $PWD
-    set app $arg[1]
+    set app $argv[1]
     cd ~/extproj/void-packages/
     set subdir (bat srcpkgs/{$app}/template|g repo|choose -f = 1)
     ./xbps-src pkg $app
