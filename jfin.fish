edfafd5b5b234a195cb92b8dbc20f2c27ed35473
Michael Rose <michael@rosenetwork.net>
2023-12-05 15:30:38 -0800

    editing jfin.fish

--- jfin.fish
+++ jfin.fish
@@ -1,6 +1,7 @@
 function jfin
-	chromium --app=http://localhost:8096 &
-	pgrep jellyfin-mpv|each kill -9
-	sleep 5
-	jellyfin-mpv-shim &
+    # chromium --app=http://localhost:8096 &
+    flatpak run com.github.iwalton3.jellyfin-media-player
+    pgrep jellyfin-mpv|each kill -9
+    sleep 5
+    jellyfin-mpv-shim &
 end
