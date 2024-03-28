136b0e773821355983d675143962455875a857a5
Michael Rose <michael@rosenetwork.net>
2024-03-27 17:58:37 -0700

    editing up?.fish

--- up?.fish
+++ up?.fish
@@ -1,4 +1,7 @@
-# Defined interactively
 function up?
-    service status $argv
+    set service_status (service status $argv)
+    echo $service_status
+   if [ (echo $service_status | choose -f : 0 ) = down]
+       return 1
+   end
 end
