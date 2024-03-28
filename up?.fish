e4ee607d8448fd1ecaac43f047a1231355d79e13
Michael Rose <michael@rosenetwork.net>
2024-03-27 17:58:55 -0700

    editing up?.fish

--- up?.fish
+++ up?.fish
@@ -1,7 +1,7 @@
 function up?
     set service_status (service status $argv)
     echo $service_status
-   if [ (echo $service_status | choose -f : 0 ) = down]
+   if [ (echo $service_status | choose -f : 0 ) = down ]
        return 1
    end
 end
