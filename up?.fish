91cac7994004716deaaf8896b22ae610cd3ae79b
Michael Rose <michael@rosenetwork.net>
2024-03-27 18:08:14 -0700

    editing up?.fish

--- up?.fish
+++ up?.fish
@@ -1,8 +1,3 @@
 function up?
-   #  set service_status (service status $argv)
-   #  echo $service_status
-   # if [ (echo $service_status | choose -f : 0 ) = down ]
-   #     return 1
-   # end
    test (service status $argv | choose -f : 0) = "run"
 end
