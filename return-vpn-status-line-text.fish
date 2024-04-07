8682f50a07d549f0c701d300d5737014cd256c33
Michael Rose <michael@rosenetwork.net>
2024-04-07 12:26:15 -0700

    editing return-vpn-status-line-text.fish

--- return-vpn-status-line-text.fish
+++ return-vpn-status-line-text.fish
@@ -1,16 +1,14 @@
 function return-vpn-status-line-text
-	set locked '{"state":"Warning", "text": "  "}'
-	set unlocked '{"state":"Info", "text": "    "}'
-	set vpn_disconnected_string '\'Wired connection 1\' is now the primary connection'
-	set vpn_connected_string '\'mullvad_us_sea\' is now the primary connection'
-	set vpn_connected_string 'tun0: using connection \'tun0\''
-	set vpn_disconnected_string 'tun0: device removed'
-	while read -l line
-		switch $line
-			case $vpn_connected_string
-				echo $locked
-			case $vpn_disconnected_string
-				echo $unlocked
-		end
-	end
+    set locked '{"state":"Warning", "text": "  "}'
+    set unlocked '{"state":"Info", "text": "    "}'
+    set vpn_connected_string 'wg-US-WA-53: connected'
+    set vpn_disconnected_string 'wg-US-WA-53: disconnected'
+    while read -l line
+        switch $line
+            case $vpn_connected_string
+                echo $locked
+            case $vpn_disconnected_string
+                echo $unlocked
+        end
+    end
 end
