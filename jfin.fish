8367aef69f35a97d3cbfc15043f0e2f3542f75f5
Michael Rose <michael@rosenetwork.net>
2024-04-25 17:03:43 -0700

    removed jfin.fish

--- jfin.fish
+++
@@ -1,7 +0,0 @@
-function jfin
-    # chromium --app=http://localhost:8096 &
-    flatpak run com.github.iwalton3.jellyfin-media-player
-    # pgrep jellyfin-mpv|each kill -9
-    # sleep 5
-    # jellyfin-mpv-shim &
-end
