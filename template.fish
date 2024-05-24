e280bebbbe7c20fadc64d8c6a639286f9d5c3297
Michael Rose <michael@rosenetwork.net>
2024-05-24 16:25:55 -0700

    editing template.fish

--- template.fish
+++ template.fish
@@ -1,7 +1,7 @@
 function template
     switch $argv[1]
         case version
-            template $argv[2] | g '^version=' | choose -f 1
+            template $argv[2] | g '^version=' | choose -f = 1
         case '*'
             bat /usr/home/michael/extproj/void-packages/srcpkgs/{$argv}/template
     end
