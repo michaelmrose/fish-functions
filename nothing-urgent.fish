25efb5027c666dc31d761a5c86b9c29f36ad294a
Michael Rose <michael@rosenetwork.net>
2024-06-02 10:20:30 -0700

    editing nothing-urgent.fish

--- nothing-urgent.fish
+++ nothing-urgent.fish
@@ -2,9 +2,9 @@ function nothing-urgent
     # for i in (windows-list class)
   #       noturgent $i
   #   end
-    for i in (xdotool search --class .\*)
-        xdotool set_window --urgency 0 $i
-    end
+    # for i in (xdotool search --class .\*)
+    #     xdotool set_window --urgency 0 $i
+    # end
     set wins (xdotool search --class .\*)
     xdotool set_window --urgency 0 $wins
 end
