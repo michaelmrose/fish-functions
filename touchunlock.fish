2510054295d2271b69872d3b912f42efc985b960
Michael Rose <michael@rosenetwork.net>
2024-06-15 15:52:16 -0700

    editing touchunlock.fish

--- touchunlock.fish
+++ touchunlock.fish
@@ -1,6 +1,6 @@
 function touchunlock
     pcsc_scan | while read -l line
-        if echo $line | grep '^ccccc'
+        if echo $line | grep -q  '^ccccc'
             echo read $line
             if locked?
                 killall i3lock
