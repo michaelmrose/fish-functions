565c1a810b7aee776507380dbc89c3f5585b49f7
Michael Rose <michael@rosenetwork.net>
2024-06-15 15:47:50 -0700

    editing touchunlock.fish

--- touchunlock.fish
+++ touchunlock.fish
@@ -1,8 +1,7 @@
 function touchunlock
-pcsc_scan | while read -l line
-    if locked?
-        killall i3lock
-        sleep 0.1
+    pcsc_scan | while read -l line
+        if locked?
+            killall i3lock
+        end
     end
-end
 end
