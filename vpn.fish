824b5d94c741c3e3166bfa6f0c757c1816958ea8
Michael Rose <michael@rosenetwork.net>
2024-06-18 12:28:30 -0700

    editing vpn.fish

--- vpn.fish
+++ vpn.fish
@@ -1,5 +1,9 @@
 function vpn
     switch $argv[1]
+        case -c
+            if not nmcli connection show wg-US-WA-53 | grep -i 'GENERAL.STATE'
+                nmcli connection up wg-US-WA-53
+            end
         case -t
             if nmcli connection show wg-US-WA-53 | grep -i 'GENERAL.STATE'
                 # disconnect
