f8c7a37cb1e7e8949a1d61668935d045a424243d
Michael Rose <michael@rosenetwork.net>
2024-07-15 12:13:43 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -2,8 +2,13 @@ function xrr
     switch $argv
         case center
             xrandr --dpi 163 --output DP-2 --auto --output DP-0 --off --output HDMI-0 --off
+            wp recall
+            stop picom
         case all
             xrandr --dpi 163 --output DP-0 --pos 0x0  --mode 3840x2160 --output DP-2 --pos 3840x0 --mode 3840x2160 --output HDMI-0 --pos 7680x0 --mode 1920x1080 --scale 2.0x2.0 --panning 3840x2160
+            wp recall
+            restart picom
+
         case 'toggle'
             if test (count (get-connected-displays)) -eq 3
                 xrr center
@@ -11,6 +16,4 @@ function xrr
                 xrr all
             end
     end
-    wp recall
-    restart picom
 end
