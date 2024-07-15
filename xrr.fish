2e8852f81a3e7ed70daa069fecb499e2b262d4cc
Michael Rose <michael@rosenetwork.net>
2024-07-15 12:16:00 -0700

    editing xrr.fish

--- xrr.fish
+++ xrr.fish
@@ -5,10 +5,9 @@ function xrr
             wp recall
             stop picom
         case all
-            xrandr --dpi 163 --output DP-0 --pos 0x0  --mode 3840x2160 --output DP-2 --pos 3840x0 --mode 3840x2160 --output HDMI-0 --pos 7680x0 --mode 1920x1080 --scale 2.0x2.0 --panning 3840x2160
+            xrandr --dpi 163 --output DP-0 --pos 0x0  --mode 3840x2160 --output DP-2 --pos 3840x0 --mode 3840x2160 --output HDMI-0 --pos 7680x0 --mode 1920x1080 --scale 2.0x2.0
             wp recall
             restart picom
-
         case 'toggle'
             if test (count (get-connected-displays)) -eq 3
                 xrr center
