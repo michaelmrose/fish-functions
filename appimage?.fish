8124ea6be0e812d4462d45f6fffed733eab6cf2c
Michael Rose <michael@rosenetwork.net>
2024-07-25 10:31:30 -0700

    editing appimage?.fish

--- appimage?.fish
+++ appimage?.fish
@@ -1,10 +1,18 @@
 function appimage?
- set output (appimagetool --list $argv[1] 2>&1)
-    if string match -q "*sqfs_open_image error*" "$output"
-        echo "$argv[1] is an app image, but there was an error reading it"
-    else if string match -q "*Listing files in AppImage*" "$output"
+  # First, check if appimagetool can list the files
+    set output (appimagetool --list $argv[1] 2>&1)
+    if string match -q "*Listing files in AppImage*" "$output"
         echo "$argv[1] is an app image"
+        return
+    end
+
+    # If appimagetool fails, check for ISO 9660 format using isoinfo
+    set iso_output (isoinfo -d -i $argv[1] 2>&1)
+    if string match -q "*No such file*" "$iso_output"
+        echo "$argv[1] is not an app image"
+    else if string match -q "*ISO 9660*" "$iso_output"
+        echo "$argv[1] might be an app image, but there was an error reading it"
     else
-        # echo "$argv[1] is not an app image"
+        echo "$argv[1] is not an app image"
     end
 end
