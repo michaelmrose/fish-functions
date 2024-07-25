b92f1e3791c16c5e3bc8b4dea557ab55d84c6930
Michael Rose <michael@rosenetwork.net>
2024-07-25 10:29:53 -0700

    editing appimage?.fish

--- appimage?.fish
+++ appimage?.fish
@@ -1,8 +1,10 @@
 function appimage?
-    if appimagetool --list $argv[1] >/dev/null 2>&1
+ set output (appimagetool --list $argv[1] 2>&1)
+    if string match -q "*sqfs_open_image error*" "$output"
+        echo "$argv[1] is an app image, but there was an error reading it"
+    else if string match -q "*Listing files in AppImage*" "$output"
         echo "$argv[1] is an app image"
     else
         # echo "$argv[1] is not an app image"
     end
-
 end
