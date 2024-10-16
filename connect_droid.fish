b0590d29c0ecb19fcb02a7d3f059fb19c7f86503
Michael Rose <michael@rosenetwork.net>
2024-10-16 16:53:19 -0700

    editing connect_droid.fish

--- connect_droid.fish
+++ connect_droid.fish
@@ -1,7 +1,7 @@
 function connect_droid
     set port (sudo nmap -p 1000-40000 192.168.1.42|g 'open.*unknown' |choose -f '/' 0)
     set ip 192.168.1.42
-    if exists $port and ; and exists ip
+    if exists $port and ; and exists $ip
         echo port is $port and ip is $pi
         adb connect $ip:$port
     end
