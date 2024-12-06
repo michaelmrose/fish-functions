a750de7ba69bc3edbaa314e4978305da3443842f
Michael Rose <michael@rosenetwork.net>
2024-12-05 21:25:37 -0800

    editing volume-status-line.fish

--- volume-status-line.fish
+++ volume-status-line.fish
@@ -1,10 +1,10 @@
 function volume-status-line
     switch (get-active-audio-port)
         case headset
-            set icon 
+            set icon " "
         case speaker
             set icon 
     end
     set volume (ponymix get-volume)
-    i3status-rs-colorize Warning $icon  $volume%
+    i3status-rs-colorize Warning $icon $volume%
 end
