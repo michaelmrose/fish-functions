4b5344a68f4e6b971197a37aebf25c1573034611
Michael Rose <michael@rosenetwork.net>
2024-12-05 21:24:46 -0800

    editing volume-status-line.fish

--- volume-status-line.fish
+++ volume-status-line.fish
@@ -6,5 +6,5 @@ function volume-status-line
             set icon 
     end
     set volume (ponymix get-volume)
-    i3status-rs-colorize Warning $icon $volume%
+    i3status-rs-colorize Warning $icon  $volume%
 end
