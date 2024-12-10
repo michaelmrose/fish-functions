bfb531c1299719362a96fbe323e32be2937fa580
Michael Rose <michael@rosenetwork.net>
2024-12-10 11:52:47 -0800

    editing desktop-number-by-name.fish

--- desktop-number-by-name.fish
+++ desktop-number-by-name.fish
@@ -1,3 +1,16 @@
 function desktop-number-by-name
-xprop -root _NET_DESKTOP_NAMES | awk -v name="$argv" 'BEGIN {FS=", \"|\""} {for (i=2; i<=NF; i++) if ($i == name) print i-2}'
+  xprop -root _NET_DESKTOP_NAMES |
+    awk -v name="$argv[1]" '
+        BEGIN { FS = ", *\"|\"| *, *"; index_found = -1 }
+        {
+            for (i = 2; i <= NF; i++) {
+                if ($i == name) {
+                    print i - 2
+                    exit
+                }
+            }
+        }
+        END {
+            if (index_found == -1) print "*"
+        }'
 end
