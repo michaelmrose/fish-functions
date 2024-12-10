426fc38980eed44be0e69a1da02ebbf90d7c7c59
Michael Rose <michael@rosenetwork.net>
2024-12-10 12:26:07 -0800

    editing desktop-number-by-name.fish

--- desktop-number-by-name.fish
+++ desktop-number-by-name.fish
@@ -1,3 +1,3 @@
 function desktop-number-by-name
-findindex i (explode-words (xprop -root _NET_DESKTOP_NAMES|choose -f '= ' 1|sd '"|,' '')) | subtract 1
+findindex $argv (explode-words (xprop -root _NET_DESKTOP_NAMES|choose -f '= ' 1|sd '"|,' '')) | subtract 1
 end
