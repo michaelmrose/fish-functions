97780b2d59bdeb0184fd43fb2040773b60eddbe1
Michael Rose <michael@rosenetwork.net>
2024-12-10 12:15:04 -0800

    editing kill-workspace.fish

--- kill-workspace.fish
+++ kill-workspace.fish
@@ -1,4 +1,16 @@
 function kill-workspace
-	select-all-in-workspace
-    i3-msg kill
+    # if exists $argv
+    #     xdotool search --desktop (desktop-number-by-name i) ''|each wmctrl -ic
+    # else
+    #     select-all-in-workspace
+    #     i3-msg kill
+    # end
+
+    if exists $argv
+        set target (desktop-number-by-name $argv)
+    else
+        set target (xprop -root _NET_CURRENT_DESKTOP)
+    end
+        xdotool search --desktop (desktop-number-by-name $target) ''|each wmctrl -ic
+
 end
