96bf22fa4bc7769a5beb2a37d57cd6455cb5e4af
Michael Rose <michael@rosenetwork.net>
2024-12-10 17:10:28 -0800

    editing get-number-of-current-workspace.fish

--- get-number-of-current-workspace.fish
+++ get-number-of-current-workspace.fish
@@ -1,3 +1,4 @@
 function get-number-of-current-workspace
-	findindex (get-focused-workspace) (get-workspaces-on-focused-display)
+    # findindex (get-focused-workspace) (get-workspaces-on-focused-display)
+    xprop -root _NET_CURRENT_DESKTOP | choose 2
 end
