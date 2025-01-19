4a80c2f274707ff98e0333a18616d82ad547fb42
Michael Rose <michael@rosenetwork.net>
2025-01-19 14:40:47 -0800

    editing ternary.fish

--- ternary.fish
+++ ternary.fish
@@ -1,17 +1,17 @@
 function ternary
-	set test (echo $argv | cut -d '@' -f1 | trim)
+    set test (echo $argv | cut -d '@' -f1 | trim)
   set s1 (echo $argv | cut -d '@' -f2 | trim)
   set s2 (echo $argv | cut -d '@' -f3 | trim)
-	if eval $test
+    if eval $test
     # echo its all true\!
     if defined (explode-words $s1)[1] 2> /dev/null
-      eval $s1
+      eval $s1 > /dev/null
     else
       echo $s1
     end
     else
       if defined (explode-words $s2)[1] 2> /dev/null
-        eval $s2
+        eval $s2 > /dev/null
       else
         echo $s2
       end
