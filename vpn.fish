f0b6a5490f68d27a04101d72da9094c307cec633
Michael Rose <michael@rosenetwork.net>
2025-01-19 13:42:52 -0800

    editing vpn.fish

--- vpn.fish
+++ vpn.fish
@@ -1,22 +1,22 @@
 function vpn
     switch $argv[1]
         case -c
-            if not nmcli connection show wg-US-WA-140 | grep -i 'GENERAL.STATE'
-                nmcli connection up wg-US-WA-140
+            if not nmcli connection show $VPN_CONNECTION | grep -i 'GENERAL.STATE'
+                nmcli connection up $VPN_CONNECTION
             end
         case -d
-            if nmcli connection show wg-US-WA-140 | grep -i 'GENERAL.STATE'
-                nmcli connection down wg-US-WA-140
+            if nmcli connection show $VPN_CONNECTION | grep -i 'GENERAL.STATE'
+                nmcli connection down $VPN_CONNECTION
             end
         case -p
             set -U VPN_CONNECTION (nmcli connection show|g wireguard|choose 0| select)
         case -t
-            if nmcli connection show wg-US-WA-140 | grep -i 'GENERAL.STATE'
+            if nmcli connection show $VPN_CONNECTION | grep -i 'GENERAL.STATE'
                 # disconnect
-                nmcli connection down wg-US-WA-140
+                nmcli connection down $VPN_CONNECTION
             else
                 # connect
-                nmcli connection up wg-US-WA-140
+                nmcli connection up $VPN_CONNECTION
             end
     end
 end
