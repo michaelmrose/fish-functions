fe2e6a1b6ac1e6b56b48eac4572082c06042d2b8
Michael Rose <michael@rosenetwork.net>
2025-01-19 14:43:57 -0800

    editing return-vpn-status-line-text.fish

--- return-vpn-status-line-text.fish
+++ return-vpn-status-line-text.fish
@@ -2,11 +2,16 @@ function return-vpn-status-line-text
     set locked '{"state":"Warning", "text": "  "}'
     set unlocked '{"state":"Info", "text": "    "}'
     while read -l line
-        switch $line
-            case "$VPN_CONNECTION: connected"
-                echo $locked
-            case "$VPN_CONNECTION: disconnected"
-                echo $unlocked
+        if string match line "$VPN_CONNECTION: connected"
+            echo $locked
+        else
+            echo $unlocked
         end
+        # switch $line
+        #     case "$VPN_CONNECTION: connected"
+        #         echo $locked
+        #     case "$VPN_CONNECTION: disconnected"
+        #         echo $unlocked
+        # end
     end
 end
