46082adf7177b044ef9d4223aa0ee92c46781fb0
Michael Rose <michael@rosenetwork.net>
2025-02-27 13:19:43 -0800

    editing rat.fish

--- rat.fish
+++ rat.fish
@@ -1,4 +1,8 @@
 function rat
     set name (ratbagctl list |choose -f ':| ' 0 )
-    ratbagctl $name $argv
+    if isnumeric $argv[1]
+        rat profile active set $argv[1]
+    else
+        ratbagctl $name $argv
+    end
 end
