31686dcef66c324b2802c19c5b1db71d40d071ec
Michael Rose <michael@rosenetwork.net>
2025-03-10 23:10:15 -0700

    editing spacefm_autojump.fish

--- spacefm_autojump.fish
+++ spacefm_autojump.fish
@@ -6,18 +6,7 @@ function spacefm_autojump
         # Get the active window ID
         set win_id (xdotool getactivewindow)
 
-        # Get the PID of the active SpaceFM window
-        set spacefm_pid (xdotool getwindowpid $win_id 2>/dev/null)
-
-            # Locate SpaceFM's socket for this PID
-            set socket_path "/tmp/spacefm-socket-$spacefm_pid"
-
-            # Ensure the socket exists before sending the command
-            if test -S "$socket_path"
-                spacefm -s --socket "$socket_path" set current_dir "$selection" 2>/dev/null
-            else
-                echo "Error: Could not find SpaceFM socket for PID $spacefm_pid" >&2
-            end
+        spacefm  set current_dir "$selection" 2>/dev/null
         wmctrl -ia $window
     end
 
