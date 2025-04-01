2f96d1f2b452d752539da4b377589aeb558035e2
Michael Rose <michael@rosenetwork.net>
2025-04-01 15:19:40 -0700

    editing soundcard-profile.fish

--- soundcard-profile.fish
+++ soundcard-profile.fish
@@ -1,3 +1,18 @@
 function soundcard-profile
-    wpctl set-profile (pw-dump | jq ".[] | select(.info.props."device.description"? // empty | test("Navi")) | .id") off
+    set search $argv[1]
+    set profile $argv[2]
+
+    if test -z "$search" -o -z "$profile"
+        echo "Usage: soundcard-profile <search-string> <profile>"
+        return 1
+    end
+
+    set id (pw-dump | jq --arg s "$search" '.[] | select(.info.props."device.description"? // empty | test($s)) | .id')
+
+    if test -z "$id"
+        echo "No matching sound card found for '$search'"
+        return 1
+    end
+
+    wpctl set-profile $id $profile
 end
