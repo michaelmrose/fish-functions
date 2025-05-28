1daaa18e8eb87899ccea84c16e476188b8cf02ea
Michael Rose <michael@rosenetwork.net>
2025-05-27 22:52:17 -0700

    editing raise-or-run.fish

--- raise-or-run.fish
+++ raise-or-run.fish
@@ -1,3 +1,3 @@
-function raise-or-run --argument-names class --argument-names toRun
+function raise-or-run --argument-names toRun --argument-names class
     i3-msg "[class=$class]" focus; or $toRun
 end
