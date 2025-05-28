cf33038718d6b87c14dc8e3be3db4d3f746f7161
Michael Rose <michael@rosenetwork.net>
2025-05-27 23:54:40 -0700

    editing vpn-status-line.fish

--- vpn-status-line.fish
+++ vpn-status-line.fish
@@ -1,4 +1,4 @@
 function vpn-status-line
-	echo '{"state":"Info", "text": "    "}'
-	nmcli monitor | return-vpn-status-line-text
+    echo '{"state":"Info", "text": "  "}'
+    nmcli monitor | return-vpn-status-line-text
 end
