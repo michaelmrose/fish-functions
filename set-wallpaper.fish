ffdd470876155f02457d3ce5c7692f8297b947f1
Michael Rose <michael@rosenetwork.net>
2025-07-06 13:46:49 -0700

    editing set-wallpaper.fish

--- set-wallpaper.fish
+++ set-wallpaper.fish
@@ -1,15 +1,15 @@
 function set-wallpaper
-		set img (pathof $argv[1])
-		set format (get-feh-format $img)
-		feh --bg-{$format} $img
-		add-to-recent-backgrounds $img
-		set -U bgstyle (cutlastn "/" 2 $img)
-		set-lightdm-bg $img
-		set -U bgimage $img
+        set img (pathof $argv[1])
+        set format (get-feh-format $img)
+        feh --bg-{$format} $img
+        add-to-recent-backgrounds $img
+        set -U bgstyle (cutlastn "/" 2 $img)
+        set-lightdm-bg $img
+        set -U bgimage $img
         walfn
-		convert $img -resize 2000x2000 ~/.cache/wal/rofi.jpg
-		pywalfox update
-		betterlockscreen -u $bgimage --fx &
+        convert $img -resize 2000x2000 ~/.cache/wal/rofi.jpg
+        pywalfox update
+        betterlockscreen -u $bgimage --fx > /dev/null &
         wal -n --saturate 0.7 -i $img
         xrdb ~/.cache/wal/colors-i3.conf
         # restart-i3
