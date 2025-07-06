369e368f17cb10b071d45f424e8c9936510703aa
Michael Rose <michael@rosenetwork.net>
2025-07-06 14:25:58 -0700

    editing set-wallpaper.fish

--- set-wallpaper.fish
+++ set-wallpaper.fish
@@ -1,8 +1,5 @@
 function set-wallpaper
         set img (pathof $argv[1])
-        if exists $argv[2]
-            set theme $argv[2]
-        end
         set format (get-feh-format $img)
         feh --bg-{$format} $img
         add-to-recent-backgrounds $img
@@ -13,11 +10,7 @@ function set-wallpaper
         convert $img -resize 2000x2000 ~/.cache/wal/rofi.jpg
         pywalfox update
         betterlockscreen -u $bgimage --fx > /dev/null &
-        if exists $theme
-            wal -n --saturate 0.7  --theme $theme
-        else
-            wal -n --saturate 0.7 -i $img
-        end
+        wal -n --saturate 0.7 -i $img
         xrdb ~/.cache/wal/colors-i3.conf
         i3 restart
         restart picom
