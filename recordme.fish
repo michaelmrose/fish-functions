89c3ee3d1af30e85bf5c3e9fa26c66e7dea025d3
Michael Rose <michael@rosenetwork.net>
2025-07-28 22:41:52 -0700

    editing recordme.fish

--- recordme.fish
+++ recordme.fish
@@ -1,9 +1,10 @@
 function recordme
 ffmpeg \
   -f v4l2 -i /dev/video0 \
-  -f pulse -i default \
-  -t 00:05:00 \
+  -f pulse -i alsa_input.usb-046d_Logi_4K_Stream_Edition_01ED6582-03.analog-stereo \
+  -t 00:01:00 \
   -c:v libx265 -preset ultrafast \
   -c:a aac \
   "recording_$(date +%Y-%m-%d_%H-%M-%S).mp4"
+
 end
