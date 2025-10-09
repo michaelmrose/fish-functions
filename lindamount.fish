3eb8d06fbff36388a490345803595c9cf945e010
Michael Rose <michael@rosenetwork.net>
2025-10-09 14:50:30 -0700

    editing lindamount.fish

--- lindamount.fish
+++ lindamount.fish
@@ -1,3 +1,3 @@
 function lindamount
-sudo mount -t cifs -o username=linda,password=$lindapw,uid=(id -u),gid=(id -g) //192.168.1.3/$argv[1] $argv[2]
+sudo mount -t cifs -o username=linda,password=$lindapw,uid=(id -u),gid=(id -g) //192.168.0.3/$argv[1] $argv[2]
 end
