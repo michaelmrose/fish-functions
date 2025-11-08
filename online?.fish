e6e1550522d85cab5efdeafdbb28063b4f5d1885
Michael Rose <michael@rosenetwork.net>
2025-11-07 17:06:39 -0800

    editing online?.fish

--- online?.fish
+++ online?.fish
@@ -1,8 +1,8 @@
 function online?
-	if exists $argv
+    if exists $argv
     set ip $argv
   else
     set ip 8.8.8.8
   end
-	ping $ip -c 1 -W 2 > /dev/null 2> /dev/null
+    ping $ip -c 1 -W 3 > /dev/null 2> /dev/null
 end
