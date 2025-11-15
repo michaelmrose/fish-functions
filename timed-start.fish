19247ac177e04140a18827d7c3e0ab1ea77c9dae
Michael Rose <michael@rosenetwork.net>
2025-11-15 08:12:06 -0800

    editing timed-start.fish

--- timed-start.fish
+++ timed-start.fish
@@ -1,7 +1,7 @@
 function timed-start
-	set app $argv[1..-2]
-	set class $argv[-1]
-	$app &
-	while not wmctrl -lx|awk '{print $3}'|cut -d . -f2|grep $class
-	end
+    set app $argv[1..-2]
+    set class $argv[-1]
+    $app &
+    while not wmctrl -lx|awk '{print $4}'|cut -d . -f2|grep $class
+    end
 end
