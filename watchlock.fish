1f100093a5d6a442177f73c1c9f3616d50ba7b28
Michael Rose <michael@rosenetwork.net>
2026-01-14 09:40:04 -0800

    editing watchlock.fish

--- watchlock.fish
+++ watchlock.fish
@@ -1,11 +1,16 @@
 function watchlock
     udevadm monitor | grep -iE --line-buffered '1050.0407.*hidraw' | while read -l line
+        echo line is $line
         if string match -q '*add*' -- $line
+            echo matched add
             if locked?
+                echo unlocking
                 killall i3lock
             end
         else if string match -q '*remove*' -- $line
+            echo matched remove
             if not locked?
+                echo locking
                 betterlockscreen -l &
             end
         end
