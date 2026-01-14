bd9e737b3ee0e0d8cc0a0e58d0a8cb2bc11006ac
Michael Rose <michael@rosenetwork.net>
2026-01-14 10:38:45 -0800

    editing monitor-device.fish

--- monitor-device.fish
+++ monitor-device.fish
@@ -1,9 +1,15 @@
 function monitor-device
     set -l device $argv[1]
-    set -l regex ".*(add|remove).*$device"
-    echo d ils $device and r is $regex
-    udevadm monitor 2>&1 \
-            | grep -iE --line-buffered ".*(add|remove).*$device" \
-            | sed -u -n 's/.*add.*/add/ip; s/.*remove.*/remove/ip'
 
+    # Escape regex metacharacters in the device string (at least dots)
+    set -l device_re (string replace -a '.' '\.' -- $device)
+
+    # Build the regex
+    set -l regex ".*(add|remove).*$device_re"
+
+    echo "d is $device and r is $regex"
+
+    udevadm monitor 2>&1 \
+        | grep -iE --line-buffered "$regex" \
+        | sed -u -n 's/.*\<add\>.*/add/ip; s/.*\<remove\>.*/remove/ip'
 end
