dbcc640bbbd4be9f7e55cdabfdea2525b6151533
Michael Rose <michael@rosenetwork.net>
2026-01-14 15:22:27 -0800

    removed monitor-device.fish

--- monitor-device.fish
+++
@@ -1,9 +0,0 @@
-function monitor-device
-    set -l device $argv[1]
-    set -l regex ".*(add|remove).*$device"
-
-    udevadm monitor 2>&1 \
-        | grep -iE --line-buffered "$regex" \
-        | sed -u -n 's/.*\<add\>.*/add/ip; s/.*\<remove\>.*/remove/ip'
-
-end
