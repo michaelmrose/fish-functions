bf4368599ed160612a54f806b82e6df698d1013d
Michael Rose <michael@rosenetwork.net>
2026-01-14 10:48:16 -0800

    editing monitor-device.fish

--- monitor-device.fish
+++ monitor-device.fish
@@ -2,7 +2,8 @@ function monitor-device
     set -l device $argv[1]
     set -l regex ".*(add|remove).*$device"
 
-    udevadm monitor 2>&1 \
-        | grep -iE --line-buffered "$regex" \
-        | sed -u -n 's/.*\<add\>.*/add/ip; s/.*\<remove\>.*/remove/ip'
+    stdbuf udevadm monitor 2>&1 \
+        | stdbuf grep -iE --line-buffered "$regex" \
+        | stdbuff sed -u -n 's/.*\<add\>.*/add/ip; s/.*\<remove\>.*/remove/ip'
+
 end
