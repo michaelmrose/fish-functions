6edd53e7e860df27157aaece0d1737ccf32e64f3
Michael Rose <michael@rosenetwork.net>
2026-01-14 10:40:01 -0800

    editing monitor-device.fish

--- monitor-device.fish
+++ monitor-device.fish
@@ -2,10 +2,10 @@ function monitor-device
     set -l device $argv[1]
 
     # Escape regex metacharacters in the device string (at least dots)
-    set -l device_re (string replace -a '.' '\.' -- $device)
+    # set -l device_re (string replace -a '.' '\.' -- $device)
 
     # Build the regex
-    set -l regex ".*(add|remove).*$device_re"
+    set -l regex ".*(add|remove).*$device"
 
     echo "d is $device and r is $regex"
 
