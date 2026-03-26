5c3536b68e1105cc57a0452fd17202bb9207963b
Michael Rose <michael@rosenetwork.net>
2026-03-26 13:58:43 -0700

    editing focus-monitor.fish

--- focus-monitor.fish
+++ focus-monitor.fish
@@ -2,7 +2,7 @@ function focus-monitor
     set mon (get-focused-display)
     switch $argv[1]
         case "left"
-            if $mon = "Displayport-1"
+            if string match $mon "Displayport-1"
                 xdotool key F10
             end
         case  '*'
