75e84ba6dc3d88a66f6c3ff44b155105ee66cc38
Michael Rose <michael@rosenetwork.net>
2026-03-26 14:02:34 -0700

    editing focus-direction.fish

---
+++ focus-direction.fish
@@ -0,0 +1,11 @@
+function focus-direction
+    switch $argv[1]
+        case "left"
+            if string match (get-focused-display) "Displayport-1"
+                echo true
+                xdotool key F10
+            end
+        case  '*'
+            i3-msg focus $argv[1]
+    end
+end
