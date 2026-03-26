a4782e4c80f2d4f68469c48c7487a460846880f9
Michael Rose <michael@rosenetwork.net>
2026-03-26 15:48:28 -0700

    editing lockme.fish

--- lockme.fish
+++ lockme.fish
@@ -1,5 +1,11 @@
 function lockme
     symbol-lock off
+    if laptop-available
+        lock-laptop
+    end
     betterlockscreen -l
+    if laptop-available
+        unlock-laptop
+    end
     symbol-lock on
 end
