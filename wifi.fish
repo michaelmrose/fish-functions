36d849dd74243dda76a351cf924be6484a2bdc04
Michael Rose <michael@rosenetwork.net>
2026-03-28 13:12:46 -0700

    editing wifi.fish

--- wifi.fish
+++ wifi.fish
@@ -2,15 +2,12 @@ function wifi
     switch $argv[1]
         case once
             nmcli device wifi connect $argv[2] --ask
+        case select
+            networks|select|fishstdin wifi once
         case '*'
             while not online?
                 nmcli device wifi connect $argv[1] --ask
                 sleep 2
             end
-        case captive
-            wifi once $argv[2]
-            firefox rosenetwork.net
-        case select
-            networks|select|fishstdin wifi once
     end
 end
