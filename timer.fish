a6f217afed3130b18fc8b935707f9354099f7d1e
Michael Rose <michael@rosenetwork.net>
2026-05-07 21:07:57 -0700

    editing timer.fish

--- timer.fish
+++ timer.fish
@@ -1,11 +1,12 @@
-# Defined in /home/michael/.config/fish/buffer/timer.fish @ line 1
 function timer
-	set timerfile /tmp/mytimers
+    set timerfile /tmp/mytimers
     set com $argv[1]
     set title $argv[2]
     # duration in seconds
     set duration $argv[3]
     switch $com
+        case clear
+            rm  /tmp/timers-complete.txt
         case ls
             if not test -f $timerfile
                 echo none
