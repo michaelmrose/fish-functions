d5a243ebbc90b5c22b05f018b8f158bf1078c00e
Michael Rose <michael@rosenetwork.net>
2026-05-12 00:07:19 -0700

    editing orgToPdf.fish

---
+++ orgToPdf.fish
@@ -0,0 +1,14 @@
+function orgToPdf
+    set file "$argv[1]"
+    set out string replace -r 'org$' pdf $file
+pandoc $file \
+            --from=org \
+            --to=pdf \
+            --pdf-engine=lualatex \
+            --highlight-style=zenburn \
+            -V geometry:top=0.45in,bottom=0.65in,left=0.65in,right=0.65in \
+            -V mainfont="Libertinus Serif" \
+            -V sansfont="Libertinus Sans" \
+            -V monofont="JetBrains Mono" \
+            -o $out
+end
