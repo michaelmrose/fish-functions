fda0d1b597199ee5e402ecb55a1da0ec6e796e06
Michael Rose <michael@rosenetwork.net>
2026-08-10 00:58:20 -0700

    editing gzip-size.fish

--- gzip-size.fish
+++ gzip-size.fish
@@ -1,7 +1,7 @@
 function gzip-size
-    printf gzip
+    printf "gzip: "
     gzip -9 -c $argv | wc -c | numfmt --to=iec
-    echo brotli
+    printf "brotli "
     brotli -q 11 -c $argv | wc -c | numfmt --to=iec
 
 end
