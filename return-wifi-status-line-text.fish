97b215a08c428414c88c6ff7f1d9e93df912a8f8
Michael Rose <michael@rosenetwork.net>
2026-08-20 11:59:07 -0700

    editing return-wifi-status-line-text.fish

--- return-wifi-status-line-text.fish
+++ return-wifi-status-line-text.fish
@@ -1,5 +1,6 @@
 function return-wifi-status-line-text
     while read -l line
-        echo 󰖩:   (nmcli connection show|g wlan|choose 0)
+        set name (nmcli connection show|g wlan|choose 0)
+        echo " 󰖩:  $name"
     end
 end
