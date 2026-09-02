9ae9da58a1fedaaad51b21347493b5e0d9798ccd
Michael Rose <michael@rosenetwork.net>
2026-09-02 12:49:40 -0700

    editing vsorted.fish

--- vsorted.fish
+++ vsorted.fish
@@ -1,3 +1,10 @@
 function vsorted
-    printf '%s\n' *.* | sort -V
+    printf '%s\n' *.* |
+        awk 'match($0, /[0-9]+(\.[^.]+)?$/) {
+            s = substr($0, RSTART, RLENGTH)
+            sub(/\.[^.]+$/, "", s)
+            print s "\t" $0
+        }' |
+        sort -n -k1,1 |
+        cut -f2-
 end
